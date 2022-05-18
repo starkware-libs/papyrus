fc4264921f9997a07dc6e3de41160d5a1da0da11
Shahar Papini <spapini@starkware.co>
2022-05-18 14:58:26 +0300

    Basic gateway with blockNumber() implementation.

--- src/gateway/mod.rs
+++ src/gateway/mod.rs
@@ -1,7 +1,45 @@
-pub struct Gateway {}
+mod api;
 
-impl Gateway {
-    pub fn run(&mut self) {
-        todo!("Not implemented yet.");
+use std::net::SocketAddr;
+
+use jsonrpsee::{
+    core::{async_trait, Error},
+    ws_server::{WsServerBuilder, WsServerHandle},
+};
+
+use crate::starknet::BlockNumber;
+
+use self::api::RpcServer;
+
+/// Rpc server.
+struct RpcServerImpl;
+
+#[async_trait]
+impl RpcServer for RpcServerImpl {
+    async fn block_number(&self) -> Result<BlockNumber, Error> {
+        Ok(BlockNumber(0))
+    }
+}
+
+async fn run_server() -> anyhow::Result<(SocketAddr, WsServerHandle)> {
+    let server = WsServerBuilder::default().build("127.0.0.1:0").await?;
+    let addr = server.local_addr()?;
+    let handle = server.start(RpcServerImpl.into_rpc())?;
+    Ok((addr, handle))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::gateway::{api::RpcServer, RpcServerImpl};
+    use jsonrpsee::types::EmptyParams;
+
+    #[tokio::test]
+    async fn get_block_number() {
+        let module = RpcServerImpl.into_rpc();
+        let result: String = module
+            .call("starknet_blockNumber", EmptyParams::new())
+            .await
+            .unwrap();
+        assert_eq!(&result, "0");
     }
 }
