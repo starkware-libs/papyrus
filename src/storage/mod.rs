8aad1cf410ce870a1ff1b157c72d547487d37b82
Lior Schejter <liors@starkware.co>
2022-05-23 14:58:39 +0300

    refactoring data store access

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,17 +1,36 @@
 use crate::starknet::{BlockBody, BlockHash, BlockHeader, BlockNumber};
 
-pub mod tests;
+#[cfg(test)]
+mod tests;
+
+pub struct StorageError {}
+
+pub trait StarknetStorageReader: Sync + Send {
+    fn get_latest_block_number(&self) -> BlockNumber;
+}
+
+pub trait StarknetStorageWriter: Sync + Send {
+    fn set_latest_block_number(&mut self, n: BlockNumber);
+}
+
+pub trait DataStore {
+    type R: StarknetStorageReader;
+    type W: StarknetStorageWriter;
+
+    fn get_state_read_access(&self) -> Result<Self::R, StorageError>;
+
+    fn get_state_write_access(&self) -> Result<Self::W, StorageError>;
+}
 
 // Storage object holding all the data. Owned by a single main thread. Responsible for doing all the
 // non thread safe operations.
 #[derive(Clone, Copy)]
 pub struct Storage {
-    latest_block_num : BlockNumber
+    latest_block_num: BlockNumber,
 }
 
-impl Storage
-{
-    fn set_latest_block_number(&mut self, n : &BlockNumber) {
+impl Storage {
+    fn set_latest_block_number(&mut self, n: &BlockNumber) {
         self.latest_block_num = BlockNumber { 0: n.0 };
     }
 }
@@ -20,31 +39,28 @@ impl Storage
 // Represents an interface for multiple consumers to read and write to the storage.
 #[derive(Clone)]
 pub struct StorageHandle {
-    storage : Storage
-
+    storage: Storage,
 }
 
-pub struct StorageError {}
-
 pub fn create_storage() -> Result<StorageHandle, StorageError> {
-
-    let s = Storage { latest_block_num : BlockNumber(0)};
-    let sh = StorageHandle { storage : s };
-    return Ok(sh)
+    let s = Storage {
+        latest_block_num: BlockNumber(0),
+    };
+    let sh = StorageHandle { storage: s };
+    return Ok(sh);
 }
 
 impl StorageHandle {
-
-
-    pub fn new(s : &Storage) -> StorageHandle {
-        return StorageHandle { storage : *s};
+    pub fn new(s: &Storage) -> StorageHandle {
+        return StorageHandle { storage: *s };
     }
 
-    pub fn storage(&self) -> Storage { return self.storage; }
+    pub fn storage(&self) -> Storage {
+        return self.storage;
+    }
 
-    pub fn set_latest_block_number(&mut self, n : BlockNumber) {
+    pub fn set_latest_block_number(&mut self, n: BlockNumber) {
         self.storage.set_latest_block_number(&n);
-
     }
 
     pub fn get_latest_block_number(&self) -> BlockNumber {
@@ -65,5 +81,3 @@ impl StorageHandle {
         todo!("Not implemented yet.");
     }
 }
-
-
