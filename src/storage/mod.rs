2d1c7ad6064341215fbea18e442f97814a1967dc
Lior Schejter <liors@starkware.co>
2022-05-23 15:17:52 +0300

    concrete implementation for storage

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,4 +1,6 @@
-use crate::starknet::{BlockBody, BlockHash, BlockHeader, BlockNumber};
+use crate::starknet::BlockNumber;
+use std::sync::Arc;
+use std::sync::Mutex;
 
 #[cfg(test)]
 mod tests;
@@ -24,60 +26,47 @@ pub trait DataStore {
 
 // Storage object holding all the data. Owned by a single main thread. Responsible for doing all the
 // non thread safe operations.
-#[derive(Clone, Copy)]
-pub struct Storage {
+struct ConcreteDataStore {
     latest_block_num: BlockNumber,
 }
 
-impl Storage {
-    fn set_latest_block_number(&mut self, n: &BlockNumber) {
-        self.latest_block_num = BlockNumber { 0: n.0 };
+pub struct DataStoreHandle {
+    inner: Arc<Mutex<ConcreteDataStore>>,
+}
+
+pub struct SNStorageReader {
+    store: Arc<Mutex<ConcreteDataStore>>,
+}
+
+impl StarknetStorageReader for SNStorageReader {
+    fn get_latest_block_number(&self) -> BlockNumber {
+        return self.store.lock().unwrap().latest_block_num;
     }
 }
 
-// Handle object that holds no storage data. Multiple instances might be held by different threads.
-// Represents an interface for multiple consumers to read and write to the storage.
-#[derive(Clone)]
-pub struct StorageHandle {
-    storage: Storage,
+pub struct SNStorageWriter {
+    store: Arc<Mutex<ConcreteDataStore>>,
 }
 
-pub fn create_storage() -> Result<StorageHandle, StorageError> {
-    let s = Storage {
-        latest_block_num: BlockNumber(0),
-    };
-    let sh = StorageHandle { storage: s };
-    return Ok(sh);
+impl StarknetStorageWriter for SNStorageWriter {
+    fn set_latest_block_number(&mut self, n: BlockNumber) {
+        self.store.lock().unwrap().latest_block_num = n;
+    }
 }
 
-impl StorageHandle {
-    pub fn new(s: &Storage) -> StorageHandle {
-        return StorageHandle { storage: *s };
+impl DataStore for DataStoreHandle {
+    type R = SNStorageReader;
+    type W = SNStorageWriter;
+
+    fn get_state_read_access(&self) -> Result<SNStorageReader, StorageError> {
+        return Ok(SNStorageReader {
+            store: self.inner.clone(),
+        });
     }
 
-    pub fn storage(&self) -> Storage {
-        return self.storage;
-    }
-
-    pub fn set_latest_block_number(&mut self, n: BlockNumber) {
-        self.storage.set_latest_block_number(&n);
-    }
-
-    pub fn get_latest_block_number(&self) -> BlockNumber {
-        return self.storage().latest_block_num;
-    }
-
-    pub fn add_block_header(&self, _block_header: BlockHeader) -> Result<BlockHash, StorageError> {
-        todo!("Not implemented yet.");
-    }
-    pub fn add_block_body(
-        &self,
-        _block_id: BlockHash,
-        _block_body: BlockBody,
-    ) -> Result<(), StorageError> {
-        todo!("Not implemented yet.");
-    }
-    pub fn get_block_header(&self, _block_id: BlockHash) -> BlockHeader {
-        todo!("Not implemented yet.");
+    fn get_state_write_access(&self) -> Result<SNStorageWriter, StorageError> {
+        return Ok(SNStorageWriter {
+            store: self.inner.clone(),
+        });
     }
 }
