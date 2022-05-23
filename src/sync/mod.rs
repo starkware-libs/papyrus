6da1ea57ca3348a9cfc2b41d9c2cfb025d114ba5
Lior Schejter <liors@starkware.co>
2022-05-23 15:17:52 +0300

    concrete implementation for storage

--- src/sync/mod.rs
+++ src/sync/mod.rs
@@ -2,16 +2,16 @@ mod sources;
 
 use log::info;
 
-use crate::storage::StorageHandle;
+use crate::storage::DataStoreHandle;
 
 // Orchestrates specific network interfaces (e.g. central, p2p, l1) and writes to Storage.
 pub struct StateSync {
     #[allow(dead_code)]
-    storage: StorageHandle,
+    storage: DataStoreHandle,
 }
 
 impl StateSync {
-    pub fn new(storage: StorageHandle) -> StateSync {
+    pub fn new(storage: DataStoreHandle) -> StateSync {
         StateSync { storage }
     }
     pub fn run(&mut self) {
