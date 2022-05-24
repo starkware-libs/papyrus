3f30627ff17f16dae215c6c1b6141ff10bf2f9fd
dan-starkware <56217775+dan-starkware@users.noreply.github.com>
2022-05-24 13:35:46 +0300

    Central client skeleton (#11)
    * Central client skeleton
    
    * Fix CR comments

---
+++ src/central/mod.rs
@@ -0,0 +1,55 @@
+use crate::starknet::BlockNumber;
+
+pub struct CentralClient {
+    central_url: url::Url,
+    internal_client: reqwest::Client,
+}
+#[derive(thiserror::Error, Debug)]
+pub enum ClientError {
+    #[error(transparent)]
+    BadUrl(#[from] url::ParseError),
+    #[error(transparent)]
+    BadRequest(#[from] reqwest::Error),
+    #[error(transparent)]
+    BadResponse(#[from] core::num::ParseIntError),
+}
+
+#[allow(dead_code)]
+impl CentralClient {
+    pub fn new(central_url_str: &str) -> Result<CentralClient, ClientError> {
+        Ok(CentralClient {
+            central_url: url::Url::parse(central_url_str)?,
+            internal_client: reqwest::Client::builder().build()?,
+        })
+    }
+
+    async fn request(&self, path: &str) -> Result<String, ClientError> {
+        let joined = self.central_url.join(path)?;
+        let res = self.internal_client.get(joined).send().await?;
+        let body = res.text().await?;
+        Ok(body)
+    }
+
+    pub async fn block_number(&self) -> Result<BlockNumber, ClientError> {
+        let block_number = self.request("feeder_gateway/get_last_batch_id").await?;
+        Ok(BlockNumber(block_number.parse()?))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use mockito::mock;
+
+    #[tokio::test]
+    async fn test_get_block_number() {
+        let central_client = CentralClient::new(&mockito::server_url()).unwrap();
+        let mock = mock("GET", "/feeder_gateway/get_last_batch_id")
+            .with_status(200)
+            .with_body("195812")
+            .create();
+        let block_number = central_client.block_number().await.unwrap();
+        mock.assert();
+        assert_eq!(block_number, BlockNumber(195812));
+    }
+}
