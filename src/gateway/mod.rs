d14e9d185f00f438075bf246a5d58a6225819471
anatg <anatg@starkware.co>
2022-06-01 18:19:33 +0300

    Fix CR.

--- src/gateway/mod.rs
+++ src/gateway/mod.rs
@@ -2,45 +2,53 @@ mod api;
 #[cfg(test)]
 mod gateway_test;
 
-use std::net::SocketAddr;
+use std::{fmt::Display, net::SocketAddr};
 
 use jsonrpsee::{
     core::{async_trait, Error},
-    ws_server::{WsServerBuilder, WsServerHandle},
+    types::error::{ErrorCode::InternalError, ErrorObject, INTERNAL_ERROR_MSG},
+    ws_server::{types::error::CallError, WsServerBuilder, WsServerHandle},
 };
-use log::{error, info};
 
-use crate::{starknet::BlockNumber, storage::StorageReader};
+use crate::{
+    starknet::BlockNumber,
+    storage::{SNStorageReader, StorageReader},
+};
 
 use self::api::JsonRpcServer;
 
+// TODO(anatg): Take from config.
+const SERVER_IP: &str = "127.0.0.1:0";
+
 /// Rpc server.
 struct JsonRpcServerImpl {
-    storage_reader: Box<dyn StorageReader>,
+    storage_reader: SNStorageReader,
+}
+
+fn internal_server_error(err: impl Display) -> Error {
+    Error::Call(CallError::Custom(ErrorObject::owned(
+        InternalError.code(),
+        format!("{}: {}", INTERNAL_ERROR_MSG, err),
+        None::<()>,
+    )))
 }
 
 #[async_trait]
 impl JsonRpcServer for JsonRpcServerImpl {
     async fn block_number(&self) -> Result<BlockNumber, Error> {
-        let res = self.storage_reader.get_latest_block_number().await;
-        match res {
-            Ok(block_number) => {
-                info!("Read block number: {:?}.", block_number);
-                Ok(block_number)
-            }
-            err => {
-                error!("Storage error: {:?}", err);
-                Err(Error::Custom(format!("Storage error: {:?}", err)))
-            }
-        }
+        Ok(self
+            .storage_reader
+            .get_latest_block_number()
+            .await
+            .map_err(internal_server_error)?)
     }
 }
 
 #[allow(dead_code)]
 pub async fn run_server(
-    storage_reader: Box<dyn StorageReader>,
+    storage_reader: SNStorageReader,
 ) -> anyhow::Result<(SocketAddr, WsServerHandle)> {
-    let server = WsServerBuilder::default().build("127.0.0.1:0").await?;
+    let server = WsServerBuilder::default().build(SERVER_IP).await?;
     let addr = server.local_addr()?;
     let handle = server.start(JsonRpcServerImpl { storage_reader }.into_rpc())?;
     Ok((addr, handle))
