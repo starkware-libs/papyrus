1fdb3fb32412e955fa5c5d4836366c0f700d49f6
anatg <anatg@starkware.co>
2022-06-01 18:19:33 +0300

    Fix CR.

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -3,6 +3,6 @@ mod storage_impl;
 #[cfg(test)]
 mod storage_test;
 
-pub use self::storage_impl::create_store_access;
+pub use self::storage_impl::{create_store_access, SNStorageReader, SNStorageWriter};
 
 pub use self::api::{StorageError, StorageReader, StorageWriter};
