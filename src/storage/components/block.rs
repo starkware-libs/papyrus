2fa86c8a881d45b119800e6eb414aa6c522b745f
Shahar Papini <spapini@starkware.co>
2022-06-04 19:41:49 +0300

    Block storage add_header

--- src/storage/components/block.rs
+++ src/storage/components/block.rs
@@ -2,10 +2,11 @@
 #[path = "block_test.rs"]
 mod block_test;
 
-use std::{path::Path, sync::Arc};
+use std::path::Path;
+use std::sync::Arc;
 
 use crate::{
-    starknet::BlockNumber,
+    starknet::{BlockHeader, BlockNumber},
     storage::db::{open_env, DbError, DbReader, DbWriter, TableIdentifier},
 };
 
@@ -13,11 +14,20 @@ use crate::{
 pub enum BlockStorageError {
     #[error(transparent)]
     InnerError(#[from] DbError),
+    #[error("Marker mismatch (expected {expected:?}, found {found:?}).")]
+    MarkerMismatch {
+        expected: BlockNumber,
+        found: BlockNumber,
+    },
 }
+pub type Result<V> = std::result::Result<V, BlockStorageError>;
 
 // Constants.
+const HEADER_MARKER_KEY: &[u8] = b"header";
+
 struct Tables {
     markers: TableIdentifier,
+    headers: TableIdentifier,
 }
 #[derive(Clone)]
 pub struct BlockStorageReader {
@@ -30,12 +40,11 @@ pub struct BlockStorageWriter {
 }
 
 #[allow(dead_code)]
-pub fn open_block_storage(
-    path: &Path,
-) -> Result<(BlockStorageReader, BlockStorageWriter), BlockStorageError> {
+pub fn open_block_storage(path: &Path) -> Result<(BlockStorageReader, BlockStorageWriter)> {
     let (db_reader, mut db_writer) = open_env(path)?;
     let tables = Arc::new(Tables {
         markers: db_writer.create_table("markers")?,
+        headers: db_writer.create_table("headers")?,
     });
     let reader = BlockStorageReader {
         db_reader,
@@ -48,23 +57,52 @@ pub fn open_block_storage(
 #[allow(dead_code)]
 impl BlockStorageReader {
     // The block number marker is the first block number that doesn't exist yet.
-    pub fn get_block_number_marker(&self) -> Result<BlockNumber, BlockStorageError> {
+    pub fn get_header_marker(&self) -> Result<BlockNumber> {
         let txn = self.db_reader.begin_ro_txn()?;
         let markers_table = txn.open_table(&self.tables.markers)?;
         Ok(txn
-            .get::<BlockNumber>(&markers_table, b"block_number")?
+            .get::<BlockNumber>(&markers_table, HEADER_MARKER_KEY)?
             .unwrap_or_default())
     }
+    pub fn get_block_header(&self, block_number: BlockNumber) -> Result<Option<BlockHeader>> {
+        let txn = self.db_reader.begin_ro_txn()?;
+        let headers_table = txn.open_table(&self.tables.headers)?;
+        let block_header =
+            txn.get::<BlockHeader>(&headers_table, &bincode::serialize(&block_number).unwrap())?;
+        Ok(block_header)
+    }
 }
-#[allow(dead_code)]
 impl BlockStorageWriter {
-    pub fn set_block_number_marker(
+    pub fn append_header(
         &mut self,
         block_number: BlockNumber,
-    ) -> Result<(), BlockStorageError> {
+        block_header: &BlockHeader,
+    ) -> Result<()> {
         let txn = self.db_writer.begin_rw_txn()?;
         let markers_table = txn.open_table(&self.tables.markers)?;
-        txn.upsert(&markers_table, b"block_number", &block_number)?;
+        let headers_table = txn.open_table(&self.tables.headers)?;
+
+        // Make sure marker is consistent.
+        let header_marker = txn
+            .get::<BlockNumber>(&markers_table, HEADER_MARKER_KEY)?
+            .unwrap_or_default();
+        if header_marker != block_number {
+            return Err(BlockStorageError::MarkerMismatch {
+                expected: header_marker,
+                found: block_number,
+            });
+        };
+
+        // Advance marker.
+        txn.upsert(&markers_table, HEADER_MARKER_KEY, &block_number.next())?;
+
+        // Write header.
+        txn.insert(
+            &headers_table,
+            &bincode::serialize(&block_number).unwrap(),
+            block_header,
+        )?;
+
         txn.commit()?;
         Ok(())
     }
--- src/storage/components/block_test.rs
+++ src/storage/components/block_test.rs
@@ -1,8 +1,8 @@
 use tempfile::tempdir;
 
-use crate::starknet::BlockNumber;
+use crate::starknet::{BlockHeader, BlockNumber};
 
-use super::{open_block_storage, BlockStorageReader, BlockStorageWriter};
+use super::{open_block_storage, BlockStorageError, BlockStorageReader, BlockStorageWriter};
 
 fn get_test_storage() -> (BlockStorageReader, BlockStorageWriter) {
     let dir = tempdir().unwrap();
@@ -10,14 +10,28 @@ fn get_test_storage() -> (BlockStorageReader, BlockStorageWriter) {
 }
 
 #[tokio::test]
-async fn test_add_block_number() {
+async fn test_append_header() {
     let (reader, mut writer) = get_test_storage();
-    let expected = BlockNumber(5);
 
-    assert_eq!(
-        reader.get_block_number_marker().unwrap(),
-        BlockNumber::default()
-    );
-    writer.set_block_number_marker(expected).unwrap();
-    assert_eq!(reader.get_block_number_marker().unwrap(), expected);
+    // Check for MarkerMismatch error  when trying to append the wrong block number.
+    assert!(matches!(
+        writer
+            .append_header(BlockNumber(5), &BlockHeader::default())
+            .unwrap_err(),
+        BlockStorageError::MarkerMismatch {
+            expected: BlockNumber(0),
+            found: BlockNumber(5)
+        }
+    ));
+
+    // Append with the right block number.
+    writer
+        .append_header(BlockNumber(0), &BlockHeader::default())
+        .unwrap();
+
+    // Check block and marker.
+    let marker = reader.get_header_marker().unwrap();
+    assert_eq!(marker, BlockNumber(1));
+    let header = reader.get_block_header(BlockNumber(0)).unwrap();
+    assert_eq!(header, Some(BlockHeader::default()));
 }
