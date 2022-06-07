7331c2138a7a34f37c593cad39eba691d64af46a
anatg <anatg@starkware.co>
2022-06-07 13:53:49 +0300

    Adapt to new storage.

--- src/gateway/gateway_test.rs
+++ src/gateway/gateway_test.rs
@@ -3,14 +3,14 @@ use jsonrpsee::{types::EmptyParams, ws_client::WsClientBuilder};
 use crate::{
     gateway::{api::JsonRpcClient, JsonRpcServerImpl},
     starknet::BlockNumber,
-    storage::create_store_access,
+    storage::components::storage_test_utils,
 };
 
 use super::*;
 
 #[tokio::test]
 async fn test_block_number() {
-    let (storage_reader, _) = create_store_access().unwrap();
+    let storage_reader = storage_test_utils::get_test_storage().block_storage_reader;
     let module = JsonRpcServerImpl { storage_reader }.into_rpc();
     let result: BlockNumber = module
         .call("starknet_blockNumber", EmptyParams::new())
@@ -21,7 +21,7 @@ async fn test_block_number() {
 
 #[tokio::test]
 async fn test_run_server() {
-    let (storage_reader, _) = create_store_access().unwrap();
+    let storage_reader = storage_test_utils::get_test_storage().block_storage_reader;
     let (addr, _handle) = run_server(storage_reader).await.unwrap();
     let client = WsClientBuilder::default()
         .build(format!("ws://{:?}", addr))
