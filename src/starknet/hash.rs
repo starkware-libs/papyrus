cabe1185f7f7d1497ddaa0061fcb13623385ac46
Dan Brownstein <dan@starkware.co>
2022-06-07 15:43:05 +0300

    Add SN-client get_block and related objects

--- src/starknet/hash.rs
+++ src/starknet/hash.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
-pub struct StarkHash([u64; 4]);
+#[derive(
+    Debug, Copy, Clone, PartialEq, Eq, Default, Hash, Deserialize, Serialize, PartialOrd, Ord,
+)]
+pub struct StarkHash(pub [u8; 32]);
