ac814acf1ecb49f0ff4cb02d1f6aa1fa2933e393
Dan Brownstein <dan@starkware.co>
2022-06-07 13:02:14 +0300

    Add bytes from hex

---
+++ src/starknet_client/object_test.rs
@@ -0,0 +1,68 @@
+use crate::starknet_client::objects::{bytes_from_hex_str, DeserializationError};
+
+#[test]
+fn test_bytes_from_hex_str() {
+    // even length.
+    let hex_str = "0x6a";
+    let res = bytes_from_hex_str::<1, true>(hex_str).unwrap();
+    assert_eq!(res, [106]);
+
+    // odd length.
+    let hex_str = "0x6";
+    let res = bytes_from_hex_str::<1, true>(hex_str).unwrap();
+    assert_eq!(res, [6]);
+
+    // No prefix.
+    let hex_str = "6";
+    let res = bytes_from_hex_str::<1, false>(hex_str).unwrap();
+    assert_eq!(res, [6]);
+}
+
+#[test]
+fn test_bytes_from_hex_str_padding() {
+    // even length.
+    let hex_str = "0xda2b";
+    let res = bytes_from_hex_str::<4, true>(hex_str).unwrap();
+    assert_eq!(res, [0, 0, 218, 43]);
+
+    // odd length.
+    let hex_str = "0xda2";
+    let res = bytes_from_hex_str::<4, true>(hex_str).unwrap();
+    assert_eq!(res, [0, 0, 13, 162]);
+}
+
+#[test]
+fn test_bytes_from_hex_str_errors() {
+    // Short buffer.
+    let hex_str = "0xda2b";
+    let err = bytes_from_hex_str::<1, true>(hex_str);
+    assert_matches!(
+        err,
+        Err(DeserializationError::BadInput {
+            expected_byte_count: 1,
+            ..
+        })
+    );
+
+    // Invalid hex char.
+    let err = bytes_from_hex_str::<1, false>("1z");
+    assert_matches!(
+        err,
+        Err(DeserializationError::FromHexError(
+            hex::FromHexError::InvalidHexCharacter { c: 'z', index: 1 }
+        ))
+    );
+
+    // Missing prefix.
+    let err = bytes_from_hex_str::<2, true>("11");
+    assert_matches!(err, Err(DeserializationError::MissingPrefix { .. }));
+
+    // Unneeded prefix.
+    let err = bytes_from_hex_str::<2, false>("0x11");
+    assert_matches!(
+        err,
+        Err(DeserializationError::FromHexError(
+            hex::FromHexError::InvalidHexCharacter { c: 'x', index: 1 }
+        ))
+    );
+}
