c63bb68642cd96596b770a1cea9f22cb9340037a
anatg <anatg@starkware.co>
2022-06-08 16:03:32 +0300

    Change BlockNumber prev.

--- src/gateway/gateway_test.rs
+++ src/gateway/gateway_test.rs
@@ -1,22 +1,22 @@
-use jsonrpsee::{types::EmptyParams, ws_client::WsClientBuilder};
+use jsonrpsee::core::Error;
+use jsonrpsee::types::EmptyParams;
+use jsonrpsee::ws_client::WsClientBuilder;
 
-use crate::{
-    gateway::{api::JsonRpcClient, JsonRpcServerImpl},
-    starknet::BlockNumber,
-    storage::components::storage_test_utils,
-};
+use crate::storage::components::storage_test_utils;
 
+use super::api::*;
 use super::*;
 
 #[tokio::test]
 async fn test_block_number() {
     let storage_reader = storage_test_utils::get_test_storage().block_storage_reader;
     let module = JsonRpcServerImpl { storage_reader }.into_rpc();
-    let result: BlockNumber = module
-        .call("starknet_blockNumber", EmptyParams::new())
+    let err = module
+        .call::<_, BlockNumber>("starknet_blockNumber", EmptyParams::new())
         .await
-        .unwrap();
-    assert_eq!(result, BlockNumber(0));
+        .unwrap_err();
+    let _expected = Error::from(api::JsonRpcError::NoBlocks);
+    assert_matches!(err, _expected);
 }
 
 #[tokio::test]
@@ -27,6 +27,7 @@ async fn test_run_server() {
         .build(format!("ws://{:?}", addr))
         .await
         .unwrap();
-    let block_number = client.block_number().await.unwrap();
-    assert_eq!(block_number, BlockNumber(0));
+    let err = client.block_number().await.unwrap_err();
+    let _expected = Error::from(api::JsonRpcError::NoBlocks);
+    assert_matches!(err, _expected);
 }
