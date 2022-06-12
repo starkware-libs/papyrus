cdf6c48851749b1628b1d3b61fa1e6fda51ebfa5
anatg <anatg@starkware.co>
2022-06-12 16:52:29 +0300

    Add retry to central source.

--- src/sync/sources/central.rs
+++ src/sync/sources/central.rs
@@ -1,9 +1,16 @@
+#[cfg(test)]
+#[path = "central_test.rs"]
+mod central_test;
+
+use std::iter::Take;
 use std::time::Duration;
 
 use async_stream::stream;
 use log::{debug, error, info};
 use reqwest::StatusCode;
 use serde::{Deserialize, Serialize};
+use tokio_retry::strategy::ExponentialBackoff;
+use tokio_retry::{Action, RetryIf};
 use tokio_stream::Stream;
 
 use crate::starknet::{BlockHeader, BlockNumber};
@@ -12,15 +19,51 @@ use crate::starknet_client::{ClientCreationError, ClientError, StarknetClient};
 #[derive(Serialize, Deserialize)]
 pub struct CentralSourceConfig {
     pub url: String,
+    pub retry_base_millis: u64,
+    pub retry_max_delay_millis: u64,
+    pub max_retries: usize,
 }
+
 pub struct CentralSource {
     starknet_client: StarknetClient,
+    retry_strategy: Take<ExponentialBackoff>,
 }
 
 impl CentralSource {
     pub fn new(config: CentralSourceConfig) -> Result<CentralSource, ClientCreationError> {
         let starknet_client = StarknetClient::new(&config.url)?;
-        Ok(CentralSource { starknet_client })
+        let retry_strategy = ExponentialBackoff::from_millis(config.retry_base_millis)
+            .max_delay(Duration::from_millis(config.retry_max_delay_millis))
+            .take(config.max_retries);
+        Ok(CentralSource {
+            starknet_client,
+            retry_strategy,
+        })
+    }
+
+    fn should_retry(err: &ClientError) -> bool {
+        match err {
+            ClientError::BadResponse { status } if *status == StatusCode::TOO_MANY_REQUESTS => {
+                debug!("Received error {:?}, retrying.", err);
+                true
+            }
+            _ => {
+                debug!("Received error {:?}, not retrying.", err);
+                false
+            }
+        }
+    }
+
+    async fn retry<I, T>(&self, action: T) -> Result<I, ClientError>
+    where
+        T: Action<Item = I, Error = ClientError>,
+    {
+        RetryIf::spawn(
+            self.retry_strategy.clone(),
+            action,
+            CentralSource::should_retry,
+        )
+        .await
     }
 
     pub async fn get_block_number(&mut self) -> Result<BlockNumber, ClientError> {
@@ -36,7 +79,7 @@ impl CentralSource {
         let mut current_block_number = initial_block_number;
         stream! {
             while current_block_number <= up_to_block_number {
-                let res = self.starknet_client.block_header(current_block_number).await;
+                let res = self.retry(|| self.starknet_client.block_header(current_block_number)).await;
                 match res {
                     Ok(block_header) => {
                         info!("Received new header: {}.", block_header.number.0);
@@ -47,13 +90,7 @@ impl CentralSource {
                         debug!("Received error for block {}: {:?}.", current_block_number.0, err);
                         // TODO(dan): proper error handling.
                         match err{
-                            ClientError::BadResponse { status } => {
-                                if status == StatusCode::TOO_MANY_REQUESTS {
-                                    // TODO(dan): replace with a retry mechanism.
-                                    debug!("Waiting for 5 sec.");
-                                    tokio::time::sleep( Duration::from_millis(5000)).await;
-                                } else {error!("{:?}",err); todo!()}
-                            },
+                            ClientError::BadResponse { status: _ } => {error!("{:?}",err); todo!()},
                             ClientError::RequestError(err) => {error!("{:?}",err); todo!()},
                             ClientError::SerdeError(err) => {error!("{:?}",err); todo!()},
                             ClientError::StarknetError(err) => {error!("{:?}",err); todo!()},
