f2f0ceeb28fa91f569d23a5a3480eb33a70d7c53
anatg <anatg@starkware.co>
2022-06-19 09:35:22 +0300

    Add block struct and get block by number to gateway.

---
+++ src/gateway/objects/mod.rs
@@ -0,0 +1,5 @@
+mod block;
+mod transaction;
+
+pub use self::block::{Block, BlockStatus};
+pub use self::transaction::Transactions;
--- src/starknet/block.rs
+++ src/starknet/block.rs
@@ -1,11 +1,8 @@
 use serde::{Deserialize, Serialize};
 
 use super::hash::StarkHash;
+use super::ContractAddress;
 
-#[derive(
-    Debug, Default, Copy, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord,
-)]
-pub struct ContractAddress(pub StarkHash);
 #[derive(
     Debug, Default, Copy, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord,
 )]
