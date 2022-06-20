daba1792d91a423533ad5c1c65a30f12518a373b
Shahar Papini <spapini@starkware.co>
2022-06-20 08:24:02 +0300

    State objects

---
+++ src/starknet/state.rs
@@ -0,0 +1,52 @@
+use serde::{Deserialize, Serialize};
+
+use crate::starknet::hash::StarkFelt;
+use crate::starknet::ContractAddress;
+
+use super::StarkHash;
+
+#[derive(
+    Debug, Default, Copy, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord,
+)]
+pub struct ClassHash(pub StarkHash);
+
+// Invariant: Addresses are strictly increasing.
+// TODO(spapini): Enforce the invariant.
+#[derive(Debug, Default, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+pub struct StateDiffForward {
+    pub deployed_contracts: Vec<DeployedContract>,
+    pub storage_diffs: Vec<StorageDiff>,
+}
+
+// Invariant: Addresses are strictly increasing. In particular, no address appears twice.
+// TODO(spapini): Enforce the invariant.
+#[derive(Debug, Default, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+pub struct StateDiffBackward {
+    pub deployed_contracts: Vec<ContractAddress>,
+    pub storage_diffs: Vec<StorageDiff>,
+}
+
+#[derive(Debug, Default, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+pub struct DeployedContract {
+    pub address: ContractAddress,
+    pub class_hash: ClassHash,
+}
+
+// Invariant: Addresses are strictly increasing. In particular, no address appears twice.
+// TODO(spapini): Enforce the invariant.
+#[derive(Debug, Default, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+pub struct StorageDiff {
+    address: ContractAddress,
+    diff: Vec<StorageEntry>,
+}
+
+// TODO: Invariant: this is in range.
+// TODO(spapini): Enforce the invariant.
+#[derive(Debug, Default, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+pub struct StorageKey(pub StarkFelt);
+
+#[derive(Debug, Default, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+pub struct StorageEntry {
+    pub addr: StorageKey,
+    pub value: StarkFelt,
+}
