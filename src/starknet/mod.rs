687a7a071e1d5bbd517f5fe9c79b25bf2947df24
Shahar Papini <spapini@starkware.co>
2022-06-21 12:54:14 +0300

    BlockStorage: state

--- src/starknet/mod.rs
+++ src/starknet/mod.rs
@@ -11,10 +11,13 @@ pub use self::block::{
 };
 pub use self::core::ContractAddress;
 pub use self::hash::{StarkFelt, StarkHash};
-pub use self::state::{DeployedContract, StateDiffBackward, StateDiffForward};
 pub use self::transaction::{
     CallData, EntryPointSelector, EthAddress, Event, Fee, L1ToL2Payload, L2ToL1Payload,
     TransactionHash,
+};
+pub use state::{
+    ClassHash, DeployedContract, StateDiffBackward, StateDiffForward, StorageDiff, StorageEntry,
+    StorageKey,
 };
 
 #[allow(unused_imports)]
