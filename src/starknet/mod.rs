bed5a6466fb1ac1c0b5c601922d43347029f5f0b
Shahar Papini <43779613+spapinistarkware@users.noreply.github.com>
2022-06-21 09:21:09 +0300

    State objects (#51)
    * State objects

--- src/starknet/mod.rs
+++ src/starknet/mod.rs
@@ -1,8 +1,10 @@
 mod block;
 mod hash;
+mod state;
 
 pub use block::{
     BlockBody, BlockHash, BlockHeader, BlockNumber, BlockTimestamp, ContractAddress,
     EventsCommitment, GasPrice, GlobalRoot, TransactionsCommitment,
 };
 pub use hash::{StarkFelt, StarkHash};
+pub use state::{DeployedContract, StateDiffBackward, StateDiffForward};
