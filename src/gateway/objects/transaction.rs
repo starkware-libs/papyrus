683732ea084b000b2b747fe62d64d87ed0fbbd88
anatg <anatg@starkware.co>
2022-06-23 16:11:47 +0300

    Add transaction structs to starknet

--- src/gateway/objects/transaction.rs
+++ src/gateway/objects/transaction.rs
@@ -1,9 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-use crate::starknet::{
-    CallData, ContractAddress, EntryPointSelector, EthAddress, Event, Fee, L1ToL2Payload,
-    L2ToL1Payload, TransactionHash,
-};
+use crate::starknet::{Transaction, TransactionHash, TransactionReceipt};
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
 pub enum TransactionStatus {
@@ -19,39 +16,6 @@ pub enum TransactionStatus {
     AcceptedOnL1,
     #[serde(rename = "REJECTED")]
     Rejected,
-}
-#[derive(Debug, Default, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
-pub struct MessageToL2 {
-    pub from_address: EthAddress,
-    pub payload: L1ToL2Payload,
-}
-
-#[derive(Debug, Default, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
-pub struct MessageToL1 {
-    pub to_address: EthAddress,
-    pub payload: L2ToL1Payload,
-}
-
-/// `contract_address` field is available for Deploy and Invoke transactions.
-/// `entry_point_selector` and `calldata` fields are available for Invoke transactions.
-#[derive(Debug, Default, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
-pub struct Transaction {
-    pub contract_address: Option<ContractAddress>,
-    pub entry_point_selector: Option<EntryPointSelector>,
-    pub calldata: Option<CallData>,
-    pub txn_hash: TransactionHash,
-    pub max_fee: Fee,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
-pub struct TransactionReceipt {
-    transaction_hash: TransactionHash,
-    actual_fee: Fee,
-    status: TransactionStatus,
-    status_data: String,
-    messages_sent: Vec<MessageToL1>,
-    l1_origin_message: Option<MessageToL2>,
-    events: Vec<Event>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
--- src/starknet/transaction.rs
+++ src/starknet/transaction.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 use web3::types::H160;
 
 use super::serde_utils::PrefixedHexAsBytes;
-use super::{ContractAddress, StarkFelt, StarkHash};
+use super::{ClassHash, ContractAddress, StarkFelt, StarkHash};
 
 #[derive(
     Debug, Default, Copy, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord,
@@ -38,6 +38,20 @@ pub struct Event {
 )]
 pub struct EntryPointSelector(pub StarkHash);
 
+#[derive(
+    Debug, Copy, Clone, Default, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord,
+)]
+pub struct EntryPointOffset(pub StarkFelt);
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+pub struct EntryPoint {
+    pub selector: EntryPointSelector,
+    pub offset: EntryPointOffset,
+}
+
+#[derive(Debug, Clone, Default, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+pub struct ProgramCode(pub String);
+
 #[derive(Debug, Clone, Default, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
 pub struct CallData(pub Vec<StarkFelt>);
 
@@ -55,4 +69,120 @@ pub struct L2ToL1Payload(pub Vec<StarkFelt>);
 #[derive(
     Debug, Copy, Clone, Default, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord,
 )]
-pub struct Nonce(pub StarkFelt);
+pub struct TransactionVersion(pub StarkHash);
+
+#[derive(Debug, Clone, Default, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+pub struct TransactionSignature(pub Vec<StarkHash>);
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+pub struct ContractClass {
+    pub class_hash: ClassHash,
+    pub program: ProgramCode,
+    pub constructor_entry_points: Vec<EntryPoint>,
+    pub external_entry_points: Vec<EntryPoint>,
+    pub l1_handler_entry_points: Vec<EntryPoint>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+pub struct DeclareTransaction {
+    pub transaction_hash: TransactionHash,
+    pub max_fee: Fee,
+    pub version: TransactionVersion,
+    pub signature: TransactionSignature,
+    pub class_hash: ClassHash,
+    pub sender_address: ContractAddress,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+pub struct InvokeTransaction {
+    pub transaction_hash: TransactionHash,
+    pub max_fee: Fee,
+    pub version: TransactionVersion,
+    pub signature: TransactionSignature,
+    pub contract_address: ContractAddress,
+    pub entry_point_selector: EntryPointSelector,
+    pub call_data: CallData,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+pub struct DeployTransaction {
+    pub transaction_hash: TransactionHash,
+    pub max_fee: Fee,
+    pub version: TransactionVersion,
+    pub signature: TransactionSignature,
+    pub contract_address: ContractAddress,
+    pub constructor_calldata: CallData,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+pub enum TransactionStatus {
+    #[serde(rename = "PENDING")]
+    Pending,
+    #[serde(rename = "ACCEPTED_ON_L2")]
+    AcceptedOnL2,
+    #[serde(rename = "ACCEPTED_ON_L1")]
+    AcceptedOnL1,
+    #[serde(rename = "REJECTED")]
+    Rejected,
+}
+impl Default for TransactionStatus {
+    fn default() -> Self {
+        TransactionStatus::AcceptedOnL2
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+pub struct MessageToL2 {
+    pub from_address: EthAddress,
+    pub payload: L1ToL2Payload,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+pub struct MessageToL1 {
+    pub to_address: EthAddress,
+    pub payload: L2ToL1Payload,
+}
+
+#[derive(Debug, Clone, Default, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+pub struct StatusData(pub Vec<StarkFelt>);
+
+#[derive(Debug, Clone, Default, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+pub struct InvokeTransactionReceipt {
+    pub transaction_hash: TransactionHash,
+    pub actual_fee: Fee,
+    pub status: TransactionStatus,
+    pub status_data: StatusData,
+    pub messages_sent: Vec<MessageToL1>,
+    pub l1_origin_message: Option<MessageToL2>,
+    pub events: Vec<Event>,
+}
+
+#[derive(Debug, Clone, Default, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+pub struct DeclareTransactionReceipt {
+    transaction_hash: TransactionHash,
+    actual_fee: Fee,
+    status: TransactionStatus,
+    status_data: StatusData,
+}
+
+#[derive(Debug, Clone, Default, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+pub struct DeployTransactionReceipt {
+    transaction_hash: TransactionHash,
+    actual_fee: Fee,
+    status: TransactionStatus,
+    status_data: StatusData,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+pub enum Transaction {
+    Declare(DeclareTransaction),
+    Deploy(DeployTransaction),
+    Invoke(InvokeTransaction),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+pub enum TransactionReceipt {
+    Declare(DeclareTransactionReceipt),
+    Deploy(DeployTransactionReceipt),
+    Invoke(InvokeTransactionReceipt),
+}
