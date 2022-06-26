b3e30e0705946e3820f982230af55668ff960ddb
Shahar Papini <43779613+spapinistarkware@users.noreply.github.com>
2022-06-26 17:24:55 +0300

    Block storage: add hash -> number mapping (#66)
    * Block storage: add hash -> number mapping

--- src/storage/components/block/mod.rs
+++ src/storage/components/block/mod.rs
@@ -3,15 +3,11 @@ mod state;
 #[cfg(test)]
 mod test_utils;
 
-use crate::starknet::BlockNumber;
+use crate::starknet::{BlockHash, BlockNumber};
 use crate::storage::db::DbConfig;
 use std::sync::Arc;
 
-use crate::storage::db::open_env;
-use crate::storage::db::DbError;
-use crate::storage::db::DbReader;
-use crate::storage::db::DbWriter;
-use crate::storage::db::TableIdentifier;
+use crate::storage::db::{open_env, DbError, DbReader, DbWriter, TableIdentifier};
 
 pub use self::header::{HeaderStorageReader, HeaderStorageWriter};
 
@@ -24,12 +20,20 @@ pub enum BlockStorageError {
         expected: BlockNumber,
         found: BlockNumber,
     },
+    #[error(
+        "Block hash {block_hash:?} already exists, when adding block number {block_number:?}."
+    )]
+    BlockHashAlreadyExists {
+        block_hash: BlockHash,
+        block_number: BlockNumber,
+    },
 }
 pub type BlockStorageResult<V> = std::result::Result<V, BlockStorageError>;
 
 pub struct Tables {
     markers: TableIdentifier,
     headers: TableIdentifier,
+    block_hash_to_number: TableIdentifier,
     state_diffs: TableIdentifier,
 }
 #[derive(Clone)]
@@ -49,6 +53,7 @@ pub fn open_block_storage(
     let tables = Arc::new(Tables {
         markers: db_writer.create_table("markers")?,
         headers: db_writer.create_table("headers")?,
+        block_hash_to_number: db_writer.create_table("block_hash_to_number")?,
         state_diffs: db_writer.create_table("state_diffs")?,
     });
     let reader = BlockStorageReader {
