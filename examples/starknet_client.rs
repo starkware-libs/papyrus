4ad1925cb6c277be2cac67a3cc024365e5cdb89d
Dan Brownstein <dan@starkware.co>
2022-06-27 08:59:31 +0300

    Add sn_client integration test

---
+++ examples/starknet_client.rs
@@ -0,0 +1,13 @@
+use papyrus_lib::config::load_config;
+use papyrus_lib::starknet::BlockNumber;
+use papyrus_lib::starknet_client::StarknetClient;
+
+#[tokio::main]
+async fn main() {
+    let config = load_config("config/config.ron").unwrap();
+    let starknet_client = StarknetClient::new(&config.central.url).unwrap();
+    let _latest_block_number = starknet_client.block_number().await.unwrap();
+    let _block_123456 = starknet_client.block(BlockNumber(123456)).await.unwrap();
+    // TODO(dan): Add state_update once Starknet sequencer returns the class_hash in
+    // get_state_update deployed_contracts prefixed.
+}
