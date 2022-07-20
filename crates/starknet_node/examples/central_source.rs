fca1b402447a39eda936661a091f4d91cb1e6ab9
dan-starkware <56217775+dan-starkware@users.noreply.github.com>
2022-07-20 15:12:41 +0300

    Fix examples. (#121)
    * Fix examples.

--- crates/starknet_node/examples/central_source.rs
+++ crates/starknet_node/examples/central_source.rs
@@ -6,11 +6,11 @@ use tokio_stream::StreamExt;
 
 #[tokio::main]
 async fn main() {
-    let config = load_config("config/config.ron").unwrap();
+    let config = load_config("config/config.ron").expect("Load config");
     let mut state_marker = BlockNumber(200);
     let last_block_number = BlockNumber(203);
 
-    let central_source = CentralSource::new(config.central).unwrap();
+    let central_source = CentralSource::new(config.central).expect("Create new client");
     let header_stream = central_source.stream_state_updates(state_marker, last_block_number).fuse();
     pin_mut!(header_stream);
     while let Some(Ok((block_number, _state_difff))) = header_stream.next().await {
@@ -22,4 +22,5 @@ async fn main() {
         );
         state_marker = state_marker.next();
     }
+    assert!(state_marker == last_block_number);
 }
--- crates/starknet_node/examples/starknet_client.rs
+++ crates/starknet_node/examples/starknet_client.rs
@@ -4,9 +4,10 @@ use starknet_node::config::load_config;
 
 #[tokio::main]
 async fn main() {
-    let config = load_config("config/config.ron").unwrap();
-    let starknet_client = StarknetClient::new(&config.central.url).unwrap();
-    let _latest_block_number = starknet_client.block_number().await.unwrap();
-    let _block_123456 = starknet_client.block(BlockNumber(123456)).await.unwrap();
-    let _state_diff = starknet_client.state_update(BlockNumber(123456)).await;
+    let config = load_config("config/config.ron").expect("Load config");
+    let starknet_client = StarknetClient::new(&config.central.url).expect("Create new client");
+    let _latest_block_number = starknet_client.block_number().await.expect("Get block number");
+    let _block_123456 = starknet_client.block(BlockNumber(123456)).await.expect("Get block");
+    let _state_diff =
+        starknet_client.state_update(BlockNumber(123456)).await.expect("Get state diff");
 }
