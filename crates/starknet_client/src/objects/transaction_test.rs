999bd4c88d71a7970adb5536c0a03b8daf6c9abd
Shahak Shama <shahak@starkware.co>
2022-07-21 14:41:15 +0300

    add test for loading invoke transaction

--- crates/starknet_client/src/objects/transaction_test.rs
+++ crates/starknet_client/src/objects/transaction_test.rs
@@ -1,11 +1,18 @@
 use assert::assert_ok;
 
 use super::super::test_utils::read_resource::read_resource_file;
-use super::transaction::DeployTransaction;
+use super::transaction::{DeployTransaction, InvokeTransaction};
 
 #[test]
 fn load_deploy_transaction_succeeds() {
     assert_ok!(serde_json::from_str::<DeployTransaction>(&read_resource_file(
         "deploy_transaction.json"
+    )));
+}
+
+#[test]
+fn load_invoke_transaction_succeeds() {
+    assert_ok!(serde_json::from_str::<InvokeTransaction>(&read_resource_file(
+        "invoke_transaction.json"
     )));
 }
