0d0109eb9111ed0eee06c181a81bd8e07e58f133
ShahakShama <70578257+ShahakShama@users.noreply.github.com>
2022-07-21 17:02:45 +0300

    shahak/sn client test/block state update resource (#128)
    * add block_state_update.json and load test

--- crates/starknet_client/src/objects/block_test.rs
+++ crates/starknet_client/src/objects/block_test.rs
@@ -1,9 +1,16 @@
 use assert::assert_ok;
 
 use super::super::test_utils::read_resource::read_resource_file;
-use super::super::Block;
+use super::block::{Block, BlockStateUpdate};
 
 #[test]
-fn load_block() {
+fn load_block_succeeds() {
     assert_ok!(serde_json::from_str::<Block>(&read_resource_file("block.json")));
+}
+
+#[test]
+fn load_block_state_update_succeeds() {
+    assert_ok!(serde_json::from_str::<BlockStateUpdate>(&read_resource_file(
+        "block_state_update.json"
+    )));
 }
