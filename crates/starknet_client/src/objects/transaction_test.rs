666debd880c9214c2e5be2d517fb969a9a40a85b
ShahakShama <70578257+ShahakShama@users.noreply.github.com>
2022-07-24 16:36:00 +0300

    add test for loading a generic transaction (#140)
    * add test for loading a generic transaction

--- crates/starknet_client/src/objects/transaction_test.rs
+++ crates/starknet_client/src/objects/transaction_test.rs
@@ -1,7 +1,7 @@
 use assert::assert_ok;
 
 use super::super::test_utils::read_resource::read_resource_file;
-use super::transaction::{DeclareTransaction, DeployTransaction, InvokeTransaction};
+use super::transaction::{DeclareTransaction, DeployTransaction, InvokeTransaction, Transaction};
 
 #[test]
 fn load_deploy_transaction_succeeds() {
@@ -22,4 +22,13 @@ fn load_declare_transaction_succeeds() {
     assert_ok!(serde_json::from_str::<DeclareTransaction>(&read_resource_file(
         "declare_transaction.json"
     )));
+}
+
+#[test]
+fn load_transaction_succeeds() {
+    for file_name in
+        ["deploy_transaction.json", "invoke_transaction.json", "declare_transaction.json"]
+    {
+        assert_ok!(serde_json::from_str::<Transaction>(&read_resource_file(file_name)));
+    }
 }
