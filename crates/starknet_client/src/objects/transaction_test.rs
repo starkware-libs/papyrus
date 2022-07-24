024b7dcf189dd364b1afde693bb8334895b21c1b
ShahakShama <70578257+ShahakShama@users.noreply.github.com>
2022-07-24 16:46:45 +0300

    add transaction receipt test (#141)
    * add transaction receipt test

--- crates/starknet_client/src/objects/transaction_test.rs
+++ crates/starknet_client/src/objects/transaction_test.rs
@@ -1,7 +1,9 @@
 use assert::assert_ok;
 
 use super::super::test_utils::read_resource::read_resource_file;
-use super::transaction::{DeclareTransaction, DeployTransaction, InvokeTransaction, Transaction};
+use super::transaction::{
+    DeclareTransaction, DeployTransaction, InvokeTransaction, Transaction, TransactionReceipt,
+};
 
 #[test]
 fn load_deploy_transaction_succeeds() {
@@ -30,5 +32,16 @@ fn load_transaction_succeeds() {
         ["deploy_transaction.json", "invoke_transaction.json", "declare_transaction.json"]
     {
         assert_ok!(serde_json::from_str::<Transaction>(&read_resource_file(file_name)));
+    }
+}
+
+#[test]
+fn load_transaction_receipt_succeeds() {
+    for file_name in [
+        "transaction_receipt.json",
+        "transaction_receipt_without_l1_to_l2.json",
+        "transaction_receipt_without_l1_to_l2_nonce.json",
+    ] {
+        assert_ok!(serde_json::from_str::<TransactionReceipt>(&read_resource_file(file_name)));
     }
 }
