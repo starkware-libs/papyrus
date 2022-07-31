df945495dea9110fa08edd6b8080af8b98f27d2e
Shahak Shama <shahak@starkware.co>
2022-07-31 17:19:16 +0300

    mock StarknetClient in stream_block_headers test

--- crates/papyrus_sync/src/sources/central_test.rs
+++ crates/papyrus_sync/src/sources/central_test.rs
@@ -1,44 +1,55 @@
+use async_trait::async_trait;
 use futures_util::pin_mut;
-use mockito::{mock, Matcher};
-use starknet_api::BlockNumber;
-use starknet_client::Block;
+use mockall::{mock, predicate};
+use starknet_api::{BlockNumber, ClassHash, ContractClass};
+use starknet_client::{Block, BlockStateUpdate, ClientError, StarknetClientTrait};
 use tokio_stream::StreamExt;
 
-use super::CentralSourceConfig;
 use crate::CentralSource;
+
+// Using mock! and not automock because StarknetClient is defined in another crate. For more
+// details, See mockall's documentation.
+mock! {
+    pub StarknetClient {}
+
+    #[async_trait]
+    impl StarknetClientTrait for StarknetClient {
+        async fn block_number(&self) -> Result<Option<BlockNumber>, ClientError>;
+
+        async fn block(&self, block_number: BlockNumber) -> Result<Option<Block>, ClientError>;
+
+        async fn class_by_hash(&self, class_hash: ClassHash) -> Result<ContractClass, ClientError>;
+
+        async fn state_update(
+            &self,
+            block_number: BlockNumber,
+        ) -> Result<BlockStateUpdate, ClientError>;
+    }
+}
 
 #[tokio::test]
 async fn stream_block_headers() {
-    let config = CentralSourceConfig { url: mockito::server_url() };
-    let central_source = CentralSource::new(config).unwrap();
+    let mut mock = MockStarknetClient::new();
 
-    // Prepare mock calls.
-    let latest_block = Block { block_number: BlockNumber(9), ..Default::default() };
-    let mock_last = mock("GET", "/feeder_gateway/get_block")
-        .with_status(200)
-        .with_body(serde_json::to_string(&latest_block).unwrap())
-        .create();
-    let mock_headers = mock("GET", "/feeder_gateway/get_block")
-        // TODO(dan): consider using a regex.
-        .match_query(Matcher::AnyOf(vec![
-            Matcher::UrlEncoded("blockNumber".to_string(), "5".to_string()),
-            Matcher::UrlEncoded("blockNumber".to_string(), "6".to_string()),
-            Matcher::UrlEncoded("blockNumber".to_string(), "7".to_string()),
-            Matcher::UrlEncoded("blockNumber".to_string(), "8".to_string()),
-        ]))
-        .with_status(200)
-        .with_body(serde_json::to_string(&Block::default()).unwrap())
-        .expect(4)
-        .create();
+    // We need to perform all the mocks before moving the mock into central_source.
+    const EXPECTED_LAST_BLOCK_NUMBER: BlockNumber = BlockNumber(9);
+    mock.expect_block_number().times(1).returning(|| Ok(Some(EXPECTED_LAST_BLOCK_NUMBER)));
 
-    let last_block_number = central_source.get_block_marker().await.unwrap().prev();
+    for i in 5..9 {
+        mock.expect_block()
+            .with(predicate::eq(BlockNumber(i)))
+            .times(1)
+            .returning(|_x| Ok(Some(Block::default())));
+    }
+    let central_source = CentralSource { starknet_client: mock };
+    let last_block_number = central_source.get_block_marker().await.unwrap().prev().unwrap();
+    assert_eq!(last_block_number, EXPECTED_LAST_BLOCK_NUMBER);
+
     let mut initial_block_num = BlockNumber(5);
-    let stream = central_source.stream_new_blocks(initial_block_num, last_block_number.unwrap());
+    let stream = central_source.stream_new_blocks(initial_block_num, last_block_number);
     pin_mut!(stream);
     while let Some(Ok((block_number, _header, _body))) = stream.next().await {
         assert_eq!(initial_block_num, block_number);
         initial_block_num = initial_block_num.next();
     }
-    mock_last.assert();
-    mock_headers.assert();
 }
