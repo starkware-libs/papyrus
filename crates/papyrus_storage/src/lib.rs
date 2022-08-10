df9265195d5fcc9cb8288df8771d72400c767c84
Dan Brownstein <dan@starkware.co>
2022-08-10 18:09:04 +0300

    Add monitoring gateway

--- crates/papyrus_storage/src/lib.rs
+++ crates/papyrus_storage/src/lib.rs
@@ -81,8 +81,8 @@ pub enum MarkerKind {
 pub type MarkersTable<'env> = TableHandle<'env, MarkerKind, BlockNumber>;
 
 macro_rules! struct_field_names {
-    (pub struct $name:ident { $($fname:ident : $ftype:ty),* }) => {
-        struct $name {
+    (struct $name:ident { $($fname:ident : $ftype:ty),* }) => {
+        pub struct $name {
             $($fname : $ftype),*
         }
 
@@ -96,7 +96,7 @@ macro_rules! struct_field_names {
 }
 
 struct_field_names! {
-    pub struct Tables {
+    struct Tables {
         markers: TableIdentifier<MarkerKind, BlockNumber>,
         nonces: TableIdentifier<(ContractAddress, BlockNumber), Nonce>,
         headers: TableIdentifier<BlockNumber, BlockHeader>,
@@ -104,13 +104,18 @@ struct_field_names! {
         transactions: TableIdentifier<(BlockNumber, TransactionOffsetInBlock), Transaction>,
         transaction_outputs: TableIdentifier<(BlockNumber, TransactionOffsetInBlock), TransactionOutput>,
         transaction_hash_to_idx:
-            TableIdentifier<TransactionHash, (BlockNumber, TransactionOffsetInBlock)>,
+        TableIdentifier<TransactionHash, (BlockNumber, TransactionOffsetInBlock)>,
         state_diffs: TableIdentifier<BlockNumber, ThinStateDiff>,
         declared_classes: TableIdentifier<ClassHash, IndexedDeclaredContract>,
         deployed_contracts: TableIdentifier<ContractAddress, IndexedDeployedContract>,
         contract_storage: TableIdentifier<(ContractAddress, StorageKey, BlockNumber), StarkFelt>
     }
 }
+
+pub fn table_names() -> &'static [&'static str] {
+    Tables::field_names()
+}
+
 #[derive(Clone)]
 pub struct StorageReader {
     db_reader: DbReader,
