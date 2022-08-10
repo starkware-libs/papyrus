12710b7657452e7bb7f0ce020b434fedac14f687
dan-starkware <56217775+dan-starkware@users.noreply.github.com>
2022-08-10 09:59:55 +0300

    Add documentation to starknet API (#173)
    * Add documentation to starknet API

--- crates/starknet_api/src/core.rs
+++ crates/starknet_api/src/core.rs
@@ -2,16 +2,19 @@ use serde::{Deserialize, Serialize};
 
 use super::{StarkFelt, StarkHash};
 
+/// The address of a StarkNet contract.
 #[derive(
     Debug, Default, Copy, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord,
 )]
 pub struct ContractAddress(pub StarkHash);
 
+/// The hash of a StarkNet [ContractClass](`super::ContractClass`).
 #[derive(
     Debug, Default, Copy, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord,
 )]
 pub struct ClassHash(pub StarkHash);
 
+/// The nonce of a StarkNet contract.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
 pub struct Nonce(pub StarkFelt);
 impl Default for Nonce {
--- crates/starknet_api/src/state.rs
+++ crates/starknet_api/src/state.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 use super::{BlockNumber, ClassHash, ContractAddress, ContractClass, Nonce, StarkFelt};
 
-// Rerpesents the sequential numbering of the states between blocks.
+/// The sequential numbering of the states between blocks in StarkNet.
 // Example:
 // States: S0       S1       S2
 // Blocks      B0->     B1->
@@ -32,6 +32,7 @@ impl StateNumber {
 
 // Invariant: Addresses are strictly increasing.
 // TODO(spapini): Enforce the invariant.
+/// The differences between two states in StarkNet.
 #[derive(Debug, Default, Clone, PartialEq, Eq, Deserialize, Serialize)]
 pub struct StateDiff {
     pub deployed_contracts: Vec<DeployedContract>,
@@ -40,24 +41,28 @@ pub struct StateDiff {
     pub nonces: Vec<(ContractAddress, Nonce)>,
 }
 
+/// A deployed contract in StarkNet.
 #[derive(Debug, Default, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
 pub struct DeployedContract {
     pub address: ContractAddress,
     pub class_hash: ClassHash,
 }
 
+// TODO(dan): move to storage.
 #[derive(Debug, Default, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
 pub struct IndexedDeployedContract {
     pub block_number: BlockNumber,
     pub class_hash: ClassHash,
 }
 
+/// A declared contract in StarkNet.
 #[derive(Debug, Default, Clone, PartialEq, Eq, Deserialize, Serialize)]
 pub struct DeclaredContract {
     pub class_hash: ClassHash,
     pub contract_class: ContractClass,
 }
 
+// TODO(dan): move to storage.
 #[derive(Debug, Default, Clone, PartialEq, Eq, Deserialize, Serialize)]
 pub struct IndexedDeclaredContract {
     pub block_number: BlockNumber,
@@ -66,6 +71,7 @@ pub struct IndexedDeclaredContract {
 
 // Invariant: Addresses are strictly increasing. In particular, no address appears twice.
 // TODO(spapini): Enforce the invariant.
+/// Storage differences in StarkNet.
 #[derive(Debug, Default, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
 pub struct StorageDiff {
     pub address: ContractAddress,
@@ -74,9 +80,11 @@ pub struct StorageDiff {
 
 // TODO: Invariant: this is in range.
 // TODO(spapini): Enforce the invariant.
+/// A storage key in a StarkNet contract.
 #[derive(Debug, Default, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
 pub struct StorageKey(pub StarkFelt);
 
+/// A storage entry in a StarkNet contract.
 #[derive(Debug, Default, Clone, PartialEq, Eq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
 pub struct StorageEntry {
     pub key: StorageKey,
