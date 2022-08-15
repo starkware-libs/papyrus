ce4a4222ce82e90b1b22909ea3335848aff6f6d2
ShahakShama <70578257+ShahakShama@users.noreply.github.com>
2022-08-15 16:10:51 +0300

    implement state diff test (#189)
    * implement state diff test
    
    * CR fix

--- crates/papyrus_sync/src/sources/central_test.rs
+++ crates/papyrus_sync/src/sources/central_test.rs
@@ -1,15 +1,23 @@
+use std::collections::HashMap;
 use std::sync::Arc;
 
 use assert_matches::assert_matches;
+use assert_unordered::assert_eq_unordered;
 use async_trait::async_trait;
 use futures_util::pin_mut;
 use mockall::{mock, predicate};
 use reqwest::StatusCode;
-use starknet_api::{BlockNumber, ClassHash, ContractClass};
-use starknet_client::{Block, BlockStateUpdate, ClientError, StarknetClientTrait};
+use starknet_api::{
+    shash, BlockHash, BlockNumber, ClassHash, ContractAddress, ContractClass, DeployedContract,
+    GlobalRoot, StarkHash, StorageDiff, StorageEntry, StorageKey,
+};
+use starknet_client::{
+    Block, BlockStateUpdate, ClientError, StarknetClientTrait, StateDiff as ClientStateDiff,
+};
 use tokio_stream::StreamExt;
 
 use crate::sources::central::{BlockNotFound, CentralError, GenericCentralSource};
+use crate::StateDiff;
 
 // Using mock! and not automock because StarknetClient is defined in another crate. For more
 // details, See mockall's documentation: https://docs.rs/mockall/latest/mockall/
@@ -161,4 +169,122 @@ async fn stream_block_headers_error() {
         expected_block_num = expected_block_num.next();
     }
     assert_eq!(expected_block_num, BlockNumber(ERROR_BLOCK_NUMBER + 1));
+}
+
+#[tokio::test]
+async fn stream_state_updates() {
+    const START_BLOCK_NUMBER: u64 = 5;
+    const END_BLOCK_NUMBER: u64 = 7;
+
+    let class_hash1 = ClassHash(shash!("0x123"));
+    let class_hash2 = ClassHash(shash!("0x456"));
+    let class_hash3 = ClassHash(shash!("0x789"));
+    let contract_address1 = ContractAddress(shash!("0xabc"));
+    let contract_address2 = ContractAddress(shash!("0xdef"));
+    let root1 = GlobalRoot(shash!("0x111"));
+    let root2 = GlobalRoot(shash!("0x222"));
+    let block_hash1 = BlockHash(shash!("0x333"));
+    let block_hash2 = BlockHash(shash!("0x444"));
+
+    let storage_entry = StorageEntry { key: StorageKey(shash!("0x555")), value: shash!("0x666") };
+
+    // TODO(shahak): Fill these contract classes with non-empty data.
+    let contract_class1 = ContractClass::default();
+    let contract_class2 = ContractClass::default();
+    let contract_class3 = ContractClass::default();
+
+    let client_state_diff1 = ClientStateDiff {
+        storage_diffs: HashMap::from([(contract_address1, vec![storage_entry.clone()])]),
+        deployed_contracts: vec![
+            DeployedContract { address: contract_address1, class_hash: class_hash2 },
+            DeployedContract { address: contract_address2, class_hash: class_hash3 },
+        ],
+        declared_classes: vec![class_hash1, class_hash3],
+    };
+    let client_state_diff2 = ClientStateDiff::default();
+
+    let block_state_update1 = BlockStateUpdate {
+        block_hash: block_hash1,
+        new_root: root2,
+        old_root: root1,
+        state_diff: client_state_diff1,
+    };
+    let block_state_update2 = BlockStateUpdate {
+        block_hash: block_hash2,
+        new_root: root2,
+        old_root: root2,
+        state_diff: client_state_diff2,
+    };
+
+    let mut mock = MockStarknetClient::new();
+    let block_state_update1_clone = block_state_update1.clone();
+    mock.expect_state_update()
+        .with(predicate::eq(BlockNumber(START_BLOCK_NUMBER)))
+        .times(1)
+        .returning(move |_x| Ok(Some(block_state_update1_clone.clone())));
+    let block_state_update2_clone = block_state_update2.clone();
+    mock.expect_state_update()
+        .with(predicate::eq(BlockNumber(START_BLOCK_NUMBER + 1)))
+        .times(1)
+        .returning(move |_x| Ok(Some(block_state_update2_clone.clone())));
+    let contract_class1_clone = contract_class1.clone();
+    mock.expect_class_by_hash()
+        .with(predicate::eq(class_hash1))
+        .times(1)
+        .returning(move |_x| Ok(Some(contract_class1_clone.clone())));
+    let contract_class2_clone = contract_class2.clone();
+    mock.expect_class_by_hash()
+        .with(predicate::eq(class_hash2))
+        .times(1)
+        .returning(move |_x| Ok(Some(contract_class2_clone.clone())));
+    let contract_class3_clone = contract_class3.clone();
+    mock.expect_class_by_hash()
+        .with(predicate::eq(class_hash3))
+        .times(1)
+        .returning(move |_x| Ok(Some(contract_class3_clone.clone())));
+
+    let central_source = GenericCentralSource { starknet_client: Arc::new(mock) };
+    let initial_block_num = BlockNumber(START_BLOCK_NUMBER);
+
+    let stream =
+        central_source.stream_state_updates(initial_block_num, BlockNumber(END_BLOCK_NUMBER));
+    pin_mut!(stream);
+
+    let (current_block_num, state_diff) = if let Some(Ok(state_diff_tuple)) = stream.next().await {
+        state_diff_tuple
+    } else {
+        panic!("Match of streamed state_update failed!");
+    };
+    assert_eq!(initial_block_num, current_block_num);
+    assert_eq!(
+        vec![
+            DeployedContract { address: contract_address1, class_hash: class_hash2 },
+            DeployedContract { address: contract_address2, class_hash: class_hash3 },
+        ],
+        state_diff.deployed_contracts
+    );
+    assert_eq!(
+        vec![StorageDiff { address: contract_address1, diff: vec![storage_entry] }],
+        state_diff.storage_diffs
+    );
+    // TODO(yair): Sort the contract classes and change this to assert_eq.
+    assert_eq_unordered!(
+        &vec![
+            (class_hash3, contract_class3),
+            (class_hash2, contract_class2),
+            (class_hash1, contract_class1),
+        ],
+        &state_diff.declared_classes,
+    );
+    assert!(state_diff.nonces.is_empty());
+
+    let (current_block_num, state_diff) = if let Some(Ok(state_diff_tuple)) = stream.next().await {
+        state_diff_tuple
+    } else {
+        panic!("Match of streamed state_update failed!");
+    };
+    assert_eq!(initial_block_num.next(), current_block_num);
+    assert_eq!(state_diff, StateDiff::default());
+
+    assert!(stream.next().await.is_none());
 }
