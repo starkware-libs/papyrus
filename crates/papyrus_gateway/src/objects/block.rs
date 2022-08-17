32845b363827065674a22a20c1664621f49b4cce
anatg <anatg@starkware.co>
2022-08-17 16:29:18 +0300

    Remove block status from  gateway

--- crates/papyrus_gateway/src/objects/block.rs
+++ crates/papyrus_gateway/src/objects/block.rs
@@ -1,35 +1,9 @@
 use serde::{Deserialize, Serialize};
 use starknet_api::{
-    BlockHash, BlockHeader as StarknetBlockHeader, BlockNumber, BlockTimestamp, ContractAddress,
-    GlobalRoot, NodeBlockStatus,
+    BlockHash, BlockNumber, BlockStatus, BlockTimestamp, ContractAddress, GlobalRoot,
 };
 
 use super::transaction::Transactions;
-
-#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
-pub enum BlockStatus {
-    #[serde(rename = "PENDING")]
-    Pending,
-    #[serde(rename = "PROVEN")]
-    Proven,
-    #[serde(rename = "ACCEPTED_ON_L2")]
-    AcceptedOnL2,
-    #[serde(rename = "ACCEPTED_ON_L1")]
-    AcceptedOnL1,
-    #[serde(rename = "REJECTED")]
-    Rejected,
-}
-
-impl From<NodeBlockStatus> for BlockStatus {
-    fn from(status: NodeBlockStatus) -> Self {
-        match status {
-            NodeBlockStatus::Pending => BlockStatus::Pending,
-            NodeBlockStatus::AcceptedOnL2 => BlockStatus::AcceptedOnL2,
-            NodeBlockStatus::AcceptedOnL1 => BlockStatus::AcceptedOnL1,
-            NodeBlockStatus::Rejected => BlockStatus::Rejected,
-        }
-    }
-}
 
 #[derive(Debug, Clone, Eq, PartialEq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
 pub struct BlockHeader {
@@ -37,27 +11,28 @@ pub struct BlockHeader {
     pub parent_hash: BlockHash,
     pub block_number: BlockNumber,
     pub status: BlockStatus,
-    pub sequencer: ContractAddress,
+    pub sequencer_address: ContractAddress,
     pub new_root: GlobalRoot,
-    pub accepted_time: BlockTimestamp,
+    pub timestamp: BlockTimestamp,
 }
 
-impl From<StarknetBlockHeader> for BlockHeader {
-    fn from(header: StarknetBlockHeader) -> Self {
+impl From<starknet_api::BlockHeader> for BlockHeader {
+    fn from(header: starknet_api::BlockHeader) -> Self {
         BlockHeader {
             block_hash: header.block_hash,
             parent_hash: header.parent_hash,
             block_number: header.block_number,
-            status: header.status.into(),
-            sequencer: header.sequencer,
+            status: header.status,
+            sequencer_address: header.sequencer,
             new_root: header.state_root,
-            accepted_time: header.timestamp,
+            timestamp: header.timestamp,
         }
     }
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
 pub struct Block {
+    #[serde(flatten)]
     pub header: BlockHeader,
     pub transactions: Transactions,
 }
