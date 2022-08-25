f6920df95f26b97da272fd0a3763eeaf1f29e8c9
Shahar Papini <spapini@starkware.co>
2022-08-25 14:55:02 +0300

    Enforce StarkHash range

--- crates/starknet_api/src/block.rs
+++ crates/starknet_api/src/block.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 
 use super::serde_utils::{HexAsBytes, NonPrefixedHexAsBytes, PrefixedHexAsBytes};
 use super::{ContractAddress, StarkHash, Transaction};
+use crate::serde_utils::DeserializationError;
 use crate::TransactionOutput;
 
 // TODO(spapini): Verify the invariant that it is in range.
@@ -15,18 +16,19 @@ pub struct BlockHash(pub StarkHash);
 #[derive(
     Debug, Copy, Clone, Default, Eq, PartialEq, Hash, Deserialize, Serialize, PartialOrd, Ord,
 )]
-#[serde(from = "NonPrefixedHexAsBytes<32_usize>", into = "NonPrefixedHexAsBytes<32_usize>")]
+#[serde(try_from = "NonPrefixedHexAsBytes<32_usize>", into = "NonPrefixedHexAsBytes<32_usize>")]
 pub struct GlobalRoot(pub StarkHash);
 // We don't use the regular StarkHash deserialization since the Starknet sequencer returns the
 // global root hash as a hex string without a "0x" prefix.
-impl From<NonPrefixedHexAsBytes<32_usize>> for GlobalRoot {
-    fn from(val: NonPrefixedHexAsBytes<32_usize>) -> Self {
-        GlobalRoot(StarkHash(val.0))
+impl TryFrom<NonPrefixedHexAsBytes<32_usize>> for GlobalRoot {
+    type Error = DeserializationError;
+    fn try_from(val: NonPrefixedHexAsBytes<32_usize>) -> Result<Self, Self::Error> {
+        Ok(GlobalRoot(StarkHash::new(val.0)?))
     }
 }
 impl From<GlobalRoot> for NonPrefixedHexAsBytes<32_usize> {
     fn from(val: GlobalRoot) -> Self {
-        HexAsBytes(val.0.0)
+        HexAsBytes(val.0.into_bytes())
     }
 }
 
