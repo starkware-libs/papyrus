8e61b914f71ad993263f72ea76b89761e8706bc5
Yair Bakalchuk <yair@starkware.co>
2022-09-04 17:55:40 +0300

    PatriciaKey test

--- crates/starknet_api/src/hash_test.rs
+++ crates/starknet_api/src/hash_test.rs
@@ -1,4 +1,7 @@
-use crate::{shash, StarkHash};
+use assert_matches::assert_matches;
+
+use crate::serde_utils::DeserializationError;
+use crate::{shash, PatriciaKey, StarkHash};
 
 #[test]
 fn test_hash_macro() {
@@ -16,4 +19,19 @@ fn test_hash_macro() {
 fn test_hash_serde() {
     let hash = shash!("0x123");
     assert_eq!(hash, serde_json::from_str(&serde_json::to_string(&hash).unwrap()).unwrap());
+}
+
+#[test]
+fn test_valid_patricia_key() {
+    let hash = shash!("0x123");
+    let patricia_key = PatriciaKey::new(hash).unwrap();
+    assert_eq!(patricia_key.into_hash(), hash);
+}
+
+#[test]
+fn test_out_of_range_patricia_key() {
+    // 2**251
+    let hash = shash!("0x800000000000000000000000000000000000000000000000000000000000000");
+    let err = PatriciaKey::new(hash);
+    assert_matches!(err, Err(DeserializationError::OutOfRange { string: _err_str }));
 }
