84e15fa8df8065339c7a5744a750676ecb49ee28
Yair Bakalchuk <yair@starkware.co>
2022-09-08 09:47:59 +0300

    Fix clippy

--- crates/starknet_api/src/block.rs
+++ crates/starknet_api/src/block.rs
@@ -80,7 +80,7 @@ impl BlockNumber {
 }
 
 impl fmt::Display for BlockNumber {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.0)
     }
 }
