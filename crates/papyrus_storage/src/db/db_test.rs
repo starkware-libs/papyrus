1f06d10f06d2ade681eab617b14b02b44ed676e0
Yair Bakalchuk <yair@starkware.co>
2022-09-11 17:03:34 +0300

    Test deleting from storage

--- crates/papyrus_storage/src/db/db_test.rs
+++ crates/papyrus_storage/src/db/db_test.rs
@@ -37,10 +37,19 @@ fn test_txns() {
     // Snapshot state by creating a read txn.
     let txn2 = reader.begin_ro_txn().unwrap();
 
+    // Delete the value.
+    let wtxn2 = writer.begin_rw_txn().unwrap();
+    table.delete(&wtxn2, b"key").unwrap();
+    wtxn2.commit().unwrap();
+
+    // Snapshot state by creating a read txn.
+    let txn3 = reader.begin_ro_txn().unwrap();
+
     // Validate data in snapshots.
     assert_eq!(table.get(&txn0, b"key").unwrap(), None);
     assert_eq!(table.get(&txn1, b"key").unwrap(), Some(*b"data0"));
     assert_eq!(table.get(&txn2, b"key").unwrap(), Some(*b"data1"));
+    assert_eq!(table.get(&txn3, b"key").unwrap(), None);
 }
 #[test]
 
@@ -70,4 +79,18 @@ fn test_stats() {
     assert_eq!(empty_stat.entries, 1);
     assert_eq!(empty_stat.overflow_pages, 0);
     assert_eq!(empty_stat.leaf_pages, 1);
+
+    // Delete the value.
+    let wtxn = writer.begin_rw_txn().unwrap();
+    let table = wtxn.open_table(&table_id).unwrap();
+    table.delete(&wtxn, b"key").unwrap();
+    wtxn.commit().unwrap();
+
+    // Empty table stats.
+    let empty_stat = reader.get_table_stats("table").unwrap();
+    assert_eq!(empty_stat.branch_pages, 0);
+    assert_eq!(empty_stat.depth, 0);
+    assert_eq!(empty_stat.entries, 0);
+    assert_eq!(empty_stat.overflow_pages, 0);
+    assert_eq!(empty_stat.leaf_pages, 0);
 }
