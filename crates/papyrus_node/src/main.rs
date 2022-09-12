ac1fa5c0e8107bde6e9c8cb251817d3a3507ce71
dan-starkware <56217775+dan-starkware@users.noreply.github.com>
2022-09-12 15:40:16 +0300

    Add monitoring gateway (#245)
    * Add monitoring gateway

--- crates/papyrus_node/src/main.rs
+++ crates/papyrus_node/src/main.rs
@@ -1,5 +1,6 @@
 use log::info;
 use papyrus_gateway::run_server;
+use papyrus_monitoring_gateway::run_server as monitoring_run_server;
 use papyrus_node::config::load_config;
 use papyrus_storage::open_storage;
 use papyrus_sync::{CentralSource, StateSync};
@@ -23,7 +24,10 @@ async fn main() -> anyhow::Result<()> {
 
     // Pass reader to storage.
     let (_, server_handle) = run_server(config.gateway, storage_reader.clone()).await?;
-    let (_, sync_thread_res) = tokio::join!(server_handle, sync_thread);
+    let (_, monitoring_server_handle) =
+        monitoring_run_server(config.monitoring_gateway, storage_reader.clone()).await?;
+    let (_, _, sync_thread_res) =
+        tokio::join!(server_handle, monitoring_server_handle, sync_thread);
     sync_thread_res??;
 
     Ok(())
