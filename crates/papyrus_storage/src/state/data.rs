cc58e31dbb1435c31ccf44edb0631f31eae1f732
anatg <anatg@starkware.co>
2022-09-19 14:18:27 +0300

    Keep the invariant of ThinStateDiff and fix state diff of gateway

--- crates/papyrus_storage/src/state/data.rs
+++ crates/papyrus_storage/src/state/data.rs
@@ -1,5 +1,8 @@
 use serde::{Deserialize, Serialize};
-use starknet_api::{BlockNumber, ClassHash, ContractNonce, DeployedContract, StorageDiff};
+use starknet_api::{
+    BlockNumber, ClassHash, ContractNonce, DeclaredContract, DeployedContract, StateDiff,
+    StorageDiff,
+};
 
 // Data structs that are serialized into the database.
 
@@ -16,11 +19,66 @@ pub struct IndexedDeclaredContract {
 }
 
 // Invariant: Addresses are strictly increasing.
-// TODO(spapini): Enforce the invariant.
+// The invariant is enforced as [`ThinStateDiff`] is created only by the [`split_diff_for_storage`]
+// function from a [`starknet_api`][`StateDiff`] where the addresses are strictly increasing.
 #[derive(Debug, Default, Clone, Eq, PartialEq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
 pub struct ThinStateDiff {
-    pub deployed_contracts: Vec<DeployedContract>,
-    pub storage_diffs: Vec<StorageDiff>,
-    pub declared_classes: Vec<ClassHash>,
-    pub nonces: Vec<ContractNonce>,
+    deployed_contracts: Vec<DeployedContract>,
+    storage_diffs: Vec<StorageDiff>,
+    declared_contract_hashes: Vec<ClassHash>,
+    nonces: Vec<ContractNonce>,
+}
+
+pub(crate) fn split_diff_for_storage(
+    state_diff: StateDiff,
+    deployed_contract_class_definitions: Vec<DeclaredContract>,
+) -> (ThinStateDiff, Vec<DeclaredContract>) {
+    let (deployed_contracts, storage_diffs, mut declared_classes, nonces) = state_diff.destruct();
+    let thin_state_diff = ThinStateDiff {
+        deployed_contracts,
+        storage_diffs,
+        declared_contract_hashes: Vec::from_iter(
+            declared_classes.iter().map(|declared_contract| declared_contract.class_hash),
+        ),
+        nonces,
+    };
+    declared_classes.extend(deployed_contract_class_definitions.into_iter());
+    (thin_state_diff, declared_classes)
+}
+
+type ThinStateDiffAsTuple =
+    (Vec<DeployedContract>, Vec<StorageDiff>, Vec<ClassHash>, Vec<ContractNonce>);
+
+impl ThinStateDiff {
+    pub fn deployed_contracts(&self) -> &Vec<DeployedContract> {
+        &self.deployed_contracts
+    }
+    pub fn storage_diffs(&self) -> &Vec<StorageDiff> {
+        &self.storage_diffs
+    }
+    pub fn declared_contract_hashes(&self) -> &Vec<ClassHash> {
+        &self.declared_contract_hashes
+    }
+    pub fn nonces(&self) -> &Vec<ContractNonce> {
+        &self.nonces
+    }
+    pub fn destruct(self) -> ThinStateDiffAsTuple {
+        (self.deployed_contracts, self.storage_diffs, self.declared_contract_hashes, self.nonces)
+    }
+}
+
+#[cfg(any(feature = "testing", test))]
+impl From<starknet_api::StateDiff> for ThinStateDiff {
+    fn from(diff: starknet_api::StateDiff) -> Self {
+        let (deployed_contracts, storage_diffs, declared_classes, nonces) = diff.destruct();
+        Self {
+            storage_diffs,
+            declared_contract_hashes: declared_classes
+                .into_iter()
+                .map(|declared_contract| declared_contract.class_hash)
+                .collect(),
+            deployed_contracts,
+            nonces,
+        }
+    }
 }
