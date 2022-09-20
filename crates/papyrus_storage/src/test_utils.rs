6576d10ffdd8ff37e84943fb57b21d7b1627ea1d
Yair Bakalchuk <yair@starkware.co>
2022-09-20 14:46:21 +0300

    Storage support for reverting state diffs

--- crates/papyrus_storage/src/test_utils.rs
+++ crates/papyrus_storage/src/test_utils.rs
@@ -5,9 +5,10 @@ use std::path::Path;
 use starknet_api::serde_utils::bytes_from_hex_str;
 use starknet_api::{
     shash, Block, BlockBody, BlockHash, BlockHeader, BlockNumber, BlockTimestamp, CallData,
-    ClassHash, ContractAddress, ContractAddressSalt, DeployTransaction, DeployTransactionOutput,
-    EntryPointSelector, EthAddress, Fee, GasPrice, GlobalRoot, InvokeTransaction,
-    InvokeTransactionOutput, L2ToL1Payload, MessageToL1, Nonce, StarkHash, Transaction,
+    ClassHash, ContractAddress, ContractAddressSalt, ContractNonce, DeclaredContract,
+    DeployTransaction, DeployTransactionOutput, DeployedContract, EntryPointSelector, EthAddress,
+    Fee, GasPrice, GlobalRoot, InvokeTransaction, InvokeTransactionOutput, L2ToL1Payload,
+    MessageToL1, Nonce, StarkHash, StorageDiff, StorageEntry, StorageKey, Transaction,
     TransactionHash, TransactionOutput, TransactionSignature, TransactionVersion,
 };
 use tempfile::tempdir;
@@ -191,4 +192,67 @@ pub fn get_alpha4_block_number_1() -> Block {
     ];
 
     Block { header, body: BlockBody::new(transactions, transaction_outputs).unwrap() }
+}
+
+fn get_test_state_diff()
+-> (BlockHeader, BlockHeader, starknet_api::StateDiff, Vec<DeclaredContract>) {
+    let parent_hash =
+        BlockHash::new(shash!("0x642b629ad8ce233b55798c83bb629a59bf0a0092f67da28d6d66776680d5483"));
+    let state_root = GlobalRoot::new(shash!("0x12"));
+    let parent_header = BlockHeader {
+        block_number: BlockNumber::new(0),
+        block_hash: parent_hash,
+        state_root,
+        ..BlockHeader::default()
+    };
+
+    let block_hash =
+        BlockHash::new(shash!("0x642b629ad8ce233b55798c83bb629a59bf0a0092f67da28d6d66776680d5493"));
+    let header = BlockHeader {
+        block_number: BlockNumber::new(1),
+        block_hash,
+        parent_hash,
+        ..BlockHeader::default()
+    };
+
+    let address0 = ContractAddress::try_from(shash!("0x11")).unwrap();
+    let hash0 = ClassHash::new(shash!("0x4"));
+    let address1 = ContractAddress::try_from(shash!("0x21")).unwrap();
+    let hash1 = ClassHash::new(shash!("0x5"));
+    let class0 = starknet_api::ContractClass::default();
+    let class1 = starknet_api::ContractClass::default();
+    let key0 = StorageKey::try_from(shash!("0x1001")).unwrap();
+    let value0 = shash!("0x200");
+    let key1 = StorageKey::try_from(shash!("0x1002")).unwrap();
+    let value1 = shash!("0x201");
+    let diff = starknet_api::StateDiff::new(
+        vec![
+            DeployedContract { address: address0, class_hash: hash0 },
+            DeployedContract { address: address1, class_hash: hash1 },
+        ],
+        vec![
+            StorageDiff {
+                address: address0,
+                storage_entries: vec![
+                    StorageEntry { key: key0.clone(), value: value0 },
+                    StorageEntry { key: key1, value: value1 },
+                ],
+            },
+            StorageDiff {
+                address: address1,
+                storage_entries: vec![StorageEntry { key: key0, value: value0 }],
+            },
+        ],
+        vec![
+            DeclaredContract { class_hash: hash0, contract_class: class0 },
+            DeclaredContract { class_hash: hash1, contract_class: class1 },
+        ],
+        vec![
+            ContractNonce { contract_address: address0, nonce: Nonce::new(StarkHash::from_u64(1)) },
+            ContractNonce { contract_address: address1, nonce: Nonce::new(StarkHash::from_u64(1)) },
+        ],
+    )
+    .unwrap();
+
+    (parent_header, header, diff, vec![])
 }
