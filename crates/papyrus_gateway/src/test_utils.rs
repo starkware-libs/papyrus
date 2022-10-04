6a06bbb6034da62a47ca50d5363dab7cd0a8a279
anatg <anatg@starkware.co>
2022-10-04 13:48:13 +0300

    Add more serde tests to gateway

--- crates/papyrus_gateway/src/test_utils.rs
+++ crates/papyrus_gateway/src/test_utils.rs
@@ -1,26 +1,15 @@
-use std::env;
-use std::fs::read_to_string;
 use std::net::SocketAddr;
-use std::path::Path;
 
 use reqwest::Client;
-
-// TODO(anatg): Remove this and use the same function when it will be in the storage test_utils.
-pub fn read_resource_file(path_in_resource_dir: &str) -> Result<String, anyhow::Error> {
-    let path = Path::new(&env::current_dir().expect("Problem with the current directory."))
-        .join("resources")
-        .join(path_in_resource_dir);
-    Ok(read_to_string(path.to_str().unwrap())?.replace('\n', "").replace(' ', ""))
-}
 
 // TODO(anatg): See if this can be usefull for the benchmark testing as well.
 pub async fn send_request(
     address: SocketAddr,
     method: &str,
     params: &str,
-) -> Result<String, anyhow::Error> {
+) -> Result<serde_json::Value, anyhow::Error> {
     let client = Client::new();
-    Ok(client
+    let res_str = client
         .post(format!("http://{:?}", address))
         .header("Content-Type", "application/json")
         .body(format!(
@@ -30,5 +19,6 @@ pub async fn send_request(
         .send()
         .await?
         .text()
-        .await?)
+        .await?;
+    Ok(serde_json::from_str(&res_str)?)
 }
