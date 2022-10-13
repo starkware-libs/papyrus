58f4495838f9bc6ba270f40d59e5a4ba72271652
Yair Bakalchuk <yair@starkware.co>
2022-10-13 15:45:51 +0300

    Rename tables

--- crates/papyrus_storage/src/ommer/mod.rs
+++ crates/papyrus_storage/src/ommer/mod.rs
@@ -47,8 +47,8 @@ impl<'env> OmmerStorageWriter for StorageTxn<'env, RW> {
         block_hash: BlockHash,
         header: &BlockHeader,
     ) -> StorageResult<Self> {
-        let headers_table = self.txn.open_table(&self.tables.ommer_headers)?;
-        headers_table.insert(&self.txn, &block_hash, header)?;
+        let ommer_headers_table = self.txn.open_table(&self.tables.ommer_headers)?;
+        ommer_headers_table.insert(&self.txn, &block_hash, header)?;
 
         Ok(self)
     }
@@ -63,15 +63,15 @@ impl<'env> OmmerStorageWriter for StorageTxn<'env, RW> {
         assert!(transactions.len() == thin_transaction_outputs.len());
         assert!(transactions.len() == transaction_outputs_events.len());
 
-        let transactions_table = self.txn.open_table(&self.tables.ommer_transactions)?;
-        let transaction_outputs_table =
+        let ommer_transactions_table = self.txn.open_table(&self.tables.ommer_transactions)?;
+        let ommer_transaction_outputs_table =
             self.txn.open_table(&self.tables.ommer_transaction_outputs)?;
-        let events_table = self.txn.open_table(&self.tables.ommer_events)?;
+        let ommer_events_table = self.txn.open_table(&self.tables.ommer_events)?;
 
         for idx in 0..transactions.len() {
             let tx_index = OmmerTransactionIndex(block_hash, TransactionOffsetInBlock(idx));
-            transactions_table.insert(&self.txn, &tx_index, &transactions[idx])?;
-            transaction_outputs_table.insert(
+            ommer_transactions_table.insert(&self.txn, &tx_index, &transactions[idx])?;
+            ommer_transaction_outputs_table.insert(
                 &self.txn,
                 &tx_index,
                 &thin_transaction_outputs[idx],
@@ -80,7 +80,7 @@ impl<'env> OmmerStorageWriter for StorageTxn<'env, RW> {
             for (event_offset, event) in events.iter().enumerate() {
                 let event_index =
                     OmmerEventIndex(tx_index, EventIndexInTransactionOutput(event_offset));
-                events_table.insert(
+                ommer_events_table.insert(
                     &self.txn,
                     &(event.from_address, event_index),
                     &event.content,
@@ -96,39 +96,40 @@ impl<'env> OmmerStorageWriter for StorageTxn<'env, RW> {
         thin_state_diff: &ThinStateDiff,
         declared_classes: &[DeclaredContract],
     ) -> StorageResult<Self> {
-        let state_diffs_table = self.txn.open_table(&self.tables.ommer_state_diffs)?;
-        let declared_classes_table = self.txn.open_table(&self.tables.ommer_declared_classes)?;
-        let deployed_contracts_table =
+        let ommer_state_diffs_table = self.txn.open_table(&self.tables.ommer_state_diffs)?;
+        let ommer_declared_classes_table =
+            self.txn.open_table(&self.tables.ommer_declared_classes)?;
+        let ommer_deployed_contracts_table =
             self.txn.open_table(&self.tables.ommer_deployed_contracts)?;
-        let storage_table = self.txn.open_table(&self.tables.ommer_contract_storage)?;
-        let nonces_table = self.txn.open_table(&self.tables.ommer_nonces)?;
+        let ommer_storage_table = self.txn.open_table(&self.tables.ommer_contract_storage)?;
+        let ommer_nonces_table = self.txn.open_table(&self.tables.ommer_nonces)?;
 
-        state_diffs_table.insert(&self.txn, &block_hash, thin_state_diff)?;
+        ommer_state_diffs_table.insert(&self.txn, &block_hash, thin_state_diff)?;
 
         for declared_class in declared_classes {
             let key = (block_hash, declared_class.class_hash);
             let value = declared_class.contract_class.to_byte_vec();
-            declared_classes_table.insert(&self.txn, &key, &value)?;
+            ommer_declared_classes_table.insert(&self.txn, &key, &value)?;
         }
 
         for deployed_contract in thin_state_diff.deployed_contracts() {
             let key = (deployed_contract.address, block_hash);
             let value = deployed_contract.class_hash;
-            deployed_contracts_table.insert(&self.txn, &key, &value)?;
+            ommer_deployed_contracts_table.insert(&self.txn, &key, &value)?;
         }
 
         for storage_diff in thin_state_diff.storage_diffs() {
             for storage_entry in &storage_diff.storage_entries {
                 let key = (storage_diff.address, storage_entry.key.clone(), block_hash);
                 let value = storage_entry.value;
-                storage_table.insert(&self.txn, &key, &value)?;
+                ommer_storage_table.insert(&self.txn, &key, &value)?;
             }
         }
 
         for contract_nonce in thin_state_diff.nonces() {
             let key = (contract_nonce.contract_address, block_hash);
             let value = contract_nonce.nonce;
-            nonces_table.insert(&self.txn, &key, &value)?;
+            ommer_nonces_table.insert(&self.txn, &key, &value)?;
         }
 
         Ok(self)
