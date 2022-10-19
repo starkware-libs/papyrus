29234f9a1577ab308d575906216ac5c61c867560
Yair Bakalchuk <yair@starkware.co>
2022-10-19 09:45:10 +0300

    Fix CR

--- crates/papyrus_storage/src/ommer/mod.rs
+++ crates/papyrus_storage/src/ommer/mod.rs
@@ -1,11 +1,11 @@
+#[cfg(test)]
+#[path = "ommer_test.rs"]
+mod ommer_test;
+
 use starknet_api::{
     BlockHash, BlockHeader, DeclaredContract, Event, EventIndexInTransactionOutput, Transaction,
     TransactionOffsetInBlock,
 };
-
-#[cfg(test)]
-#[path = "ommer_test.rs"]
-mod ommer_test;
 
 use crate::db::RW;
 use crate::{
@@ -78,11 +78,11 @@ impl<'env> OmmerStorageWriter for StorageTxn<'env, RW> {
             )?;
             let events = &transaction_outputs_events[idx];
             for (event_offset, event) in events.iter().enumerate() {
-                let event_index =
+                let event_key =
                     OmmerEventKey(tx_index, EventIndexInTransactionOutput(event_offset));
                 ommer_events_table.insert(
                     &self.txn,
-                    &(event.from_address, event_index),
+                    &(event.from_address, event_key),
                     &event.content,
                 )?;
             }
@@ -90,6 +90,7 @@ impl<'env> OmmerStorageWriter for StorageTxn<'env, RW> {
 
         Ok(self)
     }
+    
     fn insert_ommer_state_diff(
         self,
         block_hash: BlockHash,
