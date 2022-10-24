6cff774e121eddced1396e608ccd221fd2fbed47
Dan Brownstein <dan@starkware.co>
2022-10-24 16:37:06 +0300

    Reorder structs

--- crates/starknet_api/src/block.rs
+++ crates/starknet_api/src/block.rs
@@ -10,6 +10,58 @@ use serde::{Deserialize, Serialize};
 use super::serde_utils::{HexAsBytes, PrefixedHexAsBytes};
 use super::{ContractAddress, StarkHash, StarknetApiError, Transaction, TransactionOutput};
 
+#[derive(Debug, Default, Clone, Eq, PartialEq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+pub struct Block {
+    pub header: BlockHeader,
+    pub body: BlockBody,
+}
+
+/// The header of a StarkNet block.
+#[derive(Debug, Default, Clone, Eq, PartialEq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+pub struct BlockHeader {
+    // TODO(anatg): Consider removing the block hash from the header (note it can be computed from
+    // the rest of the fields.
+    pub block_hash: BlockHash,
+    pub parent_hash: BlockHash,
+    pub block_number: BlockNumber,
+    pub gas_price: GasPrice,
+    pub state_root: GlobalRoot,
+    pub sequencer: ContractAddress,
+    pub timestamp: BlockTimestamp,
+    // TODO(dan): add missing commitments.
+}
+
+/// The transactions in a StarkNet block.
+#[derive(Debug, Default, Clone, Eq, PartialEq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+pub struct BlockBody {
+    transactions: Vec<Transaction>,
+    transaction_outputs: Vec<TransactionOutput>,
+}
+impl BlockBody {
+    pub fn new(
+        transactions: Vec<Transaction>,
+        transaction_outputs: Vec<TransactionOutput>,
+    ) -> Result<Self, StarknetApiError> {
+        if transactions.len() == transaction_outputs.len() {
+            Ok(BlockBody { transactions, transaction_outputs })
+        } else {
+            Err(StarknetApiError::TransationsLengthDontMatch)
+        }
+    }
+
+    pub fn transactions(&self) -> &Vec<Transaction> {
+        &self.transactions
+    }
+
+    pub fn transaction_outputs(&self) -> &Vec<TransactionOutput> {
+        &self.transaction_outputs
+    }
+
+    pub fn transaction_outputs_into_iter(self) -> IntoIter<TransactionOutput> {
+        self.transaction_outputs.into_iter()
+    }
+}
+
 // TODO(spapini): Verify the invariant that it is in range.
 /// The hash of a StarkNet block.
 #[derive(
@@ -22,21 +74,6 @@ impl BlockHash {
         Self(hash)
     }
     pub fn block_hash(&self) -> &StarkHash {
-        &self.0
-    }
-}
-
-/// The root of the global state at a StarkNet block.
-#[derive(
-    Debug, Copy, Clone, Default, Eq, PartialEq, Hash, Deserialize, Serialize, PartialOrd, Ord,
-)]
-pub struct GlobalRoot(StarkHash);
-
-impl GlobalRoot {
-    pub fn new(hash: StarkHash) -> Self {
-        Self(hash)
-    }
-    pub fn root(&self) -> &StarkHash {
         &self.0
     }
 }
@@ -91,6 +128,28 @@ impl BlockTimestamp {
     }
 }
 
+/// The status a StarkNet block.
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
+pub enum BlockStatus {
+    /// A pending block; i.e., a block that is yet to be closed.
+    #[serde(rename = "PENDING")]
+    Pending,
+    /// A block that was created on L2.
+    #[serde(rename = "ACCEPTED_ON_L2")]
+    AcceptedOnL2,
+    /// A block that was accepted on L1.
+    #[serde(rename = "ACCEPTED_ON_L1")]
+    AcceptedOnL1,
+    /// A block rejected on L1.
+    #[serde(rename = "REJECTED")]
+    Rejected,
+}
+impl Default for BlockStatus {
+    fn default() -> Self {
+        BlockStatus::AcceptedOnL2
+    }
+}
+
 /// The gas price at a StarkNet block.
 #[derive(
     Debug, Copy, Clone, Default, Eq, PartialEq, Hash, Deserialize, Serialize, PartialOrd, Ord,
@@ -117,76 +176,17 @@ impl GasPrice {
     }
 }
 
-/// The status a StarkNet block.
-#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
-pub enum BlockStatus {
-    /// A pending block; i.e., a block that is yet to be closed.
-    #[serde(rename = "PENDING")]
-    Pending,
-    /// A block that was created on L2.
-    #[serde(rename = "ACCEPTED_ON_L2")]
-    AcceptedOnL2,
-    /// A block that was accepted on L1.
-    #[serde(rename = "ACCEPTED_ON_L1")]
-    AcceptedOnL1,
-    /// A block rejected on L1.
-    #[serde(rename = "REJECTED")]
-    Rejected,
-}
-impl Default for BlockStatus {
-    fn default() -> Self {
-        BlockStatus::AcceptedOnL2
+/// The root of the global state at a StarkNet block.
+#[derive(
+    Debug, Copy, Clone, Default, Eq, PartialEq, Hash, Deserialize, Serialize, PartialOrd, Ord,
+)]
+pub struct GlobalRoot(StarkHash);
+
+impl GlobalRoot {
+    pub fn new(hash: StarkHash) -> Self {
+        Self(hash)
     }
-}
-
-/// The header of a StarkNet block.
-#[derive(Debug, Default, Clone, Eq, PartialEq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
-pub struct BlockHeader {
-    // TODO(anatg): Consider removing the block hash from the header (note it can be computed from
-    // the rest of the fields.
-    pub block_hash: BlockHash,
-    pub parent_hash: BlockHash,
-    pub block_number: BlockNumber,
-    pub gas_price: GasPrice,
-    pub state_root: GlobalRoot,
-    pub sequencer: ContractAddress,
-    pub timestamp: BlockTimestamp,
-    // TODO(dan): add missing commitments.
-}
-
-/// The transactions in a StarkNet block.
-#[derive(Debug, Default, Clone, Eq, PartialEq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
-pub struct BlockBody {
-    transactions: Vec<Transaction>,
-    transaction_outputs: Vec<TransactionOutput>,
-}
-impl BlockBody {
-    pub fn new(
-        transactions: Vec<Transaction>,
-        transaction_outputs: Vec<TransactionOutput>,
-    ) -> Result<Self, StarknetApiError> {
-        if transactions.len() == transaction_outputs.len() {
-            Ok(BlockBody { transactions, transaction_outputs })
-        } else {
-            Err(StarknetApiError::TransationsLengthDontMatch)
-        }
+    pub fn root(&self) -> &StarkHash {
+        &self.0
     }
-
-    pub fn transactions(&self) -> &Vec<Transaction> {
-        &self.transactions
-    }
-
-    pub fn transaction_outputs(&self) -> &Vec<TransactionOutput> {
-        &self.transaction_outputs
-    }
-
-    pub fn transaction_outputs_into_iter(self) -> IntoIter<TransactionOutput> {
-        self.transaction_outputs.into_iter()
-    }
-}
-
-#[derive(Debug, Default, Clone, Eq, PartialEq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
-pub struct Block {
-    pub header: BlockHeader,
-    pub body: BlockBody,
 }
