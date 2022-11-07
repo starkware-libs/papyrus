8f0237eeedf1db34ba5d89afad0e9e083af04230
Yair Bakalchuk <yair@starkware.co>
2022-11-07 16:13:45 +0200

    Sync test (implemented as a flow test)

--- crates/papyrus_sync/src/sources/mod.rs
+++ crates/papyrus_sync/src/sources/mod.rs
@@ -2,6 +2,8 @@ mod central;
 #[cfg(test)]
 mod central_test;
 mod stream_utils;
+#[cfg(test)]
+mod sync_test;
 
 pub use central::{
     CentralError, CentralResult, CentralSource, CentralSourceConfig, CentralSourceTrait,
