968eb4f2578ec7d6e1834d1c9145c4c9dc1a5b59
anatg <anatg@starkware.co>
2022-11-15 13:55:29 +0200

    Create the tests in a macro for complete coverage

--- crates/papyrus_storage/src/compression_utils_test.rs
+++ crates/papyrus_storage/src/compression_utils_test.rs
@@ -5,7 +5,8 @@ use crate::test_utils::read_json_file;
 
 #[test]
 fn gzip_encode_decode_contract_program() -> Result<(), anyhow::Error> {
-    let program = get_test_program();
+    let program_json = read_json_file("program.json")?;
+    let program: Program = serde_json::from_value(program_json)?;
     let program_as_bytes = serde_json::to_vec(&program)?;
     let len_before_compression = program_as_bytes.len();
 
--- crates/papyrus_storage/src/serializers.rs
+++ crates/papyrus_storage/src/serializers.rs
@@ -26,6 +26,10 @@ use starknet_api::transaction::{
     TransactionVersion,
 };
 
+#[cfg(test)]
+use self::serializers_test::{
+    auto_storage_serde_test, create_test, impl_get_test_instance, GetTestInstance, StorageSerdeTest,
+};
 use crate::body::events::{
     ThinDeclareTransactionOutput, ThinDeployAccountTransactionOutput, ThinDeployTransactionOutput,
     ThinInvokeTransactionOutput, ThinL1HandlerTransactionOutput, ThinTransactionOutput,
@@ -87,27 +91,6 @@ impl StorageSerde for StorageKey {
 
     fn deserialize_from(bytes: &mut impl std::io::Read) -> Option<Self> {
         StorageKey::try_from(StarkHash::deserialize(bytes)?).ok()
-    }
-}
-
-impl StorageSerde for TransactionOffsetInBlock {
-    fn serialize_into(&self, res: &mut impl std::io::Write) -> Result<(), StorageSerdeError> {
-        (self.0 as u64).serialize_into(res)
-    }
-
-    fn deserialize_from(bytes: &mut impl std::io::Read) -> Option<Self> {
-        Some(Self(u64::deserialize_from(bytes)? as usize))
-    }
-}
-
-// TODO(anatg): Consider using wrapper instead.
-impl StorageSerde for EventIndexInTransactionOutput {
-    fn serialize_into(&self, res: &mut impl std::io::Write) -> Result<(), StorageSerdeError> {
-        (self.0 as u64).serialize_into(res)
-    }
-
-    fn deserialize_from(bytes: &mut impl std::io::Read) -> Option<Self> {
-        Some(Self(u64::deserialize_from(bytes)? as usize))
     }
 }
 
@@ -219,6 +202,8 @@ macro_rules! auto_storage_serde {
                 Some(Self (<$ty>::deserialize_from(bytes)?))
             }
         }
+        #[cfg(test)]
+        auto_storage_serde_test!($name, struct $name ($ty));
         auto_storage_serde!($($rest)*);
     };
     // Tuple structs (no names associated with fields) - two fields.
@@ -232,6 +217,8 @@ macro_rules! auto_storage_serde {
                 Some($name(<$ty0>::deserialize_from(bytes)?, <$ty1>::deserialize_from(bytes)?))
             }
         }
+        #[cfg(test)]
+        auto_storage_serde_test!($name, struct $name ($ty0, $ty1));
         auto_storage_serde!($($rest)*);
     };
     // Structs with public fields.
@@ -251,6 +238,8 @@ macro_rules! auto_storage_serde {
                 })
             }
         }
+        #[cfg(test)]
+        auto_storage_serde_test!($name, struct $name { $(pub $field : $ty ,)* });
         auto_storage_serde!($($rest)*);
     };
     // Tuples - two elements.
@@ -267,6 +256,8 @@ macro_rules! auto_storage_serde {
                 ))
             }
         }
+        #[cfg(test)]
+        auto_storage_serde_test!(($ty0, $ty1));
         auto_storage_serde!($($rest)*);
     };
     // Tuples - three elements.
@@ -285,6 +276,8 @@ macro_rules! auto_storage_serde {
                 ))
             }
         }
+        #[cfg(test)]
+        auto_storage_serde_test!(($ty0, $ty1, $ty2));
         auto_storage_serde!($($rest)*);
     };
     // enums.
@@ -315,6 +308,8 @@ macro_rules! auto_storage_serde {
                     _ => None,}
             }
         }
+        #[cfg(test)]
+        auto_storage_serde_test!($name, enum $name { $($variant $( ($ty) )? = $num ,)* });
         auto_storage_serde!($($rest)*);
     };
     // Binary.
@@ -329,6 +324,8 @@ macro_rules! auto_storage_serde {
                 bincode::deserialize_from(bytes).ok()
             }
         }
+        #[cfg(test)]
+        auto_storage_serde_test!($name, bincode($name));
         auto_storage_serde!($($rest)*);
     }
 }
@@ -565,6 +562,8 @@ auto_storage_serde! {
     }
     pub struct TransactionVersion(pub StarkFelt);
     pub struct TransactionSignature(pub Vec<StarkFelt>);
+    pub struct TransactionOffsetInBlock(pub usize);
+    pub struct EventIndexInTransactionOutput(pub usize);
 
     pub struct BlockHash(pub StarkHash);
     pub struct BlockNumber(pub u64);
