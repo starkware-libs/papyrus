e09e0897be8adfe5c5c343eee4849f99a2b20458
Yair Bakalchuk <yair@starkware.co>
2022-11-21 15:19:23 +0200

    Use shash instead of StarkHash::from + unwrap

--- crates/papyrus_node/src/bin/starknet_client_integration_test.rs
+++ crates/papyrus_node/src/bin/starknet_client_integration_test.rs
@@ -1,5 +1,5 @@
 use papyrus_node::config::load_config;
-use starknet_api::{BlockNumber, ClassHash, StarkHash};
+use starknet_api::{shash, BlockNumber, ClassHash, StarkHash};
 use starknet_client::{StarknetClient, StarknetClientTrait};
 
 #[tokio::main]
@@ -17,10 +17,8 @@ async fn main() {
     let _block_123456 = starknet_client.block(BlockNumber(123456)).await.expect("Get block");
     let _state_diff =
         starknet_client.state_update(BlockNumber(123456)).await.expect("Get state diff");
-    let class_hash = ClassHash(
-        StarkHash::try_from("0x7af612493193c771c1b12f511a8b4d3b0c6d0648242af4680c7cd0d06186f17")
-            .unwrap(),
-    );
+    let class_hash =
+        ClassHash(shash!("0x7af612493193c771c1b12f511a8b4d3b0c6d0648242af4680c7cd0d06186f17"));
     let _contract_class_by_hash =
         starknet_client.class_by_hash(class_hash).await.expect("Get class by hash");
 }
