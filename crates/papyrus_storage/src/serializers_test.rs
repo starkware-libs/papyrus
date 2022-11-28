20a718d35ca20ccf31f146f8035f8732240bf2e8
anatg <anatg@starkware.co>
2022-11-28 12:26:15 +0200

    Fix CR comments

--- crates/papyrus_storage/src/serializers_test.rs
+++ crates/papyrus_storage/src/serializers_test.rs
@@ -13,21 +13,19 @@ use starknet_api::{patky, shash};
 use crate::{StorageSerde, ThinStateDiff};
 
 pub trait StorageSerdeTest: StorageSerde {
-    fn storage_serde_test() -> Result<(), anyhow::Error>;
+    fn storage_serde_test();
 }
 
 // Implements the [`storage_serde_test`] function for every type that
 // implements the [`StorageSerde`] and [`GetTestInstance`] traits.
 impl<T: StorageSerde + GetTestInstance + Eq + Debug> StorageSerdeTest for T {
-    fn storage_serde_test() -> Result<(), anyhow::Error> {
+    fn storage_serde_test() {
         let item = T::get_test_instance();
         let mut serialized: Vec<u8> = Vec::new();
-        item.serialize_into(&mut serialized)?;
+        item.serialize_into(&mut serialized).unwrap();
         let bytes = serialized.into_boxed_slice();
         let deserialized = T::deserialize_from(&mut bytes.as_ref());
         assert_eq!(item, deserialized.unwrap());
-
-        Ok(())
     }
 }
 
@@ -38,8 +36,8 @@ pub trait GetTestInstance: Sized {
 // Tests all types that implement the [`StorageSerde`] trait
 // via the [`auto_storage_serde`] macro.
 macro_rules! auto_storage_serde_test {
-    ($name:ident, $($rest:tt)*) => {
-        impl_get_test_instance!($($rest)*);
+    ($name:ident, $($full_exp:tt)*) => {
+        impl_get_test_instance!($($full_exp)*);
         create_test!($name);
     };
     (($ty0:ty, $ty1:ty)) => {
@@ -58,7 +56,7 @@ macro_rules! create_test {
     ($name:ident) => {
         paste::paste! {
             #[test]
-            fn [<"storage_serde_test" _$name:snake>]() -> Result<(), anyhow::Error> {
+            fn [<"storage_serde_test" _$name:snake>]() {
                 $name::storage_serde_test()
             }
         }
@@ -66,7 +64,7 @@ macro_rules! create_test {
     (($ty0:ty, $ty1:ty)) => {
         paste::paste! {
             #[test]
-            fn [<"storage_serde_test" _$ty0:snake _$ty1:snake>]() -> Result<(), anyhow::Error> {
+            fn [<"storage_serde_test" _$ty0:snake _$ty1:snake>]() {
                 <($ty0, $ty1)>::storage_serde_test()
             }
         }
@@ -74,7 +72,7 @@ macro_rules! create_test {
     (($ty0:ty, $ty1:ty, $ty2:ty)) => {
         paste::paste! {
             #[test]
-            fn [<"storage_serde_test" _$ty0:snake _$ty1:snake _$ty2:snake>]() -> Result<(), anyhow::Error> {
+            fn [<"storage_serde_test" _$ty0:snake _$ty1:snake _$ty2:snake>]() {
                 <($ty0, $ty1, $ty2)>::storage_serde_test()
             }
         }
