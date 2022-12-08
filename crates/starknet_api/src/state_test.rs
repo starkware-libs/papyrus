2a1812ebd4c1d78157bdad5ccfd6da32b64827c8
dan-starkware <56217775+dan-starkware@users.noreply.github.com>
2022-12-08 11:20:02 +0200

    Use the public SN API crate (#359)
    * Use the public SN API crate

--- crates/starknet_api/src/state_test.rs
+++
@@ -1,9 +0,0 @@
-use crate::state::EntryPointOffset;
-
-#[test]
-fn entry_point_offset_from_str() {
-    let offset = EntryPointOffset(123);
-    let as_str: String = offset.into();
-    assert_eq!("0x7b", as_str);
-    assert_eq!(EntryPointOffset::try_from(as_str).unwrap(), offset);
-}
