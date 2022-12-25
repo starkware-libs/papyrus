5490d9231eae95d6f5ebe8ca638af514496e2f9b
Yair Bakalchuk <yair@starkware.co>
2022-12-25 15:33:37 +0200

    Handle reverts

--- crates/papyrus_sync/src/lib.rs
+++ crates/papyrus_sync/src/lib.rs
@@ -11,13 +11,15 @@ use async_stream::stream;
 use futures_util::{pin_mut, select, Stream, StreamExt};
 use log::{error, info};
 use papyrus_storage::{
-    BodyStorageWriter, HeaderStorageReader, HeaderStorageWriter, StateStorageReader,
-    StateStorageWriter, StorageError, StorageReader, StorageWriter,
+    BodyStorageReader, BodyStorageWriter, HeaderStorageReader, HeaderStorageWriter,
+    OmmerStorageWriter, StateStorageReader, StateStorageWriter, StorageError, StorageReader,
+    StorageResult, StorageWriter, TransactionIndex,
 };
 use serde::{Deserialize, Serialize};
-use starknet_api::block::{Block, BlockNumber};
+use starknet_api::block::{Block, BlockHash, BlockNumber};
 use starknet_api::core::ClassHash;
 use starknet_api::state::{ContractClass, StateDiff};
+use starknet_api::transaction::TransactionOffsetInBlock;
 
 pub use self::sources::{CentralError, CentralSource, CentralSourceConfig, CentralSourceTrait};
 
@@ -88,6 +90,10 @@ impl<TCentralSource: CentralSourceTrait + Sync + Send + 'static> GenericStateSyn
                 };
                 match sync_event {
                     Some(SyncEvent::BlockAvailable { block_number, block }) => {
+                        let revert_happend = self.handle_block_reverts().await?;
+                        if revert_happend {
+                            break;
+                        }
                         self.writer
                             .begin_rw_txn()?
                             .append_header(block_number, &block.header)?
@@ -96,17 +102,29 @@ impl<TCentralSource: CentralSourceTrait + Sync + Send + 'static> GenericStateSyn
                     }
                     Some(SyncEvent::StateDiffAvailable {
                         block_number,
+                        block_hash,
                         state_diff,
                         deployed_contract_class_definitions,
                     }) => {
-                        self.writer
-                            .begin_rw_txn()?
-                            .append_state_diff(
-                                block_number,
-                                state_diff,
-                                deployed_contract_class_definitions,
-                            )?
-                            .commit()?;
+                        if !self.is_reverted_state_diff(block_number, block_hash).await {
+                            self.writer
+                                .begin_rw_txn()?
+                                .append_state_diff(
+                                    block_number,
+                                    state_diff,
+                                    deployed_contract_class_definitions,
+                                )?
+                                .commit()?;
+                        } else {
+                            self.writer
+                                .begin_rw_txn()?
+                                .insert_ommer_state_diff(
+                                    block_hash,
+                                    &state_diff.into(),
+                                    &deployed_contract_class_definitions,
+                                )?
+                                .commit()?;
+                        }
                     }
                     None => {
                         return Err(StateSyncError::SyncError {
@@ -115,6 +133,126 @@ impl<TCentralSource: CentralSourceTrait + Sync + Send + 'static> GenericStateSyn
                     }
                 }
             }
+        }
+    }
+
+    // Reverts data if needed, returns whether a revert happend.
+    async fn handle_block_reverts(&mut self) -> StorageResult<bool> {
+        let header_marker = self
+            .reader
+            .begin_ro_txn()
+            .expect("Cannot read from block storage.")
+            .get_header_marker()
+            .expect("Cannot read from block storage.");
+        let mut revert_happend = false;
+
+        // Revert last blocks if needed.
+        let mut last_block_in_storage = header_marker.prev();
+        while let Some(block_number) = last_block_in_storage {
+            if self.should_revert_block(block_number).await {
+                self.revert_block(block_number)?;
+                last_block_in_storage = block_number.prev();
+                revert_happend = true;
+            } else {
+                break;
+            }
+        }
+        Ok(revert_happend)
+    }
+
+    // Deletes the block data from the storage, moving it to the ommer tables.
+    fn revert_block(&mut self, block_number: BlockNumber) -> StorageResult<()> {
+        let header = self
+            .reader
+            .begin_ro_txn()?
+            .get_block_header(block_number)?
+            .expect("Error reading header from storage");
+        let transactions = self
+            .reader
+            .begin_ro_txn()?
+            .get_block_transactions(block_number)?
+            .expect("Error reading transactions from storage");
+        let transaction_outputs = self
+            .reader
+            .begin_ro_txn()?
+            .get_block_transaction_outputs(block_number)?
+            .expect("Error reading transaction outputs from storage");
+        let mut events: Vec<_> = vec![];
+        for idx in 0..transactions.len() {
+            let tx_idx = TransactionIndex(block_number, TransactionOffsetInBlock(idx));
+            events.push(
+                self.reader.begin_ro_txn()?.get_transaction_events(tx_idx)?.unwrap_or_default(),
+            );
+        }
+
+        let mut txn = self
+            .writer
+            .begin_rw_txn()?
+            .revert_header(block_number)?
+            .insert_ommer_header(header.block_hash, &header)?
+            .revert_body(block_number)?
+            .insert_ommer_body(
+                header.block_hash,
+                &transactions,
+                &transaction_outputs,
+                events.as_slice(),
+            )?;
+
+        // State diff might not yet written to the storage. In that case, it will be handled in a
+        // separate function.
+        if let Some(state_diff) = self.reader.begin_ro_txn()?.get_state_diff(block_number)? {
+            // TODO: fill the declared classes.
+            txn = txn.revert_state_diff(block_number)?.insert_ommer_state_diff(
+                header.block_hash,
+                &state_diff,
+                &[],
+            )?;
+        }
+
+        txn.commit()
+    }
+
+    /// Checks if centrals block hash at the block number is different from ours (or doesn't exist).
+    /// If so, a revert is required.
+    async fn should_revert_block(&self, block_number: BlockNumber) -> bool {
+        if let Some(central_block_hash) = self
+            .central_source
+            .get_block_hash(block_number)
+            .await
+            .expect("Cannot read from central.")
+        {
+            let storage_block_header = self
+                .reader
+                .begin_ro_txn()
+                .expect("Cannot read from block storage.")
+                .get_block_header(block_number)
+                .expect("Cannot read from block storage.");
+
+            match storage_block_header {
+                Some(block_header) => block_header.block_hash != central_block_hash,
+                None => false,
+            }
+        } else {
+            // Block number doesn't exist in central, revert.
+            true
+        }
+    }
+
+    async fn is_reverted_state_diff(
+        &self,
+        block_number: BlockNumber,
+        block_hash: BlockHash,
+    ) -> bool {
+        let storage_header = self
+            .reader
+            .begin_ro_txn()
+            .expect("Cannot read from block storage.")
+            .get_block_header(block_number)
+            .expect("Cannot read from block storage.");
+
+        match storage_header {
+            None => true,
+            Some(header) => header.block_hash != block_hash,
         }
     }
 }
@@ -129,10 +267,12 @@ fn stream_new_blocks<TCentralSource: CentralSourceTrait + Sync + Send>(
             let header_marker = reader.begin_ro_txn().expect("Cannot read from block storage.")
                 .get_header_marker()
                 .expect("Cannot read from block storage.");
+
             let last_block_number = central_source
                 .get_block_marker()
                 .await
-                .expect("Cannot read from block storage.");
+                .expect("Cannot read from central.");
+
             info!(
                 "Downloading blocks [{} - {}).",
                 header_marker, last_block_number
