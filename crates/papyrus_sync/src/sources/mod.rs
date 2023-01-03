bf84b1ff20af92828195b0c97c30dd60baf024c7
Yair Bakalchuk <yair@starkware.co>
2023-01-03 12:08:02 +0200

    Fix mod

--- crates/papyrus_sync/src/sources/mod.rs
+++ crates/papyrus_sync/src/sources/mod.rs
@@ -1,9 +1,9 @@
 mod central;
 #[cfg(test)]
+mod central_sync_test;
+#[cfg(test)]
 mod central_test;
 mod stream_utils;
-#[cfg(test)]
-mod sync_test;
 
 pub use central::{
     CentralError, CentralResult, CentralSource, CentralSourceConfig, CentralSourceTrait,
