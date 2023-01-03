baa1fa47fb5956e59f315d07511884452c554f76
anatg <anatg@starkware.co>
2023-01-03 11:24:32 +0200

    Move get test instance from serializers test to test utils (#397)
    * Move get test instance from serializers test to test utils

--- crates/papyrus_storage/src/serializers_test.rs
+++ crates/papyrus_storage/src/serializers_test.rs
@@ -1,13 +1,11 @@
-use std::collections::HashMap;
 use std::fmt::Debug;
-use std::hash::Hash;
 
-use indexmap::IndexMap;
 use starknet_api::core::ContractAddress;
 use starknet_api::hash::StarkHash;
 use starknet_api::state::StorageKey;
 
-use crate::StorageSerde;
+use crate::db::serialization::StorageSerde;
+use crate::test_utils::GetTestInstance;
 
 pub trait StorageSerdeTest: StorageSerde {
     fn storage_serde_test();
@@ -26,41 +24,9 @@ impl<T: StorageSerde + GetTestInstance + Eq + Debug> StorageSerdeTest for T {
     }
 }
 
-pub trait GetTestInstance: Sized {
-    fn get_test_instance() -> Self;
-}
-
-macro_rules! default_impl_get_test_instance {
-    ($name:path) => {
-        impl GetTestInstance for $name {
-            fn get_test_instance() -> Self {
-                Self::default()
-            }
-        }
-    };
-}
-pub(crate) use default_impl_get_test_instance;
-
 // Tests all types that implement the [`StorageSerde`] trait
 // via the [`auto_storage_serde`] macro.
-macro_rules! auto_storage_serde_test {
-    ($name:ident, $($full_exp:tt)*) => {
-        impl_get_test_instance!($($full_exp)*);
-        create_test!($name);
-    };
-    (($ty0:ty, $ty1:ty)) => {
-        impl_get_test_instance!(($ty0, $ty1));
-        create_test!(($ty0, $ty1));
-    };
-    (($ty0:ty, $ty1:ty, $ty2:ty)) => {
-        impl_get_test_instance!(($ty0, $ty1, $ty2));
-        create_test!(($ty0, $ty1, $ty2));
-    };
-}
-pub(crate) use auto_storage_serde_test;
-
-// Creates tests that call the [`storage_serde_test`] function.
-macro_rules! create_test {
+macro_rules! create_storage_serde_test {
     ($name:ident) => {
         paste::paste! {
             #[test]
@@ -86,144 +52,13 @@ macro_rules! create_test {
         }
     };
 }
-pub(crate) use create_test;
-
-////////////////////////////////////////////////////////////////////////
-// Implements the [`GetTestInstance`] trait for primitive types.
-////////////////////////////////////////////////////////////////////////
-default_impl_get_test_instance!(serde_json::Value);
-default_impl_get_test_instance!(String);
-impl<T: GetTestInstance> GetTestInstance for Option<T> {
-    fn get_test_instance() -> Self {
-        Some(T::get_test_instance())
-    }
-}
-impl<T: GetTestInstance> GetTestInstance for Vec<T> {
-    fn get_test_instance() -> Self {
-        vec![T::get_test_instance()]
-    }
-}
-impl<K: GetTestInstance + Eq + Hash, V: GetTestInstance> GetTestInstance for HashMap<K, V> {
-    fn get_test_instance() -> Self {
-        let mut res = HashMap::with_capacity(1);
-        let k = K::get_test_instance();
-        let v = V::get_test_instance();
-        res.insert(k, v);
-        res
-    }
-}
-impl<K: GetTestInstance + Eq + Hash, V: GetTestInstance> GetTestInstance for IndexMap<K, V> {
-    fn get_test_instance() -> Self {
-        let mut res = IndexMap::with_capacity(1);
-        let k = K::get_test_instance();
-        let v = V::get_test_instance();
-        res.insert(k, v);
-        res
-    }
-}
-
-////////////////////////////////////////////////////////////////////////
-// Implements the [`GetTestInstance`] trait for the types that the
-// [`auto_storage_serde`] macro is called with.
-////////////////////////////////////////////////////////////////////////
-macro_rules! impl_get_test_instance {
-    // Tuple structs (no names associated with fields) - one field.
-    (struct $name:ident($ty:ty)) => {
-        impl GetTestInstance for $name {
-            fn get_test_instance() -> Self {
-                Self(<$ty>::get_test_instance())
-            }
-        }
-    };
-    // Tuple structs (no names associated with fields) - two fields.
-    (struct $name:ident($ty0:ty, $ty1:ty)) => {
-        impl GetTestInstance for $name {
-            fn get_test_instance() -> Self {
-                Self(<$ty0>::get_test_instance(), <$ty1>::get_test_instance())
-            }
-        }
-    };
-    // Structs with public fields.
-    (struct $name:ident { $(pub $field:ident : $ty:ty ,)* }) => {
-        impl GetTestInstance for $name {
-            fn get_test_instance() -> Self {
-                Self {
-                    $(
-                        $field: <$ty>::get_test_instance(),
-                    )*
-                }
-            }
-        }
-    };
-    // Tuples - two elements.
-    (($ty0:ty, $ty1:ty)) => {
-        impl GetTestInstance for ($ty0, $ty1) {
-            fn get_test_instance() -> Self {
-                (
-                    <$ty0>::get_test_instance(),
-                    <$ty1>::get_test_instance(),
-                )
-            }
-        }
-    };
-    // Tuples - three elements.
-    (($ty0:ty, $ty1:ty, $ty2:ty)) => {
-        impl GetTestInstance for ($ty0, $ty1, $ty2) {
-            fn get_test_instance() -> Self {
-                (
-                    <$ty0>::get_test_instance(),
-                    <$ty1>::get_test_instance(),
-                    <$ty2>::get_test_instance(),
-                )
-            }
-        }
-    };
-    // Enums.
-    ($(pub)? enum $name:ident { $($variant:ident $( ($ty:ty) )? = $num:expr ,)* } $($rest:tt)*) => {
-        impl GetTestInstance for $name {
-            fn get_test_instance() -> Self {
-                let mut rng = rand::thread_rng();
-                let variant = rng.gen_range(0..get_number_of_variants!(enum $name { $($variant $( ($ty) )? = $num ,)* }));
-                match variant {
-                    $(
-                        $num => {
-                            Self::$variant$((<$ty>::get_test_instance()))?
-                        }
-                    )*
-                    _ => {
-                        panic!("Variant {:?} should match one of the enum {:?} variants.", variant, stringify!($name));
-                    }
-                }
-            }
-        }
-    };
-    // Binary.
-    (bincode($name:ident)) => {
-        default_impl_get_test_instance!($name);
-    }
-}
-pub(crate) use impl_get_test_instance;
-
-// Counts the number of variants of an enum.
-macro_rules! get_number_of_variants {
-    (enum $name:ident { $($variant:ident $( ($ty:ty) )? = $num:expr ,)* }) => {
-        get_number_of_variants!(@count $($variant),+)
-    };
-    (@count $t1:tt, $($t:tt),+) => { 1 + get_number_of_variants!(@count $($t),+) };
-    (@count $t:tt) => { 1 };
-}
-pub(crate) use get_number_of_variants;
+pub(crate) use create_storage_serde_test;
 
 ////////////////////////////////////////////////////////////////////////
 // Implements the [`GetTestInstance`] trait for types not supported
 // by the macro [`impl_get_test_instance`] and calls the [`create_test`]
 // macro to create the tests for them.
 ////////////////////////////////////////////////////////////////////////
-default_impl_get_test_instance!(StarkHash);
-create_test!(StarkHash);
-
-default_impl_get_test_instance!(ContractAddress);
-create_test!(ContractAddress);
-
-default_impl_get_test_instance!(StorageKey);
-create_test!(StorageKey);
+create_storage_serde_test!(StarkHash);
+create_storage_serde_test!(ContractAddress);
+create_storage_serde_test!(StorageKey);
--- crates/papyrus_storage/src/test_utils.rs
+++ crates/papyrus_storage/src/test_utils.rs
@@ -1,22 +1,43 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs::read_to_string;
+use std::hash::Hash;
 use std::path::Path;
 
 use indexmap::IndexMap;
-use starknet_api::block::{Block, BlockBody, BlockHash, BlockHeader, BlockNumber};
-use starknet_api::core::{ClassHash, ContractAddress, GlobalRoot, Nonce, PatriciaKey};
+use rand::Rng;
+use starknet_api::block::{
+    Block, BlockBody, BlockHash, BlockHeader, BlockNumber, BlockStatus, BlockTimestamp, GasPrice,
+};
+use starknet_api::core::{
+    ClassHash, ContractAddress, EntryPointSelector, GlobalRoot, Nonce, PatriciaKey,
+};
 use starknet_api::hash::{StarkFelt, StarkHash};
-use starknet_api::state::{ContractClass, StateDiff, StorageKey};
+use starknet_api::state::{
+    ContractClass, ContractClassAbiEntry, EntryPoint, EntryPointOffset, EntryPointType,
+    EventAbiEntry, FunctionAbiEntry, FunctionAbiEntryType, FunctionAbiEntryWithType, Program,
+    StateDiff, StorageKey, StructAbiEntry, StructMember, TypedParameter,
+};
 use starknet_api::transaction::{
-    CallData, ContractAddressSalt, DeployTransaction, DeployTransactionOutput, EthAddress, Event,
-    EventContent, EventData, EventKey, Fee, L2ToL1Payload, MessageToL1, Transaction,
-    TransactionHash, TransactionOutput, TransactionVersion,
+    CallData, ContractAddressSalt, DeclareTransaction, DeployAccountTransaction, DeployTransaction,
+    DeployTransactionOutput, EthAddress, Event, EventContent, EventData,
+    EventIndexInTransactionOutput, EventKey, Fee, InvokeTransaction, L1HandlerTransaction,
+    L1ToL2Payload, L2ToL1Payload, MessageToL1, MessageToL2, Transaction, TransactionHash,
+    TransactionOffsetInBlock, TransactionOutput, TransactionSignature, TransactionVersion,
 };
 use starknet_api::{patky, shash};
 use tempfile::tempdir;
 
+use crate::body::events::{
+    ThinDeclareTransactionOutput, ThinDeployAccountTransactionOutput, ThinDeployTransactionOutput,
+    ThinInvokeTransactionOutput, ThinL1HandlerTransactionOutput, ThinTransactionOutput,
+};
 use crate::db::DbConfig;
-use crate::{open_storage, StorageReader, StorageWriter};
+use crate::state::data::{IndexedDeclaredContract, IndexedDeployedContract, ThinStateDiff};
+use crate::{
+    open_storage, EventIndex, MarkerKind, OmmerEventKey, OmmerTransactionKey, StorageReader,
+    StorageWriter, TransactionIndex,
+};
 
 pub fn get_test_config() -> DbConfig {
     let dir = tempdir().unwrap();
@@ -39,6 +60,402 @@ pub fn read_json_file(path_in_resource_dir: &str) -> serde_json::Value {
     let json_str = read_to_string(path.to_str().unwrap()).unwrap();
     serde_json::from_str(&json_str).unwrap()
 }
+
+// TODO(anatg): Consider moving GetTestInstance and auto_impl_get_test_instance
+// to a test utils crate.
+pub trait GetTestInstance: Sized {
+    fn get_test_instance() -> Self;
+}
+
+auto_impl_get_test_instance! {
+    pub struct BlockHash(pub StarkHash);
+    pub struct BlockHeader {
+        pub block_hash: BlockHash,
+        pub parent_hash: BlockHash,
+        pub block_number: BlockNumber,
+        pub gas_price: GasPrice,
+        pub state_root: GlobalRoot,
+        pub sequencer: ContractAddress,
+        pub timestamp: BlockTimestamp,
+    }
+    pub struct BlockNumber(pub u64);
+    pub enum BlockStatus {
+        Pending = 0,
+        AcceptedOnL2 = 1,
+        AcceptedOnL1 = 2,
+        Rejected = 3,
+    }
+    pub struct BlockTimestamp(pub u64);
+    pub struct CallData(pub Vec<StarkFelt>);
+    pub struct ClassHash(pub StarkHash);
+    pub struct ContractAddressSalt(pub StarkHash);
+    // TODO(anatg): Consider using the compression utils.
+    pub struct ContractClass {
+        pub abi: Option<Vec<ContractClassAbiEntry>>,
+        pub program: Program,
+        pub entry_points_by_type: HashMap<EntryPointType, Vec<EntryPoint>>,
+    }
+    pub enum ContractClassAbiEntry {
+        Event(EventAbiEntry) = 0,
+        Function(FunctionAbiEntryWithType) = 1,
+        Struct(StructAbiEntry) = 2,
+    }
+    pub struct DeclareTransaction {
+        pub transaction_hash: TransactionHash,
+        pub max_fee: Fee,
+        pub version: TransactionVersion,
+        pub signature: TransactionSignature,
+        pub nonce: Nonce,
+        pub class_hash: ClassHash,
+        pub sender_address: ContractAddress,
+    }
+    pub struct DeployAccountTransaction {
+        pub transaction_hash: TransactionHash,
+        pub max_fee: Fee,
+        pub version: TransactionVersion,
+        pub signature: TransactionSignature,
+        pub nonce: Nonce,
+        pub class_hash: ClassHash,
+        pub contract_address: ContractAddress,
+        pub contract_address_salt: ContractAddressSalt,
+        pub constructor_calldata: CallData,
+    }
+    pub struct DeployTransaction {
+        pub transaction_hash: TransactionHash,
+        pub version: TransactionVersion,
+        pub class_hash: ClassHash,
+        pub contract_address: ContractAddress,
+        pub contract_address_salt: ContractAddressSalt,
+        pub constructor_calldata: CallData,
+    }
+    pub struct EntryPoint {
+        pub selector: EntryPointSelector,
+        pub offset: EntryPointOffset,
+    }
+    pub struct EntryPointOffset(pub usize);
+    pub struct EntryPointSelector(pub StarkHash);
+    pub enum EntryPointType {
+        Constructor = 0,
+        External = 1,
+        L1Handler = 2,
+    }
+    pub struct EventAbiEntry {
+        pub name: String,
+        pub keys: Vec<TypedParameter>,
+        pub data: Vec<TypedParameter>,
+    }
+    pub struct EventContent {
+        pub keys: Vec<EventKey>,
+        pub data: EventData,
+    }
+    pub struct EventData(pub Vec<StarkFelt>);
+    struct EventIndex(pub TransactionIndex, pub EventIndexInTransactionOutput);
+    pub struct EventIndexInTransactionOutput(pub usize);
+    pub struct EventKey(pub StarkFelt);
+    pub struct Fee(pub u128);
+    pub struct FunctionAbiEntry {
+        pub name: String,
+        pub inputs: Vec<TypedParameter>,
+        pub outputs: Vec<TypedParameter>,
+    }
+    pub enum FunctionAbiEntryType {
+        Constructor = 0,
+        L1Handler = 1,
+        Regular = 2,
+    }
+    pub struct FunctionAbiEntryWithType {
+        pub r#type: FunctionAbiEntryType,
+        pub entry: FunctionAbiEntry,
+    }
+    pub struct GasPrice(pub u128);
+    pub struct GlobalRoot(pub StarkHash);
+    pub struct IndexedDeclaredContract {
+        pub block_number: BlockNumber,
+        pub contract_class: ContractClass,
+    }
+    pub struct IndexedDeployedContract {
+        pub block_number: BlockNumber,
+        pub class_hash: ClassHash,
+    }
+    pub struct InvokeTransaction {
+        pub transaction_hash: TransactionHash,
+        pub max_fee: Fee,
+        pub version: TransactionVersion,
+        pub signature: TransactionSignature,
+        pub nonce: Nonce,
+        pub sender_address: ContractAddress,
+        pub entry_point_selector: Option<EntryPointSelector>,
+        pub calldata: CallData,
+    }
+    pub struct L1ToL2Payload(pub Vec<StarkFelt>);
+    pub struct L2ToL1Payload(pub Vec<StarkFelt>);
+    enum MarkerKind {
+        Header = 0,
+        Body = 1,
+        State = 2,
+    }
+    pub struct MessageToL1 {
+        pub to_address: EthAddress,
+        pub payload: L2ToL1Payload,
+    }
+    pub struct MessageToL2 {
+        pub from_address: EthAddress,
+        pub payload: L1ToL2Payload,
+    }
+    pub struct Nonce(pub StarkFelt);
+    struct OmmerTransactionKey(pub BlockHash, pub TransactionOffsetInBlock);
+    struct OmmerEventKey(pub OmmerTransactionKey, pub EventIndexInTransactionOutput);
+    pub struct Program {
+        pub attributes: serde_json::Value,
+        pub builtins: serde_json::Value,
+        pub compiler_version: serde_json::Value,
+        pub data: serde_json::Value,
+        pub debug_info: serde_json::Value,
+        pub hints: serde_json::Value,
+        pub identifiers: serde_json::Value,
+        pub main_scope: serde_json::Value,
+        pub prime: serde_json::Value,
+        pub reference_manager: serde_json::Value,
+    }
+    pub struct StructAbiEntry {
+        pub name: String,
+        pub size: usize,
+        pub members: Vec<StructMember>,
+    }
+    pub struct StructMember {
+        pub param: TypedParameter,
+        pub offset: usize,
+    }
+    pub struct ThinDeclareTransactionOutput {
+        pub actual_fee: Fee,
+        pub messages_sent: Vec<MessageToL1>,
+        pub events_contract_addresses: Vec<ContractAddress>,
+    }
+    pub struct ThinDeployTransactionOutput {
+        pub actual_fee: Fee,
+        pub messages_sent: Vec<MessageToL1>,
+        pub events_contract_addresses: Vec<ContractAddress>,
+    }
+    pub struct ThinDeployAccountTransactionOutput {
+        pub actual_fee: Fee,
+        pub messages_sent: Vec<MessageToL1>,
+        pub events_contract_addresses: Vec<ContractAddress>,
+    }
+    pub struct TypedParameter {
+        pub name: String,
+        pub r#type: String,
+    }
+    pub struct ThinInvokeTransactionOutput {
+        pub actual_fee: Fee,
+        pub messages_sent: Vec<MessageToL1>,
+        pub events_contract_addresses: Vec<ContractAddress>,
+    }
+    pub struct L1HandlerTransaction {
+        pub transaction_hash: TransactionHash,
+        pub version: TransactionVersion,
+        pub nonce: Nonce,
+        pub contract_address: ContractAddress,
+        pub entry_point_selector: EntryPointSelector,
+        pub calldata: CallData,
+    }
+    pub struct ThinL1HandlerTransactionOutput {
+        pub actual_fee: Fee,
+        pub messages_sent: Vec<MessageToL1>,
+        pub events_contract_addresses: Vec<ContractAddress>,
+    }
+    pub struct ThinStateDiff {
+        pub deployed_contracts: IndexMap<ContractAddress, ClassHash>,
+        pub storage_diffs: IndexMap<ContractAddress, IndexMap<StorageKey, StarkFelt>>,
+        pub declared_contract_hashes: Vec<ClassHash>,
+        pub nonces: IndexMap<ContractAddress, Nonce>,
+    }
+    pub enum ThinTransactionOutput {
+        Declare(ThinDeclareTransactionOutput) = 0,
+        Deploy(ThinDeployTransactionOutput) = 1,
+        DeployAccount(ThinDeployAccountTransactionOutput) = 2,
+        Invoke(ThinInvokeTransactionOutput) = 3,
+        L1Handler(ThinL1HandlerTransactionOutput) = 4,
+    }
+    pub enum Transaction {
+        Declare(DeclareTransaction) = 0,
+        Deploy(DeployTransaction) = 1,
+        DeployAccount(DeployAccountTransaction) = 2,
+        Invoke(InvokeTransaction) = 3,
+        L1Handler(L1HandlerTransaction) = 4,
+    }
+    pub struct TransactionHash(pub StarkHash);
+    struct TransactionIndex(pub BlockNumber, pub TransactionOffsetInBlock);
+    pub struct TransactionOffsetInBlock(pub usize);
+    pub struct TransactionSignature(pub Vec<StarkFelt>);
+    pub struct TransactionVersion(pub StarkFelt);
+
+    bincode(bool);
+    bincode(EthAddress);
+    bincode(u8);
+    bincode(u32);
+    bincode(u64);
+    bincode(u128);
+    bincode(usize);
+
+    (BlockNumber, TransactionOffsetInBlock);
+    (BlockHash, ClassHash);
+    (ContractAddress, BlockHash);
+    (ContractAddress, BlockNumber);
+    (ContractAddress, Nonce);
+    (ContractAddress, EventIndex);
+    (ContractAddress, OmmerEventKey);
+    (ContractAddress, StorageKey, BlockHash);
+    (ContractAddress, StorageKey, BlockNumber);
+}
+
+macro_rules! auto_impl_get_test_instance {
+    () => {};
+    // Tuple structs (no names associated with fields) - one field.
+    ($(pub)? struct $name:ident($(pub)? $ty:ty); $($rest:tt)*) => {
+        impl GetTestInstance for $name {
+            fn get_test_instance() -> Self {
+                Self(<$ty>::get_test_instance())
+            }
+        }
+        auto_impl_get_test_instance!($($rest)*);
+    };
+    // Tuple structs (no names associated with fields) - two fields.
+    ($(pub)? struct $name:ident($(pub)? $ty0:ty, $(pub)? $ty1:ty) ; $($rest:tt)*) => {
+        impl GetTestInstance for $name {
+            fn get_test_instance() -> Self {
+                Self(<$ty0>::get_test_instance(), <$ty1>::get_test_instance())
+            }
+        }
+        auto_impl_get_test_instance!($($rest)*);
+    };
+    // Structs with public fields.
+    ($(pub)? struct $name:ident { $(pub $field:ident : $ty:ty ,)* } $($rest:tt)*) => {
+        impl GetTestInstance for $name {
+            fn get_test_instance() -> Self {
+                Self {
+                    $(
+                        $field: <$ty>::get_test_instance(),
+                    )*
+                }
+            }
+        }
+        auto_impl_get_test_instance!($($rest)*);
+    };
+    // Tuples - two elements.
+    (($ty0:ty, $ty1:ty) ; $($rest:tt)*) => {
+        impl GetTestInstance for ($ty0, $ty1) {
+            fn get_test_instance() -> Self {
+                (
+                    <$ty0>::get_test_instance(),
+                    <$ty1>::get_test_instance(),
+                )
+            }
+        }
+        auto_impl_get_test_instance!($($rest)*);
+    };
+    // Tuples - three elements.
+    (($ty0:ty, $ty1:ty, $ty2:ty) ; $($rest:tt)*) => {
+        impl GetTestInstance for ($ty0, $ty1, $ty2) {
+            fn get_test_instance() -> Self {
+                (
+                    <$ty0>::get_test_instance(),
+                    <$ty1>::get_test_instance(),
+                    <$ty2>::get_test_instance(),
+                )
+            }
+        }
+        auto_impl_get_test_instance!($($rest)*);
+    };
+    // enums.
+    ($(pub)? enum $name:ident { $($variant:ident $( ($ty:ty) )? = $num:expr ,)* } $($rest:tt)*) => {
+        impl GetTestInstance for $name {
+            fn get_test_instance() -> Self {
+                let mut rng = rand::thread_rng();
+                let variant = rng.gen_range(0..get_number_of_variants!(enum $name { $($variant $( ($ty) )? = $num ,)* }));
+                match variant {
+                    $(
+                        $num => {
+                            Self::$variant$((<$ty>::get_test_instance()))?
+                        }
+                    )*
+                    _ => {
+                        panic!("Variant {:?} should match one of the enum {:?} variants.", variant, stringify!($name));
+                    }
+                }
+            }
+        }
+        auto_impl_get_test_instance!($($rest)*);
+    };
+    // Binary.
+    (bincode($name:ident); $($rest:tt)*) => {
+        default_impl_get_test_instance!($name);
+        auto_impl_get_test_instance!($($rest)*);
+    }
+}
+pub(crate) use auto_impl_get_test_instance;
+
+macro_rules! default_impl_get_test_instance {
+    ($name:path) => {
+        impl GetTestInstance for $name {
+            fn get_test_instance() -> Self {
+                Self::default()
+            }
+        }
+    };
+}
+pub(crate) use default_impl_get_test_instance;
+
+////////////////////////////////////////////////////////////////////////
+// Implements the [`GetTestInstance`] trait for primitive types.
+////////////////////////////////////////////////////////////////////////
+default_impl_get_test_instance!(serde_json::Value);
+default_impl_get_test_instance!(String);
+impl<T: GetTestInstance> GetTestInstance for Option<T> {
+    fn get_test_instance() -> Self {
+        Some(T::get_test_instance())
+    }
+}
+impl<T: GetTestInstance> GetTestInstance for Vec<T> {
+    fn get_test_instance() -> Self {
+        vec![T::get_test_instance()]
+    }
+}
+impl<K: GetTestInstance + Eq + Hash, V: GetTestInstance> GetTestInstance for HashMap<K, V> {
+    fn get_test_instance() -> Self {
+        let mut res = HashMap::with_capacity(1);
+        let k = K::get_test_instance();
+        let v = V::get_test_instance();
+        res.insert(k, v);
+        res
+    }
+}
+impl<K: GetTestInstance + Eq + Hash, V: GetTestInstance> GetTestInstance for IndexMap<K, V> {
+    fn get_test_instance() -> Self {
+        let mut res = IndexMap::with_capacity(1);
+        let k = K::get_test_instance();
+        let v = V::get_test_instance();
+        res.insert(k, v);
+        res
+    }
+}
+
+// Counts the number of variants of an enum.
+macro_rules! get_number_of_variants {
+    (enum $name:ident { $($variant:ident $( ($ty:ty) )? = $num:expr ,)* }) => {
+        get_number_of_variants!(@count $($variant),+)
+    };
+    (@count $t1:tt, $($t:tt),+) => { 1 + get_number_of_variants!(@count $($t),+) };
+    (@count $t:tt) => { 1 };
+}
+pub(crate) use get_number_of_variants;
+
+////////////////////////////////////////////////////////////////////////
+// Implements the [`GetTestInstance`] trait for types not supported
+// by the macro [`impl_get_test_instance`].
+////////////////////////////////////////////////////////////////////////
+default_impl_get_test_instance!(StarkHash);
+default_impl_get_test_instance!(ContractAddress);
+default_impl_get_test_instance!(StorageKey);
 
 /// Returns a test block body with a variable number of transactions.
 pub fn get_test_body(transaction_count: usize) -> BlockBody {
