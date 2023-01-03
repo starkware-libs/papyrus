9ff82a9c14905f1866911d9f13c823c87bcf6d63
Dan Brownstein <dan@starkware.co>
2023-01-03 20:48:26 +0200

    Host from CLA in load test

--- crates/papyrus_load_test/src/main.rs
+++ crates/papyrus_load_test/src/main.rs
@@ -1,5 +1,5 @@
 // This code is inspired by the pathfinder load test.
-use goose::config::{GooseDefault, GooseDefaultType};
+
 use goose::goose::{GooseUser, Scenario, Transaction, TransactionError, TransactionResult};
 use goose::{scenario, transaction, GooseAttack, GooseError};
 use serde::de::DeserializeOwned;
@@ -34,10 +34,6 @@ fn jsonrpc_request(method: &str, params: serde_json::Value) -> serde_json::Value
     })
 }
 
-/// Tests the rpc:
-/// ```
-/// getBlockWithTxHashes
-/// ```
 async fn loadtest_get_block_with_tx_hashes_by_number(user: &mut GooseUser) -> TransactionResult {
     let _: serde_json::Value = get_block_with_tx_hashes_by_number(user, 1).await?;
     Ok(())
@@ -51,15 +47,11 @@ pub async fn get_block_with_tx_hashes_by_number<T: DeserializeOwned>(
         .await
 }
 
-/// Tests the rpc:
-/// ```
-/// getBlockWithTxHashes
-/// ```
 async fn loadtest_get_block_with_tx_hashes_by_hash(user: &mut GooseUser) -> TransactionResult {
     // TODO(shahak): Get a hash by getting a block instead of relying on that this hash exists.
     let _: serde_json::Value = get_block_with_tx_hashes_by_hash(
         user,
-        "0x2a70fb03fe363a2d6be843343a1d81ce6abeda1e9bd5cc6ad8fa9f45e30fdeb",
+        "0x21d419ca19117002954f58f1fdf879791973107ccdb2004c5d635411bb90c0e",
     )
     .await?;
     Ok(())
@@ -84,7 +76,6 @@ async fn main() -> Result<(), GooseError> {
             scenario!("block_by_hash")
                 .register_transaction(transaction!(loadtest_get_block_with_tx_hashes_by_hash)),
         )
-        .set_default(GooseDefault::Host, "http://127.0.0.1:8080")?
         .execute()
         .await?;
     Ok(())
