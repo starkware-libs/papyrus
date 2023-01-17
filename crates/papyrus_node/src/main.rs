f0b046dd977cf0b71ae1dde24ef3822a7ca3cc68
Yair Bakalchuk <yair@starkware.co>
2023-01-17 10:19:34 +0200

    Use tracing subscriber instead of log4rs

--- crates/papyrus_node/src/main.rs
+++ crates/papyrus_node/src/main.rs
@@ -1,11 +1,16 @@
 use std::env::args;
 
-use log::info;
 use papyrus_gateway::run_server;
 use papyrus_monitoring_gateway::run_server as monitoring_run_server;
 use papyrus_node::config::Config;
 use papyrus_storage::{open_storage, StorageReader, StorageWriter};
 use papyrus_sync::{CentralError, CentralSource, StateSync, StateSyncError};
+use tracing::info;
+use tracing_subscriber::prelude::*;
+use tracing_subscriber::{fmt, EnvFilter};
+
+// TODO: Add to config.
+const DEFAULT_LEVEL: &str = "info";
 
 async fn run_threads(config: Config) -> anyhow::Result<()> {
     let (storage_reader, storage_writer) = open_storage(config.storage.db_config.clone())?;
@@ -40,10 +45,22 @@ async fn run_threads(config: Config) -> anyhow::Result<()> {
     }
 }
 
+// TODO: add dynamic level filtering.
+// TODO: filter out logs from dependencies (happens when RUST_LOG=DEBUG)
+// TODO: define and implement configurable filtering.
+fn configure_tracing() {
+    let fmt_layer = fmt::layer().compact().with_target(false);
+    let level_filter_layer =
+        EnvFilter::try_from_default_env().or_else(|_| EnvFilter::try_new(DEFAULT_LEVEL)).unwrap();
+
+    let subscriber = tracing_subscriber::registry().with(fmt_layer).with(level_filter_layer);
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let config = Config::load(args().collect())?;
-    log4rs::init_file("config/log4rs.yaml", Default::default())?;
+    configure_tracing();
     info!("Booting up.");
     run_threads(config).await
 }
