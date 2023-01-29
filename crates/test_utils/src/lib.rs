b9dce06f4010bb2604bf3202978c7597bdc15b2c
Dvir Yosef <dvir@starkware.co>
2023-01-29 11:37:25 +0200

    Fix reviewer comments.

--- crates/test_utils/src/lib.rs
+++ crates/test_utils/src/lib.rs
@@ -29,7 +29,7 @@ use starknet_api::transaction::{
     TransactionSignature, TransactionVersion,
 };
 
-// Return the absolute  path from the project root.
+// Returns the absolute  path from the project root.
 pub fn get_absolute_path(relative_path: &str) -> PathBuf {
     Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("../..").join(relative_path)
 }
