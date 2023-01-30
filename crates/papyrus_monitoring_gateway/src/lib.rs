d71c4b6e6388271411f8eaa1ab8a8c480848871c
Yair Bakalchuk <yair@starkware.co>
2023-01-30 15:50:21 +0200

    Tracing: open span for monitoring gateway callbacks

--- crates/papyrus_monitoring_gateway/src/lib.rs
+++ crates/papyrus_monitoring_gateway/src/lib.rs
@@ -13,11 +13,11 @@ use jsonrpsee::types::error::ErrorCode::InternalError;
 use jsonrpsee::types::error::{ErrorObject, INTERNAL_ERROR_MSG};
 use papyrus_storage::{DbTablesStats, StorageReader};
 use serde::{Deserialize, Serialize};
-use tracing::{error, info};
+use tracing::{debug, error, info, instrument};
 
 use self::api::JsonRpcServer;
 
-#[derive(Clone, Serialize, Deserialize)]
+#[derive(Clone, Serialize, Deserialize, Debug)]
 pub struct MonitoringGatewayConfig {
     pub server_address: String,
 }
@@ -39,6 +39,7 @@ fn internal_server_error(err: impl Display) -> Error {
 
 #[async_trait]
 impl JsonRpcServer for JsonRpcServerImpl {
+    #[instrument(skip(self), level = "debug", err(Display), ret)]
     fn db_tables_stats(&self) -> Result<DbTablesStats, Error> {
         self.storage_reader.db_tables_stats().map_err(internal_server_error)
     }
@@ -48,16 +49,17 @@ impl JsonRpcServer for JsonRpcServerImpl {
     }
 }
 
+#[instrument(skip(storage_reader), level = "debug", err)]
 pub async fn run_server(
     general_config_representation: serde_yaml::Value,
     config: MonitoringGatewayConfig,
     storage_reader: StorageReader,
 ) -> anyhow::Result<(SocketAddr, HttpServerHandle)> {
-    info!("Starting monitoring gateway.");
+    debug!("Starting monitoring gateway.");
     let server = HttpServerBuilder::default().build(&config.server_address).await?;
     let addr = server.local_addr()?;
     let handle = server
         .start(JsonRpcServerImpl { storage_reader, general_config_representation }.into_rpc())?;
-    info!("Monitoring gateway is running - {}.", addr);
+    info!(local_address = %addr, "Monitoring gateway is running.");
     Ok((addr, handle))
 }
