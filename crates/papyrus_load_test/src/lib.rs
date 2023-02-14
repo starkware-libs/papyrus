d320402b3354dcc86c9dfeda21955081e93c3a10
Dvir Yosef <dvir@starkware.co>
2023-02-14 10:45:31 +0200

    Remove unnecessary use of generic.

--- crates/papyrus_load_test/src/lib.rs
+++ crates/papyrus_load_test/src/lib.rs
@@ -2,29 +2,16 @@ pub mod gateway_endpoints;
 pub mod load_tests;
 
 use goose::goose::{GooseUser, TransactionError};
-use serde::de::DeserializeOwned;
-use serde::Deserialize;
-use serde_json::json;
-type MethodResult<T> = Result<T, Box<TransactionError>>;
+use serde_json::{json, Value as jsonVal};
+type PostResult = Result<jsonVal, Box<TransactionError>>;
 
-pub async fn post_jsonrpc_request<T: DeserializeOwned>(
-    user: &mut GooseUser,
-    method: &str,
-    params: serde_json::Value,
-) -> MethodResult<T> {
-    let request = jsonrpc_request(method, params);
-    let response = user.post_json("", &request).await?.response.map_err(|e| Box::new(e.into()))?;
-    #[derive(Deserialize)]
-    struct TransactionReceiptResponse<T> {
-        result: T,
-    }
-    let response: TransactionReceiptResponse<T> =
-        response.json().await.map_err(|e| Box::new(e.into()))?;
-
-    Ok(response.result)
+pub async fn post_jsonrpc_request(user: &mut GooseUser, request: &jsonVal) -> PostResult {
+    let response = user.post_json("", request).await?.response.map_err(|e| Box::new(e.into()))?;
+    let response = response.json::<jsonVal>().await.map_err(|e| Box::new(e.into()))?;
+    Ok(response)
 }
 
-pub fn jsonrpc_request(method: &str, params: serde_json::Value) -> serde_json::Value {
+pub fn jsonrpc_request(method: &str, params: jsonVal) -> jsonVal {
     json!({
         "jsonrpc": "2.0",
         "id": "0",
