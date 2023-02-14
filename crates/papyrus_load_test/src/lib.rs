2e9d41f8247ebc6965b0115257fb2b25268cc532
Dvir Yosef <dvir@starkware.co>
2023-02-14 10:45:31 +0200

    Remove unnecessary use of generic.

--- crates/papyrus_load_test/src/lib.rs
+++ crates/papyrus_load_test/src/lib.rs
@@ -8,7 +8,7 @@ type PostResult = Result<jsonVal, Box<TransactionError>>;
 pub async fn post_jsonrpc_request(user: &mut GooseUser, request: &jsonVal) -> PostResult {
     let response = user.post_json("", request).await?.response.map_err(|e| Box::new(e.into()))?;
     let response = response.json::<jsonVal>().await.map_err(|e| Box::new(e.into()))?;
-    Ok(response)
+    Ok(response) 
 }
 
 pub fn jsonrpc_request(method: &str, params: jsonVal) -> jsonVal {
