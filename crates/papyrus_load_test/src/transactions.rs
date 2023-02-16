3c9003e1f918e396e98e2f9a3d1a3d5eea40cfa0
Dvir Yosef <dvir@starkware.co>
2023-02-16 17:14:23 +0200

    Add function create_request_vector.

--- crates/papyrus_load_test/src/transactions.rs
+++ crates/papyrus_load_test/src/transactions.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 use std::sync::Arc;
 
 use goose::goose::{Transaction, TransactionFunction};
@@ -41,4 +44,14 @@ fn random_request_transaction(requests: Vec<jsonVal>) -> Transaction {
         })
     });
     Transaction::new(func)
+}
+
+fn create_requests_vector(path: &str, convert_to_request: fn(String) -> jsonVal) -> Vec<jsonVal> {
+    let file = File::open(path).unwrap();
+    let reader = BufReader::new(file);
+    let mut requests = Vec::<jsonVal>::new();
+    for line in reader.lines() {
+        requests.push(convert_to_request(line.unwrap()));
+    }
+    requests
 }
