a17c7025b090a7d024c34ad13d6ab217c711c0a0
Dvir Yosef <dvir@starkware.co>
2023-02-19 12:28:14 +0200

    Add a macro for creating the functions that return Transaction.

--- crates/papyrus_load_test/src/transactions.rs
+++ crates/papyrus_load_test/src/transactions.rs
@@ -9,22 +9,10 @@ use serde_json::Value as jsonVal;
 
 use crate::{create_request, post_jsonrpc_request};
 
-pub fn block_by_number() -> Transaction {
+pub fn get_block_with_tx_hashes_by_number() -> Transaction {
     let requests = vec![
         create_request::get_block_with_tx_hashes_by_number(0),
         create_request::get_block_with_tx_hashes_by_number(1),
-    ];
-    random_request_transaction(requests)
-}
-
-pub fn block_by_hash() -> Transaction {
-    let requests = vec![
-        create_request::get_block_with_tx_hashes_by_hash(
-            "0x47c3637b57c2b079b93c61539950c17e868a28f46cdef28f88521067f21e943",
-        ),
-        create_request::get_block_with_tx_hashes_by_hash(
-            "0x2a70fb03fe363a2d6be843343a1d81ce6abeda1e9bd5cc6ad8fa9f45e30fdeb",
-        ),
     ];
     random_request_transaction(requests)
 }
@@ -52,4 +40,24 @@ fn create_requests_vector(path: &str, convert_to_request: fn(String) -> jsonVal)
         requests.push(convert_to_request(line.unwrap()));
     }
     requests
+}
+
+// Given [Name; "Path"] write the function:
+// pub fn Name() -> Transaction {
+//     let requests = create_requests_vector("Path", create_request::Name);
+//     random_request_transaction(requests)
+// }
+macro_rules! create_read_from_file_transaction {
+    () => {};
+    ($name:tt, $file_name:literal; $($rest:tt)*) => {
+        pub fn $name() -> Transaction {
+            let requests = create_requests_vector($file_name, create_request::$name);
+            random_request_transaction(requests)
+        }
+        create_read_from_file_transaction!($($rest)*);
+    };
+}
+
+create_read_from_file_transaction! {
+    get_block_with_tx_hashes_by_hash, "crates/papyrus_load_test/src/resources/block_hash.txt";
 }
