85400923da87a9053b06e5e8af0af6af5eaf1984
Dvir Yosef <dvir@starkware.co>
2023-02-21 10:55:22 +0200

    Delete unnecessary scenarios.

--- crates/papyrus_load_test/src/scenarios.rs
+++ crates/papyrus_load_test/src/scenarios.rs
@@ -3,16 +3,6 @@ use goose::scenario;
 
 use crate::transactions;
 
-pub fn block_by_number() -> Scenario {
-    scenario!("block_by_number")
-        .register_transaction(transactions::get_block_with_tx_hashes_by_number())
-}
-
-pub fn block_by_hash() -> Scenario {
-    scenario!("block_by_hash")
-        .register_transaction(transactions::get_block_with_tx_hashes_by_hash())
-}
-
 pub fn general_request() -> Scenario {
     scenario!("general_request")
         .register_transaction(
