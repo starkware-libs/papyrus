6b1cfb6b07bcca75fbb74aae19e4562f8f41930f
Dvir Yosef <dvir@starkware.co>
2023-02-22 12:34:50 +0200

    Replace hard coded values with call to create_request.

--- crates/papyrus_load_test/src/transactions.rs
+++ crates/papyrus_load_test/src/transactions.rs
@@ -2,27 +2,25 @@ use std::sync::Arc;
 
 use goose::goose::{Transaction, TransactionFunction};
 use rand::Rng;
-use serde_json::{json, Value as jsonVal};
+use serde_json::Value as jsonVal;
 
-use crate::{jsonrpc_request, post_jsonrpc_request};
+use crate::{create_request, post_jsonrpc_request};
 
 pub fn block_by_number() -> Transaction {
     let requests = vec![
-        jsonrpc_request("starknet_getBlockWithTxHashes", json!([{ "block_number": 0 }])),
-        jsonrpc_request("starknet_getBlockWithTxHashes", json!([{ "block_number": 1 }])),
+        create_request::get_block_with_tx_hashes_by_number(0),
+        create_request::get_block_with_tx_hashes_by_number(1),
     ];
     random_request_transaction(requests)
 }
 
 pub fn block_by_hash() -> Transaction {
     let requests = vec![
-        jsonrpc_request(
-            "starknet_getBlockWithTxHashes",
-            json!([{ "block_hash": "0x47c3637b57c2b079b93c61539950c17e868a28f46cdef28f88521067f21e943" }]),
+        create_request::get_block_with_tx_hashes_by_hash(
+            "0x47c3637b57c2b079b93c61539950c17e868a28f46cdef28f88521067f21e943".to_string(),
         ),
-        jsonrpc_request(
-            "starknet_getBlockWithTxHashes",
-            json!([{ "block_hash": "0x2a70fb03fe363a2d6be843343a1d81ce6abeda1e9bd5cc6ad8fa9f45e30fdeb" }]),
+        create_request::get_block_with_tx_hashes_by_hash(
+            "0x2a70fb03fe363a2d6be843343a1d81ce6abeda1e9bd5cc6ad8fa9f45e30fdeb".to_string(),
         ),
     ];
     random_request_transaction(requests)
