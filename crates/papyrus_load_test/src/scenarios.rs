631dc5bd1a52d2ee6e8806554fc029ea7f02651f
Dvir Yosef <dvir@starkware.co>
2023-02-22 15:35:35 +0200

    Add serial_get_block transaction.

--- crates/papyrus_load_test/src/scenarios.rs
+++ crates/papyrus_load_test/src/scenarios.rs
@@ -12,3 +12,8 @@ pub fn general_request() -> Scenario {
             transactions::get_block_with_tx_hashes_by_hash().set_weight(1).unwrap(),
         )
 }
+
+pub fn serial_get_block() -> Scenario {
+    scenario!("general_request")
+        .register_transaction(transactions::serial_get_block(1000))
+}
