ac0e464c23757e987e14360cd35f9a03bf6e967b
DvirYo-starkware <115620476+DvirYo-starkware@users.noreply.github.com>
2023-02-23 13:45:36 +0200

    Dvir/endpoints to create request (#505)
    * Rename gateway_endpoints create_request.
    
    * Change the function in create_request.
    
    * Replace hard coded values with call to create_request.
    
    * Change String parameter with &str.

---
+++ crates/papyrus_load_test/src/create_request.rs
@@ -0,0 +1,11 @@
+use serde_json::{json, Value as jsonVal};
+
+use crate::jsonrpc_request;
+
+pub fn get_block_with_tx_hashes_by_number(block_number: u64) -> jsonVal {
+    jsonrpc_request("starknet_getBlockWithTxHashes", json!([{ "block_number": block_number }]))
+}
+
+pub fn get_block_with_tx_hashes_by_hash(block_hash: &str) -> jsonVal {
+    jsonrpc_request("starknet_getBlockWithTxHashes", json!([{ "block_hash": block_hash }]))
+}
