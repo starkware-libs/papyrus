e08c5cd2f5267ed33676786c430487a58dbee6d4
Dvir Yosef <dvir@starkware.co>
2023-02-26 13:43:33 +0200

    Error handling for the load test.

--- crates/papyrus_load_test/src/scenarios.rs
+++ crates/papyrus_load_test/src/scenarios.rs
@@ -2,13 +2,18 @@ use goose::goose::Scenario;
 use goose::scenario;
 
 use crate::transactions;
+pub type ScenariosResult = Result<Scenario, ScenariosError>;
 
-pub fn block_by_number() -> Scenario {
-    scenario!("block_by_number")
-        .register_transaction(transactions::get_block_with_tx_hashes_by_number())
+#[derive(thiserror::Error, Debug)]
+pub enum ScenariosError {
+    #[error(transparent)]
+    CreateTransaction(#[from] transactions::TransactionsError),
+    #[error(transparent)]
+    Goose(#[from] goose::GooseError),
 }
 
-pub fn block_by_hash() -> Scenario {
-    scenario!("block_by_hash")
-        .register_transaction(transactions::get_block_with_tx_hashes_by_hash())
+pub fn general_request() -> ScenariosResult {
+    Ok(scenario!("general_request")
+        .register_transaction(transactions::get_block_with_tx_hashes_by_number()?.set_weight(1)?)
+        .register_transaction(transactions::get_block_with_tx_hashes_by_hash()?.set_weight(1)?))
 }
