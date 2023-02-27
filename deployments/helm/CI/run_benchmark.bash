98eae95ef92cae915819783d3fc504c6515de8d6
eranreshef-starkware <eran@starkware.co>
2023-02-27 15:54:11 +0200

    parse load-test output, delete job after completion

--- deployments/helm/CI/run_benchmark.bash
+++ deployments/helm/CI/run_benchmark.bash
@@ -5,17 +5,25 @@ BUILD_ID=$1
 DURRATION=$2
 DURRATION_TIMEOUT=$3
 BASE_PATH="deployments/helm/CI"
-TMP_FILE="${BASE_PATH}/load_test_job-${BUILD_ID}.yaml"
+TMP_FILE="$BASE_PATH/load_test_job-$BUILD_ID.yaml"
 
-cp "${BASE_PATH}/load_test_job.yaml.tmpl" "${TMP_FILE}"
-sed -i "s/XXX/$BUILD_ID/g" "${TMP_FILE}"
-sed -i "s/DURRATION/$DURRATION/g" "${TMP_FILE}"
-kubectl --namespace papyrus apply -f "${TMP_FILE}" --wait=true
+# copy the load-test tmeplate file and render it with supplied values
+cp "$BASE_PATH/load_test_job.yaml.tmpl" "$TMP_FILE"
+sed -i "s/BUILD_ID/$BUILD_ID/g" "$TMP_FILE"
+sed -i "s/DURRATION/$DURRATION/g" "$TMP_FILE"
 
-#wait for load-test pod to start
+# create the load-test job
+kubectl --namespace papyrus apply -f "$TMP_FILE" --wait=true
+
+# wait for load-test pod to start
 kubectl wait --namespace=papyrus --for=condition=ready pod -l job-name=papyrus-"$BUILD_ID"-load-test
 
 # wait $DURRATION_TIMEOUT for the load-test application to finish
-kubectl wait --namespace=papyrus --for=condition=complete job/papyrus-"${BUILD_ID}"-load-test --timeout "$DURRATION_TIMEOUT"
+kubectl wait --namespace=papyrus --for=condition=complete job/papyrus-"$BUILD_ID"-load-test --timeout "$DURRATION_TIMEOUT"
 
-rm "${TMP_FILE}"
+# Save logs to file
+kubectl logs --namespace=papyrus -l job-name=papyrus-"$BUILD_ID"-load-test --tail=-1 >"load_test_job-$BUILD_ID.out"
+
+# delete the load-test job
+kubectl --namespace papyrus delete -f "$TMP_FILE" --wait=true
+rm "$TMP_FILE"
