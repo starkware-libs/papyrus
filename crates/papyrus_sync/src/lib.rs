4ccc808562a687865410308b42c75ca810f0f769
anatg <anatg@starkware.co>
2023-02-27 10:49:29 +0200

    Separate to modules and change store functions to restart download task.

--- crates/papyrus_sync/src/lib.rs
+++ crates/papyrus_sync/src/lib.rs
@@ -1,27 +1,18 @@
-#[cfg(test)]
-#[path = "sync_test.rs"]
-mod sync_test;
-
+mod block;
 mod sources;
+mod state;
 
 use std::sync::Arc;
 use std::time::Duration;
 
-use async_stream::try_stream;
-use futures_util::{pin_mut, select, Stream, StreamExt};
-use indexmap::IndexMap;
-use papyrus_storage::body::BodyStorageWriter;
-use papyrus_storage::header::{HeaderStorageReader, HeaderStorageWriter};
-use papyrus_storage::ommer::{OmmerStorageReader, OmmerStorageWriter};
-use papyrus_storage::state::{StateStorageReader, StateStorageWriter};
 use papyrus_storage::{StorageError, StorageReader, StorageWriter};
 use serde::{Deserialize, Serialize};
-use starknet_api::block::{Block, BlockHash, BlockNumber};
-use starknet_api::core::ClassHash;
-use starknet_api::state::{ContractClass, StateDiff};
-use tracing::{debug, error, info, trace, warn};
+use starknet_api::block::{BlockHash, BlockNumber};
+use tracing::{error, info, warn};
 
 pub use self::sources::{CentralError, CentralSource, CentralSourceConfig, CentralSourceTrait};
+use crate::block::BlockSync;
+use crate::state::StateDiffSync;
 
 #[derive(Clone, Copy, Serialize, Deserialize)]
 pub struct SyncConfig {
@@ -45,6 +36,8 @@ pub enum StateSyncError {
     StorageError(#[from] StorageError),
     #[error(transparent)]
     CentralSourceError(#[from] CentralError),
+    #[error("Sync internal error - {msg}")]
+    SyncInternalError { msg: String },
     #[error(
         "Parent block hash of block {block_number} is not consistent with the stored block. \
          Expected {expected_parent_block_hash}, found {stored_parent_block_hash}."
@@ -61,41 +54,16 @@ pub enum StateSyncError {
     StateDiffWithoutMatchingHeader { block_number: BlockNumber, block_hash: BlockHash },
 }
 
-#[allow(clippy::large_enum_variant)]
-pub enum SyncEvent {
-    BlockAvailable {
-        block_number: BlockNumber,
-        block: Block,
-    },
-    StateDiffAvailable {
-        block_number: BlockNumber,
-        block_hash: BlockHash,
-        state_diff: StateDiff,
-        // TODO(anatg): Remove once there are no more deployed contracts with undeclared classes.
-        // Class definitions of deployed contracts with classes that were not declared in this
-        // state diff.
-        deployed_contract_class_definitions: IndexMap<ClassHash, ContractClass>,
-    },
-}
-
 impl<TCentralSource: CentralSourceTrait + Sync + Send + 'static> GenericStateSync<TCentralSource> {
     pub async fn run(&mut self) -> StateSyncResult {
         info!("State sync started.");
+        let mut block_sync =
+            BlockSync::new(self.config, self.central_source.clone(), self.reader.clone());
+        let mut state_diff_sync =
+            StateDiffSync::new(self.config, self.central_source.clone(), self.reader.clone());
+
         loop {
-            match self.sync_while_ok().await {
-                Err(StateSyncError::ParentBlockHashMismatch {
-                    block_number,
-                    expected_parent_block_hash,
-                    stored_parent_block_hash,
-                }) => {
-                    // A revert detected, log and restart sync loop.
-                    info!(
-                        "Detected revert while processing block {}. Parent hash of the incoming \
-                         block is {}, current block hash is {}.",
-                        block_number, expected_parent_block_hash, stored_parent_block_hash
-                    );
-                    continue;
-                }
+            match self.sync_while_ok(&mut block_sync, &mut state_diff_sync).await {
                 // A recoverable error occurred. Sleep and try syncing again.
                 Err(err) if is_recoverable(&err) => {
                     warn!("{}", err);
@@ -107,9 +75,7 @@ impl<TCentralSource: CentralSourceTrait + Sync + Send + 'static> GenericStateSyn
                     error!("{}", err);
                     return Err(err);
                 }
-                Ok(_) => {
-                    unreachable!("Sync should either return with an error or continue forever.")
-                }
+                Ok(_) => continue,
             }
         }
 
@@ -122,323 +88,24 @@ impl<TCentralSource: CentralSourceTrait + Sync + Send + 'static> GenericStateSyn
                 {
                     true
                 }
-                StateSyncError::StateDiffWithoutMatchingHeader {
-                    block_number: _,
-                    block_hash: _,
-                } => true,
                 _ => false,
             }
         }
     }
 
-    // Sync until encountering an error:
-    //  1. If needed, revert blocks from the end of the chain.
-    //  2. Create infinite block and state diff streams to fetch data from the central source.
-    //  3. Fetch data from the streams with unblocking wait while there is no new data.
-    async fn sync_while_ok(&mut self) -> StateSyncResult {
-        self.handle_block_reverts().await?;
-        let block_stream = stream_new_blocks(
-            self.reader.clone(),
-            self.central_source.clone(),
-            self.config.block_propagation_sleep_duration,
-        )
-        .fuse();
-        let state_diff_stream = stream_new_state_diffs(
-            self.reader.clone(),
-            self.central_source.clone(),
-            self.config.block_propagation_sleep_duration,
-        )
-        .fuse();
-        pin_mut!(block_stream, state_diff_stream);
-
-        loop {
-            debug!("Selecting between block sync and state diff sync.");
-            let sync_event = select! {
-              res = block_stream.next() => res,
-              res = state_diff_stream.next() => res,
-              complete => break,
-            }
-            .expect("Received None as a sync event.")?;
-            self.process_sync_event(sync_event).await?;
-            debug!("Finished processing sync event.");
-        }
-        unreachable!("Fetching data loop should never return.");
-    }
-
-    // Tries to store the incoming data.
-    async fn process_sync_event(&mut self, sync_event: SyncEvent) -> StateSyncResult {
-        match sync_event {
-            SyncEvent::BlockAvailable { block_number, block } => {
-                debug!("Got block sync event.");
-                self.store_block(block_number, block)
-            }
-            SyncEvent::StateDiffAvailable {
-                block_number,
-                block_hash,
-                state_diff,
-                deployed_contract_class_definitions,
-            } => {
-                debug!("Got state diff sync event.");
-                self.store_state_diff(
-                    block_number,
-                    block_hash,
-                    state_diff,
-                    deployed_contract_class_definitions,
-                )
-            }
-        }
-    }
-
-    fn store_block(&mut self, block_number: BlockNumber, block: Block) -> StateSyncResult {
-        // Assuming the central source is trusted, detect reverts by comparing the incoming block's
-        // parent hash to the current hash.
-        self.verify_parent_block_hash(block_number, &block)?;
-
-        debug!("Storing block {block_number} with hash {}.", block.header.block_hash);
-        trace!("Block data: {block:#?}");
-        self.writer
-            .begin_rw_txn()?
-            .append_header(block_number, &block.header)?
-            .append_body(block_number, block.body)?
-            .commit()?;
-        Ok(())
-    }
-
-    fn store_state_diff(
+    async fn sync_while_ok(
         &mut self,
-        block_number: BlockNumber,
-        block_hash: BlockHash,
-        state_diff: StateDiff,
-        deployed_contract_class_definitions: IndexMap<ClassHash, ContractClass>,
+        block_sync: &mut BlockSync<TCentralSource>,
+        state_diff_sync: &mut StateDiffSync<TCentralSource>,
     ) -> StateSyncResult {
-        if !self.is_reverted_state_diff(block_number, block_hash)? {
-            debug!("Storing state diff of block {block_number} with hash {block_hash}.");
-            trace!("StateDiff data: {state_diff:#?}");
-            self.writer
-                .begin_rw_txn()?
-                .append_state_diff(block_number, state_diff, deployed_contract_class_definitions)?
-                .commit()?;
+        let txn1 = self.writer.begin_rw_txn()?;
+        block_sync.step(txn1)?;
 
-            // Info the user on syncing the block once all the data is stored.
-            info!("Added block {} with hash {}.", block_number, block_hash);
-        } else {
-            debug!(
-                "Storing ommer state diff of block {} with hash {:?}.",
-                block_number, block_hash
-            );
-            self.writer
-                .begin_rw_txn()?
-                .insert_ommer_state_diff(
-                    block_hash,
-                    &state_diff.into(),
-                    &deployed_contract_class_definitions,
-                )?
-                .commit()?;
-        }
+        let txn2 = self.writer.begin_rw_txn()?;
+        state_diff_sync.step(txn2)?;
+
+        tokio::time::sleep(std::time::Duration::from_millis(1)).await;
         Ok(())
-    }
-
-    // Compares the block's parent hash to the stored block.
-    fn verify_parent_block_hash(
-        &self,
-        block_number: BlockNumber,
-        block: &Block,
-    ) -> StateSyncResult {
-        let prev_block_number = match block_number.prev() {
-            None => return Ok(()),
-            Some(bn) => bn,
-        };
-        let prev_hash = self
-            .reader
-            .begin_ro_txn()?
-            .get_block_header(prev_block_number)?
-            .ok_or(StorageError::DBInconsistency {
-                msg: format!(
-                    "Missing block {prev_block_number} in the storage (for verifying block \
-                     {block_number}).",
-                ),
-            })?
-            .block_hash;
-
-        if prev_hash != block.header.parent_hash {
-            return Err(StateSyncError::ParentBlockHashMismatch {
-                block_number,
-                expected_parent_block_hash: block.header.parent_hash,
-                stored_parent_block_hash: prev_hash,
-            });
-        }
-
-        Ok(())
-    }
-
-    // Reverts data if needed.
-    async fn handle_block_reverts(&mut self) -> Result<(), StateSyncError> {
-        debug!("Handling block reverts.");
-        let header_marker = self.reader.begin_ro_txn()?.get_header_marker()?;
-
-        // Revert last blocks if needed.
-        let mut last_block_in_storage = header_marker.prev();
-        while let Some(block_number) = last_block_in_storage {
-            if self.should_revert_block(block_number).await? {
-                info!("Reverting block {}.", block_number);
-                self.revert_block(block_number)?;
-                last_block_in_storage = block_number.prev();
-            } else {
-                break;
-            }
-        }
-        Ok(())
-    }
-
-    // Deletes the block data from the storage, moving it to the ommer tables.
-    #[allow(clippy::expect_fun_call)]
-    fn revert_block(&mut self, block_number: BlockNumber) -> StateSyncResult {
-        let mut txn = self.writer.begin_rw_txn()?;
-
-        let res = txn.revert_header(block_number)?;
-        txn = res.0;
-        if let Some(header) = res.1 {
-            txn = txn.insert_ommer_header(header.block_hash, &header)?;
-
-            let res = txn.revert_body(block_number)?;
-            txn = res.0;
-            if let Some((transactions, transaction_outputs, events)) = res.1 {
-                txn = txn.insert_ommer_body(
-                    header.block_hash,
-                    &transactions,
-                    &transaction_outputs,
-                    events.as_slice(),
-                )?;
-            }
-
-            let res = txn.revert_state_diff(block_number)?;
-            txn = res.0;
-            if let Some((thin_state_diff, declared_classes)) = res.1 {
-                txn = txn.insert_ommer_state_diff(
-                    header.block_hash,
-                    &thin_state_diff,
-                    &declared_classes,
-                )?;
-            }
-        }
-
-        txn.commit()?;
-        Ok(())
-    }
-
-    /// Checks if centrals block hash at the block number is different from ours (or doesn't exist).
-    /// If so, a revert is required.
-    async fn should_revert_block(&self, block_number: BlockNumber) -> Result<bool, StateSyncError> {
-        if let Some(central_block_hash) = self.central_source.get_block_hash(block_number).await? {
-            let storage_block_header =
-                self.reader.begin_ro_txn()?.get_block_header(block_number)?;
-
-            match storage_block_header {
-                Some(block_header) => Ok(block_header.block_hash != central_block_hash),
-                None => Ok(false),
-            }
-        } else {
-            // Block number doesn't exist in central, revert.
-            Ok(true)
-        }
-    }
-
-    fn is_reverted_state_diff(
-        &self,
-        block_number: BlockNumber,
-        block_hash: BlockHash,
-    ) -> Result<bool, StateSyncError> {
-        let txn = self.reader.begin_ro_txn()?;
-        let storage_header = txn.get_block_header(block_number)?;
-        match storage_header {
-            Some(storage_header) if storage_header.block_hash == block_hash => Ok(false),
-            _ => {
-                // No matching header, check in the ommer headers.
-                match txn.get_ommer_header(block_hash)? {
-                    Some(_) => Ok(true),
-                    None => Err(StateSyncError::StateDiffWithoutMatchingHeader {
-                        block_number,
-                        block_hash,
-                    }),
-                }
-            }
-        }
-    }
-}
-
-fn stream_new_blocks<TCentralSource: CentralSourceTrait + Sync + Send>(
-    reader: StorageReader,
-    central_source: Arc<TCentralSource>,
-    block_propation_sleep_duration: Duration,
-) -> impl Stream<Item = Result<SyncEvent, StateSyncError>> {
-    try_stream! {
-        loop {
-            let header_marker = reader.begin_ro_txn()?.get_header_marker()?;
-            let last_block_number = central_source.get_block_marker().await?;
-            if header_marker == last_block_number {
-                debug!("Waiting for more blocks.");
-                tokio::time::sleep(block_propation_sleep_duration).await;
-                continue;
-            }
-            debug!("Downloading blocks [{} - {}).", header_marker, last_block_number);
-            let block_stream =
-                central_source.stream_new_blocks(header_marker, last_block_number).fuse();
-            pin_mut!(block_stream);
-            while let Some(maybe_block) = block_stream.next().await {
-                let (block_number, block) = maybe_block?;
-                yield SyncEvent::BlockAvailable { block_number, block };
-            }
-        }
-    }
-}
-
-fn stream_new_state_diffs<TCentralSource: CentralSourceTrait + Sync + Send>(
-    reader: StorageReader,
-    central_source: Arc<TCentralSource>,
-    block_propation_sleep_duration: Duration,
-) -> impl Stream<Item = Result<SyncEvent, StateSyncError>> {
-    try_stream! {
-        loop {
-            let txn = reader.begin_ro_txn()?;
-            let state_marker = txn.get_state_marker()?;
-            let last_block_number = txn.get_header_marker()?;
-            drop(txn);
-            if state_marker == last_block_number {
-                debug!("Waiting for the block chain to advance.");
-                tokio::time::sleep(block_propation_sleep_duration).await;
-                continue;
-            }
-            debug!("Downloading state diffs [{} - {}).", state_marker, last_block_number);
-            let state_diff_stream =
-                central_source.stream_state_updates(state_marker, last_block_number).fuse();
-            pin_mut!(state_diff_stream);
-
-            while let Some(maybe_state_diff) = state_diff_stream.next().await {
-                let (
-                    block_number,
-                    block_hash,
-                    mut state_diff,
-                    deployed_contract_class_definitions,
-                ) = maybe_state_diff?;
-                sort_state_diff(&mut state_diff);
-                yield SyncEvent::StateDiffAvailable {
-                    block_number,
-                    block_hash,
-                    state_diff,
-                    deployed_contract_class_definitions,
-                };
-            }
-        }
-    }
-}
-
-pub fn sort_state_diff(diff: &mut StateDiff) {
-    diff.declared_classes.sort_unstable_keys();
-    diff.deployed_contracts.sort_unstable_keys();
-    diff.nonces.sort_unstable_keys();
-    diff.storage_diffs.sort_unstable_keys();
-    for storage_entries in diff.storage_diffs.values_mut() {
-        storage_entries.sort_unstable_keys();
     }
 }
 
