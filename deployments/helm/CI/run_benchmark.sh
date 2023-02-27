ad0b0f9a3ef8def62a5502e9382873707638595c
eranreshef-starkware <eran@starkware.co>
2023-02-27 14:35:38 +0200

    wait for load-test to finish

--- deployments/helm/CI/run_benchmark.sh
+++ deployments/helm/CI/run_benchmark.sh
@@ -9,6 +9,14 @@ TMP_FILE="${BASE_PATH}/load_test_job-${BUILD_ID}.yaml"
 cp "${BASE_PATH}/load_test_job.yaml.tmpl" "${TMP_FILE}"
 sed -i "s/XXX/$BUILD_ID/g" "${TMP_FILE}"
 sed -i "s/DURRATION/$DURRATION/g" "${TMP_FILE}"
-kubectl --namespace papyrus apply -f "${TMP_FILE}" --wait
+kubectl --namespace papyrus apply -f "${TMP_FILE}" --wait=true
 
+#wait for load-test pod to start
+kubectl wait --namespace=papyrus --for=condition=ready pod -l job-name=papyrus-"$BUILD_ID"-load-test
+
+# wait additional 10 seconds in order to let the actual load-test application to start
+sleep 10s
+
+# wait $DURRATION for the load-test application to finish
+kubectl wait --namespace=papyrus --for=condition=complete job/papyrus-"${BUILD_ID}"-load-test --timeout "$DURRATION"
 rm "${TMP_FILE}"
