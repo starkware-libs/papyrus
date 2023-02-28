0fa73a7ce69d4184a3c4732ba2a30bf41bcf2c52
DvirYo-starkware <115620476+DvirYo-starkware@users.noreply.github.com>
2023-02-28 14:09:10 +0200

    Fix create request for get block by number. (#524)
    * Fix create request for get block by number.
    
    * Fix the unreported error.

--- crates/papyrus_load_test/src/create_request.rs
+++ crates/papyrus_load_test/src/create_request.rs
@@ -3,7 +3,10 @@ use serde_json::{json, Value as jsonVal};
 use crate::jsonrpc_request;
 
 pub fn get_block_with_tx_hashes_by_number(block_number: &str) -> jsonVal {
-    jsonrpc_request("starknet_getBlockWithTxHashes", json!([{ "block_number": block_number }]))
+    jsonrpc_request(
+        "starknet_getBlockWithTxHashes",
+        json!([{ "block_number": block_number.parse::<u64>().unwrap() }]),
+    )
 }
 
 pub fn get_block_with_tx_hashes_by_hash(block_hash: &str) -> jsonVal {
--- crates/papyrus_load_test/src/lib.rs
+++ crates/papyrus_load_test/src/lib.rs
@@ -1,15 +1,20 @@
 pub mod create_request;
 pub mod scenarios;
 pub mod transactions;
-
 use goose::goose::{GooseUser, TransactionError};
+use serde::Deserialize;
 use serde_json::{json, Value as jsonVal};
 type PostResult = Result<jsonVal, Box<TransactionError>>;
 
 pub async fn post_jsonrpc_request(user: &mut GooseUser, request: &jsonVal) -> PostResult {
     let response = user.post_json("", request).await?.response.map_err(|e| Box::new(e.into()))?;
-    let response = response.json::<jsonVal>().await.map_err(|e| Box::new(e.into()))?;
-    Ok(response)
+    #[derive(Deserialize)]
+    struct TransactionReceiptResponse {
+        result: jsonVal,
+    }
+    let response =
+        response.json::<TransactionReceiptResponse>().await.map_err(|e| Box::new(e.into()))?;
+    Ok(response.result)
 }
 
 pub fn jsonrpc_request(method: &str, params: jsonVal) -> jsonVal {
