4994dd1fd8a70b1df0e2bfdb7120c3f6756554e8
Dvir Yosef <dvir@starkware.co>
2023-03-01 11:38:13 +0200

    Add ArgsIter to create_request.

--- crates/papyrus_load_test/src/create_request.rs
+++ crates/papyrus_load_test/src/create_request.rs
@@ -1,11 +1,33 @@
 use serde_json::{json, Value as jsonVal};
+use std::str::SplitWhitespace;
 
 use crate::jsonrpc_request;
 
+struct ArgsIter<'a>{
+    iter: SplitWhitespace<'a>,
+}
+
+impl<'a> ArgsIter<'a>{
+    fn new(args: &'a str) -> Self{
+        ArgsIter { iter: args.split_whitespace() }
+    }
+
+    fn next_str(&mut self) -> &str{
+        self.iter.next().unwrap()
+    }
+
+    fn next_u64(&mut self) ->u64{
+        self.iter.next().unwrap().parse::<u64>().unwrap()
+    }
+}
+
 pub fn get_block_with_transaction_hashes_by_number(args: &str) -> jsonVal {
-    jsonrpc_request("starknet_getBlockWithTxHashes", json!([{ "block_number": args.parse::<u64>().unwrap() }]))
+    let mut iter=ArgsIter::new(args);
+    jsonrpc_request("starknet_getBlockWithTxHashes", json!([{ "block_number": iter.next_u64() }]))
 }
 
 pub fn get_block_with_transaction_hashes_by_hash(args: &str) -> jsonVal {
-    jsonrpc_request("starknet_getBlockWithTxHashes", json!([{ "block_hash": args }]))
+    let mut iter=ArgsIter::new(args);
+    jsonrpc_request("starknet_getBlockWithTxHashes", json!([{ "block_hash": iter.next_str() }]))
 }
+
