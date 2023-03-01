d5f413817e8234fdec761081b3738f08c651c062
Dvir Yosef <dvir@starkware.co>
2023-03-01 11:42:24 +0200

    Format changes.

--- crates/papyrus_load_test/src/create_request.rs
+++ crates/papyrus_load_test/src/create_request.rs
@@ -1,33 +1,33 @@
-use serde_json::{json, Value as jsonVal};
 use std::str::SplitWhitespace;
+
+use serde_json::{json, Value as jsonVal};
 
 use crate::jsonrpc_request;
 
-struct ArgsIter<'a>{
+struct ArgsIter<'a> {
     iter: SplitWhitespace<'a>,
 }
 
-impl<'a> ArgsIter<'a>{
-    fn new(args: &'a str) -> Self{
+impl<'a> ArgsIter<'a> {
+    fn new(args: &'a str) -> Self {
         ArgsIter { iter: args.split_whitespace() }
     }
 
-    fn next_str(&mut self) -> &str{
+    fn next_str(&mut self) -> &str {
         self.iter.next().unwrap()
     }
 
-    fn next_u64(&mut self) ->u64{
+    fn next_u64(&mut self) -> u64 {
         self.iter.next().unwrap().parse::<u64>().unwrap()
     }
 }
 
 pub fn get_block_with_transaction_hashes_by_number(args: &str) -> jsonVal {
-    let mut iter=ArgsIter::new(args);
+    let mut iter = ArgsIter::new(args);
     jsonrpc_request("starknet_getBlockWithTxHashes", json!([{ "block_number": iter.next_u64() }]))
 }
 
 pub fn get_block_with_transaction_hashes_by_hash(args: &str) -> jsonVal {
-    let mut iter=ArgsIter::new(args);
+    let mut iter = ArgsIter::new(args);
     jsonrpc_request("starknet_getBlockWithTxHashes", json!([{ "block_hash": iter.next_str() }]))
 }
-
--- crates/papyrus_load_test/src/scenarios.rs
+++ crates/papyrus_load_test/src/scenarios.rs
@@ -4,13 +4,13 @@ use goose::scenario;
 use crate::transactions::*;
 
 pub fn general_request() -> Scenario {
-    let mut scenario= scenario!("general_request");
-    let trans_and_weights=vec![
+    let mut scenario = scenario!("general_request");
+    let trans_and_weights = vec![
         (get_block_with_transaction_hashes_by_number(), 1),
-        (get_block_with_transaction_hashes_by_hash(),   1),
+        (get_block_with_transaction_hashes_by_hash(), 1),
     ];
-    for (transaction, weight) in trans_and_weights.into_iter(){
-        scenario=scenario.register_transaction(transaction.set_weight(weight).unwrap());
+    for (transaction, weight) in trans_and_weights.into_iter() {
+        scenario = scenario.register_transaction(transaction.set_weight(weight).unwrap());
     }
     scenario
 }
