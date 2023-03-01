12cb291b5019864211ebcc0ee627bde9b40c0298
Dvir Yosef <dvir@starkware.co>
2023-03-01 16:05:24 +0200

    use transactions module in scenarios as txs.

--- crates/papyrus_load_test/src/scenarios.rs
+++ crates/papyrus_load_test/src/scenarios.rs
@@ -1,16 +1,16 @@
 use goose::goose::Scenario;
 use goose::scenario;
 
-use crate::transactions::*;
+use crate::transactions as txs;
 
 pub fn general_request() -> Scenario {
     let mut scenario = scenario!("general_request");
     let trans_and_weights = vec![
-        (get_block_with_transaction_hashes_by_number(), 1),
-        (get_block_with_transaction_hashes_by_hash(), 1),
-        (block_number(), 1),
-        (block_hash_and_number(), 1),
-        (chain_id(), 1),
+        (txs::get_block_with_transaction_hashes_by_number(), 1),
+        (txs::get_block_with_transaction_hashes_by_hash(), 1),
+        (txs::block_number(), 1),
+        (txs::block_hash_and_number(), 1),
+        (txs::chain_id(), 1),
     ];
     for (transaction, weight) in trans_and_weights.into_iter() {
         scenario = scenario.register_transaction(transaction.set_weight(weight).unwrap());
