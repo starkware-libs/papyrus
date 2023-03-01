c87706df0047920b58a09b5a8e3cc2c82d64ad98
Dvir Yosef <dvir@starkware.co>
2023-03-01 11:31:44 +0200

    Register transaction in loop.

--- crates/papyrus_load_test/src/scenarios.rs
+++ crates/papyrus_load_test/src/scenarios.rs
@@ -1,14 +1,16 @@
 use goose::goose::Scenario;
 use goose::scenario;
 
-use crate::transactions;
+use crate::transactions::*;
 
 pub fn general_request() -> Scenario {
-    scenario!("general_request")
-        .register_transaction(
-            transactions::get_block_with_transaction_hashes_by_number().set_weight(1).unwrap(),
-        )
-        .register_transaction(
-            transactions::get_block_with_transaction_hashes_by_hash().set_weight(1).unwrap(),
-        )
+    let mut scenario= scenario!("general_request");
+    let trans_and_weights=vec![
+        (get_block_with_transaction_hashes_by_number(), 1),
+        (get_block_with_transaction_hashes_by_hash(),   1),
+    ];
+    for (transaction, weight) in trans_and_weights.into_iter(){
+        scenario=scenario.register_transaction(transaction.set_weight(weight).unwrap());
+    }
+    scenario
 }
