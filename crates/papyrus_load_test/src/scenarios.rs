d72b50f09226a55c1fe71892f4bb71702a0badb5
Dvir Yosef <dvir@starkware.co>
2023-03-01 14:45:13 +0200

    Fix reviewer comments and format.

--- crates/papyrus_load_test/src/scenarios.rs
+++ crates/papyrus_load_test/src/scenarios.rs
@@ -4,16 +4,20 @@ use goose::scenario;
 use crate::transactions::*;
 
 pub fn general_request() -> Scenario {
-    let mut scenario= scenario!("general_request");
-    let trans_and_weights=vec![
+    let mut scenario = scenario!("general_request");
+    let trans_and_weights = vec![
         (get_block_with_transaction_hashes_by_number(), 1),
+<<<<<<< HEAD
         (get_block_with_transaction_hashes_by_hash(),   1),
         (block_number(),1),
         (block_hash_and_number(),1 ),
         (chain_id(), 1),
+=======
+        (get_block_with_transaction_hashes_by_hash(), 1),
+>>>>>>> Fix reviewer comments and format.
     ];
-    for (transaction, weight) in trans_and_weights.into_iter(){
-        scenario=scenario.register_transaction(transaction.set_weight(weight).unwrap());
+    for (transaction, weight) in trans_and_weights.into_iter() {
+        scenario = scenario.register_transaction(transaction.set_weight(weight).unwrap());
     }
     scenario
 }
