9a94c586e71c162050a98bdbd6ae353579b9f787
Dvir Yosef <dvir@starkware.co>
2023-03-01 15:44:50 +0200

    Rebase fix.

--- crates/papyrus_load_test/src/scenarios.rs
+++ crates/papyrus_load_test/src/scenarios.rs
@@ -7,14 +7,10 @@ pub fn general_request() -> Scenario {
     let mut scenario = scenario!("general_request");
     let trans_and_weights = vec![
         (get_block_with_transaction_hashes_by_number(), 1),
-<<<<<<< HEAD
-        (get_block_with_transaction_hashes_by_hash(),   1),
-        (block_number(),1),
-        (block_hash_and_number(),1 ),
-        (chain_id(), 1),
-=======
         (get_block_with_transaction_hashes_by_hash(), 1),
->>>>>>> Fix reviewer comments and format.
+        (block_number(), 1),
+        (block_hash_and_number(), 1),
+        (chain_id(), 1),
     ];
     for (transaction, weight) in trans_and_weights.into_iter() {
         scenario = scenario.register_transaction(transaction.set_weight(weight).unwrap());
