13e6c0141ce2c3cfff7ad70409fc60d3626d1dd4
DvirYo-starkware <115620476+DvirYo-starkware@users.noreply.github.com>
2023-03-01 15:18:25 +0200

    Add constant gateway requests to the load test. (#518)
    * Add constant gateway requests to the load test.
    
    * Fix reviewer comments.

--- crates/papyrus_load_test/src/scenarios.rs
+++ crates/papyrus_load_test/src/scenarios.rs
@@ -11,4 +11,7 @@ pub fn general_request() -> Scenario {
         .register_transaction(
             transactions::get_block_with_tx_hashes_by_hash().set_weight(1).unwrap(),
         )
+        .register_transaction(transactions::block_number().set_weight(1).unwrap())
+        .register_transaction(transactions::block_hash_and_number().set_weight(1).unwrap())
+        .register_transaction(transactions::chain_id().set_weight(1).unwrap())
 }
