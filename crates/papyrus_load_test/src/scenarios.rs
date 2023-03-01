b9c9e97cdc8646f76b6a51bb436661611cf5f123
Dvir Yosef <dvir@starkware.co>
2023-03-01 11:20:38 +0200

    Naming changes.

--- crates/papyrus_load_test/src/scenarios.rs
+++ crates/papyrus_load_test/src/scenarios.rs
@@ -6,9 +6,9 @@ use crate::transactions;
 pub fn general_request() -> Scenario {
     scenario!("general_request")
         .register_transaction(
-            transactions::get_block_with_tx_hashes_by_number().set_weight(1).unwrap(),
+            transactions::get_block_with_transaction_hashes_by_number().set_weight(1).unwrap(),
         )
         .register_transaction(
-            transactions::get_block_with_tx_hashes_by_hash().set_weight(1).unwrap(),
+            transactions::get_block_with_transaction_hashes_by_hash().set_weight(1).unwrap(),
         )
 }
