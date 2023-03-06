868f02d714ab23c7c16d7907dab22012a2ca831d
Dvir Yosef <dvir@starkware.co>
2023-03-06 16:43:08 +0200

    Using SockedAddr instead of &str to name the node address.

--- crates/papyrus_load_test/src/main.rs
+++ crates/papyrus_load_test/src/main.rs
@@ -2,7 +2,7 @@
 // To run this load test, run locally a node and then run:
 //      cargo run -r -p papyrus_load_test -- -t 5m -H http://127.0.0.1:8080
 // To create the files of requests run:
-//      cargo run -r -p papyrus_load_test -- --create_files http://127.0.0.1:8080
+//      cargo run -r -p papyrus_load_test -- --create_files 127.0.0.1:8080
 // For more options run:
 //      cargo run -r -p papyrus_load_test -- --help
 
