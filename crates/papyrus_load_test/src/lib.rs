6fe80e2d0ab94f2310ee18f9e07e6b3a5d052e4b
Dvir Yosef <dvir@starkware.co>
2023-03-06 15:10:19 +0200

    Load test, create file block_number.

--- crates/papyrus_load_test/src/lib.rs
+++ crates/papyrus_load_test/src/lib.rs
@@ -4,8 +4,8 @@ pub mod scenarios;
 pub mod transactions;
 
 use std::fs;
-
 use goose::goose::{GooseUser, TransactionError};
+use rand::Rng;
 use serde::Deserialize;
 use serde_json::{json, Value as jsonVal};
 
@@ -37,8 +37,12 @@ pub fn jsonrpc_request(method: &str, params: jsonVal) -> jsonVal {
 
 // Returns the last block number for which this load test is relevant.
 pub fn get_last_block_number() -> u64 {
-    fs::read_to_string("crates/papyrus_load_test/src/resources/last_block_number.txt")
-        .unwrap()
-        .parse::<u64>()
-        .unwrap()
+    fs::read_to_string("crates/papyrus_load_test/src/resources/last_block_number.txt").unwrap().parse::<u64>().unwrap()
+}
+
+// Returns a random block from zero to the last block for which this load test is relevant.
+pub fn get_random_block_number() -> u64 {
+    let last_block = get_last_block_number();
+    let mut rng = rand::thread_rng();
+    rng.gen_range(0..=last_block)
 }
