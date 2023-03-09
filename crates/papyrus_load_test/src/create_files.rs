86a47a8f471e389d0c4451654d1dea707157d892
Dvir Yosef <dvir@starkware.co>
2023-03-09 11:56:57 +0200

    Fix reviewer comments.

--- crates/papyrus_load_test/src/create_files.rs
+++ crates/papyrus_load_test/src/create_files.rs
@@ -1,35 +1,38 @@
 use std::fs::File;
-use std::io::{BufWriter, Write};
+use std::io::Write;
 use std::net::SocketAddr;
 
 use test_utils::send_request;
 
-use crate::get_random_block_number;
+use crate::{get_random_block_number, path_to_resources};
 
+const BLOCK_NUMBER_NUMS: u32 = 5;
+
+// Creates the files to run the load test.
 pub async fn create_files(node_address: &str) {
     let node_socket = node_address.parse::<SocketAddr>().unwrap();
     last_block_number(node_socket).await;
-    block_number(5);
+    block_number(BLOCK_NUMBER_NUMS);
 }
 
-// Creates the block_number.txt file. Write to the file lines_num blocks number.
-fn block_number(lines_num: u32) {
-    let file = File::create("crates/papyrus_load_test/src/resources/block_number.txt").unwrap();
-    let mut writer = BufWriter::new(file);
-
-    for _ in 0..lines_num - 1 {
-        writer.write_all(get_random_block_number().to_string().as_bytes()).unwrap();
-        writer.write_all("\n".as_bytes()).unwrap();
+// Creates the block_number.txt file. Write to the file block_num block numbers.
+fn block_number(block_number_nums: u32) {
+    let mut to_write = String::new();
+    for _ in 0..block_number_nums {
+        let block_number = get_random_block_number().to_string();
+        to_write = to_write + &block_number + "\n";
     }
-    writer.write_all(get_random_block_number().to_string().as_bytes()).unwrap();
-    writer.flush().unwrap();
+    // Remove the last '\n'.
+    to_write.pop().expect("to_write String is empty, the block_number_nums is zero.");
+    let mut file = File::create(path_to_resources("block_number.txt"))
+        .expect("Create file \"block_number.txt\" failed.");
+    file.write_all(to_write.as_bytes()).unwrap();
 }
 
 // Creates the file last_block_number.txt. Write to the file the last block number for the load
 // test.
 async fn last_block_number(node_address: SocketAddr) {
-    let last_block_answer = &send_request(node_address, "starknet_blockNumber", "").await["result"];
-    let mut file =
-        File::create("crates/papyrus_load_test/src/resources/last_block_number.txt").unwrap();
-    file.write_all(last_block_answer.to_string().as_bytes()).unwrap();
+    let last_block_number = &send_request(node_address, "starknet_blockNumber", "").await["result"];
+    let mut file = File::create(path_to_resources("last_block_number.txt")).unwrap();
+    file.write_all(last_block_number.to_string().as_bytes()).unwrap();
 }
