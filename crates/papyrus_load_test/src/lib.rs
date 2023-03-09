886ee46d1eb33fdac4e8ff08efab742121a9e4f9
Dvir Yosef <dvir@starkware.co>
2023-03-09 12:43:48 +0200

    Read last_block_file only once.

--- crates/papyrus_load_test/src/lib.rs
+++ crates/papyrus_load_test/src/lib.rs
@@ -6,6 +6,7 @@ pub mod transactions;
 use std::{env, fs};
 
 use goose::goose::{GooseUser, TransactionError};
+use once_cell::sync::OnceCell;
 use rand::Rng;
 use serde::Deserialize;
 use serde_json::{json, Value as jsonVal};
@@ -36,12 +37,15 @@ pub fn jsonrpc_request(method: &str, params: jsonVal) -> jsonVal {
     })
 }
 
+static LAST_BLOCK_NUMBER: OnceCell<u64> = OnceCell::new();
 // Returns the last block number for which this load test is relevant.
 pub fn get_last_block_number() -> u64 {
-    fs::read_to_string("crates/papyrus_load_test/src/resources/last_block_number.txt")
-        .unwrap()
-        .parse::<u64>()
-        .unwrap()
+    *LAST_BLOCK_NUMBER.get_or_init(|| {
+        fs::read_to_string(path_to_resources("last_block_number.txt"))
+            .unwrap()
+            .parse::<u64>()
+            .unwrap()
+    })
 }
 
 // Returns a random block from zero to the last block for which this load test is relevant.
