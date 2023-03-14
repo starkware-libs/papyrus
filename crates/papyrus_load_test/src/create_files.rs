ffc3523ee1297acfde73ae3f0e7425a776c68042
Dvir Yosef <dvir@starkware.co>
2023-03-14 10:49:48 +0200

    Create block_hash file.

--- crates/papyrus_load_test/src/create_files.rs
+++ crates/papyrus_load_test/src/create_files.rs
@@ -1,21 +1,47 @@
 use std::fs::File;
+use std::future::Future;
 use std::io::Write;
 use std::net::SocketAddr;
 
+use serde_json::Value as jsonVal;
 use test_utils::send_request;
 
 use crate::{get_random_block_number, path_in_resources};
 
-// The number of block numbers to write.
-// Currently, this number is random; we will decide how to choose this number
+// Currently, those numbers are random; we will decide how to choose them
 // in the future.
+// The number of block numbers to write.
 const BLOCK_NUMBER_COUNT: u32 = 5;
+// The number of block hashes.
+const BLOCK_HASH_COUNT: u32 = 5;
 
 // Creates the files to run the load test.
 pub async fn create_files(node_address: &str) {
     let node_socket = node_address.parse::<SocketAddr>().unwrap();
     last_block_number(node_socket).await;
     block_number(BLOCK_NUMBER_COUNT);
+    create_file("block_hash.txt", BLOCK_HASH_COUNT, || get_block_hash_args(node_socket)).await;
+}
+
+// Write to file_name, arg_sets_num of arguments that return from get_args function.
+pub async fn create_file<Fut>(file_name: &str, arg_sets_num: u32, get_args: impl Fn() -> Fut)
+where
+    Fut: Future<Output = Vec<String>>,
+{
+    let mut to_write = String::new();
+    for _ in 0..arg_sets_num {
+        for arg in get_args().await {
+            to_write.push_str(&arg);
+            to_write.push(' ');
+        }
+        to_write.pop().unwrap();
+        to_write.push('\n');
+    }
+    // Remove the last '\n'.
+    to_write.pop().unwrap();
+    let mut file =
+        File::create(path_in_resources(file_name)).expect("Create file \"{file_name}\" failed.");
+    file.write_all(to_write.as_bytes()).unwrap();
 }
 
 // Creates the block_number.txt file. Write to the file block_number_count random block numbers.
@@ -30,6 +56,25 @@ fn block_number(block_number_count: u32) {
     let mut file = File::create(path_in_resources("block_number.txt"))
         .expect("Create file \"block_number.txt\" failed.");
     file.write_all(to_write.as_bytes()).unwrap();
+}
+
+pub async fn get_block_with_tx_hashes(node_address: SocketAddr, block_number: u64) -> jsonVal {
+    let params = format!("{{ \"block_number\": {block_number} }}");
+    send_request(node_address, "starknet_getBlockWithTxHashes", &params).await
+}
+
+// Returns a vector with a random block hash.
+pub async fn get_block_hash_args(node_address: SocketAddr) -> Vec<String> {
+    let block_number = get_random_block_number();
+    let response =
+        &get_block_with_tx_hashes(node_address, block_number).await["result"]["block_hash"];
+    let block_hash = match response {
+        jsonVal::String(block_hash) => block_hash,
+        _ => {
+            panic!("No block hash in the given response")
+        }
+    };
+    vec![block_hash.to_string()]
 }
 
 // Creates the file last_block_number.txt. Write to the file the last block number for the load
