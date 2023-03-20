ec38a934587a37ac05597040825051843a2fd473
dan-starkware <56217775+dan-starkware@users.noreply.github.com>
2023-03-20 10:08:09 +0200

    Fix monitoring GW tests. (#560)

--- crates/papyrus_monitoring_gateway/src/gateway_test.rs
+++ crates/papyrus_monitoring_gateway/src/gateway_test.rs
@@ -7,30 +7,33 @@ use super::JsonRpcServerImpl;
 const TEST_CONFIG_REPRESENTATION: &str = "general_config_representation";
 
 #[tokio::test]
-async fn test_stats() -> Result<(), anyhow::Error> {
+async fn test_stats() {
     let (storage_reader, mut _storage_writer) = test_utils::get_test_storage();
     let module = JsonRpcServerImpl {
         storage_reader,
-        general_config_representation: serde_yaml::to_value(TEST_CONFIG_REPRESENTATION)?,
+        general_config_representation: serde_yaml::to_value(TEST_CONFIG_REPRESENTATION).unwrap(),
     }
     .into_rpc();
-    let stats =
-        module.call::<_, DbTablesStats>("starknet_dbTablesStats", EmptyParams::new()).await?;
+    let stats = module
+        .call::<_, DbTablesStats>("starknet_dbTablesStats", EmptyParams::new())
+        .await
+        .expect("Monitoring gateway should respond with DB table statistics");
     for &name in table_names() {
         assert!(stats.stats.contains_key(name))
     }
-    Ok(())
 }
 
 #[tokio::test]
-async fn test_config() -> Result<(), anyhow::Error> {
+async fn test_config() {
     let (storage_reader, mut _storage_writer) = test_utils::get_test_storage();
     let module = JsonRpcServerImpl {
         storage_reader,
-        general_config_representation: serde_yaml::to_value(TEST_CONFIG_REPRESENTATION)?,
+        general_config_representation: serde_yaml::to_value(TEST_CONFIG_REPRESENTATION).unwrap(),
     }
     .into_rpc();
-    let rep = module.call::<_, String>("starknet_nodeConfig", EmptyParams::new()).await?;
+    let rep = module
+        .call::<_, String>("starknet_nodeConfig", EmptyParams::new())
+        .await
+        .expect("Monitoring gateway should respond the node configuration");
     assert_eq!(rep, TEST_CONFIG_REPRESENTATION);
-    Ok(())
 }
