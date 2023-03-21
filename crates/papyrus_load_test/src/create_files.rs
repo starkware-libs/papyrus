794843c384fba993e10ba7714c44ee805246f74e
DvirYo-starkware <115620476+DvirYo-starkware@users.noreply.github.com>
2023-03-21 11:09:25 +0200

    Dvir/create block hash file and general changes in files (#548)
    * Create block_hash file.
    
    * Use the general way for block_hash file also for the block_number file.
    
    * Create_file function better explenation.
    
    * Reorder create_files functions by importance.
    
    * Better documentation to create_file function.
    
    * Fix comment.

--- crates/papyrus_load_test/src/create_files.rs
+++ crates/papyrus_load_test/src/create_files.rs
@@ -1,35 +1,56 @@
 use std::fs::File;
+use std::future::Future;
 use std::io::Write;
 use std::net::SocketAddr;
 
+use serde_json::Value as jsonVal;
 use test_utils::send_request;
 
 use crate::{get_random_block_number, path_in_resources};
 
-// The number of block numbers to write.
-// Currently, this number is random; we will decide how to choose this number
+// Currently, those numbers are random; we will decide how to choose them
 // in the future.
+// The number of block numbers to write.
 const BLOCK_NUMBER_COUNT: u32 = 5;
+// The number of block hashes.
+const BLOCK_HASH_COUNT: u32 = 5;
 
 // Creates the files to run the load test.
 pub async fn create_files(node_address: &str) {
     let node_socket = node_address.parse::<SocketAddr>().unwrap();
     last_block_number(node_socket).await;
-    block_number(BLOCK_NUMBER_COUNT);
+    create_file("block_number.txt", BLOCK_NUMBER_COUNT, get_block_number_args).await;
+    create_file("block_hash.txt", BLOCK_HASH_COUNT, || get_block_hash_args(node_socket)).await;
 }
 
-// Creates the block_number.txt file. Write to the file block_number_count random block numbers.
-fn block_number(block_number_count: u32) {
+// Write to a file lines with parameters to requests.
+// - file_name: the file to write to.
+// - params_set_count: the number of lines with parameters to write to the file.
+// - get_params: a function that returns a vector with parameters to a request. The use of Fn is to
+//   enable closure, and the reason get_args is async is that creating the parameters is IO bound.
+pub async fn create_file<Fut>(file_name: &str, param_set_count: u32, get_params: impl Fn() -> Fut)
+where
+    Fut: Future<Output = Vec<String>>,
+{
     let mut to_write = String::new();
-    for _ in 0..block_number_count {
-        let block_number = get_random_block_number().to_string();
-        to_write = to_write + &block_number + "\n";
+    for _ in 0..param_set_count {
+        for arg in get_params().await {
+            to_write.push_str(&arg);
+            to_write.push(' ');
+        }
+        to_write.pop().unwrap();
+        to_write.push('\n');
     }
     // Remove the last '\n'.
-    to_write.pop().expect("to_write String is empty, the block_number_nums is zero.");
-    let mut file = File::create(path_in_resources("block_number.txt"))
-        .expect("Create file \"block_number.txt\" failed.");
+    to_write.pop().unwrap();
+    let mut file =
+        File::create(path_in_resources(file_name)).expect("Create file \"{file_name}\" failed.");
     file.write_all(to_write.as_bytes()).unwrap();
+}
+
+pub async fn get_block_with_tx_hashes(node_address: SocketAddr, block_number: u64) -> jsonVal {
+    let params = format!("{{ \"block_number\": {block_number} }}");
+    send_request(node_address, "starknet_getBlockWithTxHashes", &params).await
 }
 
 // Creates the file last_block_number.txt. Write to the file the last block number for the load
@@ -38,4 +59,23 @@ async fn last_block_number(node_address: SocketAddr) {
     let last_block_number = &send_request(node_address, "starknet_blockNumber", "").await["result"];
     let mut file = File::create(path_in_resources("last_block_number.txt")).unwrap();
     file.write_all(last_block_number.to_string().as_bytes()).unwrap();
+}
+
+// Returns a vector with a random block number.
+pub async fn get_block_number_args() -> Vec<String> {
+    vec![get_random_block_number().to_string()]
+}
+
+// Returns a vector with a random block hash.
+pub async fn get_block_hash_args(node_address: SocketAddr) -> Vec<String> {
+    let block_number = get_random_block_number();
+    let response =
+        &get_block_with_tx_hashes(node_address, block_number).await["result"]["block_hash"];
+    let block_hash = match response {
+        jsonVal::String(block_hash) => block_hash,
+        _ => {
+            panic!("No block hash in the given response")
+        }
+    };
+    vec![block_hash.to_string()]
 }
