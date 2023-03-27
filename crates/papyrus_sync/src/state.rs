09b217d19da6d11a880883afe7f49b6acc1c7013
anatg <anatg@starkware.co>
2023-03-27 13:29:29 +0300

    Move shared code of state_diff_sync and block_sync to a separate module

--- crates/papyrus_sync/src/state.rs
+++ crates/papyrus_sync/src/state.rs
@@ -5,180 +5,66 @@ mod state_test;
 use std::sync::Arc;
 use std::time::Duration;
 
-use indexmap::IndexMap;
-use papyrus_storage::db::RW;
+use async_trait::async_trait;
+use papyrus_storage::db::{RO, RW};
 use papyrus_storage::header::HeaderStorageReader;
 use papyrus_storage::state::{StateStorageReader, StateStorageWriter};
 use papyrus_storage::{StorageReader, StorageTxn};
-use starknet_api::block::{BlockHash, BlockNumber};
-use starknet_api::core::ClassHash;
-use starknet_api::state::{ContractClass, StateDiff};
-use tokio::sync::mpsc;
-use tokio::task::JoinHandle;
-use tracing::{debug, info, trace, warn};
+use starknet_api::block::BlockNumber;
+use starknet_api::state::StateDiff;
 
 use crate::data::StateDiffSyncData;
-use crate::downloads_manager::DownloadsManager;
 use crate::sources::CentralSourceTrait;
+use crate::sync::SyncExtensionTrait;
 use crate::{StateSyncError, StateSyncResult, SyncConfig};
 
-pub struct StateDiffSync<TCentralSource: CentralSourceTrait + Sync + Send> {
-    config: SyncConfig,
-    reader: StorageReader,
-    task: JoinHandle<()>,
-    downloads_manager: DownloadsManager<TCentralSource, StateDiffSyncData>,
-    last_state_diff_received: BlockNumber,
-}
-
-impl<TCentralSource: CentralSourceTrait + Sync + Send + 'static> StateDiffSync<TCentralSource> {
-    pub fn new(
-        config: SyncConfig,
-        central_source: Arc<TCentralSource>,
-        reader: StorageReader,
-    ) -> Result<Self, StateSyncError> {
-        let (sender, receiver) = mpsc::channel(200);
-        let state_marker = reader.begin_ro_txn()?.get_state_marker()?;
-        let downloads_manager =
-            DownloadsManager::new(central_source, 10, 100, receiver, state_marker);
-        let task = run_stream_new_state_diffs(config, reader.clone(), sender);
-        Ok(StateDiffSync {
-            config,
-            reader,
-            task,
-            downloads_manager,
-            last_state_diff_received: state_marker,
-        })
+pub struct StateDiffSync {}
+#[async_trait]
+impl<T: CentralSourceTrait + Sync + Send + 'static> SyncExtensionTrait<T, StateDiffSyncData>
+    for StateDiffSync
+{
+    fn get_from(reader: &StorageReader) -> Result<BlockNumber, StateSyncError> {
+        Ok(reader.begin_ro_txn()?.get_state_marker()?)
     }
 
-    pub fn step(&mut self, txn: StorageTxn<'_, RW>) -> StateSyncResult {
-        // Check if there was a revert.
-        let state_marker = self.reader.begin_ro_txn()?.get_state_marker()?;
-        if state_marker < self.last_state_diff_received {
-            info!("Restart state diff sync because of block {state_marker}.");
-            return self.restart();
-        }
-
-        let res = self.downloads_manager.step();
-        if let Err(err) = res {
-            warn!("{}", err);
-            return self.restart();
-        }
-
-        if let Some(StateDiffSyncData {
-            block_number,
-            block_hash,
-            state_diff,
-            deployed_contract_class_definitions,
-        }) = res.unwrap()
-        {
-            return self.store_state_diff(
-                txn,
-                block_number,
-                block_hash,
-                state_diff,
-                deployed_contract_class_definitions,
-            );
-        }
-
-        Ok(())
+    async fn get_range(
+        txn: StorageTxn<'_, RO>,
+        _source: Arc<T>,
+    ) -> Result<(BlockNumber, BlockNumber), StateSyncError> {
+        Ok((txn.get_state_marker()?, txn.get_header_marker()?))
     }
 
-    pub fn restart(&mut self) -> Result<(), StateSyncError> {
-        info!("Restarting state diff sync");
-        self.task.abort();
-        self.downloads_manager.drop();
-
-        let (sender, receiver) = mpsc::channel(200);
-        let state_marker = self.reader.begin_ro_txn()?.get_state_marker()?;
-        self.last_state_diff_received = state_marker;
-        self.downloads_manager.reset(receiver, state_marker);
-
-        self.task = run_stream_new_state_diffs(self.config, self.reader.clone(), sender);
-        Ok(())
+    fn get_sleep_duration(_config: SyncConfig) -> Duration {
+        Duration::from_millis(10)
     }
 
-    fn store_state_diff(
-        &mut self,
-        txn: StorageTxn<'_, RW>,
-        block_number: BlockNumber,
-        block_hash: BlockHash,
-        mut state_diff: StateDiff,
-        deployed_contract_class_definitions: IndexMap<ClassHash, ContractClass>,
-    ) -> StateSyncResult {
-        trace!("StateDiff data: {state_diff:#?}");
-
-        if self.should_store(block_number, block_hash)? {
-            info!("Storing state diff of block {block_number} with hash {block_hash}.");
-            sort_state_diff(&mut state_diff);
-            txn.append_state_diff(block_number, state_diff, deployed_contract_class_definitions)?
-                .commit()?;
-            self.last_state_diff_received = block_number;
-        } else {
-            info!("Restart state diff sync because of block {block_number}.");
-            self.restart()?;
-        }
-
-        Ok(())
+    fn store(txn: StorageTxn<'_, RW>, mut data: StateDiffSyncData) -> StateSyncResult {
+        sort_state_diff(&mut data.state_diff);
+        Ok(txn
+            .append_state_diff(
+                data.block_number,
+                data.state_diff,
+                data.deployed_contract_class_definitions,
+            )?
+            .commit()?)
     }
 
     fn should_store(
-        &self,
-        block_number: BlockNumber,
-        block_hash: BlockHash,
+        reader: &StorageReader,
+        data: &StateDiffSyncData,
     ) -> Result<bool, StateSyncError> {
-        let txn = self.reader.begin_ro_txn()?;
-        if let Some(storage_header) = txn.get_block_header(block_number)? {
-            if storage_header.block_hash == block_hash {
+        let txn = reader.begin_ro_txn()?;
+        if let Some(storage_header) = txn.get_block_header(data.block_number)? {
+            if storage_header.block_hash == data.block_hash {
                 return Ok(true);
             }
         }
 
         Ok(false)
     }
-}
 
-fn run_stream_new_state_diffs(
-    config: SyncConfig,
-    reader: StorageReader,
-    sender: mpsc::Sender<BlockNumber>,
-) -> JoinHandle<()> {
-    let task = async move {
-        if let Err(err) = stream_new_state_diffs(reader, sender).await {
-            warn!("{}", err);
-            tokio::time::sleep(config.recoverable_error_sleep_duration).await;
-        }
-    };
-
-    tokio::spawn(task)
-}
-
-async fn stream_new_state_diffs(
-    reader: StorageReader,
-    sender: mpsc::Sender<BlockNumber>,
-) -> Result<(), StateSyncError> {
-    let mut last_sent = reader.begin_ro_txn()?.get_state_marker()?;
-    loop {
-        let txn = reader.begin_ro_txn()?;
-        let state_marker = txn.get_state_marker()?;
-        let last_block_number = txn.get_header_marker()?;
-        drop(txn);
-        if state_marker == last_block_number {
-            trace!("Stored all state diffs - waiting for the block chain to advance.");
-            tokio::time::sleep(Duration::from_millis(10)).await; // TODO(anatg): Add to config file.
-            continue;
-        }
-
-        if last_sent >= last_block_number {
-            trace!("Sent last range update - waiting for the block chain to advance.");
-            tokio::time::sleep(Duration::from_millis(10)).await;
-            continue;
-        }
-
-        debug!("Sending upto {}.", last_block_number);
-        sender.send(last_block_number).await.map_err(|e| StateSyncError::Channel {
-            msg: format!("Problem with sending upto {last_block_number}: {e}."),
-        })?;
-        last_sent = last_block_number;
+    fn revert_if_necessary(_txn: StorageTxn<'_, RW>, _data: &StateDiffSyncData) -> StateSyncResult {
+        Ok(())
     }
 }
 
---
+++ crates/papyrus_sync/src/sync.rs
@@ -0,0 +1,167 @@
+use std::marker::PhantomData;
+use std::sync::Arc;
+use std::time::Duration;
+
+use async_trait::async_trait;
+use papyrus_storage::db::{RO, RW};
+use papyrus_storage::{StorageReader, StorageTxn};
+use starknet_api::block::BlockNumber;
+use tokio::sync::mpsc;
+use tokio::task::JoinHandle;
+use tracing::{debug, info, trace, warn};
+
+use crate::data::SyncDataTrait;
+use crate::downloads_manager::DownloadsManager;
+use crate::sources::CentralSourceTrait;
+use crate::{StateSyncError, StateSyncResult, SyncConfig};
+
+pub struct SingleDataTypeSync<T, D, S>
+where
+    T: CentralSourceTrait + Sync + Send,
+    D: SyncDataTrait,
+    S: SyncExtensionTrait<T, D>,
+{
+    sync_ext: PhantomData<S>,
+    config: SyncConfig,
+    source: Arc<T>,
+    reader: StorageReader,
+    task: JoinHandle<()>,
+    downloads_manager: DownloadsManager<T, D>,
+    last_received: BlockNumber,
+}
+
+impl<T, D, S> SingleDataTypeSync<T, D, S>
+where
+    T: CentralSourceTrait + Sync + Send + 'static,
+    D: SyncDataTrait,
+    S: SyncExtensionTrait<T, D>,
+{
+    pub fn new(
+        config: SyncConfig,
+        source: Arc<T>,
+        reader: StorageReader,
+    ) -> Result<Self, StateSyncError> {
+        let (sender, receiver) = mpsc::channel(200);
+        let marker = S::get_from(&reader)?;
+        let downloads_manager = DownloadsManager::new(source.clone(), 10, 100, receiver, marker);
+        let task = Self::run_update_range(config, source.clone(), reader.clone(), sender);
+        Ok(Self {
+            sync_ext: PhantomData::<S>,
+            config,
+            source,
+            reader,
+            task,
+            downloads_manager,
+            last_received: marker,
+        })
+    }
+
+    pub fn step(&mut self, txn: StorageTxn<'_, RW>) -> StateSyncResult {
+        // Check if there was a revert.
+        let marker = S::get_from(&self.reader)?;
+        if marker < self.last_received {
+            info!("Restart {:?} sync because of block {marker}.", D::r#type());
+            return self.restart();
+        }
+
+        let res = self.downloads_manager.step();
+        if let Err(err) = res {
+            warn!("{}", err);
+            return self.restart();
+        }
+
+        if let Some(data) = res.unwrap() {
+            trace!("Storing data: {data:#?}");
+            let block_number = data.block_number();
+            let block_hash = data.block_hash();
+            if S::should_store(&self.reader, &data)? {
+                info!(
+                    "Storing {:?} data of block {block_number} with hash {block_hash}.",
+                    D::r#type()
+                );
+                S::store(txn, data)?;
+                self.last_received = block_number;
+            } else {
+                S::revert_if_necessary(txn, &data)?;
+                self.restart()?;
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn restart(&mut self) -> Result<(), StateSyncError> {
+        info!("Restarting {:?} sync", D::r#type());
+        self.task.abort();
+        self.downloads_manager.drop();
+
+        let (sender, receiver) = mpsc::channel(200);
+        let marker = S::get_from(&self.reader)?;
+        self.last_received = marker;
+        self.downloads_manager.reset(receiver, marker);
+
+        self.task =
+            Self::run_update_range(self.config, self.source.clone(), self.reader.clone(), sender);
+        Ok(())
+    }
+
+    fn run_update_range(
+        config: SyncConfig,
+        source: Arc<T>,
+        reader: StorageReader,
+        sender: mpsc::Sender<BlockNumber>,
+    ) -> JoinHandle<()> {
+        let task = async move {
+            if let Err(err) = Self::update_range(config, source, reader, sender).await {
+                warn!("{}", err);
+                tokio::time::sleep(config.recoverable_error_sleep_duration).await;
+            }
+        };
+
+        tokio::spawn(task)
+    }
+
+    async fn update_range(
+        config: SyncConfig,
+        source: Arc<T>,
+        reader: StorageReader,
+        sender: mpsc::Sender<BlockNumber>,
+    ) -> Result<(), StateSyncError> {
+        let (mut last_sent, _) = S::get_range(reader.begin_ro_txn()?, source.clone()).await?;
+        loop {
+            let (marker, last_block_number) =
+                S::get_range(reader.begin_ro_txn()?, source.clone()).await?;
+
+            if marker == last_block_number {
+                trace!("[{:?}]: Stored all data - waiting for more blocks.", D::r#type());
+                tokio::time::sleep(S::get_sleep_duration(config)).await;
+                continue;
+            }
+
+            if last_sent >= last_block_number {
+                trace!("[{:?}]: Sent last range update - waiting for more blocks.", D::r#type());
+                tokio::time::sleep(S::get_sleep_duration(config)).await;
+                continue;
+            }
+
+            debug!("Sending upto {last_block_number} for {:?} sync.", D::r#type());
+            sender.send(last_block_number).await.map_err(|e| StateSyncError::Channel {
+                msg: format!("Problem with sending upto {last_block_number}: {e}."),
+            })?;
+            last_sent = last_block_number;
+        }
+    }
+}
+
+#[async_trait]
+pub trait SyncExtensionTrait<T: CentralSourceTrait + Sync + Send, D: SyncDataTrait> {
+    fn get_from(reader: &StorageReader) -> Result<BlockNumber, StateSyncError>;
+    fn store(txn: StorageTxn<'_, RW>, data: D) -> StateSyncResult;
+    fn should_store(reader: &StorageReader, data: &D) -> Result<bool, StateSyncError>;
+    fn revert_if_necessary(txn: StorageTxn<'_, RW>, data: &D) -> StateSyncResult;
+    async fn get_range(
+        txn: StorageTxn<'_, RO>,
+        source: Arc<T>,
+    ) -> Result<(BlockNumber, BlockNumber), StateSyncError>;
+    fn get_sleep_duration(config: SyncConfig) -> Duration;
+}
