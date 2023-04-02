42470147c5fd1b72ff9549164069b916a5bc7d87
Yair Bakalchuk <yair@starkware.co>
2023-04-02 17:36:20 +0300

    Central integration test on problematic blocks

--- crates/papyrus_node/src/bin/central_source_integration_test.rs
+++ crates/papyrus_node/src/bin/central_source_integration_test.rs
@@ -12,9 +12,9 @@ async fn main() {
     ])
     .expect("Load config");
     let central_source = CentralSource::new(config.central).expect("Create new client");
-    let last_block_number = BlockNumber(203);
+    let last_block_number = BlockNumber(283414);
 
-    let mut block_marker = BlockNumber(200);
+    let mut block_marker = BlockNumber(283410);
     let block_stream = central_source.stream_new_blocks(block_marker, last_block_number).fuse();
     pin_mut!(block_stream);
     while let Some(Ok((block_number, _block))) = block_stream.next().await {
@@ -26,7 +26,7 @@ async fn main() {
     }
     assert!(block_marker == last_block_number);
 
-    let mut state_marker = BlockNumber(200);
+    let mut state_marker = BlockNumber(283410);
     let header_stream = central_source.stream_state_updates(state_marker, last_block_number).fuse();
     pin_mut!(header_stream);
     while let Some(Ok((block_number, _block_hash, _state_difff, _deployed_classes))) =
