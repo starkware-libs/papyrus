284e445fa6d751d46e3b3bf96d5addaff1e0d864
Nevo Agmon <nevo@starkware.co>
2023-04-20 14:53:50 +0300

    make get_rand_test_block_with_events private
    commit-id:e8238dd7

--- crates/test_utils/src/lib.rs
+++ crates/test_utils/src/lib.rs
@@ -100,7 +100,7 @@ pub fn get_rng(seed: Option<u64>) -> ChaCha8Rng {
 //////////////////////////////////////////////////////////////////////////
 
 /// Returns a test block with a variable number of transactions and events.
-pub fn get_rand_test_block_with_events(
+fn get_rand_test_block_with_events(
     rng: &mut ChaCha8Rng,
     transaction_count: usize,
     events_per_tx: usize,
@@ -211,10 +211,22 @@ fn set_transaction_hash(tx: &mut Transaction, hash: TransactionHash) {
 //////////////////////////////////////////////////////////////////////////
 
 // Returns a test block with a variable number of transactions.
-pub fn get_test_block(transaction_count: usize, events_per_tx: Option<usize>) -> Block {
-    let mut rng = get_rng(Some(0));
+pub fn get_test_block(
+    seed: Option<u64>,
+    transaction_count: usize,
+    events_per_tx: Option<usize>,
+    from_addresses: Option<Vec<ContractAddress>>,
+    keys: Option<Vec<Vec<EventKey>>>,
+) -> Block {
+    let mut rng = get_rng(seed);
     let events_per_tx = if let Some(events_per_tx) = events_per_tx { events_per_tx } else { 0 };
-    get_rand_test_block_with_events(&mut rng, transaction_count, events_per_tx, None, None)
+    get_rand_test_block_with_events(
+        &mut rng,
+        transaction_count,
+        events_per_tx,
+        from_addresses,
+        keys,
+    )
 }
 
 // Returns a test block body with a variable number of transactions.
