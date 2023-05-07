98c3d655d6ba45d8db8f8cc16ee2bb105f4788a9
Dvir Yosef <dvir@starkware.co>
2023-05-07 18:51:25 +0300

    Delete ThinStateDiff from the storage.

--- crates/papyrus_storage/src/state/data.rs
+++ crates/papyrus_storage/src/state/data.rs
@@ -11,56 +11,6 @@ pub type DeprecatedDeclaredClasses = IndexMap<ClassHash, DeprecatedContractClass
 
 /// Data structs that are serialized into the database.
 
-// Invariant: Addresses are strictly increasing.
-// The invariant is enforced as [`ThinStateDiff`] is created only from [`starknet_api`][`StateDiff`]
-// where the addresses are strictly increasing.
-#[derive(Debug, Clone, Eq, PartialEq, Deserialize, Serialize)]
-pub struct ThinStateDiff {
-    pub deployed_contracts: IndexMap<ContractAddress, ClassHash>,
-    pub storage_diffs: IndexMap<ContractAddress, IndexMap<StorageKey, StarkFelt>>,
-    pub declared_classes: IndexMap<ClassHash, CompiledClassHash>,
-    pub deprecated_declared_classes: Vec<ClassHash>,
-    pub nonces: IndexMap<ContractAddress, Nonce>,
-    pub replaced_classes: IndexMap<ContractAddress, ClassHash>,
-}
-
-impl ThinStateDiff {
-    // Returns also the declared classes without cloning them.
-    pub(crate) fn from_state_diff(
-        diff: StateDiff,
-    ) -> (Self, DeclaredClasses, DeprecatedDeclaredClasses) {
-        (
-            Self {
-                deployed_contracts: diff.deployed_contracts,
-                storage_diffs: diff.storage_diffs,
-                declared_classes: diff
-                    .declared_classes
-                    .iter()
-                    .map(|(class_hash, (compiled_hash, _class))| (*class_hash, *compiled_hash))
-                    .collect(),
-                deprecated_declared_classes: diff
-                    .deprecated_declared_classes
-                    .keys()
-                    .copied()
-                    .collect(),
-                nonces: diff.nonces,
-                replaced_classes: diff.replaced_classes,
-            },
-            diff.declared_classes
-                .into_iter()
-                .map(|(class_hash, (_compiled_class_hash, class))| (class_hash, class))
-                .collect(),
-            diff.deprecated_declared_classes,
-        )
-    }
-}
-
-impl From<StateDiff> for ThinStateDiff {
-    fn from(diff: StateDiff) -> Self {
-        Self::from_state_diff(diff).0
-    }
-}
-
 #[derive(Debug, Default, Clone, Eq, PartialEq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
 pub(crate) struct IndexedDeployedContract {
     pub block_number: BlockNumber,
