3060a35a3e430fa548a9e272403499a6bb4fec84
Dvir Yosef <dvir@starkware.co>
2023-05-15 15:21:06 +0300

    Compression infrastructure.

--- crates/papyrus_storage/src/compression_utils.rs
+++ crates/papyrus_storage/src/compression_utils.rs
@@ -1,58 +1,69 @@
-#[cfg(test)]
-#[path = "compression_utils_test.rs"]
-mod compression_utils_test;
-
 use std::io::Read;
-use std::marker::PhantomData;
 
-use flate2::read::GzDecoder;
-use flate2::write::GzEncoder;
+use flate2::bufread::{GzDecoder, GzEncoder};
 use flate2::Compression;
-use serde::{Deserialize, Serialize};
 
 use crate::db::serialization::{StorageSerde, StorageSerdeError};
 
-/// Errors that may be returned when encoding or decoding with one of the functions in this file.
-#[derive(thiserror::Error, Debug)]
-pub enum CompressionError {
-    /// An error representing reading and writing errors.
-    #[error(transparent)]
-    IO(#[from] std::io::Error),
-    /// An internal deserialization problem.
-    #[error("Internal deserialization error.")]
-    InnerDeserialization,
-    /// An internal serialization problem.
-    #[error(transparent)]
-    StorageSerde(#[from] StorageSerdeError),
+// TODO: consider changing the compression hyperparameters: compression level and algorithm.
+
+/// Returns the compressed data in a vector.
+pub fn compress(data: &[u8]) -> Result<Vec<u8>, std::io::Error> {
+    let mut encoder = GzEncoder::new(data, Compression::default());
+    let mut compressed_data = Vec::new();
+    encoder.read_to_end(&mut compressed_data)?;
+    Ok(compressed_data)
 }
 
-/// An object that was encoded with [`GzEncoder`].
-/// The phantom data represents the type of the object that was encoded.
-#[derive(Debug, Clone, Default, Eq, PartialEq, Deserialize, Serialize)]
-pub struct GzEncoded<I>(Vec<u8>, PhantomData<I>);
-
-impl<I> GzEncoded<I>
-where
-    I: StorageSerde,
-{
-    /// Returns a gzip compression of a given item.
-    pub fn encode(item: I) -> Result<Self, CompressionError> {
-        let mut encoder = GzEncoder::new(Vec::new(), Compression::fast());
-        item.serialize_into(&mut encoder)?;
-        let bytes = encoder.finish()?;
-        Ok(Self(bytes, PhantomData))
-    }
-
-    /// Returns a decompressed item.
-    pub fn decode(&self, buff: &mut Vec<u8>) -> Result<I, CompressionError> {
-        let mut decoder = GzDecoder::new(self.0.as_slice());
-        decoder.read_to_end(buff)?;
-        I::deserialize_from(&mut buff.as_slice()).ok_or(CompressionError::InnerDeserialization)
-    }
+/// Compress object and returns his compression in a vector.
+pub fn compress_object(object: &impl StorageSerde) -> Result<Vec<u8>, StorageSerdeError> {
+    let mut buf = Vec::new();
+    object.serialize_into(&mut buf)?;
+    Ok(compress(buf.as_slice())?)
 }
 
-impl<I> AsRef<[u8]> for GzEncoded<I> {
-    fn as_ref(&self) -> &[u8] {
-        self.0.as_slice()
+/// Decompress data and returns the uncompressed bytes in a vector.
+pub fn decompress(data: &[u8]) -> Result<Vec<u8>, std::io::Error> {
+    let mut decoder = GzDecoder::new(data);
+    let mut uncompressed = Vec::new();
+    decoder.read_to_end(&mut uncompressed)?;
+    Ok(uncompressed)
+}
+
+/// Decompress a vector directly from a reader.
+pub fn decompress_from_reader(bytes: &mut impl std::io::Read) -> Option<Vec<u8>> {
+    let compressed_data = Vec::<u8>::deserialize_from(bytes)?;
+    decompress(compressed_data.as_slice()).ok()
+}
+
+#[cfg(test)]
+mod compression_utils_test {
+    use rand::distributions::Uniform;
+    use rand::Rng;
+    use starknet_api::deprecated_contract_class::Program;
+    use test_utils::read_json_file;
+
+    use super::{compress, compress_object, decompress, decompress_from_reader};
+    use crate::db::serialization::StorageSerde;
+
+    #[test]
+    fn bytes_compression() {
+        let length = rand::thread_rng().gen_range(0..10000);
+        let range = Uniform::from(0..u8::MAX);
+        let values: Vec<u8> = rand::thread_rng().sample_iter(&range).take(length).collect();
+        let x = decompress(compress(values.as_slice()).unwrap().as_slice()).unwrap();
+        assert_eq!(values, x);
+    }
+
+    #[test]
+    fn object_compression() {
+        let program_json = read_json_file("program.json");
+        let program = serde_json::from_value::<Program>(program_json).unwrap();
+        let compressed = compress_object(&program).unwrap();
+        let mut buf = Vec::new();
+        compressed.serialize_into(&mut buf).unwrap();
+        let decompressed = decompress_from_reader(&mut buf.as_slice()).unwrap();
+        let restored_program = Program::deserialize_from(&mut decompressed.as_slice()).unwrap();
+        assert_eq!(program, restored_program);
     }
 }
