e9f82e0346b53adfc17fd95e36509fe4255d28ea
Nevo Agmon <nevo@starkware.co>
2023-05-16 09:21:34 +0300

    fix doc typo

--- crates/papyrus_gateway/src/api/version_config.rs
+++ crates/papyrus_gateway/src/api/version_config.rs
@@ -1,8 +1,8 @@
 #[derive(Eq, PartialEq, Hash)]
 /// Labels the jsonRPC versions we have such that one and only one version is Latest,
 /// there can ve multiple versions that are supported (latest is implicitly supported), and there
-/// can be multiple versions that are deprecated. Latest -> method exposed via the http path "/" and "" (e.g. http://<host>:<port>/)
-/// Supported -> method exposed via the http path "/version_id" (e.g. http://<host>:<port/V0_3_0)
+/// can be multiple versions that are deprecated. Latest -> method exposed via the http path "/" and "" (e.g. http://host:port/)
+/// Supported -> method exposed via the http path "/version_id" (e.g. http://host:port/V0_3_0)
 /// Deprecated -> method not exposed.
 pub enum VersionState {
     // TODO: nevo - remove the dead_code attribute once other versions are implemented - hides
