e4d7ab1ea591e420edce79fc425e16d4672287a2
Shahak Shama <shahak@starkware.co>
2023-05-18 14:14:48 +0300

    Revert "perform query every second"
    This reverts commit 4bc5279e323387f12102e7331f611353a51c9949.

--- crates/papyrus_discovery/src/discovery_test.rs
+++ crates/papyrus_discovery/src/discovery_test.rs
@@ -55,33 +55,32 @@ where
         cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Option<Self::Item>> {
         let unpinned_self = Pin::into_inner(self);
-        loop {
-            for ((i, stream), is_consumed) in unpinned_self
-                .streams
-                .iter_mut()
-                .enumerate()
-                .zip(unpinned_self.is_stream_consumed_vec.iter_mut())
-            {
-                if *is_consumed {
+        for ((i, stream), is_consumed) in unpinned_self
+            .streams
+            .iter_mut()
+            .enumerate()
+            .zip(unpinned_self.is_stream_consumed_vec.iter_mut())
+        {
+            if *is_consumed {
+                continue;
+            }
+            match stream.poll_next_unpin(cx) {
+                Poll::Ready(Some(item)) => {
+                    return Poll::Ready(Some((i, item)));
+                }
+                Poll::Ready(None) => {
+                    *is_consumed = true;
                     continue;
                 }
-                match stream.poll_next_unpin(cx) {
-                    Poll::Ready(Some(item)) => {
-                        return Poll::Ready(Some((i, item)));
-                    }
-                    Poll::Ready(None) => {
-                        *is_consumed = true;
-                        continue;
-                    }
-                    Poll::Pending => {
-                        continue;
-                    }
+                Poll::Pending => {
+                    continue;
                 }
             }
-            if unpinned_self.is_stream_consumed_vec.iter().all(|x| *x) {
-                return Poll::Ready(None);
-            }
         }
+        if unpinned_self.is_stream_consumed_vec.iter().all(|x| *x) {
+            return Poll::Ready(None);
+        }
+        Poll::Pending
     }
 }
 
@@ -144,7 +143,7 @@ fn test_found_all_peers(stream: MergedStream<Discovery>) {
     let peer_ids: Vec<PeerId> =
         stream.streams.iter().map(|discovery| discovery.peer_id().clone()).collect();
     let n_peers = peer_ids.len();
-    let result: HashSet<(usize, PeerId)> = block_on(stream.take(n_peers * (n_peers + 1)).collect());
+    let result: HashSet<(usize, PeerId)> = block_on(stream.take(n_peers * (n_peers - 1)).collect());
     let expected_result: HashSet<(usize, PeerId)> = (0..n_peers)
         .flat_map(|i| peer_ids.iter().cloned().map(move |peer_id| (i, peer_id)))
         .filter(|(i, peer_id)| *peer_id != peer_ids[*i])
