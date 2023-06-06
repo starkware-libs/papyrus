f04a522c58dd890b704ab65fc7db0101d30f991d
Yoav Gross <yoav@starkware.co>
2023-06-06 17:59:53 +0300

    Add load trait for sub configs.

---
+++ crates/papyrus_config/src/config_test.rs
@@ -0,0 +1,57 @@
+use std::collections::BTreeMap;
+
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+
+use crate::{prefix_by_name, ParamPath, SerdeConfig, SerializedParam};
+
+#[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
+pub struct InnerConfig {
+    pub a: usize,
+}
+
+impl SerdeConfig for InnerConfig {
+    fn dump(&self) -> BTreeMap<ParamPath, SerializedParam> {
+        BTreeMap::from([(
+            String::from("a"),
+            SerializedParam { description: String::from("This is a."), value: json!(self.a) },
+        )])
+    }
+}
+
+#[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
+pub struct OuterConfig {
+    pub inner: InnerConfig,
+    pub b: usize,
+}
+
+impl SerdeConfig for OuterConfig {
+    fn dump(&self) -> BTreeMap<ParamPath, SerializedParam> {
+        BTreeMap::from([(
+            String::from("b"),
+            SerializedParam { description: String::from("This is b."), value: json!(self.b) },
+        )])
+        .into_iter()
+        .chain(prefix_by_name(self.inner.dump(), "inner"))
+        .collect()
+    }
+}
+
+#[test]
+fn dump_and_load_config() {
+    let outer_config = OuterConfig { b: 1, inner: InnerConfig { a: 0 } };
+    let dump = outer_config.dump();
+    let expected = BTreeMap::from([
+        (
+            String::from("inner.a"),
+            SerializedParam { description: String::from("This is a."), value: json!(0) },
+        ),
+        (
+            String::from("b"),
+            SerializedParam { description: String::from("This is b."), value: json!(1) },
+        ),
+    ]);
+    assert_eq!(dump, expected);
+    let loaded_config = OuterConfig::load(&dump).unwrap();
+    assert_eq!(loaded_config, outer_config);
+}
