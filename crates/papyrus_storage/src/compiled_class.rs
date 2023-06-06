0c04cbbb679e7da9460f664cd29346841690f30e
Yair <92672946+yair-starkware@users.noreply.github.com>
2023-06-06 11:31:26 +0300

    Revert execution code (#722)
    * Revert "Refactor blcokifier state trait implementation (#690)"
    
    This reverts commit 127833ed4decc599a18d96a98ade3b3ab80e5472.
    
    * Revert "Add execution crate with blockifier state implementation (#685)"
    
    This reverts commit f9c9512fc454613dcb78859d4c893662fc170a7d.

--- crates/papyrus_storage/src/compiled_class.rs
+++ crates/papyrus_storage/src/compiled_class.rs
@@ -12,7 +12,7 @@ use crate::{MarkerKind, MarkersTable, StorageError, StorageResult, StorageTxn};
 
 pub trait CasmStorageReader {
     /// Returns the Cairo assembly of a class given its Sierra class hash.
-    fn get_casm(&self, class_hash: &ClassHash) -> StorageResult<Option<CasmContractClass>>;
+    fn get_casm(&self, class_hash: ClassHash) -> StorageResult<Option<CasmContractClass>>;
     fn get_compiled_class_marker(&self) -> StorageResult<BlockNumber>;
 }
 
@@ -26,9 +26,9 @@ where
 }
 
 impl<'env, Mode: TransactionKind> CasmStorageReader for StorageTxn<'env, Mode> {
-    fn get_casm(&self, class_hash: &ClassHash) -> StorageResult<Option<CasmContractClass>> {
+    fn get_casm(&self, class_hash: ClassHash) -> StorageResult<Option<CasmContractClass>> {
         let casm_table = self.txn.open_table(&self.tables.casms)?;
-        Ok(casm_table.get(&self.txn, class_hash)?)
+        Ok(casm_table.get(&self.txn, &class_hash)?)
     }
 
     fn get_compiled_class_marker(&self) -> StorageResult<BlockNumber> {
