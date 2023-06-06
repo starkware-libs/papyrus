75552f07dc309842cd47b8d9446236dd38083bce
Yoav Gross <yoav@starkware.co>
2023-06-06 17:59:53 +0300

    Add load trait for sub configs.

--- crates/papyrus_config/src/lib.rs
+++ crates/papyrus_config/src/lib.rs
@@ -1,12 +1,17 @@
 use std::collections::BTreeMap;
 use std::fs::File;
 use std::io::{BufWriter, Write};
+use std::ops::IndexMut;
 
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
+use serde_json::{json, Value};
+
 pub type ParamPath = String;
-pub type SerializedValue = Value;
 pub type Description = String;
+
+#[cfg(test)]
+#[path = "config_test.rs"]
+mod config_test;
 
 pub const DEFAULT_CHAIN_ID: &str = "SN_MAIN";
 
@@ -16,34 +21,44 @@ pub struct SerializedParam {
     pub value: Value,
 }
 
+#[derive(thiserror::Error, Debug)]
+pub enum SubConfigError {
+    #[error(transparent)]
+    MissingParam(#[from] serde_json::Error),
+}
 /// Serialization and deserialization for configs.
-pub trait SerdeConfig: Serialize + Sized {
-    fn config_name() -> String;
-
+/// For an explanation of `for<'a> Deserialize<'a>` see https://doc.rust-lang.org/nomicon/hrtb.html.
+pub trait SerdeConfig: for<'a> Deserialize<'a> + Serialize + Sized {
     /// Serializes the config into flatten JSON.
     fn dump(&self) -> BTreeMap<ParamPath, SerializedParam>;
 
-    /// Serializes the config into flatten JSON, where the path of each param is rooted at this
-    /// config name.
-    /// Used by `dump` when a field itself implements `SerdeConfig`.
-    fn dump_sub_config(&self) -> BTreeMap<ParamPath, SerializedParam> {
-        let descriptions = self.dump();
-        let mut named_map = BTreeMap::new();
-
-        for (field_name, val) in descriptions {
-            named_map.insert(param_path::<Self>(&field_name), val);
+    /// Deserializes the config from flatten JSON.
+    fn load(config_dump: &BTreeMap<ParamPath, SerializedParam>) -> Result<Self, SubConfigError> {
+        let mut nested_map = json!({});
+        for (param_path, serialized_param) in config_dump {
+            let mut entry = &mut nested_map;
+            for config_name in param_path.split('.') {
+                entry = entry.index_mut(config_name);
+            }
+            *entry = serialized_param.value.clone();
         }
-        named_map
+        Ok(serde_json::from_value(nested_map)?)
     }
 }
 
-fn param_path<T: SerdeConfig>(param_name: &str) -> ParamPath {
-    let config_name = T::config_name();
-    format!("{config_name}.{param_name}")
+pub fn append_sub_config_name(
+    config_dump: BTreeMap<ParamPath, SerializedParam>,
+    prefix: &str,
+) -> BTreeMap<ParamPath, SerializedParam> {
+    BTreeMap::from_iter(
+        config_dump
+            .into_iter()
+            .map(|(field_name, val)| (format!("{}.{}", prefix, field_name), val)),
+    )
 }
 
-pub fn dump_sub_config_to_file<T: SerdeConfig>(config: &T, file_path: &str) {
-    let dumped = config.dump_sub_config();
+pub fn dump_to_file<T: SerdeConfig>(config: &T, file_path: &str) {
+    let dumped = config.dump();
     let file = File::create(file_path).expect("creating failed");
     let mut writer = BufWriter::new(file);
     serde_json::to_writer_pretty(&mut writer, &dumped).expect("writing failed");
