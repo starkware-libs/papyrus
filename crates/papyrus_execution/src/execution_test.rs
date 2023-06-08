65a276aab1fccee854a6e158375153162308f459
Yair Bakalchuk <yair@starkware.co>
2023-06-08 16:16:35 +0300

    Execution

--- crates/papyrus_execution/src/execution_test.rs
+++ crates/papyrus_execution/src/execution_test.rs
@@ -1,1 +1,93 @@
+use blockifier::abi::abi_utils::selector_from_name;
+use blockifier::execution::entry_point::Retdata;
+use cairo_lang_starknet::casm_contract_class::CasmContractClass;
+use indexmap::indexmap;
+use papyrus_storage::body::BodyStorageWriter;
+use papyrus_storage::compiled_class::CasmStorageWriter;
+use papyrus_storage::header::HeaderStorageWriter;
+use papyrus_storage::state::StateStorageWriter;
+use papyrus_storage::test_utils::get_test_storage;
+use papyrus_storage::StorageWriter;
+use starknet_api::block::{BlockBody, BlockHeader, BlockNumber};
+use starknet_api::core::{ClassHash, CompiledClassHash, ContractAddress, Nonce, PatriciaKey};
+use starknet_api::deprecated_contract_class::ContractClass as DeprecatedContractClass;
+use starknet_api::hash::{StarkFelt, StarkHash};
+use starknet_api::patricia_key;
+use starknet_api::state::{ContractClass, StateDiff, StateNumber};
+use starknet_api::transaction::Calldata;
+use test_utils::read_json_file;
 
+use crate::execute_call;
+
+// Based on get_deprecated_contract_class of Blockifier.
+// TODO(yair): keep a valid contract class in the repo (in the SN_API format).
+fn get_test_deprecated_contract_class() -> DeprecatedContractClass {
+    let mut raw_contract_class = read_json_file("deprecated_class.json");
+    // ABI is not required for execution.
+    raw_contract_class
+        .as_object_mut()
+        .expect("A compiled contract must be a JSON object.")
+        .remove("abi");
+
+    serde_json::from_value(raw_contract_class).unwrap()
+}
+
+fn prepare_storage(mut storage_writer: StorageWriter) {
+    let class_hash0 = ClassHash(StarkFelt::from(0u128));
+    let class0 = serde_json::from_value::<ContractClass>(read_json_file("class.json")).unwrap();
+    let casm0 =
+        serde_json::from_value::<CasmContractClass>(read_json_file("compiled_class.json")).unwrap();
+    let address0 = ContractAddress(patricia_key!("0x0"));
+
+    let class_hash1 = ClassHash(StarkFelt::from(1u128));
+    let class1 = get_test_deprecated_contract_class();
+    let address1 = ContractAddress(patricia_key!("0x1"));
+
+    storage_writer
+        .begin_rw_txn()
+        .unwrap()
+        .append_header(BlockNumber(0), &BlockHeader::default())
+        .unwrap()
+        .append_body(BlockNumber(0), BlockBody::default())
+        .unwrap()
+        .append_state_diff(
+            BlockNumber(0),
+            StateDiff {
+                deployed_contracts: indexmap!(address0 => class_hash0, address1 => class_hash1),
+                storage_diffs: indexmap!(),
+                declared_classes: indexmap!(
+                    class_hash0 =>
+                    (CompiledClassHash::default(), class0)
+                ),
+                deprecated_declared_classes: indexmap!(
+                    class_hash1 => class1
+                ),
+                nonces: indexmap!(address0 => Nonce::default(), address1 => Nonce::default()),
+                replaced_classes: indexmap!(),
+            },
+            indexmap!(),
+        )
+        .unwrap()
+        .append_casm(class_hash0, &casm0)
+        .unwrap()
+        .commit()
+        .unwrap();
+}
+
+#[test]
+fn execute_call_cairo0() {
+    let (storage_reader, storage_writer) = get_test_storage();
+    prepare_storage(storage_writer);
+
+    let address1 = ContractAddress(patricia_key!("0x1"));
+    let retdata = execute_call(
+        storage_reader,
+        StateNumber::right_after_block(BlockNumber(0)),
+        &address1,
+        selector_from_name("without_arg"),
+        Calldata::default(),
+    )
+    .unwrap();
+
+    assert_eq!(retdata, Retdata::default());
+}
--- crates/papyrus_execution/src/lib.rs
+++ crates/papyrus_execution/src/lib.rs
@@ -2,27 +2,88 @@
 #[path = "execution_test.rs"]
 mod execution_test;
 
-use blockifier::execution::entry_point::Retdata;
-use blockifier::execution::errors::EntryPointExecutionError;
+mod execution_utils;
+mod state_reader;
+
+use blockifier::execution::entry_point::{CallEntryPoint, CallType, Retdata};
+use blockifier::execution::errors::{EntryPointExecutionError, PreExecutionError};
+use blockifier::state::cached_state::CachedState;
+use execution_utils::ExecutionUtilsError;
+use papyrus_storage::compiled_class::CasmStorageReader;
+use papyrus_storage::state::StateStorageReader;
+use papyrus_storage::{StorageError, StorageReader};
 use starknet_api::block::BlockNumber;
 use starknet_api::core::{ContractAddress, EntryPointSelector};
+use starknet_api::deprecated_contract_class::EntryPointType;
+use starknet_api::state::StateNumber;
 use starknet_api::transaction::Calldata;
+use state_reader::ExecutionStateReader;
 
 pub type ExecutionResult<T> = Result<T, ExecutionError>;
 
 #[derive(thiserror::Error, Debug)]
 pub enum ExecutionError {
     #[error(transparent)]
-    BlockifierError(#[from] EntryPointExecutionError),
+    EntryPointExecutionError(#[from] EntryPointExecutionError),
+    #[error(transparent)]
+    ExecutionUtilsError(#[from] ExecutionUtilsError),
+    #[error(transparent)]
+    StorageError(#[from] StorageError),
+    #[error(
+        "The contract was not found. state_number: {state_number:?}, contract_address: \
+         {contract_address:?}"
+    )]
+    ContractNotFound { contract_address: ContractAddress, state_number: StateNumber },
+    #[error("No blocks at state 0.")]
+    NoData,
+    #[error(
+        "The node is not synced. state_number: {state_number:?}, compiled_class_marker: \
+         {compiled_class_marker:?}"
+    )]
+    NotSynced { state_number: StateNumber, compiled_class_marker: BlockNumber },
+    #[error(transparent)]
+    PreExecutionError(#[from] PreExecutionError),
 }
 
 /// Executes a StarkNet call and returns the retdata.
 // TODO(yair): Consider adding Retdata to StarkNetApi.
 pub fn execute_call(
-    block_number: &BlockNumber,
+    storage_reader: StorageReader,
+    state_number: StateNumber,
     contract_address: &ContractAddress,
-    entry_point_selector: &EntryPointSelector,
+    entry_point_selector: EntryPointSelector,
     calldata: Calldata,
 ) -> ExecutionResult<Retdata> {
-    todo!()
+    let txn = storage_reader.begin_ro_txn()?;
+
+    // Verify the node is synced.
+    let synced_up_to = StateNumber::right_before_block(txn.get_compiled_class_marker()?);
+    if state_number > synced_up_to {
+        return Err(ExecutionError::NotSynced {
+            state_number,
+            compiled_class_marker: synced_up_to.block_after(),
+        });
+    }
+
+    let class_hash =
+        txn.get_state_reader()?.get_class_hash_at(state_number, contract_address)?.ok_or(
+            ExecutionError::ContractNotFound { contract_address: *contract_address, state_number },
+        )?;
+
+    let call_entry_point = CallEntryPoint {
+        class_hash: Some(class_hash),
+        code_address: None,
+        entry_point_type: EntryPointType::External,
+        entry_point_selector,
+        calldata,
+        storage_address: *contract_address,
+        caller_address: ContractAddress::default(),
+        call_type: CallType::Call,
+    };
+
+    let mut execution_state_reader =
+        CachedState::new(ExecutionStateReader { txn: &txn, state_number });
+
+    let res = call_entry_point.execute_directly(&mut execution_state_reader)?;
+    Ok(res.execution.retdata)
 }
