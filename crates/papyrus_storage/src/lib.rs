019ea9b6881e53cecfbd1611dda8a776f40a2a3c
Yair <92672946+yair-starkware@users.noreply.github.com>
2023-06-13 09:43:15 +0300

    Bump storage version from 0 to 1 (#752)

--- crates/papyrus_storage/src/lib.rs
+++ crates/papyrus_storage/src/lib.rs
@@ -37,7 +37,7 @@ use crate::db::{
 use crate::state::data::IndexedDeprecatedContractClass;
 use crate::version::{VersionStorageReader, VersionStorageWriter};
 
-pub const STORAGE_VERSION: Version = Version(0);
+pub const STORAGE_VERSION: Version = Version(1);
 
 pub fn open_storage(db_config: DbConfig) -> StorageResult<(StorageReader, StorageWriter)> {
     let (db_reader, mut db_writer) = open_env(db_config)?;
