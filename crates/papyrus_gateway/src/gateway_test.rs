e5ddfef6e5a53082787737aa8fc220efb538aa8c
Nevo Agmon <nevo@starkware.co>
2023-06-25 12:00:21 +0300

    add test gateway_trascation_from_starknet_api_transaction

--- crates/papyrus_gateway/src/gateway_test.rs
+++ crates/papyrus_gateway/src/gateway_test.rs
@@ -147,6 +147,56 @@ fn get_block_status_test() {
 }
 
 #[tokio::test]
+async fn test_gateway_trascation_from_starknet_api_transaction() {
+    let mut rng = get_rng();
+    let _transaction: crate::Transaction =
+        Transaction::Declare(starknet_api::transaction::DeclareTransaction::V0(
+            starknet_api::transaction::DeclareTransactionV0V1::default(),
+        ))
+        .try_into()
+        .unwrap();
+    let _transaction: crate::Transaction =
+        Transaction::Declare(starknet_api::transaction::DeclareTransaction::V1(
+            starknet_api::transaction::DeclareTransactionV0V1::default(),
+        ))
+        .try_into()
+        .unwrap();
+    let _transaction: crate::Transaction =
+        Transaction::Declare(starknet_api::transaction::DeclareTransaction::V2(
+            starknet_api::transaction::DeclareTransactionV2::default(),
+        ))
+        .try_into()
+        .unwrap();
+    let _transaction: crate::Transaction =
+        Transaction::Invoke(starknet_api::transaction::InvokeTransaction::V0(
+            starknet_api::transaction::InvokeTransactionV0::default(),
+        ))
+        .try_into()
+        .unwrap();
+    let _transaction: crate::Transaction =
+        Transaction::Invoke(starknet_api::transaction::InvokeTransaction::V1(
+            starknet_api::transaction::InvokeTransactionV1::default(),
+        ))
+        .try_into()
+        .unwrap();
+    let _transaction: crate::Transaction = Transaction::L1Handler(
+        starknet_api::transaction::L1HandlerTransaction::get_test_instance(&mut rng),
+    )
+    .try_into()
+    .unwrap();
+    let _transaction: crate::Transaction = Transaction::Deploy(
+        starknet_api::transaction::DeployTransaction::get_test_instance(&mut rng),
+    )
+    .try_into()
+    .unwrap();
+    let _transaction: crate::Transaction = Transaction::DeployAccount(
+        starknet_api::transaction::DeployAccountTransaction::get_test_instance(&mut rng),
+    )
+    .try_into()
+    .unwrap();
+}
+
+#[tokio::test]
 async fn test_contractclassabientrywithtype_from_api_contractclassabientry() {
     let mut rng = get_rng();
     let _: ContractClassAbiEntryWithType =
