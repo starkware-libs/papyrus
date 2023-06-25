28cd4806141b436e485b179a5042b273380f3ab6
Nevo Agmon <nevo@starkware.co>
2023-06-25 10:03:47 +0300

    add test for ContractClassAbiEntryType from FunctionAbiEntryType

--- crates/papyrus_gateway/src/gateway_test.rs
+++ crates/papyrus_gateway/src/gateway_test.rs
@@ -21,7 +21,7 @@ use tower::BoxError;
 
 use crate::api::version_config::{LATEST_VERSION_ID, VERSION_CONFIG};
 use crate::api::JsonRpcError;
-use crate::deprecated_contract_class::ContractClassAbiEntryWithType;
+use crate::deprecated_contract_class::ContractClassAbiEntryType;
 use crate::middleware::proxy_request;
 use crate::test_utils::get_test_gateway_config;
 use crate::{get_block_status, run_server, SERVER_MAX_BODY_SIZE};
@@ -161,7 +161,24 @@ async fn test_contractclassabientrywithtype_from_api_contractclassabientry() {
         )
         .try_into()
         .unwrap();
+
+macro_rules! test_ContractClassAbiEntryType_from_FunctionAbiEntryType {
+    ($variant:ident) => {
+        paste! {
+            #[tokio::test]
+            #[allow(non_snake_case)]
+            async fn [< ContractClassAbiEntryType_from_FunctionAbiEntryType_ $variant:lower>]() {
+                let _: ContractClassAbiEntryType =
+                starknet_api::deprecated_contract_class::FunctionAbiEntryType::$variant
+                    .try_into()
+                    .unwrap();
+            }
+        }
+    };
 }
+test_ContractClassAbiEntryType_from_FunctionAbiEntryType!(Constructor);
+test_ContractClassAbiEntryType_from_FunctionAbiEntryType!(L1Handler);
+test_ContractClassAbiEntryType_from_FunctionAbiEntryType!(Function);
 
 // macro to generate a test that creates a ContractClassAbiEntry with a variant based on the given
 // variant input and call try_into().unwrap()
