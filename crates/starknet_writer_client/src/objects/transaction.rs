bab8d0a9f50a688b1d117c076ebb2a9edddb122b
Shahak Shama <shahak@starkware.co>
2023-07-02 17:41:56 +0300

    add DeployAccountTransaction to SN writer client

--- crates/starknet_writer_client/src/objects/transaction.rs
+++ crates/starknet_writer_client/src/objects/transaction.rs
@@ -4,6 +4,20 @@ use starknet_api::transaction::{
     Calldata, ContractAddressSalt, Fee, TransactionSignature, TransactionVersion,
 };
 
+#[derive(Debug, Default, Deserialize, Serialize, Clone, Eq, PartialEq)]
+#[serde(deny_unknown_fields)]
+pub struct DeployAccountTransaction {
+    pub contract_address_salt: ContractAddressSalt,
+    pub class_hash: ClassHash,
+    pub constructor_calldata: Calldata,
+    pub nonce: Nonce,
+    pub max_fee: Fee,
+    pub signature: TransactionSignature,
+    #[serde(default)]
+    pub version: TransactionVersion,
+    pub r#type: TransactionType,
+}
+
 // TODO(shahak): Remove code duplication with starknet_reader_client.
 #[derive(
     Debug, Copy, Clone, Eq, PartialEq, Hash, Deserialize, Serialize, PartialOrd, Ord, Default,
--- crates/starknet_writer_client/src/objects/transaction_test.rs
+++ crates/starknet_writer_client/src/objects/transaction_test.rs
@@ -14,3 +14,10 @@ fn read_resource_file(path_in_resource_dir: &str) -> String {
         .join(path_in_resource_dir);
     return read_to_string(path.to_str().unwrap()).unwrap();
 }
+
+#[test]
+fn load_deploy_account_transaction_succeeds() {
+    assert_ok!(serde_json::from_str::<DeployAccountTransaction>(&read_resource_file(
+        "deploy_account.json"
+    )));
+}
