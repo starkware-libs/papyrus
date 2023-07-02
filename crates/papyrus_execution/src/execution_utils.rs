5fee81058a9b00fd261a2b9102d15222bb45f522
Yair Bakalchuk <yair@starkware.co>
2023-07-02 11:06:45 +0300

    Execution api

---
+++ crates/papyrus_execution/src/execution_utils.rs
@@ -0,0 +1,41 @@
+use blockifier::execution::contract_class::{
+    ContractClass as BlockifierContractClass, ContractClassV0, ContractClassV1,
+};
+use cairo_vm::types::errors::program_errors::ProgramError;
+use papyrus_storage::compiled_class::CasmStorageReader;
+use papyrus_storage::db::RO;
+use papyrus_storage::state::StateStorageReader;
+use papyrus_storage::{StorageError, StorageTxn};
+use starknet_api::core::ClassHash;
+use starknet_api::state::StateNumber;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum ExecutionUtilsError {
+    #[error(transparent)]
+    ProgramError(#[from] ProgramError),
+    #[error(transparent)]
+    StorageError(#[from] StorageError),
+}
+
+pub fn get_contract_class(
+    txn: &StorageTxn<'_, RO>,
+    class_hash: &ClassHash,
+    state_number: StateNumber,
+) -> Result<Option<BlockifierContractClass>, ExecutionUtilsError> {
+    if let Some(casm) = txn.get_casm(class_hash)? {
+        return Ok(Some(BlockifierContractClass::V1(
+            ContractClassV1::try_from(casm).map_err(ExecutionUtilsError::ProgramError)?,
+        )));
+    }
+    if let Some(contract_class) = txn
+        .get_state_reader()
+        .map_err(ExecutionUtilsError::StorageError)?
+        .get_deprecated_class_definition_at(state_number, class_hash)?
+    {
+        return Ok(Some(BlockifierContractClass::V0(
+            ContractClassV0::try_from(contract_class).map_err(ExecutionUtilsError::ProgramError)?,
+        )));
+    }
+    Ok(None)
+}
---
+++ crates/papyrus_execution/src/state_reader.rs
@@ -0,0 +1,96 @@
+use blockifier::execution::contract_class::ContractClass as BlockifierContractClass;
+use blockifier::state::errors::StateError;
+use blockifier::state::state_api::{StateReader as BlockifierStateReader, StateResult};
+use papyrus_storage::db::RO;
+use papyrus_storage::state::StateStorageReader;
+use papyrus_storage::StorageTxn;
+use starknet_api::core::{ClassHash, CompiledClassHash, ContractAddress, Nonce};
+use starknet_api::hash::StarkFelt;
+use starknet_api::state::{StateNumber, StorageKey};
+
+use crate::execution_utils::{get_contract_class, ExecutionUtilsError};
+
+pub struct ExecutionStateReader<'a> {
+    pub txn: &'a StorageTxn<'a, RO>,
+    pub state_number: StateNumber,
+}
+
+impl BlockifierStateReader for ExecutionStateReader<'_> {
+    fn get_storage_at(
+        &mut self,
+        contract_address: ContractAddress,
+        key: StorageKey,
+    ) -> StateResult<StarkFelt> {
+        self.txn
+            .get_state_reader()
+            .map_err(|err| StateError::StateReadError(err.to_string()))?
+            .get_storage_at(self.state_number, &contract_address, &key)
+            .map_err(|err| StateError::StateReadError(err.to_string()))
+    }
+
+    fn get_nonce_at(&mut self, contract_address: ContractAddress) -> StateResult<Nonce> {
+        self.txn
+            .get_state_reader()
+            .map_err(|err| StateError::StateReadError(err.to_string()))?
+            .get_nonce_at(self.state_number, &contract_address)
+            .map_err(|err| StateError::StateReadError(err.to_string()))?
+            .ok_or(StateError::StateReadError(format!(
+                "Nonce not found, contract_address = {contract_address:?}, state_number = {:?}.",
+                self.state_number
+            )))
+    }
+
+    fn get_class_hash_at(&mut self, contract_address: ContractAddress) -> StateResult<ClassHash> {
+        self.txn
+            .get_state_reader()
+            .map_err(|err| StateError::StateReadError(err.to_string()))?
+            .get_class_hash_at(self.state_number, &contract_address)
+            .map_err(|err| StateError::StateReadError(err.to_string()))?
+            .ok_or(StateError::StateReadError(format!(
+                "Class hash not found, contract_address = {contract_address:?}, state_number = \
+                 {:?}.",
+                self.state_number
+            )))
+    }
+
+    fn get_compiled_contract_class(
+        &mut self,
+        class_hash: &ClassHash,
+    ) -> StateResult<BlockifierContractClass> {
+        match get_contract_class(self.txn, class_hash, self.state_number) {
+            Ok(Some(contract_class)) => Ok(contract_class),
+            Ok(None) => Err(StateError::UndeclaredClassHash(*class_hash)),
+            Err(ExecutionUtilsError::ProgramError(err)) => Err(StateError::ProgramError(err)),
+            Err(ExecutionUtilsError::StorageError(err)) => {
+                Err(StateError::StateReadError(err.to_string()))
+            }
+        }
+    }
+
+    fn get_compiled_class_hash(&mut self, class_hash: ClassHash) -> StateResult<CompiledClassHash> {
+        let block_number = self
+            .txn
+            .get_state_reader()
+            .map_err(|err| StateError::StateReadError(err.to_string()))?
+            .get_class_definition_block_number(&class_hash)
+            .map_err(|err| StateError::StateReadError(err.to_string()))?
+            .ok_or(StateError::UndeclaredClassHash(class_hash))?;
+
+        let state_diff = self
+            .txn
+            .get_state_diff(block_number)
+            .map_err(|err| StateError::StateReadError(err.to_string()))?
+            .ok_or(StateError::StateReadError(format!(
+                "Inner storage error. Missing state diff at block {block_number}."
+            )))?;
+
+        let compiled_class_hash = state_diff.declared_classes.get(&class_hash).ok_or(
+            StateError::StateReadError(format!(
+                "Inner storage error. Missing class declaration at block {block_number}, class \
+                 {class_hash}."
+            )),
+        )?;
+
+        Ok(*compiled_class_hash)
+    }
+}
