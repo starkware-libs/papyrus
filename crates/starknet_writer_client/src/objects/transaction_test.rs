32fd41ce0691e4475cc29c287945f53a43d60558
Shahak Shama <shahak@starkware.co>
2023-07-03 18:23:48 +0300

    CR fix

--- crates/starknet_writer_client/src/objects/transaction_test.rs
+++ crates/starknet_writer_client/src/objects/transaction_test.rs
@@ -1,34 +1,25 @@
-use std::env;
-use std::fs::read_to_string;
-use std::path::Path;
-use std::string::String;
-
 use assert::assert_ok;
-use serde::{Deserialize, Serialize};
+use assert_matches::assert_matches;
+use test_utils::read_json_file;
 
-use super::transaction::DeployAccountTransaction;
+use super::transaction::Transaction;
 
-// TODO(shahak): Remove code duplication with starknet_reader_client.
-fn read_resource_file(path_in_resource_dir: &str) -> String {
-    let path = Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap())
-        .join("resources")
-        .join(path_in_resource_dir);
-    return read_to_string(path.to_str().unwrap()).unwrap();
-}
-
-fn validate_load_and_dump<TransactionType: for<'a> Deserialize<'a> + Serialize>(
+fn validate_load_and_dump<F: Fn(&Transaction)>(
     path_in_resource_dir: &str,
+    validate_transaction_matches: F,
 ) {
-    let json_str = read_resource_file(path_in_resource_dir);
-    let load_result = serde_json::from_str::<TransactionType>(&json_str);
+    let json_value = read_json_file(path_in_resource_dir);
+    let load_result = serde_json::from_value::<Transaction>(json_value.clone());
     assert_ok!(load_result);
-    let dump_result = serde_json::to_value(&load_result.unwrap());
+    validate_transaction_matches(load_result.as_ref().unwrap());
+    let dump_result = serde_json::to_value(&(load_result.unwrap()));
     assert_ok!(dump_result);
-    let json_value: serde_json::Value = serde_json::from_str(&json_str).unwrap();
     assert_eq!(json_value, dump_result.unwrap());
 }
 
 #[test]
 fn load_and_dump_deploy_account_same_string() {
-    validate_load_and_dump::<DeployAccountTransaction>("deploy_account.json");
+    validate_load_and_dump("deploy_account.json", |transaction| {
+        assert_matches!(transaction, Transaction::DeployAccount(_));
+    });
 }
