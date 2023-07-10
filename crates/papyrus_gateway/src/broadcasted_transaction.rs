578bbc7e6f7998bb47a21f319b500576209032a4
Shahak Shama <shahak@starkware.co>
2023-07-10 13:39:47 +0300

    Add broadcasted tx struct with openrpc tests

---
+++ crates/papyrus_gateway/src/broadcasted_transaction.rs
@@ -0,0 +1,42 @@
+use serde::{Deserialize, Serialize};
+use starknet_api::core::{CompiledClassHash, ContractAddress, Nonce};
+use starknet_api::transaction::{Fee, TransactionSignature, TransactionVersion};
+pub(crate) use starknet_writer_client::objects::transaction::{
+    DeclareV1Transaction as ClientDeclareV1Transaction,
+    DeployAccountTransaction as ClientDeployAccountTransaction,
+    InvokeTransaction as ClientInvokeTransaction,
+};
+
+use crate::state::ContractClass;
+
+#[derive(Debug, Deserialize, Serialize, Clone, Eq, PartialEq)]
+#[serde(tag = "type")]
+pub enum BroadcastedTransaction {
+    #[serde(rename = "DECLARE")]
+    Declare(BroadcastedDeclareTransaction),
+    #[serde(rename = "DEPLOY_ACCOUNT")]
+    DeployAccount(ClientDeployAccountTransaction),
+    #[serde(rename = "INVOKE")]
+    Invoke(ClientInvokeTransaction),
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone, Eq, PartialEq)]
+#[serde(untagged)]
+pub enum BroadcastedDeclareTransaction {
+    DeclareV1(ClientDeclareV1Transaction),
+    DeclareV2(BroadcastedDeclareV2Transaction),
+}
+
+// The only difference between this and DeclareV2Transaction in starknet_writer_client is the
+// type of contract_class.
+#[derive(Debug, Deserialize, Serialize, Clone, Eq, PartialEq, Default)]
+#[serde(deny_unknown_fields)]
+pub struct BroadcastedDeclareV2Transaction {
+    pub contract_class: ContractClass,
+    pub compiled_class_hash: CompiledClassHash,
+    pub sender_address: ContractAddress,
+    pub nonce: Nonce,
+    pub max_fee: Fee,
+    pub version: TransactionVersion,
+    pub signature: TransactionSignature,
+}
