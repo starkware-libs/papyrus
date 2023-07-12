bc8a089b10f68b189c308be90bbef4ced1a91e48
Ori Friedlender <ori@starkware.co>
2023-07-12 14:07:28 +0300

    Change if let to let else

--- crates/papyrus_storage/src/version_test.rs
+++ crates/papyrus_storage/src/version_test.rs
@@ -21,16 +21,16 @@ async fn version() {
     assert_eq!(version.unwrap(), higher_version);
 
     // Fail to set a version which is not higher than the existing one.
-    if let Err(err) = writer.begin_rw_txn().unwrap().set_version(&higher_version) {
-        assert_matches!(
-            err,
-            StorageError::StorageVersionInconcistency(StorageVersionError::SetLowerVersion {
-                crate_version,
-                storage_version
-            })
-            if crate_version == higher_version && storage_version == higher_version
-        );
-    } else {
+    let Err(err) = writer.begin_rw_txn().unwrap().set_version(&higher_version) else {
         panic!("Unexpected Ok.");
     };
+
+    assert_matches!(
+        err,
+        StorageError::StorageVersionInconcistency(StorageVersionError::SetLowerVersion {
+            crate_version,
+            storage_version
+        })
+        if crate_version == higher_version && storage_version == higher_version
+    );
 }
