c2c085be6040cdd5a305e0bba4e21379d54e899f
Yair <92672946+yair-starkware@users.noreply.github.com>
2023-07-16 22:22:30 +0300

    Avoid precision errors in json serde in the storage (programs are sto… (#857)
    Avoid precision errors in json serde in the storage (programs are stored as jsons)

--- crates/papyrus_storage/src/serializers_test.rs
+++ crates/papyrus_storage/src/serializers_test.rs
@@ -81,3 +81,12 @@ fn hint_modified() {
     let hint_schema = schemars::schema_for!(CoreHintBase);
     insta::assert_yaml_snapshot!(hint_schema);
 }
+
+#[test]
+fn serialization_precision() {
+    let input =
+        "{\"value\":244116128358498188146337218061232635775543270890529169229936851982759783745}";
+    let serialized = serde_json::from_str::<serde_json::Value>(input).unwrap();
+    let deserialized = serde_json::to_string(&serialized).unwrap();
+    assert_eq!(input, deserialized);
+}
