c061166b71dc6f5ed780715684b98691655e7912
Nevo Agmon <nevo@starkware.co>
2023-07-18 10:54:19 +0300

    add script as step in CI

--- check_starknet_api_version_dependency.sh
+++ check_starknet_api_version_dependency.sh
@@ -2,21 +2,30 @@
 
 set -e
 
-# Fetch the package's dependencies and check if "starknet_api" is one of them.
-DEPENDENCIES=$(cargo metadata --no-deps --format-version 1 | jq '.packages[].dependencies[].name' | grep -oE "\"[^\"]+\"" | tr -d "\"")
+# Fetch the package's dependencies using cargo metadata
+CARGO_METADATA=$(cargo metadata --no-deps --format-version 1)
+
+# Extract the names of the dependencies from the metadata using jq
+DEPENDENCIES=$(echo "$CARGO_METADATA" | jq '.packages[].dependencies[].name')
+
+# cleanup the dependencies names from duble quotes
+DEPENDENCIES=$(echo "$DEPENDENCIES" | grep -oE "\"[^\"]+\"" | tr -d "\"")
 
 if [[ $DEPENDENCIES =~ starknet_api ]]; then
-    # If "starknet_api" is a dependency, fetch its Git revision.
-    REVISION=$(cargo metadata --no-deps --format-version 1 | jq '.packages[].dependencies[] | select(.name == "starknet_api") | .source' | tr -d "\"" | sort -u)
+    # Use jq to extract the dependencies which have the name "starknet_api" and get their source information.
+    # The result will be the Git revision or other source details, if "starknet_api" is a dependency.
+    SOURCE_INFO=$(echo "$CARGO_METADATA" | jq '.packages[].dependencies[] | select(.name == "starknet_api") | .source')
+
+    # cleanup and sort.
+    REVISION=$(echo "$SOURCE_INFO" | tr -d "\"" | sort -u)
 
     if [[ $REVISION =~ ^git\+https:\/\/github.com\/starkware-libs\/starknet-api\?rev=([a-f0-9]+)$ ]]; then
         # Extract the commit hash from the Git URL.
         COMMIT_HASH=${BASH_REMATCH[1]}
-        
+
         # Check if the commit exists in the main branch of the "starknet-api" repository.
         GIT_API_URL="https://api.github.com/search/commits?q=repo:starkware-libs/starknet-api+hash:"
         HTTP_RESPONSE=$(curl -s -w "%{http_code}" "${GIT_API_URL}${COMMIT_HASH}" | tr -d '\n' | grep -o --regex='{.*}')
-        echo $HTTP_RESPONSE
         TOTAL_COUNT=$(echo "$HTTP_RESPONSE" | jq '.total_count')
 
         if [[ "$TOTAL_COUNT" -eq 1 ]]; then
