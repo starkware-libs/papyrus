eb96ecad4782e572fc08eeb39867f4c0383fc388
Dan Brownstein <dan@starkware.co>
2023-07-26 11:01:21 +0300

    ci: add commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,61 @@
+const Configuration = {
+    /*
+     * Resolve and load @commitlint/config-conventional from node_modules.
+     * Referenced packages must be installed
+     */
+    extends: ['@commitlint/config-conventional'],
+    /*
+     * Resolve and load conventional-changelog-atom from node_modules.
+     * Referenced packages must be installed
+     */
+    // parserPreset: 'conventional-changelog-atom',
+    /*
+     * Resolve and load @commitlint/format from node_modules.
+     * Referenced package must be installed
+     */
+    formatter: '@commitlint/format',
+    /*
+     * Any rules defined here will override rules from @commitlint/config-conventional
+     */
+    rules: {
+        'scope-enum': [2, 'always', [
+            'base_layer',
+            'common',
+            'config',
+            'JSON-RPC',
+            'load_test',
+            'monitoring',
+            'release',
+            'starknet_client',
+            'storage',
+            'sync',
+            'test_utils',
+        ]],
+    },
+    /*
+     * Functions that return true if commitlint should ignore the given message.
+     */
+    ignores: [(commit) => commit === ''],
+    /*
+     * Whether commitlint uses the default ignore rules.
+     */
+    defaultIgnores: true,
+    /*
+     * Custom URL to show upon failure
+     */
+    helpUrl:
+        'https://github.com/conventional-changelog/commitlint/#what-is-commitlint',
+    /*
+     * Custom prompt configs, not used currently.
+     */
+    prompt: {
+        messages: {},
+        questions: {
+            type: {
+                description: 'please input type:',
+            },
+        },
+    },
+};
+
+module.exports = Configuration;
