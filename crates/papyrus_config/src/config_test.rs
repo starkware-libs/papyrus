9e18d30f53638d36e7a8f252d2bf7c12e75d252d
Yoav Gross <yoav@starkware.co>
2023-07-26 10:45:24 +0300

    feat(config): support optional config params

--- crates/papyrus_config/src/config_test.rs
+++ crates/papyrus_config/src/config_test.rs
@@ -12,30 +12,21 @@ use test_utils::get_absolute_path;
 use crate::command::{get_command_matches, update_config_map_by_command_args};
 use crate::converters::deserialize_milliseconds_to_duration;
 use crate::dumping::{
-    append_sub_config_name, combine_config_map_and_pointers, ser_param, SerializeConfig,
+    append_sub_config_name, combine_config_map_and_pointers, ser_optional_param,
+    ser_optional_sub_config, ser_param, SerializeConfig,
 };
 use crate::loading::{
-    get_maps_from_raw_json, load, update_config_map_by_custom_config, update_config_map_by_pointers,
+    get_maps_from_raw_json, load, update_config_map_by_custom_config,
+    update_config_map_by_pointers, update_optional_values,
 };
 use crate::{ConfigError, ParamPath, PointerParam, SerializedParam};
 
-#[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Clone, Copy, Default, Serialize, Deserialize, Debug, PartialEq)]
 struct InnerConfig {
-    a: usize,
-}
-
-impl SerializeConfig for InnerConfig {
-    fn dump(&self) -> BTreeMap<ParamPath, SerializedParam> {
-        BTreeMap::from([ser_param("a", &self.a, "This is a.")])
-    }
-}
-
-#[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
-struct OptionalConfig {
     o: usize,
 }
 
-impl SerializeConfig for OptionalConfig {
+impl SerializeConfig for InnerConfig {
     fn dump(&self) -> BTreeMap<ParamPath, SerializedParam> {
         BTreeMap::from([ser_param("o", &self.o, "This is o.")])
     }
@@ -43,29 +34,17 @@ impl SerializeConfig for OptionalConfig {
 
 #[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
 struct OuterConfig {
-    inner: InnerConfig,
-    b: usize,
-    some_optional: Option<OptionalConfig>,
-    none_optional: Option<OptionalConfig>,
+    opt_elem: Option<usize>,
+    opt_config: Option<InnerConfig>,
+    inner_config: InnerConfig,
 }
 
 impl SerializeConfig for OuterConfig {
     fn dump(&self) -> BTreeMap<ParamPath, SerializedParam> {
         chain!(
-            BTreeMap::from([ser_param("b", &self.b, "This is b.")]),
-            append_sub_config_name(self.inner.dump(), "inner"),
-            match &self.some_optional {
-                None => BTreeMap::new(),
-                Some(optional_config) => {
-                    append_sub_config_name(optional_config.dump(), "some_optional")
-                }
-            },
-            match &self.none_optional {
-                None => BTreeMap::new(),
-                Some(optional_config) => {
-                    append_sub_config_name(optional_config.dump(), "none_optional")
-                }
-            },
+            ser_optional_param(&self.opt_elem, 1, "opt_elem", "This is elem."),
+            ser_optional_sub_config(&self.opt_config, "opt_config"),
+            append_sub_config_name(self.inner_config.dump(), "inner_config"),
         )
         .collect()
     }
@@ -73,28 +52,20 @@ impl SerializeConfig for OuterConfig {
 
 #[test]
 fn dump_and_load_config() {
-    let optional_config = OptionalConfig { o: 2 };
-    let outer_config = OuterConfig {
-        b: 1,
-        inner: InnerConfig { a: 0 },
-        some_optional: { Some(optional_config) },
-        none_optional: None,
+    let some_outer_config = OuterConfig {
+        opt_elem: Some(2),
+        opt_config: Some(InnerConfig { o: 3 }),
+        inner_config: InnerConfig { o: 4 },
     };
-    let dump = outer_config.dump();
-    let expected = BTreeMap::from([
-        (
-            "inner.a".to_owned(),
-            SerializedParam { description: "This is a.".to_owned(), value: json!(0) },
-        ),
-        ("b".to_owned(), SerializedParam { description: "This is b.".to_owned(), value: json!(1) }),
-        (
-            "some_optional.o".to_owned(),
-            SerializedParam { description: "This is o.".to_owned(), value: json!(2) },
-        ),
-    ]);
-    assert_eq!(dump, expected);
-    let loaded_config = load::<OuterConfig>(&dump).unwrap();
-    assert_eq!(loaded_config, outer_config);
+    let none_outer_config =
+        OuterConfig { opt_elem: None, opt_config: None, inner_config: InnerConfig { o: 5 } };
+
+    for outer_config in [some_outer_config, none_outer_config] {
+        let mut dumped = outer_config.dump();
+        update_optional_values(&mut dumped);
+        let loaded_config = load::<OuterConfig>(&dumped).unwrap();
+        assert_eq!(loaded_config, outer_config);
+    }
 }
 
 #[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
