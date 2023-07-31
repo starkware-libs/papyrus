a24e7483828d2defad2b17b298345afff7d8ce66
Shahak Shama <shahak@starkware.co>
2023-07-31 18:13:24 +0300

    feat(starknet_client): move code

--- crates/starknet_client/src/reader/mod.rs
+++ crates/starknet_client/src/reader/mod.rs
@@ -32,6 +32,36 @@ pub use crate::reader::objects::transaction::TransactionReceipt;
 use crate::retry::RetryConfig;
 use crate::{ClientCreationError, StarknetClient};
 
+/// Errors that may be returned from a reader client.
+#[derive(thiserror::Error, Debug)]
+pub enum ReaderClientError {
+    // The variants of ClientError are duplicated here so that the user won't need to know about
+    // ClientError.
+    /// A client error representing bad status http responses.
+    #[error("Bad response status code: {:?} message: {:?}.", code, message)]
+    BadResponseStatus { code: StatusCode, message: String },
+    /// A client error representing http request errors.
+    #[error(transparent)]
+    RequestError(#[from] reqwest::Error),
+    /// A client error representing errors that might be solved by retrying mechanism.
+    #[error("Retry error code: {:?}, message: {:?}.", code, message)]
+    RetryError { code: RetryErrorCode, message: String },
+    /// A client error representing deserialization errors.
+    #[error(transparent)]
+    SerdeError(#[from] serde_json::Error),
+    /// A client error representing errors returned by the starknet client.
+    #[error(transparent)]
+    StarknetError(#[from] ReaderStarknetError),
+    /// A client error representing errors from [`starknet_api`].
+    #[error(transparent)]
+    StarknetApiError(#[from] StarknetApiError),
+    /// A client error representing transaction receipts errors.
+    #[error(transparent)]
+    TransactionReceiptsError(#[from] TransactionReceiptsError),
+    #[error("Invalid transaction: {:?}, error: {:?}.", tx_hash, msg)]
+    BadTransaction { tx_hash: TransactionHash, msg: String },
+}
+
 pub type ReaderClientResult<T> = Result<T, ReaderClientError>;
 
 /// A trait describing an object that can communicate with [`Starknet`] and read data from it.
