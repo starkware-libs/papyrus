d2d9810ecea7ad51db202f10b7d7420022ea8761
Eran Reshef <104721006+eranreshef-starkware@users.noreply.github.com>
2023-07-31 08:47:16 +0300

    fix(helm): add prometheus scrape annotations to papyrus pod(s) (#931)
    * fix(helm): add prometheus scrape annotations to the papyrus pods
    
    * fix(helm): bump chart versioin and delete appVersion (not needed for now)
    
    * fix: add a 'helm' category to commitlint config
    
    * fix: limit rollout timeout to 5m

--- commitlint.config.js
+++ commitlint.config.js
@@ -22,6 +22,7 @@ const Configuration = {
             'base_layer',
             'common',
             'config',
+            'helm',
             'JSON-RPC',
             'load_test',
             'monitoring',
