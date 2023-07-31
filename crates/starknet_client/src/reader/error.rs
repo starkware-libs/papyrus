e349f736326cd9a7b17a8509840c5e0e0b2df441
Shahak Shama <shahak@starkware.co>
2023-07-31 15:32:09 +0300

    feat(starknet_client): add StarknetErrorCode variants and ReaderStarknetErrorCode

--- crates/starknet_client/src/reader/error.rs
+++ crates/starknet_client/src/reader/error.rs
@@ -1,3 +1,44 @@
+#[cfg(test)]
+#[path = "error_test.rs"]
+mod error_test;
+
+use std::fmt::{self, Display, Formatter};
+
+use serde::de::Error;
+use serde::{Deserialize, Serialize};
+use starknet_api::transaction::TransactionHash;
+use starknet_api::StarknetApiError;
+
+pub use crate::reader::objects::block::TransactionReceiptsError;
+use crate::starknet_error::{KnownStarknetErrorCode, StarknetErrorCode};
+use crate::{ClientError, RetryErrorCode, StarknetError, StatusCode};
+
+/// A Starknet error code that can be returned when reading data from Starknet.
+#[derive(Clone, Debug, Deserialize, Serialize, Eq, PartialEq)]
+pub enum ReaderStarknetErrorCode {
+    UnknownErrorCode(String),
+    #[serde(rename = "StarknetErrorCode.BLOCK_NOT_FOUND")]
+    BlockNotFound,
+    #[serde(rename = "StarkErrorCode.MALFORMED_REQUEST")]
+    MalformedRequest,
+    #[serde(rename = "StarknetErrorCode.OUT_OF_RANGE_CLASS_HASH")]
+    OutOfRangeClassHash,
+    #[serde(rename = "StarknetErrorCode.UNDECLARED_CLASS")]
+    UndeclaredClass,
+}
+
+#[derive(thiserror::Error, Debug, Deserialize, Serialize, Eq, PartialEq)]
+pub struct ReaderStarknetError {
+    pub code: ReaderStarknetErrorCode,
+    pub message: String,
+}
+
+impl Display for ReaderStarknetError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{self:?}")
+    }
+}
+
 /// Errors that may be returned from a reader client.
 #[derive(thiserror::Error, Debug)]
 pub enum ReaderClientError {
@@ -17,7 +58,7 @@ pub enum ReaderClientError {
     SerdeError(#[from] serde_json::Error),
     /// A client error representing errors returned by the starknet client.
     #[error(transparent)]
-    StarknetError(#[from] StarknetError),
+    StarknetError(#[from] ReaderStarknetError),
     /// A client error representing errors from [`starknet_api`].
     #[error(transparent)]
     StarknetApiError(#[from] StarknetApiError),
@@ -39,7 +80,47 @@ impl From<ClientError> for ReaderClientError {
                 ReaderClientError::RetryError { code, message }
             }
             ClientError::SerdeError(err) => ReaderClientError::SerdeError(err),
-            ClientError::StarknetError(err) => ReaderClientError::StarknetError(err),
+            ClientError::StarknetError(err) => match err.try_into() {
+                Ok(reader_starknet_error) => {
+                    ReaderClientError::StarknetError(reader_starknet_error)
+                }
+                Err(serde_error) => ReaderClientError::SerdeError(serde_error),
+            },
         }
+    }
+}
+
+impl TryFrom<StarknetError> for ReaderStarknetError {
+    type Error = serde_json::Error;
+
+    fn try_from(error: StarknetError) -> Result<Self, Self::Error> {
+        let StarknetError { code, message } = error;
+        let reader_code = match code {
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::BlockNotFound) => {
+                ReaderStarknetErrorCode::BlockNotFound
+            }
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::MalformedRequest) => {
+                ReaderStarknetErrorCode::MalformedRequest
+            }
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::OutOfRangeClassHash) => {
+                ReaderStarknetErrorCode::OutOfRangeClassHash
+            }
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::UndeclaredClass) => {
+                ReaderStarknetErrorCode::UndeclaredClass
+            }
+            StarknetErrorCode::UnknownErrorCode(unknown_code) => {
+                ReaderStarknetErrorCode::UnknownErrorCode(unknown_code)
+            }
+            other_code => {
+                let json_value = serde_json::to_value(&other_code)?;
+                let other_code_str =
+                    json_value.as_str().ok_or(serde_json::Error::custom(format!(
+                        "Starknet error code {:?} wasn't serialized into a JSON string",
+                        other_code
+                    )))?;
+                ReaderStarknetErrorCode::UnknownErrorCode(other_code_str.to_string())
+            }
+        };
+        Ok(ReaderStarknetError { code: reader_code, message })
     }
 }
