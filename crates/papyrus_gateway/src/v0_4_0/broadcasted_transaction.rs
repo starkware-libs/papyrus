3cae8a37ad807f15c2e3588ddb0c39646b075c65
Yair Bakalchuk <yair@starkware.co>
2023-08-03 14:22:04 +0300

    feat(JSON-RPC): broadcasted declare tx

---
+++ crates/papyrus_gateway/src/v0_4_0/broadcasted_transaction.rs
@@ -0,0 +1,74 @@
+//! This module contains structs for representing a broadcasted transaction.
+//!
+//! A broadcasted transaction is a transaction that wasn't accepted yet to Starknet.
+//!
+//! The broadcasted transaction follows the same structure as described in the [`Starknet specs`]
+//!
+//! [`Starknet specs`]: https://github.com/starkware-libs/starknet-specs/blob/master/api/starknet_api_openrpc.json
+
+use serde::{Deserialize, Serialize};
+use starknet_api::core::{CompiledClassHash, ContractAddress, Nonce};
+use starknet_api::transaction::{Fee, TransactionSignature};
+use starknet_client::writer::objects::transaction::DeprecatedContractClass;
+
+use super::state::ContractClass;
+
+/// A broadcasted declare transaction.
+///
+/// This transaction is equivalent to the component BROADCASTED_DECLARE_TXN in the
+/// [`Starknet specs`].
+///
+/// [`Starknet specs`]: https://github.com/starkware-libs/starknet-specs/blob/master/api/starknet_api_openrpc.json
+#[derive(Debug, Deserialize, Serialize, Clone, Eq, PartialEq)]
+#[serde(deny_unknown_fields)]
+#[serde(tag = "version")]
+pub enum BroadcastedDeclareTransaction {
+    #[serde(rename = "0x1")]
+    V1(BroadcastedDeclareV1Transaction),
+    #[serde(rename = "0x2")]
+    V2(BroadcastedDeclareV2Transaction),
+}
+
+/// A broadcasted declare transaction of a Cairo-v0 contract.
+///
+/// This transaction is equivalent to the component BROADCASTED_DECLARE_TXN_V1 in the
+/// [`Starknet specs`].
+///
+/// [`Starknet specs`]: https://github.com/starkware-libs/starknet-specs/blob/master/api/starknet_api_openrpc.json
+#[derive(Debug, Default, Deserialize, Serialize, Clone, Eq, PartialEq)]
+#[serde(deny_unknown_fields)]
+pub struct BroadcastedDeclareV1Transaction {
+    pub r#type: DeclareType,
+    pub contract_class: DeprecatedContractClass,
+    pub sender_address: ContractAddress,
+    pub nonce: Nonce,
+    pub max_fee: Fee,
+    pub signature: TransactionSignature,
+}
+
+/// A broadcasted declare transaction of a Cairo-v1 contract.
+///
+/// This transaction is equivalent to the component BROADCASTED_DECLARE_TXN_V2 in the
+/// [`Starknet specs`].
+///
+/// [`Starknet specs`]: https://github.com/starkware-libs/starknet-specs/blob/master/api/starknet_api_openrpc.json
+#[derive(Debug, Deserialize, Serialize, Clone, Eq, PartialEq, Default)]
+#[serde(deny_unknown_fields)]
+pub struct BroadcastedDeclareV2Transaction {
+    pub r#type: DeclareType,
+    pub contract_class: ContractClass,
+    pub compiled_class_hash: CompiledClassHash,
+    pub sender_address: ContractAddress,
+    pub nonce: Nonce,
+    pub max_fee: Fee,
+    pub signature: TransactionSignature,
+}
+
+/// The type field of a declare transaction. This enum serializes/deserializes into a constant
+/// string.
+#[derive(Debug, Deserialize, Serialize, Default, Clone, Copy, Eq, PartialEq)]
+pub enum DeclareType {
+    #[serde(rename = "DECLARE")]
+    #[default]
+    Declare,
+}
---
+++ crates/papyrus_gateway/src/v0_4_0/broadcasted_transaction_test.rs
@@ -0,0 +1,127 @@
+use std::collections::HashMap;
+
+use futures::executor::block_on;
+use jsonschema::JSONSchema;
+use lazy_static::lazy_static;
+use starknet_api::core::{CompiledClassHash, ContractAddress, Nonce};
+use starknet_api::deprecated_contract_class::{
+    EntryPoint as DeprecatedEntryPoint, EntryPointType as DeprecatedEntryPointType, EventAbiEntry,
+    FunctionAbiEntry, StructAbiEntry,
+};
+use starknet_api::hash::StarkFelt;
+use starknet_api::state::EntryPoint;
+use starknet_api::transaction::{Fee, TransactionSignature};
+use starknet_client::writer::objects::transaction::{
+    DeprecatedContractClass, DeprecatedContractClassAbiEntry,
+};
+use test_utils::{auto_impl_get_test_instance, get_number_of_variants, get_rng, GetTestInstance};
+
+use super::broadcasted_transaction::{
+    BroadcastedDeclareTransaction, BroadcastedDeclareV1Transaction,
+    BroadcastedDeclareV2Transaction, DeclareType,
+};
+use super::state::{ContractClass, EntryPointByType};
+use crate::test_utils::{get_starknet_spec_api_schema, SpecFile};
+use crate::version_config::VERSION_0_4;
+
+fn validate_tx_fits_rpc(tx: BroadcastedDeclareTransaction) {
+    lazy_static! {
+        static ref SCHEMA: JSONSchema = block_on(get_starknet_spec_api_schema(
+            &[(SpecFile::StarknetApiOpenrpc, &["BROADCASTED_DECLARE_TXN"])],
+            &VERSION_0_4
+        ));
+    }
+    assert!(SCHEMA.is_valid(&serde_json::to_value(tx).unwrap()));
+}
+
+auto_impl_get_test_instance! {
+    pub struct BroadcastedDeclareV1Transaction {
+        pub r#type: DeclareType,
+        pub contract_class: DeprecatedContractClass,
+        pub sender_address: ContractAddress,
+        pub nonce: Nonce,
+        pub max_fee: Fee,
+        pub signature: TransactionSignature,
+    }
+    pub struct BroadcastedDeclareV2Transaction {
+        pub r#type: DeclareType,
+        pub contract_class: ContractClass,
+        pub compiled_class_hash: CompiledClassHash,
+        pub sender_address: ContractAddress,
+        pub nonce: Nonce,
+        pub max_fee: Fee,
+        pub signature: TransactionSignature,
+    }
+    pub struct ContractClass {
+        pub sierra_program: Vec<StarkFelt>,
+        pub contract_class_version: String,
+        pub entry_points_by_type: EntryPointByType,
+        pub abi: String,
+    }
+    pub struct EntryPointByType {
+        pub contructor: Vec<EntryPoint>,
+        pub external: Vec<EntryPoint>,
+        pub l1handler: Vec<EntryPoint>,
+    }
+    pub enum DeclareType {
+        Declare = 0,
+    }
+}
+
+#[ignore]
+#[test]
+fn declare_v1_fits_rpc() {
+    let mut rng = get_rng();
+    for has_abi in [true, false] {
+        let abi = if has_abi {
+            Some(vec![
+                DeprecatedContractClassAbiEntry::Event(EventAbiEntry::get_test_instance(&mut rng)),
+                DeprecatedContractClassAbiEntry::Function(FunctionAbiEntry::get_test_instance(
+                    &mut rng,
+                )),
+                DeprecatedContractClassAbiEntry::Constructor(FunctionAbiEntry::get_test_instance(
+                    &mut rng,
+                )),
+                DeprecatedContractClassAbiEntry::L1Handler(FunctionAbiEntry::get_test_instance(
+                    &mut rng,
+                )),
+                DeprecatedContractClassAbiEntry::Struct(StructAbiEntry::get_test_instance(
+                    &mut rng,
+                )),
+            ])
+        } else {
+            None
+        };
+        let tx = BroadcastedDeclareTransaction::V1(BroadcastedDeclareV1Transaction {
+            contract_class: DeprecatedContractClass {
+                abi,
+                entry_points_by_type: HashMap::from([
+                    (
+                        DeprecatedEntryPointType::Constructor,
+                        vec![DeprecatedEntryPoint::get_test_instance(&mut rng)],
+                    ),
+                    (
+                        DeprecatedEntryPointType::External,
+                        vec![DeprecatedEntryPoint::get_test_instance(&mut rng)],
+                    ),
+                    (
+                        DeprecatedEntryPointType::L1Handler,
+                        vec![DeprecatedEntryPoint::get_test_instance(&mut rng)],
+                    ),
+                ]),
+                ..GetTestInstance::get_test_instance(&mut rng)
+            },
+            ..GetTestInstance::get_test_instance(&mut rng)
+        });
+        validate_tx_fits_rpc(tx);
+    }
+}
+
+#[test]
+fn declare_v2_fits_rpc() {
+    let mut rng = get_rng();
+    let tx = BroadcastedDeclareTransaction::V2(BroadcastedDeclareV2Transaction::get_test_instance(
+        &mut rng,
+    ));
+    validate_tx_fits_rpc(tx);
+}
