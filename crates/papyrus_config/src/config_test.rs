e9a82bc95a34bb3fdba471081f0561f67f7f4a30
Yoav Gross <yoav@starkware.co>
2023-08-07 16:22:36 +0300

    refactor(config): process loaded values instead of serialized params

--- crates/papyrus_config/src/config_test.rs
+++ crates/papyrus_config/src/config_test.rs
@@ -22,6 +22,7 @@ use crate::dumping::{
 use crate::loading::{
     get_maps_from_raw_json,
     load,
+    remove_description,
     update_config_map_by_custom_config,
     update_config_map_by_pointers,
     update_optional_values,
@@ -68,7 +69,7 @@ fn dump_and_load_config() {
         OuterConfig { opt_elem: None, opt_config: None, inner_config: InnerConfig { o: 5 } };
 
     for outer_config in [some_outer_config, none_outer_config] {
-        let mut dumped = outer_config.dump();
+        let mut dumped = remove_description(outer_config.dump());
         update_optional_values(&mut dumped);
         let loaded_config = load::<OuterConfig>(&dumped).unwrap();
         assert_eq!(loaded_config, outer_config);
@@ -96,20 +97,21 @@ impl SerializeConfig for TypicalConfig {
 #[test]
 fn test_update_dumped_config() {
     let command = Command::new("Testing");
-    let mut dumped_config =
+    let dumped_config =
         TypicalConfig { a: Duration::from_secs(1), b: "bbb".to_owned(), c: false }.dump();
     let args = vec!["Testing", "--a", "1234", "--b", "15"];
     env::set_var("C", "true");
     let args: Vec<String> = args.into_iter().map(|s| s.to_owned()).collect();
 
     let arg_matches = get_command_matches(&dumped_config, command, args).unwrap();
-    update_config_map_by_command_args(&mut dumped_config, &arg_matches).unwrap();
+    let mut config_map = remove_description(dumped_config);
+    update_config_map_by_command_args(&mut config_map, &arg_matches).unwrap();
 
-    assert_eq!(json!(1234), dumped_config["a"].value);
-    assert_eq!(json!("15"), dumped_config["b"].value);
-    assert_eq!(json!(true), dumped_config["c"].value);
+    assert_eq!(json!(1234), config_map["a"]);
+    assert_eq!(json!("15"), config_map["b"]);
+    assert_eq!(json!(true), config_map["c"]);
 
-    let loaded_config: TypicalConfig = load(&dumped_config).unwrap();
+    let loaded_config: TypicalConfig = load(&config_map).unwrap();
     assert_eq!(Duration::from_millis(1234), loaded_config.a);
 }
 
@@ -139,15 +141,17 @@ fn test_pointers_flow() {
 
     let serialized = serde_json::to_string(&stored_map).unwrap();
     let loaded = serde_json::from_str(&serialized).unwrap();
-    let (mut loaded_config_map, loaded_pointers_map) = get_maps_from_raw_json(loaded);
-    update_config_map_by_pointers(&mut loaded_config_map, &loaded_pointers_map).unwrap();
-    assert_eq!(loaded_config_map["a1"].value, json!(10));
-    assert_eq!(loaded_config_map["a1"], loaded_config_map["a2"]);
+    let (loaded_config_map, loaded_pointers_map) = get_maps_from_raw_json(loaded);
+    let mut config_map = remove_description(loaded_config_map);
+    update_config_map_by_pointers(&mut config_map, &loaded_pointers_map).unwrap();
+    assert_eq!(config_map["a1"], json!(10));
+    assert_eq!(config_map["a1"], config_map["a2"]);
 }
 
 #[test]
 fn test_replace_pointers() {
-    let mut config_map = BTreeMap::from([ser_param("a", &json!(5), "This is a.")]);
+    let mut config_map =
+        remove_description(BTreeMap::from([ser_param("a", &json!(5), "This is a.")]));
     let pointers_map =
         BTreeMap::from([("b".to_owned(), "a".to_owned()), ("c".to_owned(), "a".to_owned())]);
     update_config_map_by_pointers(&mut config_map, &pointers_map).unwrap();
@@ -160,12 +164,15 @@ fn test_replace_pointers() {
 
 #[test]
 fn test_update_by_custom_config() {
-    let mut config_map =
-        BTreeMap::from([ser_param("param_path", &json!("default value"), "This is a.")]);
+    let mut config_map = remove_description(BTreeMap::from([ser_param(
+        "param_path",
+        &json!("default value"),
+        "This is a.",
+    )]));
     let custom_config_path =
         get_absolute_path("crates/papyrus_config/resources/custom_config_example.json");
     update_config_map_by_custom_config(&mut config_map, &custom_config_path).unwrap();
-    assert_eq!(config_map["param_path"].value, json!("custom value"));
+    assert_eq!(config_map["param_path"], json!("custom value"));
 }
 
 #[test]
