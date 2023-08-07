77211ef9b66b08c4452365d4793b97a41718dd94
ShahakShama <70578257+ShahakShama@users.noreply.github.com>
2023-08-07 08:24:04 +0300

    shahak/json rpc/add ok result from response (#974)
    feat(JSON-RPC): add conversion from response to AddOkResult

--- crates/papyrus_gateway/src/v0_4_0/write_api_result_test.rs
+++ crates/papyrus_gateway/src/v0_4_0/write_api_result_test.rs
@@ -1,6 +1,11 @@
 use serde::Serialize;
-use starknet_api::core::{ClassHash, ContractAddress};
+use starknet_api::core::{ClassHash, ContractAddress, PatriciaKey};
+use starknet_api::hash::StarkFelt;
+use starknet_api::stark_felt;
 use starknet_api::transaction::TransactionHash;
+use starknet_client::writer::objects::response::{
+    DeclareResponse, DeployAccountResponse, InvokeResponse, SuccessfulStarknetErrorCode,
+};
 use test_utils::{auto_impl_get_test_instance, get_rng, GetTestInstance};
 
 use super::{AddDeclareOkResult, AddDeployAccountOkResult, AddInvokeOkResult};
@@ -43,4 +48,41 @@ fn add_declare_ok_result_fits_rpc() {
 #[test]
 fn add_deploy_account_ok_result_fits_rpc() {
     test_ok_result_fits_rpc::<AddDeployAccountOkResult>("starknet_addDeployAccountTransaction");
+}
+
+#[test]
+fn add_invoke_ok_result_from_response() {
+    let transaction_hash = TransactionHash(stark_felt!("0x12345"));
+    let ok_result = AddInvokeOkResult::from(InvokeResponse {
+        code: SuccessfulStarknetErrorCode::default(),
+        transaction_hash,
+    });
+    let expected_ok_result = AddInvokeOkResult { transaction_hash };
+    assert_eq!(expected_ok_result, ok_result);
+}
+
+#[test]
+fn add_declare_ok_result_from_response() {
+    let transaction_hash = TransactionHash(stark_felt!("0x12345"));
+    let class_hash = ClassHash(stark_felt!("0xabcde"));
+    let ok_result = AddDeclareOkResult::from(DeclareResponse {
+        code: SuccessfulStarknetErrorCode::default(),
+        transaction_hash,
+        class_hash,
+    });
+    let expected_ok_result = AddDeclareOkResult { transaction_hash, class_hash };
+    assert_eq!(expected_ok_result, ok_result);
+}
+
+#[test]
+fn add_deploy_account_ok_result_from_response() {
+    let transaction_hash = TransactionHash(stark_felt!("0x12345"));
+    let contract_address = ContractAddress(PatriciaKey::try_from(stark_felt!("0xabcde")).unwrap());
+    let ok_result = AddDeployAccountOkResult::from(DeployAccountResponse {
+        code: SuccessfulStarknetErrorCode::default(),
+        transaction_hash,
+        address: contract_address,
+    });
+    let expected_ok_result = AddDeployAccountOkResult { transaction_hash, contract_address };
+    assert_eq!(expected_ok_result, ok_result);
 }
