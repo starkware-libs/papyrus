70650f4aaa4c8414190e5929dc900fdfa36f4a05
ShahakShama <70578257+ShahakShama@users.noreply.github.com>
2023-08-07 13:01:24 +0300

    feat(JSON-RPC): add conversion from StarknetError to AddTransactionError (#989)

--- crates/papyrus_gateway/src/v0_4_0/write_api_error_test.rs
+++ crates/papyrus_gateway/src/v0_4_0/write_api_error_test.rs
@@ -1,4 +1,5 @@
 use serde::Serialize;
+use starknet_client::starknet_error::{KnownStarknetErrorCode, StarknetError, StarknetErrorCode};
 
 use super::{
     AddDeclareError, AddDeployAccountError, AddInvokeError, ClassAlreadyDeclared,
@@ -18,85 +19,259 @@ fn test_error_fits_rpc<AddError: Serialize>(error: AddError, spec_method: &str) 
     assert!(schema.is_valid(&serde_json::to_value(error).unwrap()));
 }
 
+const MESSAGE: &str = "message";
+
+fn invoke_errors_and_starknet_error_codes() -> Vec<(AddInvokeError, StarknetErrorCode)> {
+    vec![
+        (
+            AddInvokeError::InsufficientAccountBalance(InsufficientAccountBalance::Error(
+                ErrorCode::default(),
+            )),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::InsufficientAccountBalance),
+        ),
+        (
+            AddInvokeError::InsufficientMaxFee(InsufficientMaxFee::Error(ErrorCode::default())),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::InsufficientMaxFee),
+        ),
+        (
+            AddInvokeError::InvalidTransactionNonce(InvalidTransactionNonce::Error(
+                ErrorCode::default(),
+            )),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::InvalidTransactionNonce),
+        ),
+        (
+            AddInvokeError::ValidationFailure(ValidationFailure::Error(ErrorCode::default())),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::ValidateFailure),
+        ),
+        (
+            AddInvokeError::NonAccount(NonAccount::Error(ErrorCode::default())),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::EntryPointNotFoundInContract),
+        ),
+        (
+            AddInvokeError::DuplicateTx(DuplicateTx::Error(ErrorCode::default())),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::DuplicatedTransaction),
+        ),
+        (
+            AddInvokeError::UnsupportedTxVersion(UnsupportedTxVersion::Error(ErrorCode::default())),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::InvalidTransactionVersion),
+        ),
+        (
+            AddInvokeError::UnexpectedError(UnexpectedError::Error(ErrorCodeWithData {
+                data: MESSAGE.to_string(),
+                ..Default::default()
+            })),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::BlockNotFound),
+        ),
+        (
+            AddInvokeError::UnexpectedError(UnexpectedError::Error(ErrorCodeWithData {
+                data: MESSAGE.to_string(),
+                ..Default::default()
+            })),
+            StarknetErrorCode::UnknownErrorCode("CODE".to_string()),
+        ),
+    ]
+}
+
+fn declare_errors_and_starknet_error_codes() -> Vec<(AddDeclareError, StarknetErrorCode)> {
+    vec![
+        (
+            AddDeclareError::ClassAlreadyDeclared(
+                ClassAlreadyDeclared::Error(ErrorCode::default()),
+            ),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::ClassAlreadyDeclared),
+        ),
+        (
+            AddDeclareError::CompilationFailed(CompilationFailed::Error(ErrorCode::default())),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::CompilationFailed),
+        ),
+        (
+            AddDeclareError::CompiledClassHashMismatch(CompiledClassHashMismatch::Error(
+                ErrorCode::default(),
+            )),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::InvalidCompiledClassHash),
+        ),
+        (
+            AddDeclareError::InsufficientAccountBalance(InsufficientAccountBalance::Error(
+                ErrorCode::default(),
+            )),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::InsufficientAccountBalance),
+        ),
+        (
+            AddDeclareError::InsufficientMaxFee(InsufficientMaxFee::Error(ErrorCode::default())),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::InsufficientMaxFee),
+        ),
+        (
+            AddDeclareError::InvalidTransactionNonce(InvalidTransactionNonce::Error(
+                ErrorCode::default(),
+            )),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::InvalidTransactionNonce),
+        ),
+        (
+            AddDeclareError::ValidationFailure(ValidationFailure::Error(ErrorCode::default())),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::ValidateFailure),
+        ),
+        (
+            AddDeclareError::NonAccount(NonAccount::Error(ErrorCode::default())),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::EntryPointNotFoundInContract),
+        ),
+        (
+            AddDeclareError::DuplicateTx(DuplicateTx::Error(ErrorCode::default())),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::DuplicatedTransaction),
+        ),
+        (
+            AddDeclareError::ContractClassSizeIsTooLarge(ContractClassSizeIsTooLarge::Error(
+                ErrorCode::default(),
+            )),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::ContractBytecodeSizeTooLarge),
+        ),
+        (
+            AddDeclareError::ContractClassSizeIsTooLarge(ContractClassSizeIsTooLarge::Error(
+                ErrorCode::default(),
+            )),
+            StarknetErrorCode::KnownErrorCode(
+                KnownStarknetErrorCode::ContractClassObjectSizeTooLarge,
+            ),
+        ),
+        (
+            AddDeclareError::UnsupportedTxVersion(
+                UnsupportedTxVersion::Error(ErrorCode::default()),
+            ),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::InvalidTransactionVersion),
+        ),
+        (
+            AddDeclareError::UnsupportedContractClassVersion(
+                UnsupportedContractClassVersion::Error(ErrorCode::default()),
+            ),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::InvalidContractClassVersion),
+        ),
+        (
+            AddDeclareError::UnexpectedError(UnexpectedError::Error(ErrorCodeWithData {
+                data: MESSAGE.to_string(),
+                ..Default::default()
+            })),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::BlockNotFound),
+        ),
+        (
+            AddDeclareError::UnexpectedError(UnexpectedError::Error(ErrorCodeWithData {
+                data: MESSAGE.to_string(),
+                ..Default::default()
+            })),
+            StarknetErrorCode::UnknownErrorCode("CODE".to_string()),
+        ),
+    ]
+}
+
+fn deploy_account_errors_and_starknet_error_codes()
+-> Vec<(AddDeployAccountError, StarknetErrorCode)> {
+    vec![
+        (
+            AddDeployAccountError::InsufficientAccountBalance(InsufficientAccountBalance::Error(
+                ErrorCode::default(),
+            )),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::InsufficientAccountBalance),
+        ),
+        (
+            AddDeployAccountError::InsufficientMaxFee(InsufficientMaxFee::Error(
+                ErrorCode::default(),
+            )),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::InsufficientMaxFee),
+        ),
+        (
+            AddDeployAccountError::InvalidTransactionNonce(InvalidTransactionNonce::Error(
+                ErrorCode::default(),
+            )),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::InvalidTransactionNonce),
+        ),
+        (
+            AddDeployAccountError::ValidationFailure(
+                ValidationFailure::Error(ErrorCode::default()),
+            ),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::ValidateFailure),
+        ),
+        (
+            AddDeployAccountError::NonAccount(NonAccount::Error(ErrorCode::default())),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::EntryPointNotFoundInContract),
+        ),
+        (
+            AddDeployAccountError::ClassHashNotFound(
+                ClassHashNotFound::Error(ErrorCode::default()),
+            ),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::UndeclaredClass),
+        ),
+        (
+            AddDeployAccountError::DuplicateTx(DuplicateTx::Error(ErrorCode::default())),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::DuplicatedTransaction),
+        ),
+        (
+            AddDeployAccountError::UnsupportedTxVersion(UnsupportedTxVersion::Error(
+                ErrorCode::default(),
+            )),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::InvalidTransactionVersion),
+        ),
+        (
+            AddDeployAccountError::UnexpectedError(UnexpectedError::Error(ErrorCodeWithData {
+                data: MESSAGE.to_string(),
+                ..Default::default()
+            })),
+            StarknetErrorCode::KnownErrorCode(KnownStarknetErrorCode::BlockNotFound),
+        ),
+        (
+            AddDeployAccountError::UnexpectedError(UnexpectedError::Error(ErrorCodeWithData {
+                data: MESSAGE.to_string(),
+                ..Default::default()
+            })),
+            StarknetErrorCode::UnknownErrorCode("CODE".to_string()),
+        ),
+    ]
+}
+
 #[test]
 fn add_invoke_error_fits_rpc() {
-    for error in [
-        AddInvokeError::InsufficientAccountBalance(InsufficientAccountBalance::Error(
-            ErrorCode::default(),
-        )),
-        AddInvokeError::InsufficientMaxFee(InsufficientMaxFee::Error(ErrorCode::default())),
-        AddInvokeError::InvalidTransactionNonce(InvalidTransactionNonce::Error(
-            ErrorCode::default(),
-        )),
-        AddInvokeError::ValidationFailure(ValidationFailure::Error(ErrorCode::default())),
-        AddInvokeError::NonAccount(NonAccount::Error(ErrorCode::default())),
-        AddInvokeError::DuplicateTx(DuplicateTx::Error(ErrorCode::default())),
-        AddInvokeError::UnsupportedTxVersion(UnsupportedTxVersion::Error(ErrorCode::default())),
-        AddInvokeError::UnexpectedError(UnexpectedError::Error(ErrorCodeWithData {
-            data: "data".to_string(),
-            ..Default::default()
-        })),
-    ] {
+    for (error, _) in invoke_errors_and_starknet_error_codes() {
         test_error_fits_rpc(error, "starknet_addInvokeTransaction");
     }
 }
 
 #[test]
+fn add_invoke_error_from_starknet_error() {
+    for (error, starknet_error_code) in invoke_errors_and_starknet_error_codes() {
+        assert_eq!(
+            error,
+            StarknetError { code: starknet_error_code, message: MESSAGE.to_string() }.into()
+        );
+    }
+}
+
+#[test]
 fn add_declare_error_fits_rpc() {
-    for error in [
-        AddDeclareError::ClassAlreadyDeclared(ClassAlreadyDeclared::Error(ErrorCode::default())),
-        AddDeclareError::CompilationFailed(CompilationFailed::Error(ErrorCode::default())),
-        AddDeclareError::CompiledClassHashMismatch(CompiledClassHashMismatch::Error(
-            ErrorCode::default(),
-        )),
-        AddDeclareError::InsufficientAccountBalance(InsufficientAccountBalance::Error(
-            ErrorCode::default(),
-        )),
-        AddDeclareError::InsufficientMaxFee(InsufficientMaxFee::Error(ErrorCode::default())),
-        AddDeclareError::InvalidTransactionNonce(InvalidTransactionNonce::Error(
-            ErrorCode::default(),
-        )),
-        AddDeclareError::ValidationFailure(ValidationFailure::Error(ErrorCode::default())),
-        AddDeclareError::NonAccount(NonAccount::Error(ErrorCode::default())),
-        AddDeclareError::DuplicateTx(DuplicateTx::Error(ErrorCode::default())),
-        AddDeclareError::ContractClassSizeIsTooLarge(ContractClassSizeIsTooLarge::Error(
-            ErrorCode::default(),
-        )),
-        AddDeclareError::UnsupportedTxVersion(UnsupportedTxVersion::Error(ErrorCode::default())),
-        AddDeclareError::UnsupportedContractClassVersion(UnsupportedContractClassVersion::Error(
-            ErrorCode::default(),
-        )),
-        AddDeclareError::UnexpectedError(UnexpectedError::Error(ErrorCodeWithData {
-            data: "data".to_string(),
-            ..Default::default()
-        })),
-    ] {
+    for (error, _) in declare_errors_and_starknet_error_codes() {
         test_error_fits_rpc(error, "starknet_addDeclareTransaction");
     }
 }
 
 #[test]
+fn add_declare_error_from_starknet_error() {
+    for (error, starknet_error_code) in declare_errors_and_starknet_error_codes() {
+        assert_eq!(
+            error,
+            StarknetError { code: starknet_error_code, message: MESSAGE.to_string() }.into()
+        );
+    }
+}
+
+#[test]
 fn add_deploy_account_error_fits_rpc() {
-    for error in [
-        AddDeployAccountError::InsufficientAccountBalance(InsufficientAccountBalance::Error(
-            ErrorCode::default(),
-        )),
-        AddDeployAccountError::InsufficientMaxFee(InsufficientMaxFee::Error(ErrorCode::default())),
-        AddDeployAccountError::InvalidTransactionNonce(InvalidTransactionNonce::Error(
-            ErrorCode::default(),
-        )),
-        AddDeployAccountError::ValidationFailure(ValidationFailure::Error(ErrorCode::default())),
-        AddDeployAccountError::NonAccount(NonAccount::Error(ErrorCode::default())),
-        AddDeployAccountError::ClassHashNotFound(ClassHashNotFound::Error(ErrorCode::default())),
-        AddDeployAccountError::DuplicateTx(DuplicateTx::Error(ErrorCode::default())),
-        AddDeployAccountError::UnsupportedTxVersion(UnsupportedTxVersion::Error(
-            ErrorCode::default(),
-        )),
-        AddDeployAccountError::UnexpectedError(UnexpectedError::Error(ErrorCodeWithData {
-            data: "data".to_string(),
-            ..Default::default()
-        })),
-    ] {
+    for (error, _) in deploy_account_errors_and_starknet_error_codes() {
         test_error_fits_rpc(error, "starknet_addDeployAccountTransaction");
+    }
+}
+
+#[test]
+fn add_deploy_account_error_from_starknet_error() {
+    for (error, starknet_error_code) in deploy_account_errors_and_starknet_error_codes() {
+        assert_eq!(
+            error,
+            StarknetError { code: starknet_error_code, message: MESSAGE.to_string() }.into()
+        );
     }
 }
