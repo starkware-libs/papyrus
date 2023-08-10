443c9c5204308b08e98b8b987bea8796ac5bb92d
Shahak Shama <shahak@starkware.co>
2023-08-10 17:28:45 +0300

    feat(starknet_client): change DeprecatedContractClass to use abi from starknet_api

--- crates/starknet_client/src/writer/objects/test_utils.rs
+++ crates/starknet_client/src/writer/objects/test_utils.rs
@@ -1,33 +1,21 @@
 use std::collections::HashMap;
 
 use starknet_api::deprecated_contract_class::{
+    ContractClassAbiEntry as DeprecatedContractClassAbiEntry,
     EntryPoint as DeprecatedEntryPoint,
     EntryPointType as DeprecatedEntryPointType,
-    EventAbiEntry,
-    FunctionAbiEntry,
-    StructAbiEntry,
 };
 use starknet_api::transaction::TransactionHash;
 use test_utils::{auto_impl_get_test_instance, get_number_of_variants, GetTestInstance};
 
 use crate::writer::objects::response::{InvokeResponse, SuccessfulStarknetErrorCode};
-use crate::writer::objects::transaction::{
-    DeprecatedContractClass,
-    DeprecatedContractClassAbiEntry,
-};
+use crate::writer::objects::transaction::DeprecatedContractClass;
 
 auto_impl_get_test_instance! {
     pub struct DeprecatedContractClass {
         pub abi: Option<Vec<DeprecatedContractClassAbiEntry>>,
         pub compressed_program: String,
         pub entry_points_by_type: HashMap<DeprecatedEntryPointType, Vec<DeprecatedEntryPoint>>,
-    }
-    pub enum DeprecatedContractClassAbiEntry {
-        Event(EventAbiEntry) = 0,
-        Function(FunctionAbiEntry) = 1,
-        Constructor(FunctionAbiEntry) = 2,
-        L1Handler(FunctionAbiEntry) = 3,
-        Struct(StructAbiEntry) = 4,
     }
     pub struct InvokeResponse {
         pub code: SuccessfulStarknetErrorCode,
