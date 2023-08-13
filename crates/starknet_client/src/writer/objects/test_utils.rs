ed753c6c11b62255f65b1ee47f5cdb149b984d80
ShahakShama <70578257+ShahakShama@users.noreply.github.com>
2023-08-13 11:45:57 +0300

    feat(JSON-RPC): add starknet_addDeployAccountTransaction (#1021)

--- crates/starknet_client/src/writer/objects/test_utils.rs
+++ crates/starknet_client/src/writer/objects/test_utils.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+use starknet_api::core::ContractAddress;
 use starknet_api::deprecated_contract_class::{
     ContractClassAbiEntry as DeprecatedContractClassAbiEntry,
     EntryPoint as DeprecatedEntryPoint,
@@ -8,7 +9,11 @@ use starknet_api::deprecated_contract_class::{
 use starknet_api::transaction::TransactionHash;
 use test_utils::{auto_impl_get_test_instance, get_number_of_variants, GetTestInstance};
 
-use crate::writer::objects::response::{InvokeResponse, SuccessfulStarknetErrorCode};
+use crate::writer::objects::response::{
+    DeployAccountResponse,
+    InvokeResponse,
+    SuccessfulStarknetErrorCode,
+};
 use crate::writer::objects::transaction::DeprecatedContractClass;
 
 auto_impl_get_test_instance! {
@@ -20,6 +25,11 @@ auto_impl_get_test_instance! {
     pub struct InvokeResponse {
         pub code: SuccessfulStarknetErrorCode,
         pub transaction_hash: TransactionHash,
+    }
+    pub struct DeployAccountResponse {
+        pub code: SuccessfulStarknetErrorCode,
+        pub transaction_hash: TransactionHash,
+        pub address: ContractAddress,
     }
     pub enum SuccessfulStarknetErrorCode {
         TransactionReceived = 0,
