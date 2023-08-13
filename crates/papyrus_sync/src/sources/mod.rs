c8abd2cec2b06e225344f0dd52de136a8474f73c
DvirYo-starkware <115620476+DvirYo-starkware@users.noreply.github.com>
2023-08-13 10:51:16 +0300

    refactor(sync, node): make the source module in sync public (#1031)
    refactor(sync): make the source module in sync public

--- crates/papyrus_sync/src/sources/mod.rs
+++ crates/papyrus_sync/src/sources/mod.rs
@@ -1,24 +1,6 @@
-mod base_layer;
-mod central;
+pub mod base_layer;
+pub mod central;
 #[cfg(test)]
 mod central_sync_test;
 #[cfg(test)]
 mod central_test;
-
-#[cfg(test)]
-pub(crate) use base_layer::MockBaseLayerSourceTrait;
-pub use base_layer::{
-    BaseLayerSourceError,
-    BaseLayerSourceErrorTrait,
-    BaseLayerSourceTrait,
-    EthereumBaseLayerSource,
-};
-#[cfg(test)]
-pub(crate) use central::MockCentralSourceTrait;
-pub use central::{
-    CentralError,
-    CentralResult,
-    CentralSource,
-    CentralSourceConfig,
-    CentralSourceTrait,
-};
