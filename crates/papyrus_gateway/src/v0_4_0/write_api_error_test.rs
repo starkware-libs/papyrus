12410d0ac8f510452f90fd709dfbd71b4dd51daf
ShahakShama <70578257+ShahakShama@users.noreply.github.com>
2023-08-15 13:30:54 +0300

    test(JSON-RPC): add regression test for starknet error conversion (#1038)

--- crates/papyrus_gateway/src/v0_4_0/write_api_error_test.rs
+++ crates/papyrus_gateway/src/v0_4_0/write_api_error_test.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeMap;
+
 use enum_iterator::all;
 use jsonrpsee::types::ErrorObjectOwned;
 use starknet_client::starknet_error::{KnownStarknetErrorCode, StarknetError, StarknetErrorCode};
@@ -14,6 +16,13 @@ use crate::version_config::VERSION_0_4;
 const MESSAGE: &str = "message";
 const UNKNOWN_CODE: &str = "code";
 
+fn starknet_errors() -> impl Iterator<Item = StarknetError> {
+    all::<KnownStarknetErrorCode>()
+        .map(StarknetErrorCode::KnownErrorCode)
+        .chain([StarknetErrorCode::UnknownErrorCode(UNKNOWN_CODE.to_owned())])
+        .map(|error_code| StarknetError { code: error_code, message: MESSAGE.to_owned() })
+}
+
 fn test_error_from_conversion_fits_rpc<F: Fn(StarknetError) -> JsonRpcError>(
     f: F,
     spec_method: &str,
@@ -22,12 +31,7 @@ fn test_error_from_conversion_fits_rpc<F: Fn(StarknetError) -> JsonRpcError>(
         &[(SpecFile::StarknetWriteApi, &[spec_method])],
         &VERSION_0_4,
     );
-    for starknet_error_code in all::<KnownStarknetErrorCode>()
-        .map(StarknetErrorCode::KnownErrorCode)
-        .chain([StarknetErrorCode::UnknownErrorCode(UNKNOWN_CODE.to_owned())])
-    {
-        let starknet_error =
-            StarknetError { code: starknet_error_code, message: MESSAGE.to_owned() };
+    for starknet_error in starknet_errors() {
         // Converting into ErrorObjectOwned since it has serialization.
         let rpc_error: ErrorObjectOwned = f(starknet_error).into();
         let mut json_value = serde_json::to_value(rpc_error).unwrap();
@@ -58,4 +62,29 @@ fn starknet_error_to_deploy_account_error_result_fits_specs() {
         starknet_error_to_deploy_account_error,
         "starknet_addDeployAccountTransaction",
     );
+}
+
+fn get_conversion_snapshot<F: Fn(StarknetError) -> JsonRpcError>(f: F) -> serde_json::Value {
+    // Using BTreeMap to keep the keys sorted.
+    let mut rpc_error_code_to_errors = BTreeMap::new();
+    for starknet_error in starknet_errors() {
+        let rpc_error: ErrorObjectOwned = f(starknet_error.clone()).into();
+        rpc_error_code_to_errors.insert(rpc_error.code(), (rpc_error, starknet_error));
+    }
+    serde_json::to_value(rpc_error_code_to_errors).unwrap()
+}
+
+#[test]
+fn starknet_error_to_invoke_error_snapshot() {
+    insta::assert_json_snapshot!(get_conversion_snapshot(starknet_error_to_invoke_error));
+}
+
+#[test]
+fn starknet_error_to_declare_error_snapshot() {
+    insta::assert_json_snapshot!(get_conversion_snapshot(starknet_error_to_declare_error));
+}
+
+#[test]
+fn starknet_error_to_deploy_account_error_snapshot() {
+    insta::assert_json_snapshot!(get_conversion_snapshot(starknet_error_to_deploy_account_error));
 }
