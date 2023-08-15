4c1f00085aa4bda6588e757b6c214ae85ac90cdc
Dvir Yosef <dvir@starkware.co>
2023-08-15 09:45:48 +0300

    CR fix.

--- crates/papyrus_node/src/main.rs
+++ crates/papyrus_node/src/main.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+mod main_test;
+
 use std::env::args;
 use std::sync::Arc;
 
@@ -125,28 +128,4 @@ async fn main() -> anyhow::Result<()> {
     configure_tracing();
     info!("Booting up.");
     run_threads(config).await
-}
-
-#[cfg(test)]
-mod main_test {
-    use assert_matches::assert_matches;
-    use papyrus_node::config::NodeConfig;
-    use tempdir::TempDir;
-
-    use crate::run_threads;
-
-    #[tokio::test]
-    async fn run_threads_stop() {
-        let tmp_data_dir = TempDir::new("./data_for_test").unwrap();
-        let mut config = NodeConfig::default();
-        config.storage.db_config.path_prefix = tmp_data_dir.path().into();
-
-        // Error when not overriding the base layer node URL.
-        assert_matches!(run_threads(config.clone()).await, Err(_));
-
-        // Error when not supplying legal central URL.
-        config.base_layer.node_url = "value".to_string();
-        config.central.url = "_not_legal_url".to_string();
-        assert_matches!(run_threads(config.clone()).await, Err(_));
-    }
 }
---
+++ crates/papyrus_node/src/main_test.rs
@@ -0,0 +1,20 @@
+use papyrus_node::config::NodeConfig;
+use tempdir::TempDir;
+
+use crate::run_threads;
+
+#[tokio::test]
+async fn run_threads_stop() {
+    let tmp_data_dir = TempDir::new("./data_for_test").unwrap();
+    let mut config = NodeConfig::default();
+    config.storage.db_config.path_prefix = tmp_data_dir.path().into();
+
+    // Error when not supplying legal base layer URL.
+    config.base_layer.node_url = "_not_legal_url".to_string();
+    assert!(run_threads(config.clone()).await.is_err());
+
+    // Error when not supplying legal central URL.
+    config.base_layer.node_url = "value".to_string();
+    config.central.url = "_not_legal_url".to_string();
+    assert!(run_threads(config.clone()).await.is_err());
+}
