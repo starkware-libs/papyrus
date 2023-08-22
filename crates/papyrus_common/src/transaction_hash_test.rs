6133bea690aaea572c26c91020072a93cedbb36b
Yoav Gross <yoav@starkware.co>
2023-08-22 17:08:15 +0300

    feat(common): calculate deploy transaction hash

--- crates/papyrus_common/src/transaction_hash_test.rs
+++ crates/papyrus_common/src/transaction_hash_test.rs
@@ -3,10 +3,10 @@ use std::io::BufReader;
 use serde::{Deserialize, Serialize};
 use starknet_api::core::ChainId;
 use starknet_api::hash::{StarkFelt, StarkHash};
-use starknet_api::transaction::DeployAccountTransaction;
+use starknet_api::transaction::Transaction;
 use test_utils::get_absolute_path;
 
-use super::{ascii_as_felt, get_deploy_account_transaction_hash};
+use super::{ascii_as_felt, get_tx_hash, validate_tx_hash};
 
 #[test]
 fn test_ascii_as_felt() {
@@ -19,18 +19,28 @@ fn test_ascii_as_felt() {
 
 #[derive(Deserialize, Serialize)]
 struct TxWithHash {
-    tx: DeployAccountTransaction,
+    tx: Transaction,
     tx_hash: StarkHash,
+    is_deprecated: bool,
 }
 
 #[test]
-fn test_deploy_account_transaction_hash() {
+fn test_transaction_hash() {
+    // The tx details were taken from
+    // https://alpha4.starknet.io/feeder_gateway/get_block?blockNumber=385429
+    // https://alpha4.starknet.io/feeder_gateway/get_block?blockNumber=0
+    let chain_id = ChainId("SN_GOERLI".to_owned());
+
     let file_path = get_absolute_path("crates/papyrus_common/resources/tx_hash.json");
     let file = std::fs::File::open(file_path).unwrap();
     let reader = BufReader::new(file);
-    let tx_with_hash: TxWithHash = serde_json::from_reader(reader).unwrap();
+    let txs_with_hash: Vec<TxWithHash> = serde_json::from_reader(reader).unwrap();
 
-    let chain_id = ChainId("SN_GOERLI".to_owned());
-    let actual_tx_hash = get_deploy_account_transaction_hash(&tx_with_hash.tx, &chain_id).unwrap();
-    assert_eq!(actual_tx_hash, tx_with_hash.tx_hash);
+    for tx_with_hash in txs_with_hash {
+        assert!(validate_tx_hash(&tx_with_hash.tx, &chain_id, tx_with_hash.tx_hash).unwrap());
+        if !tx_with_hash.is_deprecated {
+            let actual_tx_hash = get_tx_hash(&tx_with_hash.tx, &chain_id).unwrap();
+            assert_eq!(actual_tx_hash, tx_with_hash.tx_hash);
+        }
+    }
 }
