967cb0371ac721c785e9cd730c17746382f2732e
Yoav Gross <yoav@starkware.co>
2023-08-29 09:31:22 +0300

    feat(common): calculate patricia root hash

--- crates/papyrus_common/src/lib.rs
+++ crates/papyrus_common/src/lib.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 use starknet_api::block::{BlockHash, BlockNumber};
 
 pub mod metrics;
+pub mod patricia_hash_tree;
 pub mod transaction_hash;
 
 #[derive(Copy, Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
---
+++ crates/papyrus_common/src/patricia_hash_tree_test.rs
@@ -0,0 +1,37 @@
+use starknet_api::hash::StarkFelt;
+
+use super::calculate_root;
+
+// The expected roots were calculated by the starkware-libs/cairo-lang repository. These are the
+// roots of PatriciaTree objects with the same leaves.
+#[test]
+fn test_patricia() {
+    let root =
+        calculate_root(vec![StarkFelt::from(1_u8), StarkFelt::from(2_u8), StarkFelt::from(3_u8)]);
+    let expected_root =
+        StarkFelt::try_from("0x231e110514ca3a27707cd6c365e00685142d43b03d26f6274db51cbfa91aa1c")
+            .unwrap();
+    assert_eq!(root, expected_root);
+}
+
+#[test]
+fn test_edge_patricia() {
+    let root = calculate_root(vec![StarkFelt::from(1_u8)]);
+    let expected_root =
+        StarkFelt::try_from("0x268a9d47dde48af4b6e2c33932ed1c13adec25555abaa837c376af4ea2f8ad4")
+            .unwrap();
+    assert_eq!(root, expected_root);
+}
+
+#[test]
+fn test_binary_patricia() {
+    let root = calculate_root(vec![StarkFelt::from(1_u8), StarkFelt::from(2_u8)]);
+    let expected_root =
+        StarkFelt::try_from("0x599927f1181d5633c6f680dbf039534de49c44e0b9903c5305b2582dfd6a56a")
+            .unwrap();
+    assert_eq!(root, expected_root);
+}
+
+#[test]
+fn test_transactions_root_block_15650() {
+}
