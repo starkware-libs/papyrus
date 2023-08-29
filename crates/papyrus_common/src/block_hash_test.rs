d0013fb11ebb9f725fbbba5ea0e75ddd0fefe1dc
Yoav Gross <yoav@starkware.co>
2023-08-29 09:31:22 +0300

    feat(common): calculate block header hash

---
+++ crates/papyrus_common/src/block_hash_test.rs
@@ -0,0 +1,41 @@
+use starknet_api::block::Block;
+use starknet_api::core::ChainId;
+use test_utils::read_json_file;
+
+use super::calculate_block_hash_by_version;
+use crate::block_hash::BlockHashVersion;
+
+fn validate_block_hash_util(file_name: &str, version: BlockHashVersion) -> bool {
+    let chain_id = ChainId("SN_MAIN".to_owned());
+    let block: Block = serde_json::from_value(read_json_file(file_name)).unwrap();
+    let calculated_hash = calculate_block_hash_by_version(&block, version, &chain_id).unwrap();
+    calculated_hash == block.header.block_hash.0
+}
+
+#[test]
+fn test_block_hash() {
+    assert!(validate_block_hash_util("block_hash.json", BlockHashVersion::V3));
+}
+
+#[test]
+fn test_deprecated_block_hash_v2() {
+    assert!(validate_block_hash_util("deprecated_block_hash_v2.json", BlockHashVersion::V2));
+}
+
+#[test]
+fn test_deprecated_block_hash_v1_no_events() {
+    assert!(validate_block_hash_util(
+        "deprecated_block_hash_v1_no_events.json",
+        BlockHashVersion::V1
+    ));
+}
+
+#[test]
+fn test_deprecated_block_hash_v1() {
+    assert!(validate_block_hash_util("deprecated_block_hash_v1.json", BlockHashVersion::V1));
+}
+
+#[test]
+fn test_deprecated_block_hash_v0() {
+    assert!(validate_block_hash_util("deprecated_block_hash_v0.json", BlockHashVersion::V0));
+}
