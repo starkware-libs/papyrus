7a940efae8242d58f4bbb1ba5eaf1801d3ea2397
ShahakShama <70578257+ShahakShama@users.noreply.github.com>
2023-08-31 13:59:17 +0300

    feat(network): add read_message and write_message (#1121)

--- crates/papyrus_network/src/messages/mod.rs
+++ crates/papyrus_network/src/messages/mod.rs
@@ -8,3 +8,26 @@ pub mod proto {
         }
     }
 }
+
+use std::io;
+
+use futures::{AsyncRead, AsyncWrite};
+use libp2p::core::upgrade::{read_length_prefixed, write_length_prefixed};
+use prost::Message;
+
+pub const MAX_MESSAGE_SIZE: usize = 1 << 20;
+
+pub async fn write_message<T: Message, Stream: AsyncWrite + Unpin>(
+    message: T,
+    mut io: Stream,
+) -> Result<(), io::Error> {
+    write_length_prefixed(&mut io, &message.encode_to_vec()).await?;
+    Ok(())
+}
+
+pub async fn read_message<T: Message + Default, Stream: AsyncRead + Unpin>(
+    mut io: Stream,
+) -> Result<T, io::Error> {
+    let buf = read_length_prefixed(&mut io, MAX_MESSAGE_SIZE).await?;
+    Ok(T::decode(buf.as_slice())?)
+}
