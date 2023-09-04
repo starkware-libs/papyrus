50376780de8455fa4f0931fc310dc8257e2c3057
Nevo Agmon <nevo@starkware.co>
2023-09-04 13:42:50 +0300

    feat(network): add get blocks inbound response logic

---
+++ crates/papyrus_network/src/get_blocks/handler.rs
@@ -0,0 +1,220 @@
+#[cfg(test)]
+#[path = "handler_test.rs"]
+mod handler_test;
+
+use std::collections::{HashMap, VecDeque};
+use std::io;
+use std::task::{Context, Poll};
+use std::time::Duration;
+
+use futures::channel::mpsc::{TrySendError, UnboundedReceiver};
+use futures::StreamExt;
+use libp2p::swarm::handler::{ConnectionEvent, DialUpgradeError, FullyNegotiatedOutbound};
+use libp2p::swarm::{
+    ConnectionHandler,
+    ConnectionHandlerEvent,
+    KeepAlive,
+    StreamUpgradeError,
+    SubstreamProtocol,
+};
+
+use super::protocol::{InboundProtocol, OutboundProtocol, OutboundProtocolError, PROTOCOL_NAME};
+use super::RequestId;
+use crate::messages::block::{GetBlocks, GetBlocksResponse};
+
+// TODO(shahak): Add a FromBehaviour event for cancelling an existing request.
+#[derive(Debug)]
+pub struct NewRequestEvent {
+    pub request: GetBlocks,
+    pub request_id: RequestId,
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum RequestError {
+    #[error("Connection timed out after {} seconds.", substream_timeout.as_secs())]
+    Timeout { substream_timeout: Duration },
+    #[error(transparent)]
+    IOError(#[from] io::Error),
+    #[error(transparent)]
+    ResponseSendError(#[from] TrySendError<GetBlocksResponse>),
+    #[error("Remote peer doesn't support the {PROTOCOL_NAME} protocol.")]
+    RemoteDoesntSupportProtocol,
+}
+
+#[derive(thiserror::Error, Debug)]
+#[error("Remote peer doesn't support the {PROTOCOL_NAME} protocol.")]
+pub struct RemoteDoesntSupportProtocolError;
+
+#[derive(Debug)]
+pub enum RequestProgressEvent {
+    ReceivedResponse { request_id: RequestId, response: GetBlocksResponse },
+    RequestFinished { request_id: RequestId },
+    RequestFailed { request_id: RequestId, error: RequestError },
+}
+
+type HandlerEvent<H> = ConnectionHandlerEvent<
+    <H as ConnectionHandler>::OutboundProtocol,
+    <H as ConnectionHandler>::OutboundOpenInfo,
+    <H as ConnectionHandler>::ToBehaviour,
+    <H as ConnectionHandler>::Error,
+>;
+
+pub struct Handler {
+    substream_timeout: Duration,
+    request_to_responses_receiver: HashMap<RequestId, UnboundedReceiver<GetBlocksResponse>>,
+    pending_events: VecDeque<HandlerEvent<Self>>,
+    ready_requests: VecDeque<(RequestId, GetBlocksResponse)>,
+}
+
+impl Handler {
+    // TODO(shahak) If we'll add more parameters, consider creating a HandlerConfig struct.
+    pub fn new(substream_timeout: Duration) -> Self {
+        Self {
+            substream_timeout,
+            request_to_responses_receiver: Default::default(),
+            pending_events: Default::default(),
+            ready_requests: Default::default(),
+        }
+    }
+
+    fn convert_upgrade_error(
+        &self,
+        error: StreamUpgradeError<OutboundProtocolError>,
+    ) -> RequestError {
+        match error {
+            StreamUpgradeError::Timeout => {
+                RequestError::Timeout { substream_timeout: self.substream_timeout }
+            }
+            StreamUpgradeError::Apply(request_protocol_error) => match request_protocol_error {
+                OutboundProtocolError::IOError(error) => RequestError::IOError(error),
+                OutboundProtocolError::ResponseSendError(error) => {
+                    RequestError::ResponseSendError(error)
+                }
+            },
+            StreamUpgradeError::NegotiationFailed => RequestError::RemoteDoesntSupportProtocol,
+            StreamUpgradeError::Io(error) => RequestError::IOError(error),
+        }
+    }
+
+    fn clear_pending_events_related_to_request(&mut self, request_id: RequestId) {
+        self.pending_events.retain(|event| match event {
+            ConnectionHandlerEvent::NotifyBehaviour(RequestProgressEvent::ReceivedResponse {
+                request_id: other_request_id,
+                ..
+            }) => request_id != *other_request_id,
+            _ => true,
+        })
+    }
+}
+
+impl ConnectionHandler for Handler {
+    type FromBehaviour = NewRequestEvent;
+    type ToBehaviour = RequestProgressEvent;
+    type Error = RemoteDoesntSupportProtocolError;
+    type InboundProtocol = InboundProtocol;
+    type OutboundProtocol = OutboundProtocol;
+    type InboundOpenInfo = ();
+    type OutboundOpenInfo = RequestId;
+
+    fn listen_protocol(&self) -> SubstreamProtocol<Self::InboundProtocol, Self::InboundOpenInfo> {
+        // TODO(nevo): use the inbound protocol channels.
+        let (inbound_protocol, _) = InboundProtocol::new();
+        SubstreamProtocol::new(inbound_protocol, ()).with_timeout(self.substream_timeout)
+    }
+
+    fn connection_keep_alive(&self) -> KeepAlive {
+        // TODO(shahak): Implement keep alive logic.
+        KeepAlive::Yes
+    }
+
+    fn poll(
+        &mut self,
+        cx: &mut Context<'_>,
+    ) -> Poll<
+        ConnectionHandlerEvent<
+            Self::OutboundProtocol,
+            Self::OutboundOpenInfo,
+            Self::ToBehaviour,
+            Self::Error,
+        >,
+    > {
+        if let Some(event) = self.pending_events.pop_front() {
+            return Poll::Ready(event);
+        }
+
+        for (request_id, responses_receiver) in &mut self.request_to_responses_receiver {
+            if let Poll::Ready(Some(response)) = responses_receiver.poll_next_unpin(cx) {
+                // Collect all ready responses to avoid starvation of the request ids at the end.
+                self.ready_requests.push_back((*request_id, response));
+            }
+        }
+        if let Some((request_id, response)) = self.ready_requests.pop_front() {
+            return Poll::Ready(ConnectionHandlerEvent::NotifyBehaviour(
+                RequestProgressEvent::ReceivedResponse { request_id, response },
+            ));
+        }
+
+        Poll::Pending
+    }
+
+    fn on_behaviour_event(&mut self, event: Self::FromBehaviour) {
+        let NewRequestEvent { request, request_id } = event;
+        let (outbound_protocol, responses_receiver) = OutboundProtocol::new(request);
+        let insert_result =
+            self.request_to_responses_receiver.insert(request_id, responses_receiver);
+        if insert_result.is_some() {
+            panic!("Multiple requests exist with the same ID {}", request_id);
+        }
+        self.pending_events.push_back(ConnectionHandlerEvent::OutboundSubstreamRequest {
+            protocol: SubstreamProtocol::new(outbound_protocol, request_id)
+                .with_timeout(self.substream_timeout),
+        });
+    }
+
+    fn on_connection_event(
+        &mut self,
+        event: ConnectionEvent<
+            '_,
+            Self::InboundProtocol,
+            Self::OutboundProtocol,
+            Self::InboundOpenInfo,
+            Self::OutboundOpenInfo,
+        >,
+    ) {
+        match event {
+            ConnectionEvent::FullyNegotiatedOutbound(FullyNegotiatedOutbound {
+                protocol: _,
+                info: request_id,
+            }) => {
+                self.pending_events.push_back(ConnectionHandlerEvent::NotifyBehaviour(
+                    RequestProgressEvent::RequestFinished { request_id },
+                ));
+                self.request_to_responses_receiver.remove(&request_id);
+            }
+            ConnectionEvent::DialUpgradeError(DialUpgradeError { info: request_id, error }) => {
+                let error = self.convert_upgrade_error(error);
+                if matches!(error, RequestError::RemoteDoesntSupportProtocol) {
+                    // This error will happen on all future connections to the peer, so we'll close
+                    // the handle after reporting to the behaviour.
+                    self.pending_events.clear();
+                    self.pending_events.push_front(ConnectionHandlerEvent::NotifyBehaviour(
+                        RequestProgressEvent::RequestFailed { request_id, error },
+                    ));
+                    self.pending_events
+                        .push_back(ConnectionHandlerEvent::Close(RemoteDoesntSupportProtocolError));
+                } else {
+                    self.clear_pending_events_related_to_request(request_id);
+                    self.pending_events.push_back(ConnectionHandlerEvent::NotifyBehaviour(
+                        RequestProgressEvent::RequestFailed { request_id, error },
+                    ));
+                }
+                self.request_to_responses_receiver.remove(&request_id);
+            }
+            ConnectionEvent::FullyNegotiatedInbound(_)
+            | ConnectionEvent::ListenUpgradeError(_)
+            | ConnectionEvent::AddressChange(_)
+            | ConnectionEvent::LocalProtocolsChange(_)
+            | ConnectionEvent::RemoteProtocolsChange(_) => {}
+        }
+    }
+}
--- crates/papyrus_network/src/get_blocks/protocol.rs
+++ crates/papyrus_network/src/get_blocks/protocol.rs
@@ -5,8 +5,9 @@ mod protocol_test;
 use std::{io, iter};
 
 use futures::channel::mpsc::{unbounded, TrySendError, UnboundedReceiver, UnboundedSender};
+use futures::channel::oneshot;
 use futures::future::BoxFuture;
-use futures::{AsyncRead, AsyncWrite, AsyncWriteExt, FutureExt};
+use futures::{AsyncRead, AsyncWrite, AsyncWriteExt, FutureExt, StreamExt};
 use libp2p::core::upgrade::{InboundUpgrade, OutboundUpgrade, UpgradeInfo};
 use libp2p::swarm::StreamProtocol;
 
@@ -18,9 +19,38 @@ pub const PROTOCOL_NAME: StreamProtocol = StreamProtocol::new("/get_blocks/1.0.0
 /// Substream upgrade protocol for sending data on blocks.
 ///
 /// Receives a request to get a range of blocks and sends a stream of data on the blocks.
-pub struct ResponseProtocol;
+pub struct InboundProtocol {
+    request_relay_sender: oneshot::Sender<GetBlocks>,
+    responses_relay_receiver: UnboundedReceiver<Option<GetBlocksResponse>>,
+}
 
-impl UpgradeInfo for ResponseProtocol {
+impl InboundProtocol {
+    pub fn new()
+    -> (Self, (oneshot::Receiver<GetBlocks>, UnboundedSender<Option<GetBlocksResponse>>)) {
+        let (request_relay_sender, request_relay_receiver) = oneshot::channel::<GetBlocks>();
+        let (response_relay_sender, responses_relay_receiver) = unbounded();
+        (
+            Self { request_relay_sender, responses_relay_receiver },
+            (request_relay_receiver, response_relay_sender),
+        )
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum InboundProtocolError {
+    #[error(transparent)]
+    IOError(#[from] io::Error),
+    #[error("Failed to send request to relay channel.")]
+    RequestSendError(GetBlocks),
+}
+
+impl From<GetBlocks> for InboundProtocolError {
+    fn from(request: GetBlocks) -> Self {
+        Self::RequestSendError(request)
+    }
+}
+
+impl UpgradeInfo for InboundProtocol {
     type Info = StreamProtocol;
     type InfoIter = iter::Once<Self::Info>;
 
@@ -29,51 +59,71 @@ impl UpgradeInfo for ResponseProtocol {
     }
 }
 
-impl<Stream> InboundUpgrade<Stream> for ResponseProtocol
+impl<Stream> InboundUpgrade<Stream> for InboundProtocol
 where
     Stream: AsyncRead + AsyncWrite + Unpin + Send + 'static,
 {
     type Output = ();
-    type Error = io::Error;
+    type Error = InboundProtocolError;
     type Future = BoxFuture<'static, Result<Self::Output, Self::Error>>;
 
-    fn upgrade_inbound(self, mut io: Stream, _: Self::Info) -> Self::Future {
-        async move {
-            read_message::<GetBlocks, _>(&mut io).await?;
-            for response in hardcoded_responses() {
-                write_message(response, &mut io).await?;
+    fn upgrade_inbound(mut self, mut io: Stream, _: Self::Info) -> Self::Future {
+        Box::pin(
+            async move {
+                if let Ok(get_blocks_msg) = read_message::<GetBlocks, _>(&mut io).await {
+                    self.request_relay_sender.send(get_blocks_msg)?;
+                }
+                loop {
+                    match self.responses_relay_receiver.next().await {
+                        Some(Some(response)) => write_message(response, &mut io).await?,
+                        Some(None) => {
+                            write_message(
+                                GetBlocksResponse { response: Some(Response::Fin(Fin {})) },
+                                &mut io,
+                            )
+                            .await?;
+                            return Ok(());
+                        }
+                        None => {
+                            return Err(InboundProtocolError::IOError(io::Error::new(
+                                io::ErrorKind::UnexpectedEof,
+                                "Unexpected end of stream",
+                            )));
+                        }
+                    };
+                }
             }
-            io.close().await?;
-            Ok(())
-        }
-        .boxed()
+            .boxed(),
+        )
     }
 }
 
 /// Substream upgrade protocol for requesting data on blocks.
 ///
 /// Sends a request to get a range of blocks and receives a stream of data on the blocks.
-pub struct RequestProtocol {
+#[derive(Debug)]
+pub struct OutboundProtocol {
     request: GetBlocks,
     responses_sender: UnboundedSender<GetBlocksResponse>,
 }
 
-impl RequestProtocol {
+impl OutboundProtocol {
     pub fn new(request: GetBlocks) -> (Self, UnboundedReceiver<GetBlocksResponse>) {
         let (responses_sender, responses_receiver) = unbounded();
         (Self { request, responses_sender }, responses_receiver)
     }
 }
 
+// TODO(nevo): consider consolidating with InboundProtocolError
 #[derive(thiserror::Error, Debug)]
-pub enum RequestProtocolError {
+pub enum OutboundProtocolError {
     #[error(transparent)]
     IOError(#[from] io::Error),
     #[error(transparent)]
     ResponseSendError(#[from] TrySendError<GetBlocksResponse>),
 }
 
-impl UpgradeInfo for RequestProtocol {
+impl UpgradeInfo for OutboundProtocol {
     type Info = StreamProtocol;
     type InfoIter = iter::Once<Self::Info>;
 
@@ -82,12 +132,12 @@ impl UpgradeInfo for RequestProtocol {
     }
 }
 
-impl<Stream> OutboundUpgrade<Stream> for RequestProtocol
+impl<Stream> OutboundUpgrade<Stream> for OutboundProtocol
 where
     Stream: AsyncRead + AsyncWrite + Unpin + Send + 'static,
 {
     type Output = ();
-    type Error = RequestProtocolError;
+    type Error = OutboundProtocolError;
     type Future = BoxFuture<'static, Result<Self::Output, Self::Error>>;
 
     fn upgrade_outbound(self, mut io: Stream, _: Self::Info) -> Self::Future {
