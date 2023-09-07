9186d8c45eb5d92e34a8db91c0506132817d67a1
Shahak Shama <shahak@starkware.co>
2023-09-07 08:29:43 +0300

    feat(network): add db executor trait

--- crates/papyrus_network/src/lib.rs
+++ crates/papyrus_network/src/lib.rs
@@ -2,5 +2,28 @@
 /// to the [`Starknet p2p specs`]
 ///
 /// [`Starknet p2p specs`]: https://github.com/starknet-io/starknet-p2p-specs/
+pub mod db_executor;
 pub mod get_blocks;
 pub mod messages;
+
+use starknet_api::block::{BlockHash, BlockNumber};
+
+pub enum Direction {
+    Forward,
+    Backward,
+}
+
+pub enum BlockID {
+    Hash(BlockHash),
+    Number(BlockNumber),
+}
+
+pub struct BlocksRange {
+    pub start: BlockID,
+    pub direction: Direction,
+    pub limit: u64,
+    pub skip: u64,
+    pub step: u64,
+}
+
+// TODO(shahak): Implement conversion from GetBlocks to BlocksRange.
