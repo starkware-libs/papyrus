34b8301a56d9fd6118bd33e85616dc5c768bceae
Yoav Gross <yoav@starkware.co>
2023-09-12 14:25:41 +0300

    feat(monitoring): two config representation types

--- crates/papyrus_monitoring_gateway/src/lib.rs
+++ crates/papyrus_monitoring_gateway/src/lib.rs
@@ -10,24 +10,28 @@ use std::fmt::Display;
 use std::net::SocketAddr;
 use std::str::FromStr;
 
+use axum::extract::Path;
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
 use axum::routing::get;
 use axum::{Json, Router};
 use metrics_exporter_prometheus::{BuildError, PrometheusBuilder, PrometheusHandle};
 use metrics_process::Collector;
-use papyrus_config::dumping::{ser_param, SerializeConfig};
-use papyrus_config::{ParamPath, ParamPrivacyInput, SerializedParam};
+use papyrus_config::dumping::{ser_param, ser_required_param, SerializeConfig};
+use papyrus_config::{ParamPath, ParamPrivacyInput, SerializationType, SerializedParam};
 use papyrus_storage::{DbTablesStats, StorageError, StorageReader};
 use serde::{Deserialize, Serialize};
 use tracing::{debug, instrument};
+use validator::Validate;
 
 const MONITORING_PREFIX: &str = "monitoring";
 
-#[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Validate)]
 pub struct MonitoringGatewayConfig {
     pub server_address: String,
     pub collect_metrics: bool,
+    #[validate(length(min = 1))]
+    pub config_representation_secret: String,
 }
 
 impl Default for MonitoringGatewayConfig {
@@ -35,6 +39,7 @@ impl Default for MonitoringGatewayConfig {
         MonitoringGatewayConfig {
             server_address: String::from("0.0.0.0:8081"),
             collect_metrics: false,
+            config_representation_secret: String::from("qwerty"),
         }
     }
 }
@@ -54,6 +59,12 @@ impl SerializeConfig for MonitoringGatewayConfig {
                 "If true, collect and return metrics in the monitoring gateway.",
                 ParamPrivacyInput::Public,
             ),
+            ser_required_param(
+                "config_representation_secret",
+                SerializationType::String,
+                "A secret for representing the full general config.",
+                ParamPrivacyInput::Private,
+            ),
         ])
     }
 }
@@ -68,6 +79,7 @@ impl Display for MonitoringGatewayConfig {
 pub struct MonitoringServer {
     config: MonitoringGatewayConfig,
     general_config_representation: serde_json::Value,
+    public_general_config_representation: serde_json::Value,
     storage_reader: StorageReader,
     version: &'static str,
     prometheus_handle: Option<PrometheusHandle>,
@@ -77,6 +89,7 @@ impl MonitoringServer {
     pub fn new(
         config: MonitoringGatewayConfig,
         general_config_representation: serde_json::Value,
+        public_general_config_representation: serde_json::Value,
         storage_reader: StorageReader,
         version: &'static str,
     ) -> Result<Self, BuildError> {
@@ -89,6 +102,7 @@ impl MonitoringServer {
             config,
             storage_reader,
             general_config_representation,
+            public_general_config_representation,
             version,
             prometheus_handle,
         })
@@ -104,7 +118,9 @@ impl MonitoringServer {
         fields(
             version = %self.version,
             config = %self.config,
-            general_config_representation = %self.general_config_representation),
+            general_config_representation = %self.general_config_representation,
+            public_general_config_representation = %self.public_general_config_representation,
+            config_representation_secret = %self.config.config_representation_secret),
         level = "debug")]
     async fn run_server(&self) -> std::result::Result<(), hyper::Error> {
         let server_address = SocketAddr::from_str(&self.config.server_address)
@@ -113,6 +129,8 @@ impl MonitoringServer {
             self.storage_reader.clone(),
             self.version,
             self.general_config_representation.clone(),
+            self.public_general_config_representation.clone(),
+            self.config.config_representation_secret.clone(),
             self.prometheus_handle.clone(),
         );
         debug!("Starting monitoring gateway.");
@@ -124,6 +142,8 @@ fn app(
     storage_reader: StorageReader,
     version: &'static str,
     general_config_representation: serde_json::Value,
+    public_general_config_representation: serde_json::Value,
+    config_representation_secret: String,
     prometheus_handle: Option<PrometheusHandle>,
 ) -> Router {
     Router::new()
@@ -132,8 +152,18 @@ fn app(
             get(move || db_tables_stats(storage_reader)),
         )
         .route(
-            format!("/{MONITORING_PREFIX}/nodeConfig").as_str(),
-            get(move || node_config(general_config_representation)),
+            format!("/{MONITORING_PREFIX}/publicNodeConfig").as_str(),
+            get(move || node_config(public_general_config_representation)),
+        )
+        .route(
+            format!("/{MONITORING_PREFIX}/nodeConfig/*secret").as_str(),
+            get(move |secret| {
+                node_config_by_secret(
+                    general_config_representation,
+                    secret,
+                    config_representation_secret,
+                )
+            }),
         )
         .route(
             format!("/{MONITORING_PREFIX}/nodeVersion").as_str(),
@@ -163,6 +193,20 @@ async fn node_config(
     general_config_representation: serde_json::Value,
 ) -> axum::Json<serde_json::Value> {
     general_config_representation.into()
+}
+
+/// Returns the node config.
+#[instrument(level = "debug", ret)]
+async fn node_config_by_secret(
+    general_config_representation: serde_json::Value,
+    given_secret: Path<String>,
+    expected_secret: String,
+) -> Result<axum::Json<serde_json::Value>, StatusCode> {
+    if given_secret.to_string() == expected_secret {
+        Ok(node_config(general_config_representation).await)
+    } else {
+        Err(StatusCode::FORBIDDEN)
+    }
 }
 
 /// Returns prometheus metrics.
