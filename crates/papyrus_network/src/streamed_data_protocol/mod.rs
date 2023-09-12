a9abbb8ee6f1f088ec0fb2b6d22981c629c2a89a
Shahak Shama <shahak@starkware.co>
2023-09-12 21:08:29 +0300

    feat(network): create trait aliases for Query and Data

--- crates/papyrus_network/src/streamed_data_protocol/mod.rs
+++ crates/papyrus_network/src/streamed_data_protocol/mod.rs
@@ -13,3 +13,11 @@ pub struct OutboundSessionId {
 pub struct InboundSessionId {
     value: usize,
 }
+
+// This is a workaround for the unstable feature trait aliases
+// https://doc.rust-lang.org/beta/unstable-book/language-features/trait-alias.html
+pub(crate) trait QueryBound: Message + 'static + Default {}
+impl<T> QueryBound for T where T: Message + 'static + Default {}
+
+pub(crate) trait DataBound: Message + 'static + Unpin {}
+impl<T> DataBound for T where T: Message + 'static + Unpin {}
