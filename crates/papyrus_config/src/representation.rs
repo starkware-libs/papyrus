f2d70b5144e75f56501f6cbd4ea922f118ed2912
yoavGrs <97383386+yoavGrs@users.noreply.github.com>
2023-09-13 11:28:45 +0300

    feat(config): privacy level for config parameters (#1155)

---
+++ crates/papyrus_config/src/representation.rs
@@ -0,0 +1,43 @@
+//! Representation of a configuration, with hiding or exposing private parameters.
+
+use std::ops::IndexMut;
+
+use itertools::Itertools;
+use serde::Serialize;
+
+use crate::dumping::SerializeConfig;
+use crate::{ConfigError, ParamPrivacy};
+
+/// Returns representation of the public parameters in the config.
+pub fn get_config_representation<T: Serialize + SerializeConfig>(
+    config: &T,
+    include_private_parameters: bool,
+) -> Result<serde_json::Value, ConfigError> {
+    let mut config_representation = serde_json::to_value(config)?;
+    if include_private_parameters {
+        return Ok(config_representation);
+    }
+
+    // Iterates over flatten param paths for removing non-public parameters from the nested config.
+    // For example, for the param path 'a.b.c.d', perform config_representation[a][b][c].remove(d).
+    for (param_path, serialized_param) in config.dump() {
+        if let ParamPrivacy::Public = serialized_param.privacy {
+            continue;
+        }
+
+        // Remove a non-public parameter.
+        let mut config_hierarchy = param_path.split('.').collect_vec();
+        let Some(element_to_remove) = config_hierarchy.pop() else {
+            continue; // Empty param path.`
+        };
+        let most_inner_config = config_hierarchy
+            .iter()
+            .fold(&mut config_representation, |entry, config_name| entry.index_mut(config_name));
+
+        most_inner_config
+            .as_object_mut()
+            .ok_or_else(|| ConfigError::ParamNotFound { param_path: param_path.to_string() })?
+            .remove(element_to_remove);
+    }
+    Ok(config_representation)
+}
