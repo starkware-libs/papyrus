daf4c24c8e656a285b3394a38846fecd22393638
Shahak Shama <shahak@starkware.co>
2023-09-14 10:29:26 +0300

    test(network): move get_connected_stream_futures to other file

--- crates/papyrus_network/src/lib.rs
+++ crates/papyrus_network/src/lib.rs
@@ -4,6 +4,8 @@
 /// [`Starknet p2p specs`]: https://github.com/starknet-io/starknet-p2p-specs/
 pub mod messages;
 pub mod streamed_data_protocol;
+#[cfg(test)]
+mod test_utils;
 
 use starknet_api::block::{BlockHash, BlockNumber};
 
