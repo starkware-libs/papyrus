327e14554ea9090cae10aa16c282a614426e4330
Shahak Shama <shahak@starkware.co>
2023-09-18 11:48:12 +0300

    test(network): add create_swarm function

--- crates/papyrus_network/src/test_utils.rs
+++ crates/papyrus_network/src/test_utils.rs
@@ -2,6 +2,34 @@ use futures::{AsyncRead, AsyncWrite, StreamExt};
 use libp2p::core::multiaddr::multiaddr;
 use libp2p::core::transport::memory::MemoryTransport;
 use libp2p::core::transport::{ListenerId, Transport};
+use libp2p::core::{multiaddr, upgrade};
+use libp2p::identity::Keypair;
+use libp2p::swarm::{NetworkBehaviour, SwarmBuilder};
+use libp2p::{noise, yamux, Multiaddr, Swarm};
+use rand::random;
+
+// TODO(shahak): Use create_swarm and remove allow(dead_code)
+#[allow(dead_code)]
+pub(crate) fn create_swarm<BehaviourT: NetworkBehaviour>(
+    behaviour: BehaviourT,
+) -> (Swarm<BehaviourT>, Multiaddr) {
+    let key_pair = Keypair::generate_ed25519();
+    let public_key = key_pair.public();
+    let transport = MemoryTransport::default()
+        .upgrade(upgrade::Version::V1)
+        .authenticate(noise::Config::new(&key_pair).unwrap())
+        .multiplex(yamux::Config::default())
+        .boxed();
+
+    let peer_id = public_key.to_peer_id();
+    let mut swarm = SwarmBuilder::without_executor(transport, behaviour, peer_id).build();
+
+    // Using a random address because if two different tests use the same address simultaneously
+    // they will fail.
+    let listen_address: Multiaddr = multiaddr::Protocol::Memory(random::<u64>()).into();
+    swarm.listen_on(listen_address.clone()).unwrap();
+    (swarm, listen_address)
+}
 
 pub(crate) async fn get_connected_streams()
 -> (impl AsyncRead + AsyncWrite, impl AsyncRead + AsyncWrite) {
