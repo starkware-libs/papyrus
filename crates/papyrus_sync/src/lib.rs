86d23ed57ceb2f47c13e051ecc00d0f86b6abb59
Yair Bakalchuk <yair@starkware.co>
2023-09-18 14:46:02 +0300

    feat(sync): add block signature to the the block returned from central source

--- crates/papyrus_sync/src/lib.rs
+++ crates/papyrus_sync/src/lib.rs
@@ -625,7 +625,7 @@ fn stream_new_blocks<
                 central_source.stream_new_blocks(header_marker, up_to).fuse();
             pin_mut!(block_stream);
             while let Some(maybe_block) = block_stream.next().await {
-                let (block_number, block, starknet_version) = maybe_block?;
+                let (block_number, block, _block_signature_data, starknet_version) = maybe_block?;
                 yield SyncEvent::BlockAvailable { block_number, block , starknet_version};
             }
         }
