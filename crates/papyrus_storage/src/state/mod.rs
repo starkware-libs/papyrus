ab8da880bd4043cb725a8d014eab2322dd744dcb
Dvir Yosef <dvir@starkware.co>
2023-09-20 17:36:46 +0300

    wip

--- crates/papyrus_storage/src/state/mod.rs
+++ crates/papyrus_storage/src/state/mod.rs
@@ -414,9 +414,17 @@ impl<'env> StateStorageWriter for StorageTxn<'env, RW> {
         )?;
 
         // Write state diff.
-        let (thin_state_diff, declared_classes, deprecated_declared_classes) =
+        let (_thin_state_diff, declared_classes, deprecated_declared_classes) =
             ThinStateDiff::from_state_diff(state_diff);
-        state_diffs_table.insert(&self.txn, &block_number, &thin_state_diff)?;
+        let default_thin_state_diff = ThinStateDiff{
+            deployed_contracts: IndexMap::new(),
+            storage_diffs: IndexMap::new(),
+            nonces: IndexMap::new(),
+            replaced_classes: IndexMap::new(),
+            declared_classes: IndexMap::new(),
+            deprecated_declared_classes: Vec::new(),
+        };
+        state_diffs_table.insert(&self.txn, &block_number, &default_thin_state_diff)?;
 
         // Write declared classes.
         write_declared_classes(
