0f10a9b4d4f3a73506de84587f9aaf3aacdbb266
Nevo Agmon <nevo@starkware.co>
2023-09-21 10:57:07 +0300

    feat(network): protobuf update

--- crates/papyrus_network/src/messages/block.rs
+++ crates/papyrus_network/src/messages/block.rs
@@ -2,26 +2,23 @@
 /// Signatures from the Starknet feeder gateway that show this block and state diffs are part of
 /// Starknet
 pub type Signatures = crate::messages::proto::p2p::proto::Signatures;
-/// Header of a block. Contains hashes for the proof and state diffs and the rest of the block
-/// data.
+/// Header of a block. Contains hashes for the proof and signatures.
 pub type BlockHeader = crate::messages::proto::p2p::proto::BlockHeader;
 /// A proof for the Kth block behind the current block.
 pub type BlockProof = crate::messages::proto::p2p::proto::BlockProof;
 /// A message announcing that a new block was created.
 pub type NewBlock = crate::messages::proto::p2p::proto::NewBlock;
-/// A request to get a range of block headers and state diffs.
-pub type GetBlocks = crate::messages::proto::p2p::proto::GetBlocks;
-/// A response of a [`GetBlocks`] request.
-pub type GetBlocksResponse = crate::messages::proto::p2p::proto::GetBlocksResponse;
-/// A request to get signatures on a block.
-pub type GetSignatures = crate::messages::proto::p2p::proto::GetSignatures;
+/// A request to get a range of block headers.
+pub type BlockHeadersRequest = crate::messages::proto::p2p::proto::BlockHeadersRequest;
+/// A response of a [`BlockHeadersRequest`] request.
+pub type BlockHeadersResponse = crate::messages::proto::p2p::proto::BlockHeadersResponse;
 
-impl GetBlocksResponse {
+impl BlockHeadersResponse {
     pub fn is_fin(&self) -> bool {
-        self.response.as_ref().map_or(false, |response| {
+        self.header_message.as_ref().map_or(false, |response| {
             matches!(
                 response,
-                crate::messages::proto::p2p::proto::get_blocks_response::Response::Fin(_)
+                crate::messages::proto::p2p::proto::block_headers_response::HeaderMessage::Fin(_)
             )
         })
     }
