5509315b39b7e4f483c6c4e1855fa85c419255cd
Shahak Shama <shahak@starkware.co>
2023-09-28 08:09:41 +0300

    fix(network): read_message returns None if EOF instead of empty message

---
+++ crates/papyrus_network/src/messages/messages_test.rs
@@ -0,0 +1,40 @@
+use std::time::Duration;
+
+use futures::AsyncWriteExt;
+use pretty_assertions::assert_eq;
+
+use super::block::GetBlocksResponse;
+use super::{read_message, write_message};
+use crate::test_utils::{get_connected_streams, hardcoded_data};
+
+#[tokio::test]
+async fn read_write_positive_flow() {
+    let (mut stream1, mut stream2, _) = get_connected_streams().await;
+    let messages = hardcoded_data();
+    for message in &messages {
+        write_message(message.clone(), &mut stream1).await.unwrap();
+    }
+    for expected_message in &messages {
+        assert_eq!(*expected_message, read_message(&mut stream2).await.unwrap().unwrap());
+    }
+}
+
+#[tokio::test]
+async fn read_when_other_stream_is_closed_returns_none() {
+    let (mut stream1, mut stream2, _) = get_connected_streams().await;
+    stream1.close().await.unwrap();
+    assert!(read_message::<GetBlocksResponse, _>(&mut stream2).await.unwrap().is_none());
+}
+
+#[tokio::test]
+async fn read_when_other_stream_didnt_send_returns_pending() {
+    let (_stream1, mut stream2, _) = get_connected_streams().await;
+    assert!(
+        tokio::time::timeout(
+            Duration::from_millis(10),
+            read_message::<GetBlocksResponse, _>(&mut stream2)
+        )
+        .await
+        .is_err()
+    );
+}
--- crates/papyrus_network/src/messages/mod.rs
+++ crates/papyrus_network/src/messages/mod.rs
@@ -1,5 +1,7 @@
 pub mod block;
 pub mod common;
+#[cfg(test)]
+mod messages_test;
 
 pub mod proto {
     pub mod p2p {
@@ -27,7 +29,14 @@ pub async fn write_message<T: Message, Stream: AsyncWrite + Unpin>(
 
 pub async fn read_message<T: Message + Default, Stream: AsyncRead + Unpin>(
     mut io: Stream,
-) -> Result<T, io::Error> {
+) -> Result<Option<T>, io::Error> {
     let buf = read_length_prefixed(&mut io, MAX_MESSAGE_SIZE).await?;
-    Ok(T::decode(buf.as_slice())?)
+    // read_length_prefixed returns an empty vec if it reaches EOF. We opened an issue in libp2p to
+    // try and change this: https://github.com/libp2p/rust-libp2p/issues/4565
+    // TODO(shahak): This currently disables reading empty messages. fix this (consider copying the
+    // code from libp2p and changing it).
+    if buf.is_empty() {
+        return Ok(None);
+    }
+    Ok(Some(T::decode(buf.as_slice())?))
 }
