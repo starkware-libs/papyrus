fde3d9a0537b396981f059489ba7fadca3b56f1e
Shahak Shama <shahak@starkware.co>
2023-10-25 16:57:54 +0300

    fix(common): fix PendingClasses interface

--- crates/papyrus_common/src/pending_classes.rs
+++ crates/papyrus_common/src/pending_classes.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 use cairo_lang_starknet::casm_contract_class::CasmContractClass;
-use starknet_api::core::ClassHash;
+use starknet_api::core::{ClassHash, CompiledClassHash};
 use starknet_api::deprecated_contract_class::ContractClass as DeprecatedContractClass;
 use starknet_api::state::ContractClass as SierraContractClass;
 
@@ -10,9 +10,16 @@ pub trait PendingClassesTrait {
 
     fn add_class(&mut self, class_hash: ClassHash, class: PendingClass);
 
-    fn get_casm(&self, class_hash: ClassHash) -> Option<CasmContractClass>;
+    fn get_compiled_class(
+        &self,
+        compiled_class_hash: CompiledClassHash,
+    ) -> Option<CasmContractClass>;
 
-    fn add_casm(&mut self, class_hash: ClassHash, casm: CasmContractClass);
+    fn add_compiled_class(
+        &mut self,
+        class_hash: CompiledClassHash,
+        compiled_class: CasmContractClass,
+    );
 
     fn clear(&mut self);
 }
@@ -20,7 +27,7 @@ pub trait PendingClassesTrait {
 #[derive(Debug, Default, Eq, PartialEq, Clone)]
 pub struct PendingClasses {
     pub classes: HashMap<ClassHash, PendingClass>,
-    pub casm: HashMap<ClassHash, CasmContractClass>,
+    pub compiled_classes: HashMap<CompiledClassHash, CasmContractClass>,
 }
 
 #[derive(Debug, Eq, PartialEq, Clone)]
@@ -38,16 +45,23 @@ impl PendingClassesTrait for PendingClasses {
         self.classes.insert(class_hash, class);
     }
 
-    fn get_casm(&self, class_hash: ClassHash) -> Option<CasmContractClass> {
-        self.casm.get(&class_hash).cloned()
+    fn get_compiled_class(
+        &self,
+        compiled_class_hash: CompiledClassHash,
+    ) -> Option<CasmContractClass> {
+        self.compiled_classes.get(&compiled_class_hash).cloned()
     }
 
-    fn add_casm(&mut self, class_hash: ClassHash, casm: CasmContractClass) {
-        self.casm.insert(class_hash, casm);
+    fn add_compiled_class(
+        &mut self,
+        compiled_class_hash: CompiledClassHash,
+        compiled_class: CasmContractClass,
+    ) {
+        self.compiled_classes.insert(compiled_class_hash, compiled_class);
     }
 
     fn clear(&mut self) {
         self.classes.clear();
-        self.casm.clear();
+        self.compiled_classes.clear();
     }
 }
