21cf42a81588d3413495c550591fa193c888e67b
Yael Doweck <yael@starkware.co>
2023-11-02 09:30:27 +0200

    feat(starknet_client): support v3 transactions in starknet_client

--- crates/starknet_client/src/writer/objects/transaction_test.rs
+++ crates/starknet_client/src/writer/objects/transaction_test.rs
@@ -3,18 +3,31 @@ use test_utils::validate_load_and_dump;
 use super::{
     DeclareV1Transaction,
     DeclareV2Transaction,
-    DeployAccountTransaction,
-    InvokeTransaction,
+    DeclareV3Transaction,
+    DeployAccountV1Transaction,
+    DeployAccountV3Transaction,
+    InvokeV1Transaction,
+    InvokeV3Transaction,
 };
 
 #[test]
-fn load_and_dump_deploy_account_same_string() {
-    validate_load_and_dump::<DeployAccountTransaction>("writer/deploy_account.json");
+fn load_and_dump_deploy_account_v1_same_string() {
+    validate_load_and_dump::<DeployAccountV1Transaction>("writer/deploy_account_v1.json");
 }
 
 #[test]
-fn load_and_dump_invoke_same_string() {
-    validate_load_and_dump::<InvokeTransaction>("writer/invoke.json");
+fn load_and_dump_deploy_account_v3_same_string() {
+    validate_load_and_dump::<DeployAccountV3Transaction>("writer/deploy_account_v3.json");
+}
+
+#[test]
+fn load_and_dump_invoke_v1_same_string() {
+    validate_load_and_dump::<InvokeV1Transaction>("writer/invoke_v1.json");
+}
+
+#[test]
+fn load_and_dump_invoke_v3_same_string() {
+    validate_load_and_dump::<InvokeV3Transaction>("writer/invoke_v3.json");
 }
 
 #[test]
@@ -25,4 +38,9 @@ fn load_and_dump_declare_v1_same_string() {
 #[test]
 fn load_and_dump_declare_v2_same_string() {
     validate_load_and_dump::<DeclareV2Transaction>("writer/declare_v2.json");
+}
+
+#[test]
+fn load_and_dump_declare_v3_same_string() {
+    validate_load_and_dump::<DeclareV3Transaction>("writer/declare_v3.json");
 }
