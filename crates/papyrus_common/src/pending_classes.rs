1ec7cf85c54694f4c89453bb977e8af5a66ab35c
ShahakShama <70578257+ShahakShama@users.noreply.github.com>
2023-11-22 14:53:20 +0200

    perf(common): improve PendingClasses::clone by storing the classes in an Arc (#1439)

--- crates/papyrus_common/src/pending_classes.rs
+++ crates/papyrus_common/src/pending_classes.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::sync::Arc;
 
 use cairo_lang_starknet::casm_contract_class::CasmContractClass;
 use starknet_api::core::ClassHash;
@@ -6,10 +7,14 @@ use starknet_api::deprecated_contract_class::ContractClass as DeprecatedContract
 use starknet_api::state::ContractClass;
 
 pub trait PendingClassesTrait {
+    // TODO(shahak) Return an Arc to avoid cloning the class. This requires to re-implement
+    // From/TryFrom for various structs in a way that the input is passed by reference.
     fn get_class(&self, class_hash: ClassHash) -> Option<ApiContractClass>;
 
     fn add_class(&mut self, class_hash: ClassHash, class: ApiContractClass);
 
+    // TODO(shahak) Return an Arc to avoid cloning the class. This requires to re-implement
+    // From/TryFrom for various structs in a way that the input is passed by reference.
     fn get_compiled_class(&self, class_hash: ClassHash) -> Option<CasmContractClass>;
 
     fn add_compiled_class(&mut self, class_hash: ClassHash, compiled_class: CasmContractClass);
@@ -19,8 +24,10 @@ pub trait PendingClassesTrait {
 
 #[derive(Debug, Default, Eq, PartialEq, Clone)]
 pub struct PendingClasses {
-    pub classes: HashMap<ClassHash, ApiContractClass>,
-    pub compiled_classes: HashMap<ClassHash, CasmContractClass>,
+    // Putting the contracts inside Arc so we won't have to clone them when we clone the entire
+    // PendingClasses struct.
+    pub classes: HashMap<ClassHash, Arc<ApiContractClass>>,
+    pub compiled_classes: HashMap<ClassHash, Arc<CasmContractClass>>,
 }
 
 #[derive(Debug, Eq, PartialEq, Clone)]
@@ -47,19 +54,19 @@ impl ApiContractClass {
 
 impl PendingClassesTrait for PendingClasses {
     fn get_class(&self, class_hash: ClassHash) -> Option<ApiContractClass> {
-        self.classes.get(&class_hash).cloned()
+        self.classes.get(&class_hash).map(|class| (**class).clone())
     }
 
     fn add_class(&mut self, class_hash: ClassHash, class: ApiContractClass) {
-        self.classes.insert(class_hash, class);
+        self.classes.insert(class_hash, Arc::new(class));
     }
 
     fn get_compiled_class(&self, class_hash: ClassHash) -> Option<CasmContractClass> {
-        self.compiled_classes.get(&class_hash).cloned()
+        self.compiled_classes.get(&class_hash).map(|compiled_class| (**compiled_class).clone())
     }
 
     fn add_compiled_class(&mut self, class_hash: ClassHash, compiled_class: CasmContractClass) {
-        self.compiled_classes.insert(class_hash, compiled_class);
+        self.compiled_classes.insert(class_hash, Arc::new(compiled_class));
     }
 
     fn clear(&mut self) {
