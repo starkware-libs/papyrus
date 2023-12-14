3ec3766e05265d7dafe8b5ed171c437d0afcd305
Dan Brownstein <dan@starkware.co>
2023-12-14 17:01:04 +0200

    chore(release): bump version to 0.3.0-dev

--- crates/papyrus_node/src/version.rs
+++ crates/papyrus_node/src/version.rs
@@ -6,7 +6,7 @@ mod version_test;
 const VERSION_MAJOR: u32 = 0;
 
 /// Minor version component of the current release.
-const VERSION_MINOR: u32 = 2;
+const VERSION_MINOR: u32 = 3;
 
 /// Patch version component of the current release.
 const VERSION_PATCH: u32 = 0;
@@ -14,7 +14,7 @@ const VERSION_PATCH: u32 = 0;
 /// Version metadata to append to the version string.
 /// Expected values are `dev` and `stable`.
 #[allow(dead_code)]
-const VERSION_META: Metadata = Metadata::Stable;
+const VERSION_META: Metadata = Metadata::Dev;
 
 /// Textual version string.
 pub const VERSION: &str = version_str();
