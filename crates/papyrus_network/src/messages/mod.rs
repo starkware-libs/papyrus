bba6f870a0661663349dc070eb35d985cdb0825d
Shahak Shama <shahak@starkware.co>
2023-12-17 15:54:22 +0200

    fix(network): upgrade libp2p to v0.53

--- crates/papyrus_network/src/messages/mod.rs
+++ crates/papyrus_network/src/messages/mod.rs
@@ -13,9 +13,9 @@ pub mod proto {
 
 use std::io;
 
-use futures::{AsyncRead, AsyncWrite};
-use libp2p::core::upgrade::{read_length_prefixed, write_length_prefixed};
+use futures::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 use prost::Message;
+use unsigned_varint::encode::usize_buffer;
 
 pub const MAX_MESSAGE_SIZE: usize = 1 << 20;
 
@@ -23,20 +23,75 @@ pub async fn write_message<T: Message, Stream: AsyncWrite + Unpin>(
     message: T,
     mut io: Stream,
 ) -> Result<(), io::Error> {
-    write_length_prefixed(&mut io, &message.encode_to_vec()).await?;
+    let message_vec = message.encode_to_vec();
+    // This code is based on write_length_prefixed from libp2p v0.52 which was erased in v0.53.
+    write_usize(&mut io, message_vec.len()).await?;
+    io.write_all(&message_vec).await?;
+    io.flush().await?;
     Ok(())
 }
 
 pub async fn read_message<T: Message + Default, Stream: AsyncRead + Unpin>(
     mut io: Stream,
 ) -> Result<Option<T>, io::Error> {
-    let buf = read_length_prefixed(&mut io, MAX_MESSAGE_SIZE).await?;
-    // read_length_prefixed returns an empty vec if it reaches EOF. We opened an issue in libp2p to
-    // try and change this: https://github.com/libp2p/rust-libp2p/issues/4565
-    // TODO(shahak): This currently disables reading empty messages. fix this by copying the
-    // code from libp2p and changing it.
-    if buf.is_empty() {
-        return Ok(None);
+    // This code is based on read_length_prefixed from libp2p v0.52 which was erased in v0.53.
+    let Some(message_len) = read_usize(&mut io).await? else { return Ok(None) };
+    if message_len > MAX_MESSAGE_SIZE {
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidData,
+            format!(
+                "Received data size ({message_len} bytes) exceeds maximum ({MAX_MESSAGE_SIZE} \
+                 bytes)"
+            ),
+        ));
     }
+    let mut buf = vec![0; message_len];
+    io.read_exact(&mut buf).await?;
     Ok(Some(T::decode(buf.as_slice())?))
+}
+
+// This code is based on read_varint from libp2p v0.52 which was erased in v0.53. The difference
+// from there is that here we return None if we have EOF before starting to read.
+pub async fn read_usize<Stream: AsyncRead + Unpin>(
+    mut io: Stream,
+) -> Result<Option<usize>, io::Error> {
+    let mut buffer = unsigned_varint::encode::usize_buffer();
+    let mut buffer_len = 0;
+
+    loop {
+        match io.read(&mut buffer[buffer_len..buffer_len + 1]).await? {
+            0 => {
+                // Reaching EOF before finishing to read the length is an error, unless the EOF is
+                // at the very beginning of the substream, in which case we return None.
+                if buffer_len == 0 {
+                    return Ok(None);
+                } else {
+                    return Err(io::ErrorKind::UnexpectedEof.into());
+                }
+            }
+            n => debug_assert_eq!(n, 1),
+        }
+
+        buffer_len += 1;
+
+        match unsigned_varint::decode::usize(&buffer[..buffer_len]) {
+            Ok((len, _)) => return Ok(Some(len)),
+            Err(unsigned_varint::decode::Error::Insufficient) => {}
+            Err(error) => {
+                return Err(io::Error::new(io::ErrorKind::InvalidData, error));
+            }
+        }
+    }
+}
+
+// This code is based on write_varint from libp2p v0.52 which was erased in v0.53.
+pub async fn write_usize<Stream: AsyncWrite + Unpin>(
+    mut io: Stream,
+    num: usize,
+) -> Result<(), io::Error> {
+    let mut buffer = usize_buffer();
+    let encoded_len = unsigned_varint::encode::usize(num, &mut buffer).len();
+    io.write_all(&buffer[..encoded_len]).await?;
+
+    Ok(())
 }
