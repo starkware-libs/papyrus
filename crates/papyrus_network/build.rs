4b78af737c8e0217f01f516ee868bad12b2da010
Nevo Agmon <nevo@starkware.co>
2023-12-19 11:28:19 +0200

    chore(network): upgrade prost

--- crates/papyrus_network/build.rs
+++ crates/papyrus_network/build.rs
@@ -1,7 +1,11 @@
+use std::env;
 use std::io::Result;
 
 fn main() -> Result<()> {
     println!("Building");
+    let dir = env::current_dir().unwrap();
+    println!("{dir:?}");
+    env::set_var("PROTOC", "./protoc");
     prost_build::compile_protos(
         &["src/messages/proto/p2p/proto/block.proto"],
         &["src/messages/proto/"],
