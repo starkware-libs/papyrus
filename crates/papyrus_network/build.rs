90f9d56413826b7ebb00c437f41f52e47e1ebf80
Dori Medini <dori@starkware.co>
2024-01-04 11:49:23 +0200

    chore(release): fix papyrus_network/build.rs
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/papyrus_network/build.rs
+++ crates/papyrus_network/build.rs
@@ -2,7 +2,9 @@ use std::io::Result;
 
 fn main() -> Result<()> {
     println!("Building");
-    prost_build::compile_protos(
+    let mut prost_build = prost_build::Config::new();
+    prost_build.protoc_arg("--experimental_allow_proto3_optional");
+    prost_build.compile_protos(
         &["src/messages/proto/p2p/proto/block.proto", "src/messages/proto_test/util.proto"],
         &["src/messages/proto/", "src/messages/proto_test"],
     )?;
