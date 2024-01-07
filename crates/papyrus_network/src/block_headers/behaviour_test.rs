478c2594d0a8b1bd62a009752980953be2ba2ec0
nagmo-starkware <127658633+nagmo-starkware@users.noreply.github.com>
2024-01-07 16:58:45 +0200

    chore(network): move functionality into behaviour trait for testing (#1580)

--- crates/papyrus_network/src/block_headers/behaviour_test.rs
+++ crates/papyrus_network/src/block_headers/behaviour_test.rs
@@ -1,71 +1,16 @@
 use std::pin::Pin;
-use std::sync::Arc;
 use std::task::{Context, Poll};
-use std::vec;
 
-use assert_matches::assert_matches;
 use futures::Stream;
 
 use super::behaviour::BehaviourTrait;
-use crate::block_headers::{BlockHeader, Signature};
+use super::Event;
 use crate::db_executor::{DBExecutor, Data, QueryId};
 use crate::messages::protobuf;
-use crate::streamed_data::{Config, OutboundSessionId, SessionId};
-use crate::{block_headers, BlockQuery};
+use crate::streamed_data::{InboundSessionId, OutboundSessionId, SessionId};
+use crate::BlockQuery;
 
 // static mut test_db_executor: TestDBExecutor = TestDBExecutor {};
-
-#[test]
-fn header_message_to_header_or_signatures() {
-    let test_db_executor = Arc::new(TestDBExecutor {});
-    let behaviour =
-        block_headers::behaviour::Behaviour::new(Config::get_test_config(), test_db_executor);
-    // TODO: add a get test instance method that returns a valid object
-    let header_message =
-        protobuf::block_headers_response_part::HeaderMessage::Header(protobuf::BlockHeader {
-            parent_header: Some(protobuf::Hash { elements: [0x01].repeat(32) }),
-            sequencer_address: Some(protobuf::Address { elements: [0x01].repeat(32) }),
-            ..Default::default()
-        });
-    assert_matches!(
-        behaviour.header_message_to_header_or_signatures(&header_message),
-        Ok((Some(BlockHeader { .. }), None))
-    );
-
-    let broken_header_message =
-        protobuf::block_headers_response_part::HeaderMessage::Header(protobuf::BlockHeader {
-            parent_header: Some(protobuf::Hash { elements: vec![0x01] }), // hash not long enough
-            ..Default::default()
-        });
-    let res = behaviour.header_message_to_header_or_signatures(&broken_header_message);
-    assert!(res.is_err());
-
-    // TODO: add a get test instance method that returns valid object for HeaderMessage::Signatures,
-    // protobuf::Signatures and protobuf::ConcensusSignature
-    let signatures_message =
-        protobuf::block_headers_response_part::HeaderMessage::Signatures(protobuf::Signatures {
-            block: Some(protobuf::BlockId { number: 1, ..Default::default() }),
-            signatures: vec![protobuf::ConsensusSignature {
-                r: Some(protobuf::Felt252 { elements: [0x01].repeat(32) }),
-                s: Some(protobuf::Felt252 { elements: [0x01].repeat(32) }),
-            }],
-        });
-    assert_matches!(
-        behaviour.header_message_to_header_or_signatures(&signatures_message),
-        Ok((None, Some(Vec::<Signature> { .. })))
-    );
-
-    let broken_signatures_message =
-        protobuf::block_headers_response_part::HeaderMessage::Signatures(protobuf::Signatures {
-            block: Some(protobuf::BlockId { number: 1, ..Default::default() }),
-            signatures: vec![protobuf::ConsensusSignature {
-                r: None,
-                s: Some(protobuf::Felt252 { elements: [0x01].repeat(32) }),
-            }],
-        });
-    let res = behaviour.header_message_to_header_or_signatures(&broken_signatures_message);
-    assert!(res.is_err());
-}
 
 #[test]
 #[ignore = "functionality not implemented completely yet"]
@@ -93,23 +38,35 @@ async fn poll_is_ready_if_streamed_data_behaviour_poll_is_ready_and_event_mappin
     unimplemented!()
 }
 
-struct TestBehaviour {
-    should_wait: bool,
-}
+struct TestBehaviour {}
 
-impl BehaviourTrait for TestBehaviour {
-    fn map_streamed_data_behaviour_event_to_own_event(
+impl BehaviourTrait<TestDBExecutor> for TestBehaviour {
+    fn handle_received_data(
         &mut self,
-        _in_event: crate::streamed_data::behaviour::Event<
-            protobuf::BlockHeadersRequest,
-            protobuf::BlockHeadersResponse,
-        >,
-        wait_to_complete_pairing: &mut bool,
-    ) -> block_headers::Event {
-        *wait_to_complete_pairing = self.should_wait;
-        block_headers::Event::SessionCompletedSuccessfully {
-            session_id: SessionId::OutboundSessionId(OutboundSessionId { value: 1 }),
-        }
+        _data: protobuf::BlockHeadersResponse,
+        _outbound_session_id: OutboundSessionId,
+        _ignore_event_and_return_pending: &mut bool,
+    ) -> Event {
+        unimplemented!()
+    }
+
+    fn handle_session_closed_by_request(&mut self, _session_id: SessionId) -> Event {
+        unimplemented!()
+    }
+
+    fn handle_outbound_session_closed_by_peer(
+        &mut self,
+        _outbound_session_id: OutboundSessionId,
+    ) -> Event {
+        unimplemented!()
+    }
+
+    fn handle_new_inbound_session(
+        &mut self,
+        _query: protobuf::BlockHeadersRequest,
+        _inbound_session_id: InboundSessionId,
+    ) -> Event {
+        unimplemented!()
     }
 }
 
