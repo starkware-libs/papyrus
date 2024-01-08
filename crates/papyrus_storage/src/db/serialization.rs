f0080154efbe4b1c6ce7f7d1183f9389bad19392
dan-starkware <56217775+dan-starkware@users.noreply.github.com>
2024-01-08 15:17:51 +0200

    feat(storage): add version zero wrapper (#1584)

--- crates/papyrus_storage/src/db/serialization.rs
+++ crates/papyrus_storage/src/db/serialization.rs
@@ -1,4 +1,5 @@
 use std::fmt::Debug;
+use std::io::Write;
 use std::marker::PhantomData;
 
 use crate::db::DbError;
@@ -48,7 +49,7 @@ pub(crate) trait ValueSerde {
 }
 
 #[derive(Debug)]
-/// Wrapper for values that do not have a version.
+/// A generic wrapper for values that do not have a version.
 pub(crate) struct NoVersionValueWrapper<T: StorageSerde> {
     _value_type: PhantomData<T>,
 }
@@ -62,6 +63,42 @@ impl<T: StorageSerde + Debug> ValueSerde for NoVersionValueWrapper<T> {
 
     fn deserialize(bytes: &mut impl std::io::Read) -> Option<Self::Value> {
         StorageSerdeEx::deserialize(bytes)
+    }
+}
+
+#[derive(Debug)]
+/// A generic wrapper for values with version zero. These values are serialized with a leading byte
+/// that is set to zero.
+pub(crate) struct VersionZeroWrapper<T: StorageSerde> {
+    _value_type: PhantomData<T>,
+}
+
+const VERSION_ZERO: u8 = 0;
+
+impl<T: StorageSerde + Debug> ValueSerde for VersionZeroWrapper<T> {
+    type Value = T;
+
+    fn serialize(obj: &Self::Value) -> Result<Vec<u8>, DbError> {
+        let mut res = Vec::new();
+        res.write_all(&[VERSION_ZERO]).expect("Failed to write version");
+        obj.serialize_into(&mut res).map_err(|_| DbError::Serialization)?;
+        Ok(res)
+    }
+
+    fn deserialize(bytes: &mut impl std::io::Read) -> Option<Self::Value> {
+        let mut version = [0u8; 1];
+        bytes.read_exact(&mut version[..]).ok()?;
+        if version[0] != VERSION_ZERO {
+            return None;
+        }
+        let res = Self::Value::deserialize_from(bytes)?;
+
+        let mut buf = [0u8, 1];
+        // Make sure we are at EOF.
+        if bytes.read(&mut buf[..]).ok()? != 0 {
+            return None;
+        }
+        Some(res)
     }
 }
 
