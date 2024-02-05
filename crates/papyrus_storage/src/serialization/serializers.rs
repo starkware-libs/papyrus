583537dab5435caa75a44677561a8af3ea6be24b
Dvir Yosef <dvir@starkware.co>
2024-02-05 16:36:29 +0200

    perf(storage)!: write thin transaction outputs to a file

--- crates/papyrus_storage/src/serialization/serializers.rs
+++ crates/papyrus_storage/src/serialization/serializers.rs
@@ -315,6 +315,7 @@ auto_storage_serde! {
         ContractClass = 1,
         Casm = 2,
         DeprecatedContractClass = 3,
+        ThinTransactionOutput = 4,
     }
     pub struct PaymasterData(pub Vec<StarkFelt>);
     pub struct Program {
