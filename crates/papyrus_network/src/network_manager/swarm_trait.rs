1d047ce219af857adff6702869f2aa7f9fe0db48
Shahak Shama <shahak@starkware.co>
2024-02-06 16:34:39 +0200

    test(network): add test for network manager

---
+++ crates/papyrus_network/src/network_manager/swarm_trait.rs
@@ -0,0 +1,46 @@
+use futures::stream::Stream;
+use libp2p::swarm::{NetworkBehaviour, SwarmEvent};
+use libp2p::{PeerId, Swarm};
+
+use crate::block_headers::behaviour::{
+    Behaviour as BlockHeadersBehaviour,
+    PeerNotConnected,
+    SessionIdNotFoundError,
+};
+use crate::db_executor::Data;
+use crate::streamed_data::{InboundSessionId, OutboundSessionId};
+use crate::InternalQuery;
+
+pub type Event = SwarmEvent<<BlockHeadersBehaviour as NetworkBehaviour>::ToSwarm>;
+
+pub trait SwarmTrait: Stream<Item = Event> + Unpin {
+    fn send_data(
+        &mut self,
+        data: Data,
+        inbound_session_id: InboundSessionId,
+    ) -> Result<(), SessionIdNotFoundError>;
+
+    fn send_query(
+        &mut self,
+        query: InternalQuery,
+        peer_id: PeerId,
+    ) -> Result<OutboundSessionId, PeerNotConnected>;
+}
+
+impl SwarmTrait for Swarm<BlockHeadersBehaviour> {
+    fn send_data(
+        &mut self,
+        data: Data,
+        inbound_session_id: InboundSessionId,
+    ) -> Result<(), SessionIdNotFoundError> {
+        self.behaviour_mut().send_data(data, inbound_session_id)
+    }
+
+    fn send_query(
+        &mut self,
+        query: InternalQuery,
+        peer_id: PeerId,
+    ) -> Result<OutboundSessionId, PeerNotConnected> {
+        self.behaviour_mut().send_query(query, peer_id)
+    }
+}
