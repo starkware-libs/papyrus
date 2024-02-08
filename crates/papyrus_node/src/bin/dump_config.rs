0801a30cbd27bb0bd89960c9417a91cc3d347df4
Shahak Shama <shahak@starkware.co>
2024-02-08 15:19:28 +0200

    build(node): add option to compile the node without rpc

--- crates/papyrus_node/src/bin/dump_config.rs
+++ crates/papyrus_node/src/bin/dump_config.rs
@@ -2,12 +2,18 @@
 // within this crate
 #![cfg_attr(coverage_nightly, feature(coverage_attribute))]
 
+#[cfg(feature = "rpc")]
 use lazy_static::lazy_static;
+#[cfg(feature = "rpc")]
 use papyrus_config::dumping::{ser_pointer_target_param, SerializeConfig};
+#[cfg(feature = "rpc")]
 use papyrus_config::{ParamPath, SerializedParam};
+#[cfg(feature = "rpc")]
 use papyrus_node::config::{NodeConfig, DEFAULT_CONFIG_PATH};
+#[cfg(feature = "rpc")]
 use starknet_api::core::ChainId;
 
+#[cfg(feature = "rpc")]
 lazy_static! {
     /// Returns vector of (pointer target name, pointer target serialized param, vec<pointer param path>)
     /// to be applied on the dumped node config.
@@ -43,7 +49,12 @@ lazy_static! {
 /// cargo run --bin dump_config -q
 #[cfg_attr(coverage_nightly, coverage_attribute)]
 fn main() {
+    #[cfg(feature = "rpc")]
     NodeConfig::default()
         .dump_to_file(&CONFIG_POINTERS, DEFAULT_CONFIG_PATH)
         .expect("dump to file error");
+    // TODO(shahak): Try to find a way to remove this binary altogether when the feature rpc is
+    // turned off.
+    #[cfg(not(feature = "rpc"))]
+    panic!("Can't dump config when the rpc feature is deactivated");
 }
