a84e89fe88533cc12f6ee12adda0a595aac95595
Shahak Shama <shahak@starkware.co>
2024-02-08 15:19:28 +0200

    build(node): add option to compile the node without rpc

--- crates/papyrus_node/src/bin/dump_config.rs
+++ crates/papyrus_node/src/bin/dump_config.rs
@@ -2,14 +2,23 @@
 // within this crate
 #![cfg_attr(coverage_nightly, feature(coverage_attribute))]
 
+#[cfg(feature = "rpc")]
 use papyrus_config::dumping::SerializeConfig;
-use papyrus_node::config::{NodeConfig, CONFIG_POINTERS, DEFAULT_CONFIG_PATH};
+#[cfg(feature = "rpc")]
+use papyrus_node::config::pointers::CONFIG_POINTERS;
+#[cfg(feature = "rpc")]
+use papyrus_node::config::{NodeConfig, DEFAULT_CONFIG_PATH};
 
 /// Updates the default config file by:
 /// cargo run --bin dump_config -q
 #[cfg_attr(coverage_nightly, coverage_attribute)]
 fn main() {
+    #[cfg(feature = "rpc")]
     NodeConfig::default()
         .dump_to_file(&CONFIG_POINTERS, DEFAULT_CONFIG_PATH)
         .expect("dump to file error");
+    // TODO(shahak): Try to find a way to remove this binary altogether when the feature rpc is
+    // turned off.
+    #[cfg(not(feature = "rpc"))]
+    panic!("Can't dump config when the rpc feature is deactivated");
 }
