ca14c4d7a9ff002448e563e733d432f6fb331ba0
Nevo Agmon <nevo@starkware.co>
2024-02-21 11:31:47 +0200

    feat(network): handle try_send error from net manager to subscriber

--- crates/papyrus_network/src/converters/mod.rs
+++ crates/papyrus_network/src/converters/mod.rs
@@ -63,6 +63,14 @@ pub(crate) struct Router {
     pub protocol_to_receiver_map: Option<HashMap<Protocol, Receiver<Vec<u8>>>>,
 }
 
+#[derive(Debug, thiserror::Error)]
+pub enum RouterError {
+    #[error("No sender for protocol {protocol:?}")]
+    NoSenderForProtocol { protocol: Protocol },
+    #[error(transparent)]
+    TrySendError(#[from] futures::channel::mpsc::TrySendError<Vec<u8>>),
+}
+
 impl Router {
     pub fn new(protocols: Vec<Protocol>, buffer_size: usize) -> Self {
         let mut protocol_to_sender_map = HashMap::new();
@@ -79,11 +87,11 @@ impl Router {
         self.protocol_to_receiver_map.take().unwrap_or_default()
     }
 
-    pub fn try_send(&mut self, protocol: Protocol, data: Vec<u8>) -> Result<(), ()> {
+    pub fn try_send(&mut self, protocol: Protocol, data: Vec<u8>) -> Result<(), RouterError> {
         self.protocol_to_sender_map
             .get_mut(&protocol)
-            .ok_or(())
-            .and_then(|sender| sender.try_send(data).map_err(|_| ()))
+            .ok_or(RouterError::NoSenderForProtocol { protocol })
+            .and_then(|sender| sender.try_send(data).map_err(RouterError::from))
     }
 }
 
