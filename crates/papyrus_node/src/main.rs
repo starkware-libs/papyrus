db8770d7c9c7a36a48d1928fb35329528c1097d2
Nevo Agmon <nevo@starkware.co>
2024-03-03 16:28:27 +0200

    fix(network): changes to p2p sync to support bytes behaviour

--- crates/papyrus_node/src/main.rs
+++ crates/papyrus_node/src/main.rs
@@ -18,7 +18,7 @@ use papyrus_config::validators::config_validate;
 use papyrus_config::ConfigError;
 use papyrus_monitoring_gateway::MonitoringServer;
 use papyrus_network::network_manager::NetworkError;
-use papyrus_network::{network_manager, NetworkConfig, Query, ResponseReceivers};
+use papyrus_network::{network_manager, NetworkConfig, Protocol, Query, ResponseReceivers};
 use papyrus_node::config::NodeConfig;
 use papyrus_node::version::VERSION_FULL;
 use papyrus_p2p_sync::{P2PSync, P2PSyncConfig, P2PSyncError};
@@ -212,7 +212,8 @@ fn run_network(config: Option<NetworkConfig>, storage_reader: StorageReader) -> 
     let Some(network_config) = config else { return (pending().boxed(), None) };
     let mut network_manager =
         network_manager::NetworkManager::new(network_config.clone(), storage_reader.clone());
-    let (query_sender, response_receivers) = network_manager.register_subscriber();
+    let (query_sender, response_receivers) =
+        network_manager.register_subscriber(vec![Protocol::SignedBlockHeader]);
     (network_manager.run().boxed(), Some((query_sender, response_receivers)))
 }
 
