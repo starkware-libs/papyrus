978374d5cfbe23b4e76793686c6b084db544368c
Nevo Agmon <nevo@starkware.co>
2024-03-04 15:38:22 +0200

    feat(network): convert network manager to work on bytes

--- crates/papyrus_network/src/network_manager/mod.rs
+++ crates/papyrus_network/src/network_manager/mod.rs
@@ -12,18 +12,20 @@ use futures::{FutureExt, StreamExt};
 use libp2p::swarm::{DialError, SwarmEvent};
 use libp2p::Swarm;
 use papyrus_storage::StorageReader;
+use prost::Message;
 use tracing::{debug, error, trace};
 
 use self::swarm_trait::SwarmTrait;
 use crate::bin_utils::{build_swarm, dial};
-use crate::block_headers::behaviour::Behaviour as BlockHeadersBehaviour;
-use crate::block_headers::Event;
+use crate::converters::Router;
 use crate::db_executor::{self, BlockHeaderDBExecutor, DBExecutor, Data, QueryId};
-use crate::streamed_data::InboundSessionId;
-use crate::{NetworkConfig, PeerAddressConfig, Query, ResponseReceivers, ResponseSenders};
+use crate::protobuf_messages::protobuf;
+use crate::streamed_bytes::behaviour::{Behaviour, SessionError};
+use crate::streamed_bytes::{Config, GenericEvent, InboundSessionId};
+use crate::{NetworkConfig, PeerAddressConfig, Protocol, Query, ResponseReceivers};
 
 type StreamCollection = SelectAll<BoxStream<'static, (Data, InboundSessionId)>>;
-type SyncSubscriberChannels = (Receiver<Query>, ResponseSenders);
+type SubscriberChannels = (Receiver<Query>, Router);
 
 #[derive(thiserror::Error, Debug)]
 pub enum NetworkError {
@@ -36,7 +38,7 @@ pub struct GenericNetworkManager<DBExecutorT: DBExecutor, SwarmT: SwarmTrait> {
     db_executor: DBExecutorT,
     header_buffer_size: usize,
     query_results_router: StreamCollection,
-    sync_subscriber_channels: Option<SyncSubscriberChannels>,
+    sync_subscriber_channels: Option<SubscriberChannels>,
     query_id_to_inbound_session_id: HashMap<QueryId, InboundSessionId>,
     peer: Option<PeerAddressConfig>,
 }
@@ -53,7 +55,7 @@ impl<DBExecutorT: DBExecutor, SwarmT: SwarmTrait> GenericNetworkManager<DBExecut
             tokio::select! {
                 Some(event) = self.swarm.next() => self.handle_swarm_event(event),
                 Some(res) = self.db_executor.next() => self.handle_db_executor_result(res),
-                Some(res) = self.query_results_router.next() => self.handle_query_result_routing(res),
+                Some(res) = self.query_results_router.next() => self.handle_query_result_routing_to_other_peer(res),
                 Some(res) = self.sync_subscriber_channels.as_mut()
                 .map(|(query_receiver, _)| query_receiver.next().boxed())
                 .unwrap_or(pending().boxed()) => self.handle_sync_subscriber_query(res),
@@ -78,16 +80,18 @@ impl<DBExecutorT: DBExecutor, SwarmT: SwarmTrait> GenericNetworkManager<DBExecut
         }
     }
 
-    pub fn register_subscriber(&mut self) -> (Sender<Query>, ResponseReceivers) {
+    pub fn register_subscriber(
+        &mut self,
+        protocols: Vec<Protocol>,
+    ) -> (Sender<Query>, ResponseReceivers) {
         let (sender, query_receiver) = futures::channel::mpsc::channel(self.header_buffer_size);
-        let (response_sender, response_receiver) =
-            futures::channel::mpsc::channel(self.header_buffer_size);
-        self.sync_subscriber_channels =
-            Some((query_receiver, ResponseSenders { signed_headers_sender: response_sender }));
-        (sender, ResponseReceivers::new(response_receiver))
+        let mut router = Router::new(protocols, self.header_buffer_size);
+        let response_receiver = ResponseReceivers::new(router.get_recievers());
+        self.sync_subscriber_channels = Some((query_receiver, router));
+        (sender, response_receiver)
     }
 
-    fn handle_swarm_event(&mut self, event: SwarmEvent<Event>) {
+    fn handle_swarm_event(&mut self, event: SwarmEvent<GenericEvent<SessionError>>) {
         match event {
             SwarmEvent::ConnectionEstablished { .. } => {
                 debug!("Connected to a peer!");
@@ -152,7 +156,13 @@ impl<DBExecutorT: DBExecutor, SwarmT: SwarmTrait> GenericNetworkManager<DBExecut
                         error!("Received error on non existing query");
                         return;
                     };
-                    if self.swarm.send_data(Data::Fin, inbound_session_id).is_err() {
+                    // TODO: consider moving conversion out of here so network manager would
+                    // only know bytes.
+                    let mut fin_bytes = vec![];
+                    <Data as Into<protobuf::BlockHeadersResponse>>::into(Data::Fin)
+                        .encode(&mut fin_bytes)
+                        .expect("failed to convert fin to bytes");
+                    if self.swarm.send_data(fin_bytes, inbound_session_id).is_err() {
                         error!(
                             "Tried to close inbound session {inbound_session_id:?} due to {err:?} \
                              but the session was already closed"
@@ -163,52 +173,63 @@ impl<DBExecutorT: DBExecutor, SwarmT: SwarmTrait> GenericNetworkManager<DBExecut
         };
     }
 
-    fn handle_behaviour_event(&mut self, event: Event) {
+    fn handle_behaviour_event(&mut self, event: GenericEvent<SessionError>) {
         match event {
-            Event::NewInboundQuery { query, inbound_session_id } => {
+            GenericEvent::NewInboundSession {
+                query,
+                inbound_session_id,
+                peer_id: _,
+                protocol_name: _,
+            } => {
                 trace!(
                     "Received new inbound query: {query:?} for session id: {inbound_session_id:?}"
                 );
                 let (sender, receiver) = futures::channel::mpsc::channel(self.header_buffer_size);
                 // TODO: use query id for bookkeeping.
-                let query_id = self.db_executor.register_query(query, sender);
+                // TODO: consider moving conversion out of network manager.
+                let internal_query = protobuf::BlockHeadersRequest::decode(&query[..])
+                    .expect("failed to decode protobuf BlockHeadersRequest")
+                    .try_into()
+                    .expect("failed to convert BlockHeadersRequest");
+                let query_id = self.db_executor.register_query(internal_query, sender);
                 self.query_id_to_inbound_session_id.insert(query_id, inbound_session_id);
                 self.query_results_router
                     .push(receiver.map(move |data| (data, inbound_session_id)).boxed());
             }
-            Event::ReceivedData { signed_header, outbound_session_id } => {
+            GenericEvent::ReceivedData { outbound_session_id, data } => {
                 debug!(
                     "Received data from peer for session id: {outbound_session_id:?}. sending to \
                      sync subscriber."
                 );
                 if let Some((_, response_senders)) = self.sync_subscriber_channels.as_mut() {
-                    if let Err(e) = response_senders.signed_headers_sender.try_send(signed_header) {
+                    // TODO: once we have more protocols map session id to protocol.
+                    if let Err(e) = response_senders.try_send(Protocol::SignedBlockHeader, data) {
                         error!("Failed to send data to sync subscriber. error: {e:?}");
                     }
                 }
             }
-            Event::SessionFailed { session_id, session_error } => {
-                debug!("Session {session_id:?} failed on {session_error:?}");
+            GenericEvent::SessionFailed { session_id, error } => {
+                debug!("Session {session_id:?} failed on {error:?}");
                 // TODO: Handle reputation and retry.
             }
-            Event::QueryConversionError(error) => {
-                debug!("Failed to convert incoming query on {error:?}");
-                // TODO: Consider adding peer_id to event and handling reputation.
-            }
-            Event::SessionFinishedSuccessfully { session_id } => {
+            GenericEvent::SessionFinishedSuccessfully { session_id } => {
                 debug!("Session completed successfully. session_id: {session_id:?}");
             }
         }
     }
 
-    fn handle_query_result_routing(&mut self, res: (Data, InboundSessionId)) {
+    fn handle_query_result_routing_to_other_peer(&mut self, res: (Data, InboundSessionId)) {
         if self.query_results_router.is_empty() {
             // We're done handling all the queries we had and the stream is exhausted.
             // Creating a new stream collection to process new queries.
             self.query_results_router = StreamCollection::new();
         }
         let (data, inbound_session_id) = res;
-        self.swarm.send_data(data, inbound_session_id).unwrap_or_else(|e| {
+        let mut data_bytes = vec![];
+        <Data as Into<protobuf::BlockHeadersResponse>>::into(data)
+            .encode(&mut data_bytes)
+            .expect("failed to convert data to bytes");
+        self.swarm.send_data(data_bytes, inbound_session_id).unwrap_or_else(|e| {
             error!("Failed to send data to peer. Session id not found error: {e:?}");
         })
     }
@@ -220,12 +241,15 @@ impl<DBExecutorT: DBExecutor, SwarmT: SwarmTrait> GenericNetworkManager<DBExecut
             .expect("Cannot send query without peer")
             // TODO: get peer id from swarm after dial id not received in config.
             .peer_id;
-        let internal_query = query.into();
-        match self.swarm.send_query(internal_query, peer_id) {
+        let mut query_bytes = vec![];
+        <Query as Into<protobuf::BlockHeadersRequest>>::into(query)
+            .encode(&mut query_bytes)
+            .expect("failed to convert query to bytes");
+        match self.swarm.send_query(query_bytes, peer_id, Protocol::SignedBlockHeader) {
             Ok(outbound_session_id) => {
                 debug!(
-                    "Sent query to peer. query: {internal_query:?}, peer_id: {peer_id:?}, \
-                     outbound_session_id: {outbound_session_id:?}"
+                    "Sent query to peer. peer_id: {peer_id:?}, outbound_session_id: \
+                     {outbound_session_id:?}"
                 );
             }
             Err(e) => error!("Failed to send query to peer. Peer not connected error: {e:?}"),
@@ -233,8 +257,7 @@ impl<DBExecutorT: DBExecutor, SwarmT: SwarmTrait> GenericNetworkManager<DBExecut
     }
 }
 
-pub type NetworkManager =
-    GenericNetworkManager<BlockHeaderDBExecutor, Swarm<BlockHeadersBehaviour>>;
+pub type NetworkManager = GenericNetworkManager<BlockHeaderDBExecutor, Swarm<Behaviour>>;
 
 impl NetworkManager {
     // TODO: add tests for this struct.
@@ -257,7 +280,10 @@ impl NetworkManager {
         let swarm = build_swarm(
             listen_addresses,
             idle_connection_timeout,
-            BlockHeadersBehaviour::new(session_timeout),
+            Behaviour::new(Config {
+                session_timeout,
+                supported_inbound_protocols: vec![Protocol::SignedBlockHeader.into()],
+            }),
         );
 
         let db_executor = BlockHeaderDBExecutor::new(storage_reader);
--- crates/papyrus_network/src/network_manager/swarm_trait.rs
+++ crates/papyrus_network/src/network_manager/swarm_trait.rs
@@ -1,39 +1,36 @@
 use futures::stream::Stream;
-use libp2p::multiaddr::Protocol;
+use libp2p::multiaddr::Protocol as LibP2pProtocol;
 use libp2p::swarm::dial_opts::DialOpts;
 use libp2p::swarm::{DialError, NetworkBehaviour, SwarmEvent};
 use libp2p::{Multiaddr, PeerId, Swarm};
 
-use crate::block_headers::behaviour::{
-    Behaviour as BlockHeadersBehaviour,
-    PeerNotConnected,
-    SessionIdNotFoundError,
-};
-use crate::db_executor::Data;
-use crate::streamed_data::{InboundSessionId, OutboundSessionId};
-use crate::{InternalQuery, PeerAddressConfig};
-pub type Event = SwarmEvent<<BlockHeadersBehaviour as NetworkBehaviour>::ToSwarm>;
+use crate::streamed_bytes::behaviour::{Behaviour, PeerNotConnected, SessionIdNotFoundError};
+use crate::streamed_bytes::{InboundSessionId, OutboundSessionId};
+use crate::{PeerAddressConfig, Protocol};
+
+pub type Event = SwarmEvent<<Behaviour as NetworkBehaviour>::ToSwarm>;
 
 pub trait SwarmTrait: Stream<Item = Event> + Unpin {
     fn send_data(
         &mut self,
-        data: Data,
+        data: Vec<u8>,
         inbound_session_id: InboundSessionId,
     ) -> Result<(), SessionIdNotFoundError>;
 
     fn send_query(
         &mut self,
-        query: InternalQuery,
+        query: Vec<u8>,
         peer_id: PeerId,
+        protocol: Protocol,
     ) -> Result<OutboundSessionId, PeerNotConnected>;
 
     fn dial(&mut self, peer: PeerAddressConfig) -> Result<(), DialError>;
 }
 
-impl SwarmTrait for Swarm<BlockHeadersBehaviour> {
+impl SwarmTrait for Swarm<Behaviour> {
     fn send_data(
         &mut self,
-        data: Data,
+        data: Vec<u8>,
         inbound_session_id: InboundSessionId,
     ) -> Result<(), SessionIdNotFoundError> {
         self.behaviour_mut().send_data(data, inbound_session_id)
@@ -41,17 +38,18 @@ impl SwarmTrait for Swarm<BlockHeadersBehaviour> {
 
     fn send_query(
         &mut self,
-        query: InternalQuery,
+        query: Vec<u8>,
         peer_id: PeerId,
+        protocol: Protocol,
     ) -> Result<OutboundSessionId, PeerNotConnected> {
-        self.behaviour_mut().send_query(query, peer_id)
+        self.behaviour_mut().send_query(query, peer_id, protocol.into())
     }
 
     fn dial(&mut self, peer: PeerAddressConfig) -> Result<(), DialError> {
         let address = format!("/ip4/{}", peer.ip)
             .parse::<Multiaddr>()
             .expect("string to multiaddr failed")
-            .with(Protocol::Tcp(peer.tcp_port));
+            .with(LibP2pProtocol::Tcp(peer.tcp_port));
 
         self.dial(DialOpts::peer_id(peer.peer_id).addresses(vec![address]).build())
     }
