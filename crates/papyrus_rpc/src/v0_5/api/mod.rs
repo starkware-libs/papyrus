27921fb26bc10bdd648e7a2a4f3fab4ee6873c68
Shahak Shama <shahak@starkware.co>
2024-03-05 09:59:48 +0200

    fix(JSON-RPC): fix serialization of trace in v0.5

--- crates/papyrus_rpc/src/v0_5/api/mod.rs
+++ crates/papyrus_rpc/src/v0_5/api/mod.rs
@@ -8,7 +8,6 @@ use jsonrpsee::types::ErrorObjectOwned;
 use papyrus_common::deprecated_class_abi::calculate_deprecated_class_abi_length;
 use papyrus_common::pending_classes::ApiContractClass;
 use papyrus_common::BlockHashAndNumber;
-use papyrus_execution::objects::TransactionTrace;
 use papyrus_execution::{AbiSize, ExecutableTransactionInput, ExecutionError, SierraSize};
 use papyrus_proc_macros::versioned_rpc;
 use papyrus_storage::compiled_class::CasmStorageReader;
@@ -33,6 +32,7 @@ use super::broadcasted_transaction::{
 };
 use super::deprecated_contract_class::ContractClass as DeprecatedContractClass;
 use super::error::{JsonRpcError, BLOCK_NOT_FOUND, INVALID_CONTINUATION_TOKEN};
+use super::execution::TransactionTrace;
 use super::state::{ContractClass, StateUpdate};
 use super::transaction::{
     DeployAccountTransaction,
