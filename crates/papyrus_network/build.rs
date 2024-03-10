991966bd6dcc1ea7555dbb1eac79f462c60607d8
ShahakShama <70578257+ShahakShama@users.noreply.github.com>
2024-03-10 14:26:41 +0200

    revert(network): add conversion from protobuf::StateDiffsResponse to ThinStateDiff (#1792) (#1794)
    Revert "feat(network): add conversion from protobuf::StateDiffsResponse to ThinStateDiff (#1792)"
    
    This reverts commit fc1aa73c1c3a52e902da4a32ac91d748e8ec1d95.

--- crates/papyrus_network/build.rs
+++ crates/papyrus_network/build.rs
@@ -3,10 +3,7 @@ use std::io::Result;
 fn main() -> Result<()> {
     println!("Building");
     prost_build::compile_protos(
-        &[
-            "src/protobuf_messages/proto/p2p/proto/header.proto",
-            "src/protobuf_messages/proto/p2p/proto/state.proto",
-        ],
+        &["src/protobuf_messages/proto/p2p/proto/header.proto"],
         &["src/protobuf_messages/proto/"],
     )?;
     Ok(())
