c4b7873dd6a2e4a2a6fe066d3e3261ee26071956
Shahak Shama <shahak@starkware.co>
2024-03-11 12:55:39 +0200

    refactor(sync): make changes to allow easily adding more types to sync

---
+++ crates/papyrus_p2p_sync/src/header.rs
@@ -0,0 +1,81 @@
+use std::pin::Pin;
+
+use futures::future::BoxFuture;
+use futures::{FutureExt, Stream, StreamExt};
+use papyrus_network::{DataType, SignedBlockHeader};
+use papyrus_storage::header::{HeaderStorageReader, HeaderStorageWriter};
+use papyrus_storage::{StorageError, StorageReader, StorageWriter};
+use starknet_api::block::BlockNumber;
+use tracing::debug;
+
+use crate::stream_factory::{BlockData, BlockNumberLimit, DataStreamFactory};
+use crate::{P2PSyncError, ALLOWED_SIGNATURES_LENGTH, NETWORK_DATA_TIMEOUT};
+
+impl BlockData for SignedBlockHeader {
+    fn write_to_storage(&self, storage_writer: &mut StorageWriter) -> Result<(), StorageError> {
+        storage_writer
+            .begin_rw_txn()?
+            .append_header(self.block_header.block_number, &self.block_header)?
+            .append_block_signature(
+                self.block_header.block_number,
+                &self
+                    .signatures
+                    // In the future we will support multiple signatures.
+                    .first()
+                    // The verification that the size of the vector is 1 is done in the data
+                    // verification.
+                    .expect("Vec::first should return a value on a vector of size 1"),
+            )?
+            .commit()
+    }
+}
+
+pub(crate) struct HeaderStreamFactory;
+
+impl DataStreamFactory for HeaderStreamFactory {
+    type InputFromNetwork = SignedBlockHeader;
+    type Output = SignedBlockHeader;
+
+    const DATA_TYPE: DataType = DataType::SignedBlockHeader;
+    const BLOCK_NUMBER_LIMIT: BlockNumberLimit = BlockNumberLimit::Unlimited;
+    const SHOULD_LOG_ADDED_BLOCK: bool = true;
+
+    fn parse_data_for_block<'a>(
+        signed_headers_receiver: &'a mut Pin<
+            Box<dyn Stream<Item = Option<Self::InputFromNetwork>> + Send>,
+        >,
+        block_number: BlockNumber,
+        _storage_reader: &'a StorageReader,
+    ) -> BoxFuture<'a, Result<Option<Self::Output>, P2PSyncError>> {
+        async move {
+            let maybe_signed_header_stream_result =
+                tokio::time::timeout(NETWORK_DATA_TIMEOUT, signed_headers_receiver.next()).await?;
+            let Some(maybe_signed_header) = maybe_signed_header_stream_result else {
+                return Err(P2PSyncError::ReceiverChannelTerminated);
+            };
+            let Some(signed_block_header) = maybe_signed_header else {
+                debug!("Header query sent to network finished");
+                return Ok(None);
+            };
+            // TODO(shahak): Check that parent_hash is the same as the previous block's hash
+            // and handle reverts.
+            if block_number != signed_block_header.block_header.block_number {
+                return Err(P2PSyncError::HeadersUnordered {
+                    expected_block_number: block_number,
+                    actual_block_number: signed_block_header.block_header.block_number,
+                });
+            }
+            if signed_block_header.signatures.len() != ALLOWED_SIGNATURES_LENGTH {
+                return Err(P2PSyncError::WrongSignaturesLength {
+                    signatures: signed_block_header.signatures,
+                });
+            }
+            Ok(Some(signed_block_header))
+        }
+        .boxed()
+    }
+
+    fn get_start_block_number(storage_reader: &StorageReader) -> Result<BlockNumber, StorageError> {
+        storage_reader.begin_ro_txn()?.get_header_marker()
+    }
+}
--- crates/papyrus_p2p_sync/src/lib.rs
+++ crates/papyrus_p2p_sync/src/lib.rs
@@ -1,20 +1,24 @@
+mod header;
 #[cfg(test)]
 mod p2p_sync_test;
+mod stream_factory;
 
 use std::collections::BTreeMap;
 use std::time::Duration;
 
 use futures::channel::mpsc::{SendError, Sender};
-use futures::{SinkExt, StreamExt};
+use futures::StreamExt;
 use papyrus_config::converters::deserialize_seconds_to_duration;
 use papyrus_config::dumping::{ser_param, SerializeConfig};
 use papyrus_config::{ParamPath, ParamPrivacyInput, SerializedParam};
-use papyrus_network::{DataType, Direction, Query, ResponseReceivers, SignedBlockHeader};
-use papyrus_storage::header::{HeaderStorageReader, HeaderStorageWriter};
+use papyrus_network::{Query, ResponseReceivers};
 use papyrus_storage::{StorageError, StorageReader, StorageWriter};
 use serde::{Deserialize, Serialize};
 use starknet_api::block::{BlockNumber, BlockSignature};
-use tracing::{debug, info, instrument};
+use tracing::instrument;
+
+use crate::header::HeaderStreamFactory;
+use crate::stream_factory::DataStreamFactory;
 
 const STEP: usize = 1;
 const ALLOWED_SIGNATURES_LENGTH: usize = 1;
@@ -67,8 +71,8 @@ pub enum P2PSyncError {
     )]
     HeadersUnordered { expected_block_number: BlockNumber, actual_block_number: BlockNumber },
     #[error("Expected to receive one signature from the network. got {signatures:?} instead.")]
-    // TODO(shahak): Move this error to network.
     // TODO(shahak): Remove this and report to network on invalid data once that's possible.
+    // Right now we support only one signature. In the future we will support many signatures.
     WrongSignaturesLength { signatures: Vec<BlockSignature> },
     // TODO(shahak): Remove this and report to network on invalid data once that's possible.
     #[error("Network returned more responses than expected for a query.")]
@@ -82,11 +86,6 @@ pub enum P2PSyncError {
     StorageError(#[from] StorageError),
     #[error(transparent)]
     SendError(#[from] SendError),
-}
-
-enum P2PSyncControl {
-    ContinueDownloading,
-    QueryFinishedPartially,
 }
 
 pub struct P2PSync {
@@ -110,83 +109,17 @@ impl P2PSync {
 
     #[instrument(skip(self), level = "debug", err)]
     pub async fn run(mut self) -> Result<(), P2PSyncError> {
-        let mut current_block_number = self.storage_reader.begin_ro_txn()?.get_header_marker()?;
-        // TODO: make control more substantial once we have more peers and peer management.
-        let mut control = P2PSyncControl::ContinueDownloading;
-        loop {
-            if matches!(control, P2PSyncControl::QueryFinishedPartially) {
-                debug!(
-                    "Query returned with partial data. Waiting {:?} before sending another query.",
-                    self.config.wait_period_for_new_data
-                );
-                tokio::time::sleep(self.config.wait_period_for_new_data).await;
-            }
-            let end_block_number = current_block_number.0
-                + u64::try_from(self.config.num_headers_per_query)
-                    .expect("Failed converting usize to u64");
-            debug!("Downloading blocks [{}, {})", current_block_number.0, end_block_number);
-            self.query_sender
-                .send(Query {
-                    start_block: current_block_number,
-                    direction: Direction::Forward,
-                    limit: self.config.num_headers_per_query,
-                    step: STEP,
-                    data_type: DataType::SignedBlockHeader,
-                })
-                .await?;
-            control = self.parse_headers(&mut current_block_number, end_block_number).await?;
-        }
-    }
+        let mut data_stream = HeaderStreamFactory::create_stream(
+            self.response_receivers.signed_headers_receiver,
+            self.query_sender,
+            self.storage_reader,
+            self.config.wait_period_for_new_data,
+            self.config.num_headers_per_query,
+        );
 
-    #[instrument(skip(self), level = "debug", err)]
-    async fn parse_headers(
-        &mut self,
-        current_block_number: &mut BlockNumber,
-        end_block_number: u64,
-    ) -> Result<P2PSyncControl, P2PSyncError> {
-        while current_block_number.0 < end_block_number {
-            let maybe_signed_header_stream_result = tokio::time::timeout(
-                NETWORK_DATA_TIMEOUT,
-                self.response_receivers.signed_headers_receiver.next(),
-            )
-            .await?;
-            let Some(maybe_signed_header) = maybe_signed_header_stream_result else {
-                return Err(P2PSyncError::ReceiverChannelTerminated);
-            };
-            let Some(SignedBlockHeader { block_header, signatures }) = maybe_signed_header else {
-                debug!("Header query sent to network finished");
-                return Ok(P2PSyncControl::QueryFinishedPartially);
-            };
-            // TODO(shahak): Check that parent_hash is the same as the previous block's hash
-            // and handle reverts.
-            if *current_block_number != block_header.block_number {
-                return Err(P2PSyncError::HeadersUnordered {
-                    expected_block_number: *current_block_number,
-                    actual_block_number: block_header.block_number,
-                });
-            }
-            if signatures.len() != ALLOWED_SIGNATURES_LENGTH {
-                return Err(P2PSyncError::WrongSignaturesLength { signatures });
-            }
-            self.storage_writer
-                .begin_rw_txn()?
-                .append_header(*current_block_number, &block_header)?
-                .append_block_signature(
-                    *current_block_number,
-                    signatures.first().expect(
-                        "Calling first on a vector of size {ALLOWED_SIGNATURES_LENGTH} returned \
-                         None",
-                    ),
-                )?
-                .commit()?;
-            info!("Added block {}.", current_block_number);
-            *current_block_number = current_block_number.next();
-        }
-        // Consume the None message signaling the end of the query.
-        match self.response_receivers.signed_headers_receiver.next().await {
-            Some(None) => Ok(P2PSyncControl::ContinueDownloading),
-            Some(Some(_)) => Err(P2PSyncError::TooManyResponses),
-            None => Err(P2PSyncError::ReceiverChannelTerminated),
+        loop {
+            let data = data_stream.next().await.expect("Sync data stream should never end")?;
+            data.write_to_storage(&mut self.storage_writer)?;
         }
     }
 }
