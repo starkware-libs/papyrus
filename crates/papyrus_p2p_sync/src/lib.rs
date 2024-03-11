b1f7587862bdf63510d06cddb333994a44604e98
Shahak Shama <shahak@starkware.co>
2024-03-11 09:31:10 +0200

    fix(sync): sleep when sync receives None and remove timeout hack

--- crates/papyrus_p2p_sync/src/lib.rs
+++ crates/papyrus_p2p_sync/src/lib.rs
@@ -14,7 +14,6 @@ use papyrus_storage::header::{HeaderStorageReader, HeaderStorageWriter};
 use papyrus_storage::{StorageError, StorageReader, StorageWriter};
 use serde::{Deserialize, Serialize};
 use starknet_api::block::{BlockNumber, BlockSignature};
-use tokio::time::timeout;
 use tracing::{debug, info, instrument};
 
 const STEP: usize = 1;
@@ -23,9 +22,8 @@ const ALLOWED_SIGNATURES_LENGTH: usize = 1;
 #[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq)]
 pub struct P2PSyncConfig {
     pub num_headers_per_query: usize,
-    // TODO(shahak): Remove timeout and check if query finished when the network reports it.
     #[serde(deserialize_with = "deserialize_seconds_to_duration")]
-    pub query_timeout: Duration,
+    pub wait_period_for_new_data: Duration,
 }
 
 impl SerializeConfig for P2PSyncConfig {
@@ -38,9 +36,10 @@ impl SerializeConfig for P2PSyncConfig {
                 ParamPrivacyInput::Public,
             ),
             ser_param(
-                "query_timeout",
-                &self.query_timeout.as_secs(),
-                "Time in seconds to wait for query responses until we mark it as failed",
+                "wait_period_for_new_data",
+                &self.wait_period_for_new_data.as_secs(),
+                "Time in seconds to wait when a query returned with partial data before sending a \
+                 new query",
                 ParamPrivacyInput::Public,
             ),
         ])
@@ -49,12 +48,16 @@ impl SerializeConfig for P2PSyncConfig {
 
 impl Default for P2PSyncConfig {
     fn default() -> Self {
-        P2PSyncConfig { num_headers_per_query: 10000, query_timeout: Duration::from_secs(5) }
+        P2PSyncConfig {
+            num_headers_per_query: 10000,
+            wait_period_for_new_data: Duration::from_secs(5),
+        }
     }
 }
 
 #[derive(thiserror::Error, Debug)]
 pub enum P2PSyncError {
+    // TODO(shahak): Remove this and report to network on invalid data once that's possible.
     // TODO(shahak): Consider removing this error and handling unordered headers without failing.
     #[error(
         "Blocks returned unordered from the network. Expected header with \
@@ -63,7 +66,11 @@ pub enum P2PSyncError {
     HeadersUnordered { expected_block_number: BlockNumber, actual_block_number: BlockNumber },
     #[error("Expected to receive one signature from the network. got {signatures:?} instead.")]
     // TODO(shahak): Move this error to network.
+    // TODO(shahak): Remove this and report to network on invalid data once that's possible.
     WrongSignaturesLength { signatures: Vec<BlockSignature> },
+    // TODO(shahak): Remove this and report to network on invalid data once that's possible.
+    #[error("Network returned more responses than expected for a query.")]
+    TooManyResponses,
     // TODO(shahak): Replicate this error for each data type.
     #[error("The sender end of the response receivers was closed.")]
     ReceiverChannelTerminated,
@@ -75,7 +82,7 @@ pub enum P2PSyncError {
 
 enum P2PSyncControl {
     ContinueDownloading,
-    PeerFinished,
+    QueryFinishedPartially,
 }
 
 pub struct P2PSync {
@@ -101,10 +108,15 @@ impl P2PSync {
     pub async fn run(mut self) -> Result<(), P2PSyncError> {
         let mut current_block_number = self.storage_reader.begin_ro_txn()?.get_header_marker()?;
         // TODO: make control more substantial once we have more peers and peer management.
-        #[allow(unused_variables)]
         let mut control = P2PSyncControl::ContinueDownloading;
-        #[allow(unused_assignments)]
         loop {
+            if matches!(control, P2PSyncControl::QueryFinishedPartially) {
+                debug!(
+                    "Query returned with partial data. Waiting {:?} before sending another query.",
+                    self.config.wait_period_for_new_data
+                );
+                tokio::time::sleep(self.config.wait_period_for_new_data).await;
+            }
             let end_block_number = current_block_number.0
                 + u64::try_from(self.config.num_headers_per_query)
                     .expect("Failed converting usize to u64");
@@ -129,29 +141,14 @@ impl P2PSync {
         end_block_number: u64,
     ) -> Result<P2PSyncControl, P2PSyncError> {
         while current_block_number.0 < end_block_number {
-            // Adding timeout because the network currently doesn't report when a query
-            // finished because the peers don't know about these blocks. If not all expected
-            // responses returned we will retry the query from the last received block.
-            // TODO(shahak): Once network reports finished queries, remove this timeout and add
-            // a sleep when a query finished with partial responses.
-            let Ok(maybe_signed_header_stream_result) = timeout(
-                self.config.query_timeout,
-                self.response_receivers.signed_headers_receiver.next(),
-            )
-            .await
-            else {
-                debug!(
-                    "Other peer returned headers until {:?} when we requested until {:?}",
-                    current_block_number, end_block_number
-                );
-                return Ok(P2PSyncControl::ContinueDownloading);
-            };
+            let maybe_signed_header_stream_result =
+                self.response_receivers.signed_headers_receiver.next().await;
             let Some(maybe_signed_header) = maybe_signed_header_stream_result else {
                 return Err(P2PSyncError::ReceiverChannelTerminated);
             };
             let Some(SignedBlockHeader { block_header, signatures }) = maybe_signed_header else {
-                debug!("Handle empty signed headers -> mark that peer sent Fin");
-                return Ok(P2PSyncControl::PeerFinished);
+                debug!("Header query sent to network finished");
+                return Ok(P2PSyncControl::QueryFinishedPartially);
             };
             // TODO(shahak): Check that parent_hash is the same as the previous block's hash
             // and handle reverts.
@@ -178,6 +175,11 @@ impl P2PSync {
             info!("Added block {}.", current_block_number);
             *current_block_number = current_block_number.next();
         }
-        Ok(P2PSyncControl::ContinueDownloading)
+        // Consume the None message signaling the end of the query.
+        match self.response_receivers.signed_headers_receiver.next().await {
+            Some(None) => Ok(P2PSyncControl::ContinueDownloading),
+            Some(Some(_)) => Err(P2PSyncError::TooManyResponses),
+            None => Err(P2PSyncError::ReceiverChannelTerminated),
+        }
     }
 }
