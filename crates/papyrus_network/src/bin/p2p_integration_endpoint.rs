6a2b6112113cdb7fcd8c4757f2c234b8cf2920e8
eitanm-starkware <eitan.m@starkware.co>
2024-03-13 15:30:32 +0200

    refactor: remove dial from NetworkManager

--- crates/papyrus_network/src/bin/p2p_integration_endpoint.rs
+++ crates/papyrus_network/src/bin/p2p_integration_endpoint.rs
@@ -1,7 +1,9 @@
+use std::str::FromStr;
 use std::time::Duration;
 
 use clap::Parser;
-use papyrus_network::{network_manager, NetworkConfig};
+use libp2p::PeerId;
+use papyrus_network::{network_manager, NetworkConfig, PeerAddressConfig};
 use papyrus_storage::{open_storage, StorageConfig};
 
 /// A dummy P2P capable node for integration with other P2P capable nodes.
@@ -16,9 +18,13 @@ struct Args {
     #[arg(short, long)]
     quic_port: u16,
 
-    /// Address this node attempts to dial to.
+    /// Peer id of this node.
     #[arg(short, long)]
-    dial_address: Option<String>,
+    peer_id: String,
+
+    /// IP address this node listens on for incoming connections.
+    #[arg(short, long)]
+    ip: String,
 
     /// Amount of time (in seconds) to wait until closing an idle connection.
     #[arg(short = 't', long, default_value_t = 1)]
@@ -31,21 +37,20 @@ async fn main() {
 
     let (storage_reader, _storage_writer) =
         open_storage(StorageConfig::default()).expect("failed to open storage");
-    let mut network_manager = network_manager::NetworkManager::new(
+    let network_manager = network_manager::NetworkManager::new(
         NetworkConfig {
             tcp_port: args.tcp_port,
             quic_port: args.quic_port,
             session_timeout: Duration::from_secs(10),
             idle_connection_timeout: Duration::from_secs(args.idle_connection_timeout),
             header_buffer_size: 100000,
-            peer: None,
+            peer: Some(PeerAddressConfig {
+                peer_id: PeerId::from_str(&args.peer_id).expect("Invalid peer ID"),
+                tcp_port: args.tcp_port,
+                ip: args.ip,
+            }),
         },
         storage_reader,
     );
-    // TODO: use peer config from the network config and remove the dial function from the network
-    // manager (use the dial within the run function instead of here).
-    if let Some(dial_address) = args.dial_address.as_ref() {
-        network_manager.dial(dial_address);
-    }
     network_manager.run().await.expect("Network manager failed");
 }
