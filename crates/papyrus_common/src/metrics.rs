ce86ff7069be086ac997b6312ae4f77b2607c459
ShahakShama <70578257+ShahakShama@users.noreply.github.com>
2024-04-02 09:59:53 +0300

    chore(network): add num_active_inbound/outbound_sessions metric (#1841)

--- crates/papyrus_common/src/metrics.rs
+++ crates/papyrus_common/src/metrics.rs
@@ -24,3 +24,9 @@ pub const PAPYRUS_HEADER_LATENCY_SEC: &str = "papyrus_header_latency";
 
 /// The number of peers this node is connected to.
 pub const PAPYRUS_NUM_CONNECTED_PEERS: &str = "papyrus_num_connected_peers";
+
+/// The number of active sessions this peer has in which it sends data.
+pub const PAPYRUS_NUM_ACTIVE_INBOUND_SESSIONS: &str = "papyrus_num_active_inbound_sessions";
+
+/// The number of active sessions this peer has in which it requests data.
+pub const PAPYRUS_NUM_ACTIVE_OUTBOUND_SESSIONS: &str = "papyrus_num_active_outbound_sessions";
