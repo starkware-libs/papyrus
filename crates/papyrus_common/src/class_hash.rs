0c9d373a593e677bd019c57daea0872be4c576d8
Yair Bakalchuk <yair@starkware.co>
2024-04-04 16:31:56 +0300

    feat(common): calculate deprecated class hash

--- crates/papyrus_common/src/class_hash.rs
+++ crates/papyrus_common/src/class_hash.rs
@@ -1,20 +1,49 @@
 #[cfg(test)]
 #[path = "class_hash_test.rs"]
 mod class_hash_test;
+use std::num::ParseIntError;
+
+use itertools::Itertools;
 use lazy_static::lazy_static;
+use serde::{Serialize, Serializer};
 use sha3::Digest;
 use starknet_api::core::ClassHash;
-use starknet_api::hash::{poseidon_hash_array, PoseidonHash, StarkFelt};
+use starknet_api::deprecated_contract_class::{
+    ContractClass as DeprecatedContractClass,
+    EntryPointType as DeprecatedEntryPointType,
+    Program as DeprecatedContractClassProgram,
+};
+use starknet_api::hash::{
+    pedersen_hash_array,
+    poseidon_hash_array,
+    PoseidonHash,
+    StarkFelt,
+    StarkHash,
+};
 use starknet_api::state::{ContractClass, EntryPointType};
 use starknet_crypto::FieldElement;
 
+use crate::deprecated_class_abi::PythonJsonFormatter;
 use crate::usize_into_felt;
 
 lazy_static! {
-    static ref API_VERSION: StarkFelt = StarkFelt::from(
+    static ref SIERRA_API_VERSION: StarkFelt = StarkFelt::from(
         FieldElement::from_byte_slice_be(b"CONTRACT_CLASS_V0.1.0")
             .expect("CONTRACT_CLASS_V0.1.0 is valid StarkFelt."),
     );
+}
+
+const DEPRECATED_CLASS_API_VERSION: StarkFelt = StarkFelt::ZERO;
+
+/// An error that occurs when calculating the hash of a deprecated contract class.
+#[derive(Debug, thiserror::Error)]
+pub enum DeprecatedClassHashCalculationError {
+    #[error("{0}")]
+    BadProgramJson(&'static str),
+    #[error(transparent)]
+    Serde(#[from] serde_json::Error),
+    #[error(transparent)]
+    InvalidUtf8(#[from] std::string::FromUtf8Error),
 }
 
 /// Calculates the hash of a contract class.
@@ -23,12 +52,11 @@ pub fn calculate_class_hash(class: &ContractClass) -> ClassHash {
     let external_entry_points_hash = entry_points_hash(class, &EntryPointType::External);
     let l1_handler_entry_points_hash = entry_points_hash(class, &EntryPointType::L1Handler);
     let constructor_entry_points_hash = entry_points_hash(class, &EntryPointType::Constructor);
-    let abi_keccak = sha3::Keccak256::default().chain_update(class.abi.as_bytes()).finalize();
-    let abi_hash = truncated_keccak(abi_keccak.into());
+    let abi_hash = abi_hash(&class.abi);
     let program_hash = poseidon_hash_array(class.sierra_program.as_slice());
 
     let class_hash = poseidon_hash_array(&[
-        *API_VERSION,
+        *SIERRA_API_VERSION,
         external_entry_points_hash.0,
         l1_handler_entry_points_hash.0,
         constructor_entry_points_hash.0,
@@ -37,6 +65,34 @@ pub fn calculate_class_hash(class: &ContractClass) -> ClassHash {
     ]);
     // TODO: Modify ClassHash Be be PoseidonHash instead of StarkFelt.
     ClassHash(class_hash.0)
+}
+
+/// Calculates the hash of a deprecated contract class.
+/// Note: This function modifies the contract class in place.
+// Based on Pathfinder code (the starknet.io doc is incorrect).
+pub fn calculate_deprecated_class_hash(
+    class: &mut DeprecatedContractClass,
+) -> Result<ClassHash, DeprecatedClassHashCalculationError> {
+    let external_entry_points_hash =
+        deprecated_entry_points_hash(class, &DeprecatedEntryPointType::External);
+    let l1_handler_entry_points_hash =
+        deprecated_entry_points_hash(class, &DeprecatedEntryPointType::L1Handler);
+    let constructor_entry_points_hash =
+        deprecated_entry_points_hash(class, &DeprecatedEntryPointType::Constructor);
+    let builtins_hash = builtins_hash(&class.program.builtins)?;
+    // Modifies the program in place.
+    let program_hash = deprecated_program_hash(class)?;
+    let bytecode_hash = bytecode_hash(&class.program.data)?;
+
+    Ok(ClassHash(pedersen_hash_array(&[
+        DEPRECATED_CLASS_API_VERSION,
+        external_entry_points_hash,
+        l1_handler_entry_points_hash,
+        constructor_entry_points_hash,
+        builtins_hash,
+        program_hash,
+        bytecode_hash,
+    ])))
 }
 
 fn entry_points_hash(class: &ContractClass, entry_point_type: &EntryPointType) -> PoseidonHash {
@@ -52,9 +108,257 @@ fn entry_points_hash(class: &ContractClass, entry_point_type: &EntryPointType) -
     )
 }
 
+fn abi_hash(abi: &str) -> StarkFelt {
+    let abi_keccak = sha3::Keccak256::default().chain_update(abi.as_bytes()).finalize();
+    truncated_keccak(abi_keccak.into())
+}
+
+fn deprecated_entry_points_hash(
+    class: &DeprecatedContractClass,
+    entry_point_type: &DeprecatedEntryPointType,
+) -> StarkHash {
+    pedersen_hash_array(
+        class
+            .entry_points_by_type
+            .get(entry_point_type)
+            .unwrap_or(&vec![])
+            .iter()
+            .flat_map(|ep| [ep.selector.0, usize_into_felt(ep.offset.0)])
+            .collect::<Vec<_>>()
+            .as_slice(),
+    )
+}
+
+fn builtins_hash(
+    builtins_json: &serde_json::Value,
+) -> Result<StarkFelt, DeprecatedClassHashCalculationError> {
+    let Some(builtins) = builtins_json.as_array() else {
+        return Err(DeprecatedClassHashCalculationError::BadProgramJson(
+            "Builtins expected to be an array of strings.",
+        ));
+    };
+    // Builtins are an array of strings, for example: ["pedersen", "range_check"]. We convert the
+    // strings to bytes and from bytes to StarkFelt.
+    // TODO(yair): Consider deserializing to a builtin enum.
+    let builtins_as_felts = builtins
+        .iter()
+        .map(|builtin_json| -> Result<StarkFelt, DeprecatedClassHashCalculationError> {
+            let builtin_as_bytes = builtin_json
+                .as_str()
+                .ok_or(DeprecatedClassHashCalculationError::BadProgramJson(
+                    "Builtin expected to be a string.",
+                ))?
+                .as_bytes();
+            let Ok(as_field_element) = FieldElement::from_byte_slice_be(builtin_as_bytes) else {
+                return Err(DeprecatedClassHashCalculationError::BadProgramJson(
+                    "Failed to convert builtin to a field element.",
+                ));
+            };
+            Ok(StarkFelt::from(as_field_element))
+        })
+        .collect::<Result<Vec<_>, _>>()?;
+    Ok(pedersen_hash_array(builtins_as_felts.as_slice()))
+}
+
+fn deprecated_program_hash(
+    class: &mut DeprecatedContractClass,
+) -> Result<StarkFelt, DeprecatedClassHashCalculationError> {
+    preprocess_program(&mut class.program)?;
+    let pythonic_class_serializtion = pythonic_serialize(class)?;
+    let class_keccak =
+        sha3::Keccak256::default().chain_update(pythonic_class_serializtion.as_bytes()).finalize();
+    Ok(truncated_keccak(class_keccak.into()))
+}
+
+fn bytecode_hash(
+    bytecode_json: &serde_json::Value,
+) -> Result<StarkFelt, DeprecatedClassHashCalculationError> {
+    let Some(bytecode) = bytecode_json.as_array() else {
+        return Err(DeprecatedClassHashCalculationError::BadProgramJson(
+            "Expecting the bytecode to be an array of string represented felts.",
+        ));
+    };
+    let bytecode_as_felts = bytecode
+        .iter()
+        .map(|j| {
+            let Some(as_str) = j.as_str() else {
+                return Err(DeprecatedClassHashCalculationError::BadProgramJson(
+                    "Expecting each bytecode entry to be a string.",
+                ));
+            };
+            let Ok(felt) = StarkFelt::try_from(as_str) else {
+                return Err(DeprecatedClassHashCalculationError::BadProgramJson(
+                    "Expecting each bytecode entry to be a string represented felt.",
+                ));
+            };
+            Ok(felt)
+        })
+        .collect::<Result<Vec<_>, _>>()?;
+    Ok(pedersen_hash_array(bytecode_as_felts.as_slice()))
+}
+
+fn preprocess_program(
+    program: &mut DeprecatedContractClassProgram,
+) -> Result<(), DeprecatedClassHashCalculationError> {
+    program.debug_info = serde_json::Value::Null;
+    if let Some(program_attributes) = program.attributes.as_array_mut() {
+        for attr_json in program_attributes.iter_mut() {
+            let Some(vals) = attr_json.as_object_mut() else {
+                return Err(DeprecatedClassHashCalculationError::BadProgramJson(
+                    "Program json is not an object",
+                ));
+            };
+
+            // Cairo 0.8 added "accessible_scopes" and "flow_tracking_data" attribute fields, which
+            // were not present in older contracts. They present as null / empty for
+            // older contracts and should not be included in the hash calculation in
+            // these cases.
+            match vals.get_mut("accessible_scopes") {
+                Some(serde_json::Value::Array(array)) => {
+                    if array.is_empty() {
+                        vals.remove("accessible_scopes");
+                    }
+                }
+                Some(_other) => {
+                    return Err(DeprecatedClassHashCalculationError::BadProgramJson(
+                        r#"A program's attribute["accessible_scopes"] was not an array type."#,
+                    ));
+                }
+                None => {}
+            }
+            if let Some(serde_json::Value::Null) = vals.get_mut("flow_tracking_data") {
+                vals.remove("flow_tracking_data");
+            }
+        }
+    }
+
+    if program.compiler_version.is_null() {
+        json_traversal(&mut program.identifiers, add_extra_space_to_cairo_named_tuples);
+        json_traversal(&mut program.reference_manager, add_extra_space_to_cairo_named_tuples);
+    }
+
+    Ok(())
+}
+
+fn add_extra_space_to_cairo_named_tuples(json: &mut serde_json::Value) {
+    let Some(obj) = json.as_object_mut() else {
+        return;
+    };
+    const KEYS_TO_ADD_SPACE: [&str; 2] = ["cairo_type", "value"];
+    for key in KEYS_TO_ADD_SPACE {
+        if let Some(serde_json::Value::String(v)) = obj.get_mut(key) {
+            let new_value = v.as_str().replace(": ", " : ").replace("  :", " :");
+            if new_value != *v {
+                *v = new_value;
+            }
+        }
+    }
+}
+
 // Python code masks with (2**250 - 1) which starts 0x03 and is followed by 31 0xff in be.
 // Truncation is needed not to overflow the field element.
 fn truncated_keccak(mut plain: [u8; 32]) -> StarkFelt {
     plain[0] &= 0x03;
     StarkFelt::new_unchecked(plain)
+}
+
+// Traverses the JSON (entering arrays and maps values) and applies the function to each value.
+fn json_traversal(json: &mut serde_json::Value, f: fn(&mut serde_json::Value)) {
+    f(json);
+    match json {
+        serde_json::Value::Object(obj) => {
+            obj.values_mut().for_each(|value| json_traversal(value, f));
+        }
+        serde_json::Value::Array(arr) => {
+            arr.iter_mut().for_each(|value| json_traversal(value, f));
+        }
+        _ => {}
+    }
+}
+
+fn pythonic_serialize(
+    class: &DeprecatedContractClass,
+) -> Result<String, DeprecatedClassHashCalculationError> {
+    let mut string_buffer = vec![];
+    let mut serializer =
+        serde_json::Serializer::with_formatter(&mut string_buffer, PythonJsonFormatter);
+
+    let class_for_hashing = ContractClassForHashing {
+        abi: &class.abi,
+        program: ProgramForHashing {
+            attributes: &class.program.attributes,
+            builtins: &class.program.builtins,
+            compiler_version: &class.program.compiler_version,
+            data: &class.program.data,
+            debug_info: &class.program.debug_info,
+            hints: &class.program.hints,
+            identifiers: &class.program.identifiers,
+            main_scope: &class.program.main_scope,
+            prime: &class.program.prime,
+            reference_manager: &class.program.reference_manager,
+        },
+    };
+
+    class_for_hashing.serialize(&mut serializer)?;
+    String::from_utf8(string_buffer).map_err(|e| e.into())
+}
+
+// A type for skipping the entry points when hashing the contract class.
+#[derive(Debug, Clone, Eq, PartialEq, Serialize)]
+pub(crate) struct ContractClassForHashing<'a> {
+    // Starknet does not verify the abi. If we can't parse it, we set it to None.
+    pub abi: &'a Option<Vec<starknet_api::deprecated_contract_class::ContractClassAbiEntry>>,
+    pub program: ProgramForHashing<'a>,
+}
+
+// A type for skipping empty fields when hashing the contract class.
+#[derive(Debug, Clone, Eq, PartialEq, Serialize)]
+pub(crate) struct ProgramForHashing<'a> {
+    #[serde(skip_serializing_if = "is_empty")]
+    pub attributes: &'a serde_json::Value,
+    pub builtins: &'a serde_json::Value,
+    #[serde(skip_serializing_if = "is_empty")]
+    pub compiler_version: &'a serde_json::Value,
+    pub data: &'a serde_json::Value,
+    pub debug_info: &'a serde_json::Value,
+    #[serde(serialize_with = "serialize_hints_sorted")]
+    pub hints: &'a serde_json::Value,
+    pub identifiers: &'a serde_json::Value,
+    pub main_scope: &'a serde_json::Value,
+    pub prime: &'a serde_json::Value,
+    pub reference_manager: &'a serde_json::Value,
+}
+
+fn is_empty(value: &serde_json::Value) -> bool {
+    match value {
+        serde_json::Value::Array(arr) => arr.is_empty(),
+        serde_json::Value::Object(obj) => obj.is_empty(),
+        serde_json::Value::Null => true,
+        _ => false,
+    }
+}
+
+// TODO(yair): Make this function public in sn_api.
+// Serialize hints as a sorted mapping for correct hash computation.
+fn serialize_hints_sorted<S>(hints: &serde_json::Value, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    if hints.is_null() {
+        return serializer.serialize_none();
+    }
+    let hints_map =
+        hints.as_object().ok_or(serde::ser::Error::custom("Hints are not a mapping."))?;
+    serializer.collect_map(
+        hints_map
+            .iter()
+            // Parse the keys as integers and sort them.
+            .map(|(k, v)| Ok((k.parse::<u32>()?, v)))
+            .collect::<Result<Vec<_>, ParseIntError>>()
+            .map_err(serde::ser::Error::custom)?
+            .iter()
+            .sorted_by_key(|(k, _v)| *k)
+            // Convert the keys back to strings.
+            .map(|(k, v)| (k.to_string(), v)),
+    )
 }
--- crates/papyrus_common/src/class_hash_test.rs
+++ crates/papyrus_common/src/class_hash_test.rs
@@ -1,10 +1,18 @@
+use itertools::Itertools;
+use pretty_assertions::assert_eq;
 use starknet_api::class_hash;
 use starknet_api::core::ClassHash;
+use starknet_api::deprecated_contract_class::ContractClass as DeprecatedContractClass;
 use starknet_api::hash::StarkHash;
 use starknet_api::state::ContractClass;
 use test_utils::read_json_file;
 
-use crate::class_hash::calculate_class_hash;
+use crate::class_hash::{
+    calculate_class_hash,
+    calculate_deprecated_class_hash,
+    ContractClassForHashing,
+    ProgramForHashing,
+};
 
 #[test]
 fn class_hash() {
@@ -13,4 +21,166 @@ fn class_hash() {
         class_hash!("0x29927c8af6bccf3f6fda035981e765a7bdbf18a2dc0d630494f8758aa908e2b");
     let calculated_class_hash = calculate_class_hash(&class);
     assert_eq!(calculated_class_hash, expected_class_hash);
+}
+
+#[test]
+fn deprecated_class_hash() {
+    let mut deprecated_class: DeprecatedContractClass =
+        serde_json::from_value(read_json_file("deprecated_class.json")).unwrap();
+    let expected_class_hash =
+        class_hash!("0x07b5e991587f0c59db1c4c4ff9b26fa8ec49198ca6d8a82823cc2c6177d918fa");
+    let calculated_class_hash = calculate_deprecated_class_hash(&mut deprecated_class).unwrap();
+    assert_eq!(calculated_class_hash, expected_class_hash);
+}
+
+#[test]
+fn deprecated_class_hash_missing_compiler_version() {
+    let mut deprecated_class: DeprecatedContractClass =
+        serde_json::from_value(read_json_file("deprecated_class_missing_compiler_version.json"))
+            .unwrap();
+    let expected_class_hash =
+        class_hash!("0x025ec026985a3bf9d0cc1fe17326b245dfdc3ff89b8fde106542a3ea56c5a918");
+    let calculated_class_hash = calculate_deprecated_class_hash(&mut deprecated_class).unwrap();
+    assert_eq!(calculated_class_hash, expected_class_hash);
+}
+
+#[test]
+fn deprecated_class_hash_with_missing_fields() {
+    let mut deprecated_class: DeprecatedContractClass =
+        serde_json::from_value(read_json_file("deprecated_class_missing_fields.json")).unwrap();
+    let expected_class_hash =
+        class_hash!("0x07319e2f01b0947afd86c0bb0e95029551b32f6dc192c47b2e8b08415eebbc25");
+    let calculated_class_hash = calculate_deprecated_class_hash(&mut deprecated_class).unwrap();
+    assert_eq!(calculated_class_hash, expected_class_hash);
+}
+
+#[test]
+fn serde_json_value_sorts_maps() {
+    // this property is leaned on and the default implementation of serde_json works like
+    // this. serde_json has a feature called "preserve_order" which could get enabled by
+    // accident, and it would destroy the ability to compute_class_hash.
+
+    let input = r#"{"foo": 1, "bar": 2}"#;
+    let parsed = serde_json::from_str::<serde_json::Value>(input).unwrap();
+    let output = serde_json::to_string(&parsed).unwrap();
+
+    assert_eq!(output, r#"{"bar":2,"foo":1}"#);
+}
+
+#[test]
+fn deprecated_class_serialization_for_hashing() {
+    let deprecated_class = starknet_api::deprecated_contract_class::ContractClass {
+        abi: Some(vec![
+            starknet_api::deprecated_contract_class::ContractClassAbiEntry::Constructor(
+                starknet_api::deprecated_contract_class::FunctionAbiEntry::<
+                    starknet_api::deprecated_contract_class::ConstructorType,
+                > {
+                    name: "constructor".to_string(),
+                    inputs: vec![starknet_api::deprecated_contract_class::TypedParameter {
+                        name: "implementation".to_string(),
+                        r#type: "felt".to_string(),
+                    }],
+                    outputs: vec![],
+                    state_mutability: None,
+                    r#type: starknet_api::deprecated_contract_class::ConstructorType::Constructor,
+                },
+            ),
+        ]),
+        entry_points_by_type: [(
+            starknet_api::deprecated_contract_class::EntryPointType::Constructor,
+            vec![],
+        )]
+        .into(),
+        program: starknet_api::deprecated_contract_class::Program {
+            hints: serde_json::json!({
+            "12": [
+                {
+                    "accessible_scopes": [
+                        "starkware.cairo.common.memcpy",
+                        "starkware.cairo.common.memcpy.memcpy"
+                    ],
+                    "code": "vm_enter_scope({'n': ids.len})",
+                    "flow_tracking_data": {
+                        "ap_tracking": {
+                            "group": 2,
+                            "offset": 0
+                        },
+                        "reference_ids": {
+                            "starkware.cairo.common.memcpy.memcpy.len": 0
+                        }
+                    }
+                }
+            ],
+            "0": [
+                {
+                    "accessible_scopes": [
+                        "starkware.cairo.common.alloc",
+                        "starkware.cairo.common.alloc.alloc"
+                    ],
+                    "code": "memory[ap] = segments.add()",
+                    "flow_tracking_data": {
+                        "ap_tracking": {
+                            "group": 0,
+                            "offset": 0
+                        },
+                        "reference_ids": {}
+                    }
+                }
+            ],
+            "20": [
+                {
+                    "accessible_scopes": [
+                        "starkware.cairo.common.memcpy",
+                        "starkware.cairo.common.memcpy.memcpy"
+                    ],
+                    "code": "n -= 1\nids.continue_copying = 1 if n > 0 else 0",
+                    "flow_tracking_data": {
+                        "ap_tracking": {
+                            "group": 2,
+                            "offset": 5
+                        },
+                        "reference_ids": {
+                            "starkware.cairo.common.memcpy.memcpy.continue_copying": 1
+                        }
+                    }
+                }
+            ]}),
+            ..Default::default()
+        },
+    };
+
+    let for_hashing = ContractClassForHashing {
+        abi: &deprecated_class.abi,
+        program: ProgramForHashing {
+            attributes: &deprecated_class.program.attributes,
+            builtins: &deprecated_class.program.builtins,
+            compiler_version: &deprecated_class.program.compiler_version,
+            data: &deprecated_class.program.data,
+            debug_info: &deprecated_class.program.debug_info,
+            hints: &deprecated_class.program.hints,
+            identifiers: &deprecated_class.program.identifiers,
+            main_scope: &deprecated_class.program.main_scope,
+            prime: &deprecated_class.program.prime,
+            reference_manager: &deprecated_class.program.reference_manager,
+        },
+    };
+
+    let serialized = serde_json::to_value(for_hashing).unwrap();
+    let class_mapping = serialized.as_object().unwrap();
+
+    // Check that the keys of abi entries are sorted lexicographically.
+    let abi_entry =
+        class_mapping.get("abi").unwrap().as_array().unwrap().first().unwrap().as_object().unwrap();
+    for (k1, k2) in abi_entry.keys().tuple_windows() {
+        assert!(k1 <= k2);
+    }
+
+    // Check that the entry points are skipped in the serialization.
+    assert!(class_mapping.get("entry_points_by_type").is_none());
+
+    // Check that the hints are sorted by their index integer value (not lexicographically).
+    let hints = class_mapping.get("program").unwrap().get("hints").unwrap().as_object().unwrap();
+    for (k1, k2) in hints.keys().tuple_windows() {
+        assert!(k1.parse::<u32>().unwrap() <= k2.parse::<u32>().unwrap());
+    }
 }
