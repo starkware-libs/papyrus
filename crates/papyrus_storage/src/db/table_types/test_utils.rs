5f0f8f4ed4aeab15fe0843e718de70a51d109e9b
Dvir Yosef <dvir@starkware.co>
2024-04-11 18:38:25 +0300

    perf(storage): add append functionallity to tables

--- crates/papyrus_storage/src/db/table_types/test_utils.rs
+++ crates/papyrus_storage/src/db/table_types/test_utils.rs
@@ -1,8 +1,10 @@
+use assert_matches::assert_matches;
+
 use super::{Table, TableType};
 use crate::db::db_test::get_test_env;
 use crate::db::serialization::{NoVersionValueWrapper, StorageSerde, StorageSerdeError};
 use crate::db::table_types::{DbCursor, DbCursorTrait};
-use crate::db::{DbResult, DbWriter, TableHandle, TableIdentifier, RW};
+use crate::db::{DbError, DbResult, DbWriter, TableHandle, TableIdentifier, RW};
 use crate::serialization::serializers::auto_storage_serde;
 
 pub(crate) type TableKey = (u32, u32);
@@ -35,6 +37,9 @@ pub(crate) fn table_test<T: TableType>(
 
     let upsert_test_table = create_table(&mut writer, "upsert_test").unwrap();
     upsert_test(upsert_test_table, &mut writer);
+
+    let append_test_table = create_table(&mut writer, "append_test").unwrap();
+    append_test(append_test_table, &mut writer);
 
     let delete_test_table = create_table(&mut writer, "delete_test").unwrap();
     delete_test(delete_test_table, &mut writer);
@@ -112,6 +117,47 @@ fn upsert_test<T: TableType>(
     // (1,1) was already inserted, so this is an update.
     table.upsert(&txn, &(1, 1), &0).unwrap();
     assert_eq!(table.get(&txn, &(1, 1)).unwrap(), Some(0));
+}
+
+fn append_test<T: TableType>(
+    table_id: TableIdentifier<TableKey, TableValue, T>,
+    writer: &mut DbWriter,
+) where
+    for<'env> TableHandle<'env, TableKey, TableValue, T>:
+        Table<'env, Key = TableKey, Value = TableValue>,
+{
+    let txn = writer.begin_rw_txn().unwrap();
+    let table = txn.open_table(&table_id).unwrap();
+
+    // Append to an empty table.
+    table.append(&txn, &(1, 1), &11).unwrap();
+    assert_eq!(table.get(&txn, &(1, 1)).unwrap(), Some(11));
+
+    // Successful appends.
+    table.append(&txn, &(1, 1), &0).unwrap();
+    table.append(&txn, &(1, 2), &12).unwrap();
+    table.append(&txn, &(2, 0), &20).unwrap();
+
+    assert_eq!(table.get(&txn, &(1, 1)).unwrap(), Some(0));
+    assert_eq!(table.get(&txn, &(1, 2)).unwrap(), Some(12));
+    assert_eq!(table.get(&txn, &(2, 0)).unwrap(), Some(20));
+
+    // Override the last key with a smaller value.
+    table.append(&txn, &(2, 0), &0).unwrap();
+    assert_eq!(table.get(&txn, &(2, 0)).unwrap(), Some(0));
+
+    // Override the last key with a bigger value.
+    table.append(&txn, &(2, 0), &100).unwrap();
+    assert_eq!(table.get(&txn, &(2, 0)).unwrap(), Some(100));
+
+    // Append key that is not the last, should fail.
+    assert_matches!(table.append(&txn, &(0, 0), &0), Err(DbError::Append));
+    assert_matches!(table.append(&txn, &(1, 3), &0), Err(DbError::Append));
+
+    // Check the final database.
+    assert_eq!(table.get(&txn, &(1, 1)).unwrap(), Some(0));
+    assert_eq!(table.get(&txn, &(1, 2)).unwrap(), Some(12));
+    assert_eq!(table.get(&txn, &(2, 0)).unwrap(), Some(100));
 }
 
 fn delete_test<T: TableType>(
