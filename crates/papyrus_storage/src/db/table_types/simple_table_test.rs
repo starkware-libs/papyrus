e126f65442746c93f3b336dba0e59d2ab902a7df
Dvir Yosef <dvir@starkware.co>
2024-04-11 18:17:51 +0300

    refactor(storage): refactor table type tests

--- crates/papyrus_storage/src/db/table_types/simple_table_test.rs
+++ crates/papyrus_storage/src/db/table_types/simple_table_test.rs
@@ -1,16 +1,7 @@
-use crate::db::db_test::get_test_env;
-use crate::db::table_types::test_utils::{table_cursor_test, table_test};
+use crate::db::table_types::test_utils::table_test;
+use crate::db::DbWriter;
 
 #[test]
 fn simple_table_test() {
-    let ((reader, mut writer), _temp_dir) = get_test_env();
-    let table_id = writer.create_simple_table("table").unwrap();
-    table_test(table_id, &reader, &mut writer);
-}
-
-#[test]
-fn simple_table_cursor_test() {
-    let ((reader, mut writer), _temp_dir) = get_test_env();
-    let table_id = writer.create_simple_table("table").unwrap();
-    table_cursor_test(table_id, &reader, &mut writer);
+    table_test(DbWriter::create_simple_table);
 }
