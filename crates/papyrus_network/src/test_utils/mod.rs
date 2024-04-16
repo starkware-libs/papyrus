afdf60c0b3f7d601c07ea40c2e518740b160bde3
Shahak Shama <shahak@starkware.co>
2024-04-16 08:43:43 +0300

    test(network): add discovery unit tests

--- crates/papyrus_network/src/test_utils/mod.rs
+++ crates/papyrus_network/src/test_utils/mod.rs
@@ -5,13 +5,16 @@ use std::collections::{HashMap, HashSet};
 use std::fmt::Debug;
 use std::hash::Hash;
 use std::pin::Pin;
-use std::task::{Context, Poll};
+use std::task::{ready, Context, Poll};
 use std::time::Duration;
 
+use futures::future::Future;
+use futures::pin_mut;
 use futures::stream::{Stream as StreamTrait, StreamExt};
 use libp2p::swarm::{NetworkBehaviour, StreamProtocol, Swarm, SwarmEvent};
 use libp2p::{PeerId, Stream};
 use libp2p_swarm_test::SwarmExt;
+use tokio::sync::Mutex;
 use tokio::task::JoinHandle;
 use tokio_stream::StreamExt as TokioStreamExt;
 
@@ -127,4 +130,30 @@ where
     }
 
     StreamHashMap::new(swarms.into_iter().map(|swarm| (*swarm.local_peer_id(), swarm)).collect())
+}
+
+// I tried making this generic on the async function we run, but it caused a lot of lifetime
+// issues.
+/// Run `next` on a mutex of a stream, unlocking it while the function is pending.
+pub(crate) fn next_on_mutex_stream<T: StreamTrait + Unpin>(
+    mutex: &Mutex<T>,
+) -> NextOnMutexStream<'_, T> {
+    NextOnMutexStream { mutex }
+}
+
+pub(crate) struct NextOnMutexStream<'a, T: StreamTrait + Unpin> {
+    mutex: &'a Mutex<T>,
+}
+
+impl<'a, T: StreamTrait + Unpin> Future for NextOnMutexStream<'a, T> {
+    type Output = Option<T::Item>;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let lock_fut = self.mutex.lock();
+        pin_mut!(lock_fut);
+        let mut locked_value = ready!(lock_fut.poll(cx));
+        let fut = StreamExt::next(&mut *locked_value);
+        pin_mut!(fut);
+        fut.poll(cx)
+    }
 }
