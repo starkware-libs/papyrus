e7a257af6742e462bda9469031f4818b1a0701ea
Shahak Shama <shahak@starkware.co>
2024-04-30 16:45:02 +0300

    test(network): discovery flow test

--- crates/papyrus_network/src/main_behaviour/mixed_behaviour.rs
+++ crates/papyrus_network/src/main_behaviour/mixed_behaviour.rs
@@ -1,9 +1,15 @@
+// TODO(shahak): Erase main_behaviour and make this a separate module.
+
+use libp2p::identity::PublicKey;
 use libp2p::kad::store::MemoryStore;
 use libp2p::swarm::behaviour::toggle::Toggle;
+use libp2p::swarm::dial_opts::DialOpts;
 use libp2p::swarm::NetworkBehaviour;
-use libp2p::{identify, kad};
+use libp2p::{identify, kad, Multiaddr, PeerId};
 
+use crate::discovery::identify_impl::IDENTIFY_PROTOCOL_VERSION;
 use crate::discovery::kad_impl::KadFromOtherBehaviourEvent;
+use crate::peer_manager::PeerManagerConfig;
 use crate::{discovery, peer_manager, streamed_bytes};
 
 // TODO: consider reducing the pulicity of all behaviour to pub(crate)
@@ -40,4 +46,36 @@ pub enum InternalEvent {
 
 pub trait BridgedBehaviour {
     fn on_other_behaviour_event(&mut self, event: InternalEvent);
+}
+
+impl MixedBehaviour {
+    // TODO: get config details from network manager config
+    /// Panics if bootstrap_peer_multiaddr doesn't have a peer id.
+    pub fn new(
+        key: PublicKey,
+        bootstrap_peer_multiaddr: Option<Multiaddr>,
+        streamed_bytes_config: streamed_bytes::Config,
+    ) -> Self {
+        let local_peer_id = PeerId::from_public_key(&key);
+        Self {
+            peer_manager: peer_manager::PeerManager::new(PeerManagerConfig::default()),
+            discovery: bootstrap_peer_multiaddr
+                .map(|bootstrap_peer_multiaddr| {
+                    discovery::Behaviour::new(
+                        DialOpts::from(bootstrap_peer_multiaddr.clone())
+                            .get_peer_id()
+                            .expect("bootstrap_peer_multiaddr doesn't have a peer id"),
+                        bootstrap_peer_multiaddr.clone(),
+                    )
+                })
+                .into(),
+            identify: identify::Behaviour::new(identify::Config::new(
+                IDENTIFY_PROTOCOL_VERSION.to_string(),
+                key,
+            )),
+            // TODO: change kademlia protocol name
+            kademlia: kad::Behaviour::new(local_peer_id, MemoryStore::new(local_peer_id)),
+            streamed_bytes: streamed_bytes::Behaviour::new(streamed_bytes_config),
+        }
+    }
 }
