4f0a8e61edc1c4c4d4cf6e77bfcbc6749f3cefcc
DvirYo-starkware <115620476+DvirYo-starkware@users.noreply.github.com>
2024-04-30 11:43:43 +0300

    test(storage): add random table test (#1816)
    * test(storage): add random table test
    
    * CR fix
    
    * Fix ci
    
    * CR fix

--- crates/papyrus_storage/src/db/table_types/dup_sort_tables_test.rs
+++ crates/papyrus_storage/src/db/table_types/dup_sort_tables_test.rs
@@ -1,10 +1,21 @@
+use crate::db::db_test::get_test_env;
 use crate::db::table_types::dup_sort_tables::add_one;
-use crate::db::table_types::test_utils::table_test;
+use crate::db::table_types::test_utils::{random_table_test, table_test};
 use crate::db::DbWriter;
 
 #[test]
 fn common_prefix_table() {
     table_test(DbWriter::create_common_prefix_table);
+}
+
+// Ignore because this test takes few seconds to run.
+#[ignore]
+#[test]
+fn common_prefix_compare_with_simple_table_random() {
+    let ((reader, mut writer), _temp_dir) = get_test_env();
+    let simple_table = writer.create_simple_table("simple_table").unwrap();
+    let common_prefix_table = writer.create_common_prefix_table("common_prefix_table").unwrap();
+    random_table_test(simple_table, common_prefix_table, &reader, &mut writer);
 }
 
 #[test]
