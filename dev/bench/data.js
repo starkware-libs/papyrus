8d2686bbcc907b9e6349c8ca87aadaa7f083b155
github-action-benchmark <github@users.noreply.github.com>
2024-05-02 14:07:40 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for d2e435ee02b8bae6ae5c8790d35f7234970d088e

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1714658860148,
+  "repoUrl": "https://github.com/starkware-libs/papyrus",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "eran@starkware.co",
+            "name": "eranreshef-starkware",
+            "username": "eranreshef-starkware"
+          },
+          "committer": {
+            "email": "eran@starkware.co",
+            "name": "eranreshef-starkware",
+            "username": "eranreshef-starkware"
+          },
+          "distinct": true,
+          "id": "d2e435ee02b8bae6ae5c8790d35f7234970d088e",
+          "message": "try auto-push",
+          "timestamp": "2024-05-02T17:06:07+03:00",
+          "tree_id": "9c2e1c91887ac7c9dd46fed06995c7b33cf18215",
+          "url": "https://github.com/starkware-libs/papyrus/commit/d2e435ee02b8bae6ae5c8790d35f7234970d088e"
+        },
+        "date": 1714658859615,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "get_class_hash_at",
+            "value": 11
+          },
+          {
+            "name": "get_nonce_at",
+            "value": 11
+          },
+          {
+            "name": "get_storage_at",
+            "value": 16
+          }
+        ]
+      }
+    ]
+  }
+}
