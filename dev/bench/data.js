729d06cc457b002bfad18fe02bb75209682a38e4
github-action-benchmark <github@users.noreply.github.com>
2024-05-05 12:47:24 +0000

    add Papyrus storage benchmark (customSmallerIsBetter) benchmark result for 7f1c10441f8326a62e6f0dc61afcf7393664cf7b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1714912509332,
+  "lastUpdate": 1714913244705,
   "repoUrl": "https://github.com/starkware-libs/papyrus",
   "entries": {
     "Papyrus storage benchmark": [
@@ -77,6 +77,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "get_storage_at",
             "value": 23,
+            "unit": "Microseconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "eran@starkware.co",
+            "name": "eranreshef-starkware",
+            "username": "eranreshef-starkware"
+          },
+          "committer": {
+            "email": "eran@starkware.co",
+            "name": "eranreshef-starkware",
+            "username": "eranreshef-starkware"
+          },
+          "distinct": true,
+          "id": "7f1c10441f8326a62e6f0dc61afcf7393664cf7b",
+          "message": "squash a lot of debuging to a single commit",
+          "timestamp": "2024-05-05T15:45:45+03:00",
+          "tree_id": "54efda55fc30e0565ee12c46c8935b29d4ac8b9b",
+          "url": "https://github.com/starkware-libs/papyrus/commit/7f1c10441f8326a62e6f0dc61afcf7393664cf7b"
+        },
+        "date": 1714913243839,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "get_class_hash_at",
+            "value": 19,
+            "unit": "Microseconds"
+          },
+          {
+            "name": "get_nonce_at",
+            "value": 27,
+            "unit": "Microseconds"
+          },
+          {
+            "name": "get_storage_at",
+            "value": 27,
             "unit": "Microseconds"
           }
         ]
