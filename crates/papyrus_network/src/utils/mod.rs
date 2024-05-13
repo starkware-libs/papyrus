47b579f872b2a113bb4ad34841f260e67ae96063
Shahak Shama <shahak@starkware.co>
2024-05-13 16:21:40 +0300

    refactor(network): make StreamHashMap usable in source code (not tests only)

--- crates/papyrus_network/src/utils/mod.rs
+++ crates/papyrus_network/src/utils/mod.rs
@@ -1,31 +1,43 @@
+use std::collections::hash_map::{Keys, ValuesMut};
+use std::collections::{HashMap, HashSet};
+use std::hash::Hash;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+
+use futures::stream::{Stream, StreamExt};
+
 // This is an implementation of `StreamMap` from tokio_stream. The reason we're implementing it
 // ourselves is that the implementation in tokio_stream requires that the values implement the
 // Stream trait from tokio_stream and not from futures.
-pub(crate) struct StreamHashMap<K: Unpin + Clone + Eq + Hash, V: StreamTrait + Unpin> {
+pub(crate) struct StreamHashMap<K: Unpin + Clone + Eq + Hash, V: Stream + Unpin> {
     map: HashMap<K, V>,
     finished_streams: HashSet<K>,
 }
 
-impl<K: Unpin + Clone + Eq + Hash, V: StreamTrait + Unpin> StreamHashMap<K, V> {
+impl<K: Unpin + Clone + Eq + Hash, V: Stream + Unpin> StreamHashMap<K, V> {
+    #[allow(dead_code)]
     pub fn new(map: HashMap<K, V>) -> Self {
         Self { map, finished_streams: Default::default() }
     }
 
+    #[allow(dead_code)]
     pub fn values_mut(&mut self) -> ValuesMut<'_, K, V> {
         self.map.values_mut()
     }
 
+    #[allow(dead_code)]
     pub fn keys(&self) -> Keys<'_, K, V> {
         self.map.keys()
     }
 
+    #[allow(dead_code)]
     pub fn get_mut(&mut self, key: &K) -> Option<&mut V> {
         self.map.get_mut(key)
     }
 }
 
-impl<K: Unpin + Clone + Eq + Hash, V: StreamTrait + Unpin> StreamTrait for StreamHashMap<K, V> {
-    type Item = (K, <V as StreamTrait>::Item);
+impl<K: Unpin + Clone + Eq + Hash, V: Stream + Unpin> Stream for StreamHashMap<K, V> {
+    type Item = (K, <V as Stream>::Item);
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let unpinned_self = Pin::into_inner(self);
