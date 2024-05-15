bcbcfe2c052f20b3d60aea4593d4a732cec2df91
Matan Markind <matan@starkware.co>
2024-05-15 11:35:39 +0300

    feat(consensus): create consensus crate
    commit-id:a554ebd0

--- commitlint.config.js
+++ commitlint.config.js
@@ -24,6 +24,7 @@ const Configuration = {
             'ci',
             'common',
             'config',
+            'consensus',
             'execution',
             'helm',
             'JSON-RPC',
