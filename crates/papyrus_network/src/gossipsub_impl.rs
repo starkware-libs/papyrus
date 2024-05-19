b5a2a95ca2673c99a7067fda764cd3aebc25364d
Shahak Shama <shahak@starkware.co>
2024-05-19 13:35:14 +0300

    fix(network): replace unimplemented broadcast behaviour with gossipsub

---
+++ crates/papyrus_network/src/gossipsub_impl.rs
@@ -0,0 +1,51 @@
+use libp2p::gossipsub::TopicHash;
+use libp2p::{gossipsub, PeerId};
+use tracing::error;
+
+use crate::mixed_behaviour;
+use crate::mixed_behaviour::BridgedBehaviour;
+use crate::sqmr::Bytes;
+
+// TODO(shahak): Consider using Sha256Topic instead.
+pub type Topic = gossipsub::IdentTopic;
+
+#[derive(Debug)]
+pub enum ExternalEvent {
+    #[allow(dead_code)]
+    Received { originated_peer_id: PeerId, message: Bytes, topic_hash: TopicHash },
+}
+
+impl From<gossipsub::Event> for mixed_behaviour::Event {
+    fn from(event: gossipsub::Event) -> Self {
+        match event {
+            gossipsub::Event::Message {
+                message: gossipsub::Message { data, topic, source, .. },
+                ..
+            } => {
+                let Some(originated_peer_id) = source else {
+                    error!(
+                        "Received a message from gossipsub without source even though we've \
+                         configured it to reject such messages"
+                    );
+                    return mixed_behaviour::Event::ToOtherBehaviourEvent(
+                        mixed_behaviour::ToOtherBehaviourEvent::NoOp,
+                    );
+                };
+                mixed_behaviour::Event::ExternalEvent(mixed_behaviour::ExternalEvent::GossipSub(
+                    ExternalEvent::Received {
+                        originated_peer_id,
+                        message: data,
+                        topic_hash: topic,
+                    },
+                ))
+            }
+            _ => mixed_behaviour::Event::ToOtherBehaviourEvent(
+                mixed_behaviour::ToOtherBehaviourEvent::NoOp,
+            ),
+        }
+    }
+}
+
+impl BridgedBehaviour for gossipsub::Behaviour {
+    fn on_other_behaviour_event(&mut self, _event: &mixed_behaviour::ToOtherBehaviourEvent) {}
+}
