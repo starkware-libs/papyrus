d618fd8f81f89e991a775e797a24d52b3fd799bd
matan-starkware <97523054+matan-starkware@users.noreply.github.com>
2024-05-21 11:02:48 +0300

    feat(consensus): define Context and ConsensusBlock traits (#1993)
    Context defines how consensus will interact with the rest of the node.
    
    ConsensusBlock defines the interface that Consensus needs for holding onto a proposal once it is
    built.
    
    commit-id:40d0cace

---
+++ crates/sequencing/papyrus_consensus/src/types_test.rs
@@ -0,0 +1,17 @@
+use crate::types::{ConsensusBlock, ConsensusContext};
+
+// This should cause compilation to fail if the traits are not object safe.
+#[test]
+fn check_object_safety() {
+    // Arbitrarily chosen types for testing.
+    type _ProposalIter = std::slice::Iter<'static, u32>;
+    type _Blk = Box<dyn ConsensusBlock<ProposalChunk = u32, ProposalIter = _ProposalIter>>;
+
+    fn _check_consensus_block() -> _Blk {
+        todo!()
+    }
+
+    fn _check_context() -> Box<dyn ConsensusContext<Block = _Blk>> {
+        todo!()
+    }
+}
