f818f9eb21b07777a31a92ba54f16caf6d30a22f
Shahak Shama <shahak@starkware.co>
2024-05-23 10:06:44 +0300

    refactor(protobuf): create protobuf crate

---
+++ crates/papyrus_protobuf/src/sync.rs
@@ -0,0 +1,35 @@
+use starknet_api::block::{BlockHash, BlockHeader, BlockNumber, BlockSignature};
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Default, Hash)]
+pub enum Direction {
+    #[default]
+    Forward,
+    Backward,
+}
+
+/// This struct represents a query that can be sent to a peer.
+#[derive(Default, Clone, Debug, PartialEq, Eq, Hash)]
+pub struct Query {
+    pub start_block: BlockHashOrNumber,
+    pub direction: Direction,
+    pub limit: u64,
+    pub step: u64,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+pub enum BlockHashOrNumber {
+    Hash(BlockHash),
+    Number(BlockNumber),
+}
+
+impl Default for BlockHashOrNumber {
+    fn default() -> Self {
+        Self::Number(BlockNumber::default())
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct SignedBlockHeader {
+    pub block_header: BlockHeader,
+    pub signatures: Vec<BlockSignature>,
+}
