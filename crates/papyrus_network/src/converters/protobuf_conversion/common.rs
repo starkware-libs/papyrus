fb693da6b98862740d12ec82fabd795ad643d479
asmaa-starkware <163830216+asmaastarkware@users.noreply.github.com>
2024-05-26 11:16:46 +0300

    feat(network): add conversion from protobuf::receipt::DeployAccount to sn api (#2047)
    feat(network): add conversion from protobuf receipt DeployAccount to sn api

--- crates/papyrus_network/src/converters/protobuf_conversion/common.rs
+++ crates/papyrus_network/src/converters/protobuf_conversion/common.rs
@@ -244,3 +244,38 @@ impl From<Query> for protobuf::Iteration {
         }
     }
 }
+
+// TODO: use the conversion in Starknet api once its upgraded
+pub(super) fn try_from_starkfelt_to_u128(
+    felt: starknet_api::hash::StarkFelt,
+) -> Result<u128, &'static str> {
+    const COMPLIMENT_OF_U128: usize = 16; // 32 - 16
+    let (rest, u128_bytes) = felt.bytes().split_at(COMPLIMENT_OF_U128);
+    if rest != [0u8; COMPLIMENT_OF_U128] {
+        return Err("Value out of range");
+    }
+
+    let bytes: [u8; 16] = match u128_bytes.try_into() {
+        Ok(b) => b,
+        Err(_) => return Err("Failed to convert bytes to u128"),
+    };
+
+    Ok(u128::from_be_bytes(bytes))
+}
+// TODO: use the conversion in Starknet api once its upgraded
+pub(super) fn try_from_starkfelt_to_u32(
+    felt: starknet_api::hash::StarkFelt,
+) -> Result<u32, &'static str> {
+    const COMPLIMENT_OF_U32: usize = 28; // 32 - 4
+    let (rest, u32_bytes) = felt.bytes().split_at(COMPLIMENT_OF_U32);
+    if rest != [0u8; COMPLIMENT_OF_U32] {
+        return Err("Value out of range");
+    }
+
+    let bytes: [u8; 4] = match u32_bytes.try_into() {
+        Ok(b) => b,
+        Err(_) => return Err("Failed to convert bytes to u32"),
+    };
+
+    Ok(u32::from_be_bytes(bytes))
+}
--- crates/papyrus_network/src/converters/protobuf_conversion/transaction.rs
+++ crates/papyrus_network/src/converters/protobuf_conversion/transaction.rs
@@ -30,40 +30,14 @@ use starknet_api::transaction::{
     TransactionVersion,
 };
 
-use super::common::{enum_int_to_volition_domain, volition_domain_to_enum_int};
+use super::common::{
+    enum_int_to_volition_domain,
+    try_from_starkfelt_to_u128,
+    try_from_starkfelt_to_u32,
+    volition_domain_to_enum_int,
+};
 use super::ProtobufConversionError;
 use crate::protobuf_messages::protobuf::{self};
-
-// TODO: use the conversion in Starknet api once its upgraded
-fn try_from_starkfelt_to_u128(felt: StarkFelt) -> Result<u128, &'static str> {
-    const COMPLIMENT_OF_U128: usize = 16; // 32 - 16
-    let (rest, u128_bytes) = felt.bytes().split_at(COMPLIMENT_OF_U128);
-    if rest != [0u8; COMPLIMENT_OF_U128] {
-        return Err("Value out of range");
-    }
-
-    let bytes: [u8; 16] = match u128_bytes.try_into() {
-        Ok(b) => b,
-        Err(_) => return Err("Failed to convert bytes to u128"),
-    };
-
-    Ok(u128::from_be_bytes(bytes))
-}
-// TODO: use the conversion in Starknet api once its upgraded
-fn try_from_starkfelt_to_u32(felt: StarkFelt) -> Result<u32, &'static str> {
-    const COMPLIMENT_OF_U32: usize = 28; // 32 - 4
-    let (rest, u32_bytes) = felt.bytes().split_at(COMPLIMENT_OF_U32);
-    if rest != [0u8; COMPLIMENT_OF_U32] {
-        return Err("Value out of range");
-    }
-
-    let bytes: [u8; 4] = match u32_bytes.try_into() {
-        Ok(b) => b,
-        Err(_) => return Err("Failed to convert bytes to u32"),
-    };
-
-    Ok(u32::from_be_bytes(bytes))
-}
 
 impl TryFrom<protobuf::transaction::DeployAccountV1> for DeployAccountTransactionV1 {
     type Error = ProtobufConversionError;
