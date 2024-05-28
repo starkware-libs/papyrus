97237462ae25a5faec330a1fa1203e77d613c1dc
ShahakShama <70578257+ShahakShama@users.noreply.github.com>
2024-05-28 10:22:21 +0300

    feat(network): add macro for impl TryFrom<Vec<u8>> and Into<Vec<u8>> and use it in query (#2060)

--- crates/papyrus_protobuf/src/converters/mod.rs
+++ crates/papyrus_protobuf/src/converters/mod.rs
@@ -6,6 +6,8 @@ mod receipt;
 pub mod state_diff;
 mod transaction;
 
+use prost::DecodeError;
+
 #[derive(thiserror::Error, Debug)]
 pub enum ProtobufConversionError {
     #[error("Type `{type_description}` got out of range value {value_as_str}")]
@@ -14,4 +16,33 @@ pub enum ProtobufConversionError {
     MissingField { field_description: &'static str },
     #[error("Type `{type_description}` should be {num_expected} bytes but it got {value:?}.")]
     BytesDataLengthMismatch { type_description: &'static str, num_expected: usize, value: Vec<u8> },
+    #[error(transparent)]
+    DecodeError(#[from] DecodeError),
+}
+
+#[macro_export]
+macro_rules! auto_impl_into_and_try_from_vec_u8 {
+    ($T:ty, $ProtobufT:ty) => {
+        impl From<$T> for Vec<u8> {
+            fn from(value: $T) -> Self {
+                let protobuf_value = <$ProtobufT>::from(value);
+                protobuf_value.encode_to_vec()
+            }
+        }
+        $crate::auto_impl_try_from_vec_u8!($T, $ProtobufT);
+    };
+}
+
+// TODO(shahak): Remove this macro once all types implement both directions.
+#[macro_export]
+macro_rules! auto_impl_try_from_vec_u8 {
+    ($T:ty, $ProtobufT:ty) => {
+        impl TryFrom<Vec<u8>> for $T {
+            type Error = ProtobufConversionError;
+            fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
+                let protobuf_value = <$ProtobufT>::decode(&value[..])?;
+                <$T>::try_from(protobuf_value)
+            }
+        }
+    };
 }
