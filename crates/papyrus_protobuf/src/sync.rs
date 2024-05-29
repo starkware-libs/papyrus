7f9ecc4e8209a0129276117da4ecbf7f756fb7b3
ShahakShama <70578257+ShahakShama@users.noreply.github.com>
2024-05-29 09:32:56 +0300

    feat(network): add state diff part struct identical to protobuf (#2061)

--- crates/papyrus_protobuf/src/sync.rs
+++ crates/papyrus_protobuf/src/sync.rs
@@ -1,4 +1,8 @@
+use indexmap::IndexMap;
 use starknet_api::block::{BlockHash, BlockHeader, BlockNumber, BlockSignature};
+use starknet_api::core::{ClassHash, CompiledClassHash, ContractAddress, Nonce};
+use starknet_api::hash::StarkFelt;
+use starknet_api::state::StorageKey;
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Default, Hash)]
 pub enum Direction {
@@ -38,4 +42,28 @@ pub struct StateDiffQuery(pub Query);
 pub struct SignedBlockHeader {
     pub block_header: BlockHeader,
     pub signatures: Vec<BlockSignature>,
+}
+
+// TODO(shahak): Implement conversion to/from Vec<u8>.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct StateDiffsResponse(pub Option<StateDiffChunk>);
+
+// TODO(shahak): Implement conversion to/from protobuf.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum StateDiffChunk {
+    ContractDiff {
+        contract_address: ContractAddress,
+        // Has value only if the contract was deployed or replaced in this block.
+        class_hash: Option<ClassHash>,
+        // Has value only if the nonce was updated in this block.
+        nonce: Option<Nonce>,
+        storage_diffs: IndexMap<StorageKey, StarkFelt>,
+    },
+    DeclaredClass {
+        class_hash: ClassHash,
+        compiled_class_hash: CompiledClassHash,
+    },
+    DeprecatedDeclaredClass {
+        class_hash: ClassHash,
+    },
 }
