2b9f513b36845ad72cbfbfb09590ae5db0ea9475
Matan Markind <matan@starkware.co>
2024-06-02 18:08:22 +0300

    feat(consensus): update the protobuf with ConsensusMessage which will wrap all consensus messages
    Also add converters to make them convertable from/to Bytes.

--- crates/papyrus_protobuf/src/converters/consensus.rs
+++ crates/papyrus_protobuf/src/converters/consensus.rs
@@ -1,12 +1,13 @@
 use std::convert::{TryFrom, TryInto};
 
+use prost::Message;
 use starknet_api::block::BlockHash;
 use starknet_api::hash::StarkHash;
 use starknet_api::transaction::Transaction;
 
-use crate::consensus::Proposal;
+use crate::consensus::{ConsensusMessage, Proposal};
 use crate::converters::ProtobufConversionError;
-use crate::protobuf;
+use crate::{auto_impl_into_and_try_from_vec_u8, protobuf};
 
 impl TryFrom<protobuf::Proposal> for Proposal {
     type Error = ProtobufConversionError;
@@ -32,3 +33,44 @@ impl TryFrom<protobuf::Proposal> for Proposal {
         Ok(Proposal { height, proposer: contract_address, transactions, block_hash })
     }
 }
+
+impl From<Proposal> for protobuf::Proposal {
+    fn from(value: Proposal) -> Self {
+        let transactions = value.transactions.into_iter().map(Into::into).collect();
+
+        protobuf::Proposal {
+            height: value.height,
+            proposer: Some(value.proposer.into()),
+            transactions,
+            block_hash: Some(value.block_hash.0.into()),
+        }
+    }
+}
+
+impl TryFrom<protobuf::ConsensusMessage> for ConsensusMessage {
+    type Error = ProtobufConversionError;
+
+    fn try_from(value: protobuf::ConsensusMessage) -> Result<Self, Self::Error> {
+        use protobuf::consensus_message::Message;
+
+        let Some(message) = value.message else {
+            return Err(ProtobufConversionError::MissingField { field_description: "message" });
+        };
+
+        match message {
+            Message::Proposal(proposal) => Ok(ConsensusMessage::Proposal(proposal.try_into()?)),
+        }
+    }
+}
+
+impl From<ConsensusMessage> for protobuf::ConsensusMessage {
+    fn from(value: ConsensusMessage) -> Self {
+        match value {
+            ConsensusMessage::Proposal(proposal) => protobuf::ConsensusMessage {
+                message: Some(protobuf::consensus_message::Message::Proposal(proposal.into())),
+            },
+        }
+    }
+}
+
+auto_impl_into_and_try_from_vec_u8!(ConsensusMessage, protobuf::ConsensusMessage);
