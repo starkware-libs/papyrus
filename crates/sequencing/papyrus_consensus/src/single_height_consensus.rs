1e6576d900e6af3ba1dcda3c7e1ed4e57bca503f
matan-starkware <97523054+matan-starkware@users.noreply.github.com>
2024-06-04 16:42:38 +0300

    feat(consensus): add an error type for consensus and utlize in SingleHeightConsensus (#2064)
    Most of the error locations remain `expect` as their meaning will change significantly
    once we actually have Tendermint, which, being a byzantine consensus protocol, is able
    to handle certain failures.

--- crates/sequencing/papyrus_consensus/src/single_height_consensus.rs
+++ crates/sequencing/papyrus_consensus/src/single_height_consensus.rs
@@ -11,6 +11,7 @@ use starknet_api::block::BlockNumber;
 use crate::types::{
     ConsensusBlock,
     ConsensusContext,
+    ConsensusError,
     NodeId,
     PeeringConsensusMessage,
     ProposalInit,
@@ -43,46 +44,64 @@ where
         Self { height, context, validators, id, to_peering_sender, from_peering_receiver }
     }
 
-    pub(crate) async fn run(mut self) -> BlockT {
+    pub(crate) async fn run(mut self) -> Result<BlockT, ConsensusError> {
         // TODO(matan): In the future this logic will be encapsulated in the state machine, and SHC
         // will await a signal from SHC to propose.
         let proposer_id = self.context.proposer(&self.validators, self.height);
         if proposer_id == self.id { self.propose().await } else { self.validate(proposer_id).await }
     }
 
-    async fn propose(&mut self) -> BlockT {
+    async fn propose(&mut self) -> Result<BlockT, ConsensusError> {
         let (content_receiver, block_receiver) = self.context.build_proposal(self.height).await;
         let (fin_sender, fin_receiver) = oneshot::channel();
         let init = ProposalInit { height: self.height, proposer: self.id };
+        // Peering is a permanent component, so if sending to it fails we cannot continue.
         self.to_peering_sender
             .send(PeeringConsensusMessage::Proposal((init, content_receiver, fin_receiver)))
             .await
-            .expect("failed to send proposal to peering");
-        let block = block_receiver.await.expect("failed to build block");
+            .expect("Failed sending Proposal to Peering");
+        let block = block_receiver.await.expect("Block building failed.");
+        // If we choose to ignore this error, we should carefully consider how this affects
+        // Tendermint. The partially synchronous model assumes all messages arrive at some point,
+        // and this failure means this proposal will never arrive.
+        //
         // TODO(matan): Switch this to the Proposal signature.
-        fin_sender.send(block.id()).expect("failed to send block hash");
-        block
+        fin_sender.send(block.id()).expect("Failed to send ProposalFin to Peering.");
+        Ok(block)
     }
 
-    async fn validate(&mut self, proposer_id: NodeId) -> BlockT {
-        let msg = self
-            .from_peering_receiver
-            .next()
-            .await
-            .expect("Peering component disconnected from SingleHeightConsensus");
-
+    async fn validate(&mut self, proposer_id: NodeId) -> Result<BlockT, ConsensusError> {
+        // Peering is a permanent component, so if receiving from it fails we cannot continue.
+        let msg = self.from_peering_receiver.next().await.expect("Cannot receive from Peering");
         let (init, content_receiver, fin_receiver) = match msg {
             PeeringConsensusMessage::Proposal((init, content_receiver, block_hash_receiver)) => {
                 (init, content_receiver, block_hash_receiver)
             }
         };
-        assert_eq!(init.height, self.height);
-        assert_eq!(init.proposer, proposer_id);
+        if init.height != self.height || init.proposer != proposer_id {
+            // Ignore the proposal.
+            // TODO(matan): Do we want to handle this gracefully and "retry" in milestone 1?
+            return Err(ConsensusError::InvalidProposal(proposer_id, self.height));
+        }
         let block_receiver = self.context.validate_proposal(self.height, content_receiver).await;
-        let block = block_receiver.await.expect("failed to build block");
-        let fin = fin_receiver.await.expect("failed to receive block hash");
-        // TODO(matan): Switch to signature validation.
-        assert_eq!(block.id(), fin);
-        block
+        // Receive the block which was build by Context. If the channel is closed without a block
+        // being sent, this is an invalid proposal.
+        // TODO(matan): Actual Tendermint should handle invalid proposals.
+        let block = block_receiver
+            .await
+            .map_err(|_| ConsensusError::InvalidProposal(proposer_id, self.height))?;
+        // Receive the fin message from the proposer. If this is not received, this is an invalid
+        // proposal.
+        // TODO(matan): Actual Tendermint should handle invalid proposals.
+        let fin = fin_receiver
+            .await
+            .map_err(|_| ConsensusError::InvalidProposal(proposer_id, self.height))?;
+        // TODO(matan): Switch to signature validation and handle invalid proposals.
+        // An invalid signature means the proposal is invalid. The signature validation on the
+        // proposal's init prevents a malicious node from sending proposals during another's round.
+        if block.id() != fin {
+            return Err(ConsensusError::InvalidProposal(proposer_id, self.height));
+        }
+        Ok(block)
     }
 }
