1bdc597d8c7e3600bb362a0b5162e89116a03579
Matan Markind <matan@starkware.co>
2024-06-24 15:52:35 +0300

    feat(consensus): add tracing instrumentation and logging to consensus

--- crates/sequencing/papyrus_consensus/src/lib.rs
+++ crates/sequencing/papyrus_consensus/src/lib.rs
@@ -19,7 +19,7 @@ use papyrus_network::network_manager::SubscriberReceiver;
 use papyrus_protobuf::consensus::{ConsensusMessage, Proposal};
 use single_height_consensus::SingleHeightConsensus;
 use starknet_api::block::{BlockHash, BlockNumber};
-use tracing::info;
+use tracing::{debug, info, instrument};
 use types::{ConsensusBlock, ConsensusContext, ConsensusError, ProposalInit, ValidatorId};
 
 // TODO(matan): Remove dead code allowance at the end of milestone 1.
@@ -37,6 +37,7 @@ pub mod types;
 use futures::StreamExt;
 
 // TODO(dvir): add test for this.
+#[instrument(skip(context, start_height, network_receiver), level = "info")]
 #[allow(missing_docs)]
 pub async fn run_consensus<BlockT: ConsensusBlock>(
     context: Arc<dyn ConsensusContext<Block = BlockT>>,
@@ -50,12 +51,11 @@ where
 {
     let mut current_height = start_height;
     loop {
-        info!("Starting consensus for height {current_height}");
+        debug!("Starting consensus for height {current_height}");
         let mut shc =
             SingleHeightConsensus::new(current_height, context.clone(), validator_id).await;
 
         let block = if let Some(block) = shc.start().await? {
-            info!("Proposer flow height {current_height}");
             block
         } else {
             info!("Validator flow height {current_height}");
@@ -73,8 +73,8 @@ where
         };
 
         info!(
-            "Finished consensus for height: {start_height}. Agreed on block with id: {}",
-            block.id()
+            "Finished consensus for height: {current_height}. Agreed on block with id: {:x}",
+            block.id().0
         );
         current_height = current_height.unchecked_next();
     }
