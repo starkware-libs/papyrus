6b19a959e8db1a3b1bddc8ebdf1d0bbe568381d0
Asmaa Magdoub <asmaa@starkware.co>
2024-06-27 12:21:28 +0300

    refactor(sync): move db_executor to p2p_sync server

--- crates/papyrus_p2p_sync/src/server/utils.rs
+++ crates/papyrus_p2p_sync/src/server/utils.rs
@@ -1,12 +1,12 @@
 use papyrus_protobuf::sync::{Direction, Query};
 
-use super::DBExecutorError;
+use super::P2PSyncServerError;
 
 pub(crate) fn calculate_block_number(
     query: &Query,
     start_block: u64,
     read_blocks_counter: u64,
-) -> Result<u64, DBExecutorError> {
+) -> Result<u64, P2PSyncServerError> {
     let direction_factor: i128 = match query.direction {
         Direction::Forward => 1,
         Direction::Backward => -1,
@@ -15,7 +15,7 @@ pub(crate) fn calculate_block_number(
     let blocks_delta: i128 = direction_factor * (query.step * read_blocks_counter) as i128;
     let block_number: i128 = start_block as i128 + blocks_delta;
     if block_number < 0 || block_number > u64::MAX as i128 {
-        return Err(DBExecutorError::BlockNumberOutOfRange {
+        return Err(P2PSyncServerError::BlockNumberOutOfRange {
             query: query.clone(),
             counter: read_blocks_counter,
         });
