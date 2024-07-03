b211afffe61f723c524a5dfb0fcad90f38f4cde9
Asmaa Magdoub <asmaa@starkware.co>
2024-07-03 10:01:46 +0300

    chore(config): add ConsensusConfig

--- crates/sequencing/papyrus_consensus/src/lib.rs
+++ crates/sequencing/papyrus_consensus/src/lib.rs
@@ -27,7 +27,57 @@ pub(crate) mod test_utils;
 #[allow(missing_docs)]
 pub mod types;
 
+use std::collections::BTreeMap;
+
 use futures::StreamExt;
+use papyrus_config::dumping::{ser_param, ser_required_param, SerializeConfig};
+use papyrus_config::{ParamPath, ParamPrivacyInput, SerializationType, SerializedParam};
+use serde::{Deserialize, Serialize};
+
+/// Configuration for consensus.
+#[derive(Debug, Deserialize, Serialize, Clone, PartialEq)]
+pub struct ConsensusConfig {
+    /// The validator ID of the node.
+    pub validator_id: ValidatorId,
+    /// The network topic of the consensus.
+    pub topic: String,
+    /// The height to start the consensus from.
+    pub start_height: BlockNumber,
+}
+impl SerializeConfig for ConsensusConfig {
+    fn dump(&self) -> BTreeMap<ParamPath, SerializedParam> {
+        BTreeMap::from_iter([
+            ser_required_param(
+                "validator_id",
+                SerializationType::String,
+                "The validator id of the node.",
+                ParamPrivacyInput::Public,
+            ),
+            ser_param(
+                "topic",
+                &self.topic,
+                "The topic of the consensus.",
+                ParamPrivacyInput::Public,
+            ),
+            ser_param(
+                "start_height",
+                &self.start_height,
+                "The height to start the consensus from.",
+                ParamPrivacyInput::Public,
+            ),
+        ])
+    }
+}
+
+impl Default for ConsensusConfig {
+    fn default() -> Self {
+        Self {
+            validator_id: ValidatorId::default(),
+            topic: "consensus".to_string(),
+            start_height: BlockNumber::default(),
+        }
+    }
+}
 
 // TODO(dvir): add test for this.
 #[instrument(skip(context, start_height, network_receiver), level = "info")]
