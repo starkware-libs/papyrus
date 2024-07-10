7a1a1bc5c3b97090fe01088badacf057cdce329d
Asmaa Magdoub <asmaa@starkware.co>
2024-07-10 09:22:27 +0300

    chore(consensus): add consensus stagnation check

--- crates/sequencing/papyrus_consensus/run_consensus.py
+++ crates/sequencing/papyrus_consensus/run_consensus.py
@@ -6,11 +6,50 @@ import argparse
 import tempfile
 import socket
 from contextlib import closing
-
+import threading
 
 # The SECRET_KEY is used for building the BOOT_NODE_PEER_ID, so they are coupled and must be used together.
 SECRET_KEY = "0xabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"
 BOOT_NODE_PEER_ID = "12D3KooWDFYi71juk6dYWo3UDvqs5gAzGDc124LSvcR5d187Tdvi"
+
+
+class Node:
+    def __init__(self, validator_id, monitoring_gateway_server_port, cmd, stagnation_threshold):
+        self.validator_id = validator_id
+        self.monitoring_gateway_server_port = monitoring_gateway_server_port
+        self.cmd = cmd
+        self.process = None
+        self.last_height = (None, None)  # (timestamp, height)
+        self.stagnation_threshold = stagnation_threshold
+
+    def start(self):
+        self.process = subprocess.Popen(self.cmd, shell=True, preexec_fn=os.setsid)
+
+    def stop(self):
+        if self.process:
+            os.killpg(os.getpgid(self.process.pid), signal.SIGINT)
+            self.process.wait()
+
+    def get_height(self, start_time):
+        port = self.monitoring_gateway_server_port
+        command = f"curl -s -X GET http://localhost:{port}/monitoring/metrics | grep -oP 'papyrus_consensus_height \\K\\d+'"
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        current_height = result.stdout.strip()
+
+        current_time = time.time() - start_time
+        last_time, last_height = self.last_height
+        if last_height is not None and current_height == last_height:
+            if current_time - last_time >= self.stagnation_threshold:
+                print(
+                    f"Consensus stops. Validator {self.validator_id} height {current_height} hasn't changed for {self.stagnation_threshold} seconds. Exiting..."
+                )
+                return None
+        else:
+            self.last_height = (
+                (current_time, current_height) if current_height != "" else (None, None)
+            )
+
+        return current_height
 
 
 def find_free_port():
@@ -25,24 +64,43 @@ def run_command(command):
     return subprocess.run(command, shell=True, check=True)
 
 
-def run_parallel_commands(commands, duration):
-    processes = []
-    for command in commands:
-        process = subprocess.Popen(command, shell=True, preexec_fn=os.setsid)
-        processes.append(process)
+def run_parallel_commands(nodes, duration):
+    for node in nodes:
+        node.start()
+
+    def periodic_height_check():
+        start_time = time.time()
+        i = 1
+        while True:
+            time.sleep(10)
+            print(f"\nAfter {i * 10} seconds:")
+            for node in nodes:
+                height = node.get_height(start_time)
+                if height is None:
+                    os.kill(os.getpid(), signal.SIGINT)
+                    return
+                print(f"Validator {node.validator_id} height: {height}")
+
+            i += 1
+
+    threading.Thread(target=periodic_height_check, daemon=True).start()
 
     try:
-        time.sleep(duration)
+        if duration is not None:
+            time.sleep(duration)
+        else:
+            while True:
+                pass
     except KeyboardInterrupt:
-        print("\nCtrl+C pressed: Terminating subprocesses...")
+        print("\nTerminating subprocesses...")
     finally:
-        for process in processes:
-            os.killpg(os.getpgid(process.pid), signal.SIGINT)
-            process.wait()
+        for node in nodes:
+            node.stop()
 
 
-def peernode_command(base_layer_node_url, temp_dir, num_validators, i):
-    return (
+def peernode_command(base_layer_node_url, temp_dir, num_validators, i, stagnation_threshold):
+    monitoring_gateway_server_port = find_free_port()
+    cmd = (
         f"RUST_LOG=papyrus_consensus=debug,papyrus=info "
         f"target/release/papyrus_node --network.#is_none false "
         f"--base_layer.node_url {base_layer_node_url} "
@@ -51,15 +109,22 @@ def peernode_command(base_layer_node_url, temp_dir, num_validators, i):
         f"--consensus.num_validators {num_validators} "
         f"--network.tcp_port {find_free_port()} "
         f"--rpc.server_address 127.0.0.1:{find_free_port()} "
-        f"--monitoring_gateway.server_address 127.0.0.1:{find_free_port()} "
+        f"--monitoring_gateway.server_address 127.0.0.1:{monitoring_gateway_server_port} "
         f"--network.bootstrap_peer_multiaddr.#is_none false "
         f"--network.bootstrap_peer_multiaddr /ip4/127.0.0.1/tcp/10000/p2p/{BOOT_NODE_PEER_ID} "
+        f"--collect_metrics true"
         # Use sed to strip special formatting characters
         f"| sed -r 's/\\x1B\\[[0-9;]*[mK]//g' > {temp_dir}/validator{i}.txt"
     )
+    return Node(
+        validator_id=i,
+        monitoring_gateway_server_port=monitoring_gateway_server_port,
+        cmd=cmd,
+        stagnation_threshold=stagnation_threshold,
+    )
 
 
-def main(base_layer_node_url, num_validators, duration):
+def main(base_layer_node_url, num_validators, stagnation_threshold, duration):
     assert num_validators >= 2, "At least 2 validators are required for the simulation."
     # Building the Papyrus Node package assuming its output will be located in the papyrus target directory.
     print("Running cargo build...")
@@ -78,7 +143,7 @@ def main(base_layer_node_url, num_validators, duration):
     # 2. Validators 2+ are started next to join the network through the bootnode.
     # 3. Validator 0, which is the proposer, is started last so the validators don't miss the proposals.
 
-    validator_commands = []
+    nodes = []
     # Ensure validator 1 runs first
     bootnode_command = (
         f"RUST_LOG=papyrus_consensus=debug,papyrus=info "
@@ -88,22 +153,33 @@ def main(base_layer_node_url, num_validators, duration):
         f"--storage.db_config.path_prefix {temp_dir}/data1 "
         f"--consensus.#is_none false --consensus.validator_id 0x1 "
         f"--consensus.num_validators {num_validators} "
+        f"--monitoring_gateway.server_address 127.0.0.1:8081 "
+        f"--collect_metrics true"
         # Use sed to strip special formatting characters
         f"| sed -r 's/\\x1B\\[[0-9;]*[mK]//g' > {temp_dir}/validator1.txt"
     )
-    validator_commands.append(bootnode_command)
+    nodes.append(
+        Node(
+            validator_id=1,
+            monitoring_gateway_server_port=8081,
+            cmd=bootnode_command,
+            stagnation_threshold=stagnation_threshold,
+        )
+    )
 
     # Add other validators
-    validator_commands.extend(
-        peernode_command(base_layer_node_url, temp_dir, num_validators, i)
+    nodes.extend(
+        peernode_command(base_layer_node_url, temp_dir, num_validators, i, stagnation_threshold)
         for i in range(2, num_validators)
     )
     # Ensure validator 0 runs last
-    validator_commands.append(peernode_command(base_layer_node_url, temp_dir, num_validators, 0))
+    nodes.append(
+        peernode_command(base_layer_node_url, temp_dir, num_validators, 0, stagnation_threshold)
+    )
 
     # Run validator commands in parallel and manage duration time
     print("Running validators...")
-    run_parallel_commands(validator_commands, duration)
+    run_parallel_commands(nodes, duration)
     print("Simulation complete.")
 
 
@@ -111,7 +187,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run Papyrus Node simulation.")
     parser.add_argument("--base_layer_node_url", required=True)
     parser.add_argument("--num_validators", type=int, required=True)
-    parser.add_argument("--duration", type=int, required=True)
+    parser.add_argument(
+        "--stagnation_threshold",
+        type=int,
+        required=False,
+        default=30,
+        help="Time in seconds to check for height stagnation.",
+    )
+    parser.add_argument("--duration", type=int, required=False, default=None)
 
     args = parser.parse_args()
-    main(args.base_layer_node_url, args.num_validators, args.duration)
+    main(args.base_layer_node_url, args.num_validators, args.stagnation_threshold, args.duration)
