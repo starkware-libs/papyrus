b3dd878571864b0129ee4fadf75cf60a7398b496
Asmaa Magdoub <asmaa@starkware.co>
2024-07-21 14:39:36 +0300

    refactor(consensus): change StartRound to GetProposal and pass leader_fn to sm from shc

--- crates/sequencing/papyrus_consensus/src/state_machine_test.rs
+++ crates/sequencing/papyrus_consensus/src/state_machine_test.rs
@@ -1,9 +1,15 @@
+use lazy_static::lazy_static;
 use starknet_api::block::BlockHash;
 use starknet_types_core::felt::Felt;
 use test_case::test_case;
 
 use super::Round;
 use crate::state_machine::{StateMachine, StateMachineEvent};
+use crate::types::ValidatorId;
+
+lazy_static! {
+    static ref VALIDATOR_ID: ValidatorId = 0_u32.into();
+}
 
 const BLOCK_HASH: BlockHash = BlockHash(Felt::ONE);
 const ROUND: Round = 0;
@@ -11,15 +17,16 @@ const ROUND: Round = 0;
 #[test_case(true; "proposer")]
 #[test_case(false; "validator")]
 fn events_arrive_in_ideal_order(is_proposer: bool) {
-    let mut state_machine = StateMachine::new(4);
-
-    let mut events = state_machine.start();
-    assert_eq!(events.pop_front().unwrap(), StateMachineEvent::StartRound(None, ROUND));
+    let mut state_machine = StateMachine::new(*VALIDATOR_ID, 4);
+    let leader_fn = |_: Round| 0_u32.into();
+    let mut events = state_machine.start(&leader_fn);
+    assert_eq!(events.pop_front().unwrap(), StateMachineEvent::GetProposal(None, ROUND));
     if is_proposer {
-        events = state_machine.handle_event(StateMachineEvent::StartRound(Some(BLOCK_HASH), ROUND));
+        events =
+            state_machine.handle_event(StateMachineEvent::GetProposal(Some(BLOCK_HASH), ROUND));
         assert_eq!(events.pop_front().unwrap(), StateMachineEvent::Proposal(BLOCK_HASH, ROUND));
     } else {
-        state_machine.handle_event(StateMachineEvent::StartRound(None, ROUND));
+        state_machine.handle_event(StateMachineEvent::GetProposal(None, ROUND));
         assert!(events.is_empty());
         events = state_machine.handle_event(StateMachineEvent::Proposal(BLOCK_HASH, ROUND));
     }
@@ -43,12 +50,13 @@ fn events_arrive_in_ideal_order(is_proposer: bool) {
 
 #[test]
 fn validator_receives_votes_first() {
-    let mut state_machine = StateMachine::new(4);
+    let mut state_machine = StateMachine::new(*VALIDATOR_ID, 4);
 
-    let mut events = state_machine.start();
-    assert_eq!(events.pop_front().unwrap(), StateMachineEvent::StartRound(None, ROUND));
+    let leader_fn = |_: Round| 0_u32.into();
+    let mut events = state_machine.start(&leader_fn);
+    assert_eq!(events.pop_front().unwrap(), StateMachineEvent::GetProposal(None, ROUND));
     assert!(events.is_empty(), "{:?}", events);
-    events = state_machine.handle_event(StateMachineEvent::StartRound(None, ROUND));
+    events = state_machine.handle_event(StateMachineEvent::GetProposal(None, ROUND));
     assert!(events.is_empty(), "{:?}", events);
 
     // Receives votes from all the other nodes first (more than minimum for a quorum).
@@ -69,10 +77,11 @@ fn validator_receives_votes_first() {
 }
 
 #[test]
-fn buffer_events_during_start_round() {
-    let mut state_machine = StateMachine::new(4);
-    let mut events = state_machine.start();
-    assert_eq!(events.pop_front().unwrap(), StateMachineEvent::StartRound(None, 0));
+fn buffer_events_during_get_proposal() {
+    let mut state_machine = StateMachine::new(*VALIDATOR_ID, 4);
+    let leader_fn = |_: Round| 0_u32.into();
+    let mut events = state_machine.start(&leader_fn);
+    assert_eq!(events.pop_front().unwrap(), StateMachineEvent::GetProposal(None, 0));
     assert!(events.is_empty(), "{:?}", events);
 
     // TODO(matan): When we support NIL votes, we should send them. Real votes without the proposal
@@ -84,7 +93,7 @@ fn buffer_events_during_start_round() {
     assert!(events.is_empty(), "{:?}", events);
 
     // Node finishes building the proposal.
-    events = state_machine.handle_event(StateMachineEvent::StartRound(None, 0));
+    events = state_machine.handle_event(StateMachineEvent::GetProposal(None, 0));
     assert_eq!(events.pop_front().unwrap(), StateMachineEvent::Prevote(BLOCK_HASH, ROUND));
     assert_eq!(events.pop_front().unwrap(), StateMachineEvent::Precommit(BLOCK_HASH, ROUND));
     assert!(events.is_empty(), "{:?}", events);
